ace93e5937b7c5bdf65d35392ebc4c9eb52d8f20
Steven Lambright <slambright@usgs.gov>
2010-09-23 16:52:23 +0000

    Updated unitTest to not test invalid points and to check for success on SetImage and SetUniversalGround calls
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@742 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/UniversalGroundMap/unitTest.cpp
+++ isis/src/base/objs/UniversalGroundMap/unitTest.cpp
@@ -22,43 +22,49 @@ int main(int argc, char *argv[]) {
     cout << "Is Projection? = " << ugm.HasProjection() << endl << endl;
 
     // Test all four corners to make sure the conversions are right
-    ugm.SetImage(1.0, 1.0);
-    cout << "For (1.0, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    cout << "For (1.0, 5.0) ..." << endl;
+    if(ugm.SetImage(1.0, 5.0)) {
+      cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
 
-    ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude());
-    cout << "Sample = " << ugm.Sample() << endl;
-    cout << "Line = " << ugm.Line() << endl << endl;
+      if(ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude())) {
+        cout << "Sample = " << ugm.Sample() << endl;
+        cout << "Line = " << ugm.Line() << endl << endl;
+      }
+    }
 
-    ugm.SetImage(1204, 1.0);
-    cout << "For (1204, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    cout << "For (1204, 5.0) ..." << endl;
+    if(ugm.SetImage(1204, 5.0)) {
+      cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    
+      if(ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude())) {
+        cout << "Sample = " << ugm.Sample() << endl;
+        cout << "Line = " << ugm.Line() << endl << endl;
+      }
+    }
 
-    ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude());
-    cout << "Sample = " << ugm.Sample() << endl;
-    cout << "Line = " << ugm.Line() << endl << endl;
-
-    ugm.SetImage(1.0, 1056);
     cout << "For (1.0, 1056) ..." << endl;
-    cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    if(ugm.SetImage(1.0, 1056)) {
+      cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    
+      if(ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude())) {
+        cout << "Sample = " << ugm.Sample() << endl;
+        cout << "Line = " << ugm.Line() << endl << endl;
+      }
+    }
 
-    ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude());
-    cout << "Sample = " << ugm.Sample() << endl;
-    cout << "Line = " << ugm.Line() << endl << endl;
-
-    ugm.SetImage(1204, 1056);
     cout << "For (1204, 1056) ..." << endl;
-    cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
+    if(ugm.SetImage(1204, 1056)) {
+      cout << "Universal Latitude = " << ugm.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm.UniversalLongitude() << endl;
 
-    ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude());
-    cout << "Sample = " << ugm.Sample() << endl;
-    cout << "Line = " << ugm.Line() << endl << endl;
-
-
+      if(ugm.SetUniversalGround(ugm.UniversalLatitude(), ugm.UniversalLongitude())) {
+        cout << "Sample = " << ugm.Sample() << endl;
+        cout << "Line = " << ugm.Line() << endl << endl;
+      }
+    }
 
     cout << "  Testing Projection..." << endl;
     Isis::Pvl p2("$base/dems/molaMarsPlanetaryRadius0001.cub");
@@ -66,41 +72,49 @@ int main(int argc, char *argv[]) {
     cout << "Is Projection? = " << ugm2.HasProjection() << endl << endl;
 
     // Test all four corners to make sure the conversions are right
-    ugm2.SetImage(1.0, 1.0);
-    cout << "For (1.0, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
+    cerr << "For (2.0, 5.0) ..." << endl;
+    if(ugm2.SetImage(2.0, 5.0)) {
+      cerr << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
+      cerr << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
 
-    ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude());
-    cout << "Sample = " << ugm2.Sample() << endl;
-    cout << "Line = " << ugm2.Line() << endl << endl;
+      if(ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude())) {
+        cerr << "Sample = " << ugm2.Sample() << endl;
+        cerr << "Line = " << ugm2.Line() << endl << endl;
+      }
+    }
 
-    ugm2.SetImage(23040, 1.0);
-    cout << "For (23040, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
+    cout << "For (23040, 5.0) ..." << endl;
+    if(ugm2.SetImage(23040, 5.0)) {
+      cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
 
-    ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude());
-    cout << "Sample = " << ugm2.Sample() << endl;
-    cout << "Line = " << ugm2.Line() << endl << endl;
+      if(ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude())) {
+        cout << "Sample = " << ugm2.Sample() << endl;
+        cout << "Line = " << ugm2.Line() << endl << endl;
+      }
+    }
 
-    ugm2.SetImage(1.0, 11520);
-    cout << "For (1.0, 11520) ..." << endl;
-    cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
+    cout << "For (2.0, 11520) ..." << endl;
+    if(ugm2.SetImage(2.0, 11520)) {
+      cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
 
-    ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude());
-    cout << "Sample = " << ugm2.Sample() << endl;
-    cout << "Line = " << ugm2.Line() << endl << endl;
+      if(ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude())) {
+        cout << "Sample = " << ugm2.Sample() << endl;
+        cout << "Line = " << ugm2.Line() << endl << endl;
+      }
+    }
 
-    ugm2.SetImage(23040, 11520);
     cout << "For (23040, 11520) ..." << endl;
-    cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
+    if(ugm2.SetImage(23040, 11520)) {
+      cout << "Universal Latitude = " << ugm2.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm2.UniversalLongitude() << endl;
 
-    ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude());
-    cout << "Sample = " << ugm2.Sample() << endl;
-    cout << "Line = " << ugm2.Line() << endl << endl;
+      if(ugm2.SetUniversalGround(ugm2.UniversalLatitude(), ugm2.UniversalLongitude())) {
+        cout << "Sample = " << ugm2.Sample() << endl;
+        cout << "Line = " << ugm2.Line() << endl << endl;
+      }
+    }
 
     cout << "  Testing Camera Model and Projection..." << endl;
     Isis::Pvl p3("$mgs/testData/m0402852.cub");
@@ -108,51 +122,52 @@ int main(int argc, char *argv[]) {
     cout << "Is Projection? = " << ugm3.HasProjection() << endl << endl;
 
     // Test all four corners to make sure the conversions are right
-    ugm3.SetImage(1.0, 1.0);
-    cout << "For (1.0, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
+    cout << "For (1.0, 5.0) ..." << endl;
+    if(ugm3.SetImage(1.0, 5.0)) {
+      cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
 
-    ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude());
-    cout << "Sample = " << ugm3.Sample() << endl;
-    cout << "Line = " << ugm3.Line() << endl << endl;
+      if(ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude())) {
+        cout << "Sample = " << ugm3.Sample() << endl;
+        cout << "Line = " << ugm3.Line() << endl << endl;
+      }
+    }
 
-    ugm3.SetImage(1721, 1.0);
-    cout << "For (1721, 1.0) ..." << endl;
-    cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
+    cout << "For (1640, 20.0) ..." << endl;
+    if(ugm3.SetImage(1640, 20.0)) {
+      cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
 
-    ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude());
-    cout << "Sample = " << ugm3.Sample() << endl;
-    cout << "Line = " << ugm3.Line() << endl << endl;
+      if(ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude())) {
+        cout << "Sample = " << ugm3.Sample() << endl;
+        cout << "Line = " << ugm3.Line() << endl << endl;
+      }
+    }
 
-    ugm3.SetImage(1.0, 667);
-    cout << "For (1.0, 667) ..." << endl;
-    cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
+    cout << "For (30.0, 415) ..." << endl;
+    if(ugm3.SetImage(30.0, 415)) {
+      cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
 
-    ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude());
-    cout << "Sample = " << ugm3.Sample() << endl;
-    cout << "Line = " << ugm3.Line() << endl << endl;
+      if(ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude())) {
+        cout << "Sample = " << ugm3.Sample() << endl;
+        cout << "Line = " << ugm3.Line() << endl << endl;
+      }
+    }
 
-    ugm3.SetImage(1721, 667);
-    cout << "For (1721, 667) ..." << endl;
-    cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
-    cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
+    cout << "For (1700, 245) ..." << endl;
+    if(ugm3.SetImage(1700, 245)) {
+      cout << "Universal Latitude = " << ugm3.UniversalLatitude() << endl;
+      cout << "Universal Longitude = " << ugm3.UniversalLongitude() << endl;
 
-    ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude());
-    cout << "Sample = " << ugm3.Sample() << endl;
-    cout << "Line = " << ugm3.Line() << endl << endl;
-
+      if(ugm3.SetUniversalGround(ugm3.UniversalLatitude(), ugm3.UniversalLongitude())) {
+        cout << "Sample = " << ugm3.Sample() << endl;
+        cout << "Line = " << ugm3.Line() << endl << endl;
+      }
+    }
   }
   catch(Isis::iException &e) {
     e.Report();
   }
 }
-
-
-
-
-
-
 
