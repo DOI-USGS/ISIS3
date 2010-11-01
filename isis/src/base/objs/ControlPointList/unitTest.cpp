9a81bc01b1aa189766645d87b64c247bcc2ffa59
Steven Lambright <slambright@usgs.gov>
2010-11-01 20:59:07 +0000

    Switched to use cerr so that output would line up on different OS's
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1025 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/ControlPointList/unitTest.cpp
+++ isis/src/base/objs/ControlPointList/unitTest.cpp
@@ -12,21 +12,21 @@ int main() {
 
   //print point ids in the list
   for(int i = 0; i < size; i++) {
-    std::cout << cpl.ControlPointId(i) << "\n";
+    std::cerr << cpl.ControlPointId(i) << "\n";
   }
 
   // index out of range
   try {
-    std::cout << cpl.ControlPointId(size) << "\n";
+    std::cerr << cpl.ControlPointId(size) << "\n";
   }
   catch(Isis::iException &e) {
     e.Report(false);
   }
 
   try {
-    std::cout << cpl.ControlPointIndex("new0007") << "\n";
-    std::cout << cpl.ControlPointIndex("new0036") << "\n";
-    std::cout << cpl.ControlPointIndex("new0000") << "\n"; //not found - invalid point
+    std::cerr << cpl.ControlPointIndex("new0007") << "\n";
+    std::cerr << cpl.ControlPointIndex("new0036") << "\n";
+    std::cerr << cpl.ControlPointIndex("new0000") << "\n"; //not found - invalid point
   }
   catch(Isis::iException &e) {
     e.Report(false);
