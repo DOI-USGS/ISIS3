06617c1e44cfc8294f98b53b0da8f30c618b3777
Steven Lambright <slambright@usgs.gov>
2010-11-02 18:23:58 +0000

    Updated to be const correct
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1044 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/UserInterface/unitTest.cpp
+++ isis/src/base/objs/UserInterface/unitTest.cpp
@@ -13,15 +13,20 @@ int main(int argc, char *argv[]) {
 
   Isis::iString unitTestXml = Isis::Filename("unitTest.xml").Expanded();
   string highpass = Isis::Filename("$ISISROOT/src/base/apps/highpass/highpass.xml").Expanded();
+  char *myArgv[15];// = {"unitTest", "from=input.cub", "to=output.cub"};
+
+  for(int i = 0; i < 15; i++)
+    myArgv[i] = new char[128];
 
   try {
     cout << "Basic FROM/TO Test" << endl;
     {
-      const int myArgc = 3;
-      char *myArgv[myArgc] = {"unitTest", "from=input.cub", "to=output.cub"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
+      strcpy(myArgv[myArgc ++], "from=input.cub");
+      strcpy(myArgv[myArgc ++], "to=output.cub");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -30,11 +35,16 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing param= value Format" << endl;
     {
-      const int myArgc = 7;
-      char *myArgv[myArgc] = {"highpass", "from=dog", "to=biscuit", "line=", "3", "samp=", "3"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from=dog");
+      strcpy(myArgv[myArgc ++], "to=biscuit");
+      strcpy(myArgv[myArgc ++], "line=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -43,8 +53,8 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing No Arguments (Defaults)" << endl;
     {
-      const int myArgc = 1;
-      char *myArgv[myArgc] = {"unitTest"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
 
       int myArgcQ = myArgc;
       Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
@@ -56,11 +66,16 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing Basic Array Argument" << endl;
     {
-      const int myArgc = 7;
-      char *myArgv[myArgc] = {"highpass", "from=dog", "to=(biscuit,bread)", "line=", "3", "samp=", "3"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from=dog");
+      strcpy(myArgv[myArgc ++], "to=(biscuit,bread)");
+      strcpy(myArgv[myArgc ++], "line=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       vector<string> vals;
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      ";
@@ -79,14 +94,16 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing Common Array Argument" << endl;
     {
-      const int myArgc = 7;
-      char *myArgv[myArgc] = {"highpass", "from=dog",
-                              "to=( \"dog\" , \"cat\", \" cow \", 'frog')",
-                              "line=", "3", "samp=", "3"
-                             };
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from=dog");
+      strcpy(myArgv[myArgc ++], "to=( \"dog\" , \"cat\", \" cow \", 'frog')");
+      strcpy(myArgv[myArgc ++], "line=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       vector<string> vals;
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << endl;
@@ -103,14 +120,16 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing Complicated Array Argument" << endl;
     {
-      const int myArgc = 7;
-      char *myArgv[myArgc] = {"highpass", "from=dog",
-                              "to=(biscuit\\\\,,'bread,',\",b,\\\\,iscuit2,\"\\,,)",
-                              "line=", "3", "samp=", "3"
-                             };
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from=dog");
+      strcpy(myArgv[myArgc ++], "to=(biscuit\\\\,,'bread,',\",b,\\\\,iscuit2,\"\\,,)");
+      strcpy(myArgv[myArgc ++], "line=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       vector<string> vals;
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << endl;
@@ -127,11 +146,22 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing param = value Format" << endl;
     {
-      const int myArgc = 13;
-      char *myArgv[myArgc] = {"highpass", "from", "=", "dog", "to", "=", "bread", "line", "=", "3", "samp", "=", "3"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "dog");
+      strcpy(myArgv[myArgc ++], "to");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "bread");
+      strcpy(myArgv[myArgc ++], "line");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       vector<string> vals;
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
@@ -141,11 +171,12 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing Space in Argument Value" << endl;
     {
-      const int myArgc = 3;
-      char *myArgv[myArgc] = {"unitTest", "from=input file.cub", "to=output.cub"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
+      strcpy(myArgv[myArgc ++], "from=input file.cub");
+      strcpy(myArgv[myArgc ++], "to=output.cub");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -154,29 +185,32 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing unitTest v. ./unitTest for GUI" << endl;
     {
-      const int myArgc = 1;
-      char *myArgv[myArgc] = {"unitTest"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "GUI:     " << ui.IsInteractive() << endl;
       cout << endl;
     }
 
     {
-      const int myArgc = 1;
-      char *myArgv[myArgc] = {"./unitTest"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "./unitTest");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "GUI:     " << ui.IsInteractive() << endl;
       cout << endl;
     }
 
+    cout << "Starting Batchlist Test" << endl;
     {
-      cout << "Starting Batchlist Test" << endl;
-      const int myArgc = 4;
-      char *myArgv[myArgc] = {"unitTest", "from=$1", "to=$2", "-batchlist=unitTest.lis"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
+      strcpy(myArgv[myArgc ++], "from=$1");
+      strcpy(myArgv[myArgc ++], "to=$2");
+      strcpy(myArgv[myArgc ++], "-batchlist=unitTest.lis");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       for(int i = 0; i < ui.BatchListSize(); i++) {
         ui.SetBatchList(i);
         cout << "FROM:    " << ui.GetAsString("FROM") << endl;
@@ -190,11 +224,13 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing =value" << endl;
     try {
-      const int myArgc = 4;
-      char *myArgv[myArgc] = {"unitTest", "=input.cub", "to", "=output.cub"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "unitTest");
+      strcpy(myArgv[myArgc ++], "=input.cub");
+      strcpy(myArgv[myArgc ++], "to");
+      strcpy(myArgv[myArgc ++], "=output.cub");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -207,11 +243,19 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing param =value" << endl;
     try {
-      const int myArgc = 10;
-      char *myArgv[myArgc] = {"highpass", "from=dog", "to", "=bread", "line", "=", "3", "samp", "=", "3"};
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "highpass");
+      strcpy(myArgv[myArgc ++], "from=dog");
+      strcpy(myArgv[myArgc ++], "to");
+      strcpy(myArgv[myArgc ++], "=bread");
+      strcpy(myArgv[myArgc ++], "line");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "3");
+      strcpy(myArgv[myArgc ++], "samp");
+      strcpy(myArgv[myArgc ++], "=");
+      strcpy(myArgv[myArgc ++], "3");
 
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      Isis::UserInterface ui(highpass, myArgc, myArgv);
       vector<string> vals;
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
@@ -225,10 +269,11 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing unterminated array-value quote" << endl;
     try {
-      const int myArgc = 2;
-      char *myArgv[myArgc] = {"./unitTest", "from=(\"hello)"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "./unitTest");
+      strcpy(myArgv[myArgc ++], "from=(\"hello)");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -241,10 +286,11 @@ int main(int argc, char *argv[]) {
 
     cout << "Testing array-value ending in backslash" << endl;
     try {
-      const int myArgc = 2;
-      char *myArgv[myArgc] = {"./unitTest", "from=(hello)\\"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "./unitTest");
+      strcpy(myArgv[myArgc ++], "from=(hello)\\");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -256,10 +302,11 @@ int main(int argc, char *argv[]) {
     }
 
     try {
-      const int myArgc = 2;
-      char *myArgv[myArgc] = {"./unitTest", "-restore=unitTest.par"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(unitTestXml, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "./unitTest");
+      strcpy(myArgv[myArgc ++], "-restore=unitTest.par");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
       cout << "FROM:    " << ui.GetAsString("FROM") << endl;
       cout << "TO:      " << ui.GetAsString("TO") << endl;
       cout << "GUI:     " << ui.IsInteractive() << endl;
@@ -271,10 +318,11 @@ int main(int argc, char *argv[]) {
     }
 
     try {
-      const int myArgc = 2;
-      char *myArgv[myArgc] = {"$ISISROOT/src/base/apps/highpass/highpass", "bogus=parameter"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "$ISISROOT/src/base/apps/highpass/highpass");
+      strcpy(myArgv[myArgc ++], "bogus=parameter");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
     }
     catch(Isis::iException &e) {
       e.Report(false);
@@ -282,10 +330,11 @@ int main(int argc, char *argv[]) {
     }
 
     try {
-      const int myArgc = 2;
-      char *myArgv[myArgc] = {"$ISISROOT/src/base/apps/highpass/highpass", "-restore=junk.par"};
-      int myArgcQ = myArgc;
-      Isis::UserInterface ui(highpass, myArgcQ, myArgv);
+      int myArgc = 0;
+      strcpy(myArgv[myArgc ++], "$ISISROOT/src/base/apps/highpass/highpass");
+      strcpy(myArgv[myArgc ++], "-restore=junk.par");
+
+      Isis::UserInterface ui(unitTestXml, myArgc, myArgv);
     }
     catch(Isis::iException &e) {
       e.Report(false);
