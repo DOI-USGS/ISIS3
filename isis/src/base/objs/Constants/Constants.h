f3878d77cc20e360afc93e8c14c688c46eb419e5
Christopher Austin <caustin@usgs.gov>
2010-11-03 21:11:03 +0000

    isis3.2.1 release
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1056 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/Constants/Constants.h
+++ isis/src/base/objs/Constants/Constants.h
@@ -48,13 +48,13 @@ namespace Isis {
    *                                       RAD2DEG
    */
 
-  const std::string version("3.2.1beta2 | 2010-03-18");
+  const std::string version("3.2.1 | 2010-11-05");
 
   const double E(2.7182818284590452354);    //!<The mathematical constant E
-  const double PI(3.14159265358979323846);  //!<The mathematical constant PI
-  const double HALFPI(1.57079632679489661923); //!<The mathematical constant PI/2
-  const double DEG2RAD(PI / 180.0);  //!<Multiplier for converting from degrees to radians
-  const double RAD2DEG(180.0 / PI);  //!<Multiplier for converting from radians to degrees
+  const double PI(3.14159265358979323846);   //!<The mathematical constant PI
+  const double HALFPI(1.57079632679489661923);   //!<The mathematical constant PI/2
+  const double DEG2RAD(PI / 180.0);   //!<Multiplier for converting from degrees to radians
+  const double RAD2DEG(180.0 / PI);   //!<Multiplier for converting from radians to degrees
 
 #if (defined(__SunOS__) || defined(__x86_64))
   typedef long int BigInt;
