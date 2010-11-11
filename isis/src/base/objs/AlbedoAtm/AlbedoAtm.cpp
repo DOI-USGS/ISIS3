1457585886b46ac2bd1b17111b5df6c2941a0d2f
Janet Barrett <jbarrett@usgs.gov>
2010-11-11 15:31:11 +0000

    Added Pharef, Emaref
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1139 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/AlbedoAtm/AlbedoAtm.cpp
+++ isis/src/base/objs/AlbedoAtm/AlbedoAtm.cpp
@@ -2,6 +2,7 @@
 #include "AlbedoAtm.h"
 #include "NumericalApproximation.h"
 #include "iException.h"
+#include "iString.h"
 
 #define MIN(x,y) (((x) < (y)) ? (x) : (y))
 #define MAX(x,y) (((x) > (y)) ? (x) : (y))
@@ -24,16 +25,29 @@ namespace Isis {
     PvlGroup &algo = pvl.FindObject("NormalizationModel")
                      .FindGroup("Algorithm", Pvl::Traverse);
     // Set default value
+    SetNormPharef(0.0);
     SetNormIncref(0.0);
+    SetNormEmaref(0.0);
+
     // Get value from user
     if(algo.HasKeyword("Incref")) {
       SetNormIncref(algo["Incref"]);
     }
 
+    if(algo.HasKeyword("Pharef")) {
+      SetNormPharef(algo["Pharef"]);
+    } else {
+      p_normPharef = p_normIncref;
+    }
+
+    if(algo.HasKeyword("Emaref")) {
+      SetNormEmaref(algo["Emaref"]);
+    }
+
     // First-time setup:
     // Calculate normalization at standard conditions
     GetPhotoModel()->SetStandardConditions(true);
-    p_normPsurfref = GetPhotoModel()->CalcSurfAlbedo(p_normIncref, p_normIncref, 0.0);
+    p_normPsurfref = GetPhotoModel()->CalcSurfAlbedo(p_normPharef, p_normIncref, p_normEmaref);
     GetPhotoModel()->SetStandardConditions(false);
 
     // Get reference hemispheric albedo
@@ -45,7 +59,7 @@ namespace Isis {
 
     // Now calculate atmosphere at standard conditions
     GetAtmosModel()->SetStandardConditions(true);
-    GetAtmosModel()->CalcAtmEffect(p_normIncref, p_normIncref, 0.0,
+    GetAtmosModel()->CalcAtmEffect(p_normPharef, p_normIncref, p_normEmaref,
                                    &p_normPstdref, &p_normTransref,
                                    &p_normTrans0ref, &p_normSbar);
     GetAtmosModel()->SetStandardConditions(false);
@@ -130,6 +144,25 @@ namespace Isis {
 
   /**
     * Set the normalization function parameter. This is the
+    * reference phase angle to which the image photometry will
+    * be normalized. This parameter is limited to values that are
+    * >=0 and <180.
+    *
+    * @param pharef  Normalization function parameter, default
+    *                is 0.0
+    */
+  void AlbedoAtm::SetNormPharef(const double pharef) {
+    if(pharef < 0.0 || pharef >= 180.0) {
+      std::string msg = "Invalid value of normalization pharef [" +
+                        iString(pharef) + "]";
+      throw iException::Message(iException::User, msg, _FILEINFO_);
+    }
+
+    p_normPharef = pharef;
+  }
+
+  /**
+    * Set the normalization function parameter. This is the
     * reference incidence angle to which the image photometry will
     * be normalized. This parameter is limited to values that are
     * >=0 and <90.
@@ -145,6 +178,25 @@ namespace Isis {
     }
 
     p_normIncref = incref;
+  }
+
+  /**
+    * Set the normalization function parameter. This is the
+    * reference emission angle to which the image photometry will
+    * be normalized. This parameter is limited to values that are
+    * >=0 and <90.
+    *
+    * @param emaref  Normalization function parameter, default
+    *                is 0.0
+    */
+  void AlbedoAtm::SetNormEmaref(const double emaref) {
+    if(emaref < 0.0 || emaref >= 90.0) {
+      std::string msg = "Invalid value of normalization emaref [" +
+                        iString(emaref) + "]";
+      throw iException::Message(iException::User, msg, _FILEINFO_);
+    }
+
+    p_normEmaref = emaref;
   }
 }
 
