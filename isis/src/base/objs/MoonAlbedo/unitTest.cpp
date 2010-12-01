9d28d2ba80c51643dc3e714cb5b98fa13331610f
Janet Barrett <jbarrett@usgs.gov>
2010-12-01 17:28:41 +0000

    Added ability to use photometric angles from ellipsoid or DEM
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1232 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/MoonAlbedo/unitTest.cpp
+++ isis/src/base/objs/MoonAlbedo/unitTest.cpp
@@ -50,17 +50,17 @@ int main() {
 
     std::cout << "Test phase=86.7207248, incidence=51.7031305, emission=38.9372914, " <<
               "dn=.0800618902 ..." << std::endl;
-    nm->CalcNrmAlbedo(86.7207248, 51.7031305, 38.9372914, .0800618902, result, mult, base);
+    nm->CalcNrmAlbedo(86.7207248, 51.7031305, 38.9372914, 51.7031305, 38.9372914, .0800618902, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
 
     std::cout << "Test phase=75.7207248, incidence=41.7031305, emission=28.9372914, " <<
               "dn=.0697334611 ..." << std::endl;
-    nm->CalcNrmAlbedo(75.7207248, 41.7031305, 28.9372914, .0697334611, result, mult, base);
+    nm->CalcNrmAlbedo(75.7207248, 41.7031305, 28.9372914, 41.7031305, 28.9372914, .0697334611, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
 
     std::cout << "Test phase=53.7187773, incidence=31.7060221, emission=18.9331391, " <<
               "dn=.0194225037 ..." << std::endl;
-    nm->CalcNrmAlbedo(53.7187773, 31.7060221, 18.9331391, .0194225037, result, mult, base);
+    nm->CalcNrmAlbedo(53.7187773, 31.7060221, 18.9331391, 31.7060221, 18.9331391, .0194225037, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
   }
   catch(iException &e) {
