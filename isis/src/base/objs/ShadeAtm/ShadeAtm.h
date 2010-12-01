d68388ebdae0b7b444c5b2bc8a5e842850f8e2d2
Janet Barrett <jbarrett@usgs.gov>
2010-12-01 17:29:55 +0000

    Added ability to use photometric angles from ellipsoid and DEM
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1240 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/ShadeAtm/ShadeAtm.h
+++ isis/src/base/objs/ShadeAtm/ShadeAtm.h
@@ -44,6 +44,8 @@ namespace Isis {
    *  @history 2010-11-10 Janet Barrett - Added reference parameters for
    *           phase and emission so user can specify normalization
    *           conditions in initialization
+   *  @history 2010-11-30 Janet Barrett - Added ability to use photometric angles
+   *           from the ellisoid and the DEM
    *
    */
   class ShadeAtm : public NormModel {
@@ -53,10 +55,10 @@ namespace Isis {
 
     protected:
       virtual void NormModelAlgorithm(double pha, double inc, double ema,
-                                      double dn, double &albedo, double &mult, double &base);
+                                      double dn, double &albedo, double &mult, double &base) {};
       virtual void NormModelAlgorithm(double pha, double inc, double ema,
                                       double deminc, double demema, double dn, double &albedo,
-                                      double &mult, double &base) {};
+                                      double &mult, double &base);
 
     private:
       void SetNormPharef(const double pharef);
