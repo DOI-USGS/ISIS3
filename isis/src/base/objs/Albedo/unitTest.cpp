5e1604ed953bfb90b272911e70b924a191ec80e9
Janet Barrett <jbarrett@usgs.gov>
2010-12-01 17:27:31 +0000

    Added ability to use photometric angles from ellipsoid or DEM
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1226 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/Albedo/unitTest.cpp
+++ isis/src/base/objs/Albedo/unitTest.cpp
@@ -41,15 +41,15 @@ int main() {
     NormModel *nm = NormModelFactory::Create(pvl, *pm);
 
     std::cout << "Test phase=0.0, incidence=0.0, emission=0.0, dn=.0800618902 ..." << std::endl;
-    nm->CalcNrmAlbedo(0.0, 0.0, 0.0, .0800618902, result, mult, base);
+    nm->CalcNrmAlbedo(0.0, 0.0, 0.0, 0.0, 0.0, .0800618902, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
 
     std::cout << "Test phase=86.7207248, incidence=51.7031305, emission=38.9372914, dn=.0797334611 ..." << std::endl;
-    nm->CalcNrmAlbedo(86.7207248, 51.7031305, 38.9372914, .0797334611, result, mult, base);
+    nm->CalcNrmAlbedo(86.7207248, 51.7031305, 38.9372914, 51.7031305, 38.9372914, .0797334611, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
 
     std::cout << "Test phase=180, incidence=90, emission=90, dn=.0794225037 ..." << std::endl;
-    nm->CalcNrmAlbedo(180.0, 90.0, 90.0, .0794225037, result, mult, base);
+    nm->CalcNrmAlbedo(180.0, 90.0, 90.0, 90.0, 90.0, .0794225037, result, mult, base);
     std::cout << "Normalization value = " << result << std::endl << std::endl;
   }
   catch(iException &e) {
