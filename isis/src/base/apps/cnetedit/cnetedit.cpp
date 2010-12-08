f693405bc48bcb41ed7c7d95f92301569d26030e
Travis Addair <taddair@usgs.gov>
2010-12-08 21:42:25 +0000

    References no longer deleted without the entire point, ignored reference results in ignored point
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1312 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/cnetedit/cnetedit.cpp
+++ isis/src/base/apps/cnetedit/cnetedit.cpp
@@ -49,28 +49,66 @@ void IsisMain() {
   preservePoints = ui.GetBoolean("PRESERVE");
   keepLog = ui.WasEntered("LOG");
 
-  // Only default options were chosen, no special ignore lists or checks
-  bool useDefaultOptions = true;
+  /*
+   * As a first pass, just try and delete anything that's already ignored
+   * in the Control Network, if the user wants to delete ignored points and
+   * measures.  Originally, this check was performed last, only if the user
+   * didn't specify any other deletion methods.  However, performing this
+   * check first will actually improve the running time in cases where there
+   * are already ignored points and measures in the input network.  The added
+   * cost of doing this check here actually doesn't add to the running time at
+   * all, because these same checks would need to have been done later
+   * regardless.
+   */
+  ControlNet cnet(ui.GetFilename("CNET"));
+  if(deleteIgnored) {
+    for(int cp = cnet.Size() - 1; cp >= 0; cp--) {
+      if(cnet[cp].Ignore()) {
+        numMeasuresDeleted += cnet[cp].Size();
+        cnet.Delete(cp);
+        numPointsDeleted++;
+      }
+      else {
+        for(int cm = cnet[cp].Size() - 1; cm >= 0; cm--) {
+          if(cnet[cp][cm].Ignore()) {
+            // Can't delete the reference without deleting the whole point
+            if(cnet[cp][cm].IsReference()) {
+              cnet[cp].SetIgnore(true);
+            }
+            else {
+              cnet[cp].Delete(cm);
+              numMeasuresDeleted++;
+            }
+          }
+        }
+
+        // Check if the number of measures in the point is zero or there are too
+        // few measures in the point and we don't want to preserve them.
+        if(((cnet[cp].Size() < 2 && !preservePoints) && cnet[cp].Type() != ControlPoint::Ground)
+            || cnet[cp].Size() == 0 || cnet[cp].Ignore()) {
+          numMeasuresDeleted += cnet[cp].Size();
+          cnet.Delete(cp);
+          numPointsDeleted++;
+        }
+      }
+    }
+  }
 
   //List has Points Ids
-  ControlNet cnet(ui.GetFilename("CNET"));
   Pvl pvlLog;
   if(ui.WasEntered("POINTLIST")) {
-    useDefaultOptions = false;
     std::string pointlistFilename = ui.GetFilename("POINTLIST");
     ProcessControlPoints(pointlistFilename, cnet, pvlLog);
   }
 
   //List has Cube file names
   if(ui.WasEntered("CUBELIST")) {
-    useDefaultOptions = false;
     std::string ignorelistFilename = ui.GetFilename("CUBELIST");
     ProcessControlMeasures(ignorelistFilename, cnet);
   }
 
+  // Perform validity check
   if(ui.GetBoolean("CHECKVALID")) {
-    useDefaultOptions = false;
-
     validator = NULL;
 
     Pvl defFile(ui.GetFilename("DEFFILE"));
@@ -88,35 +126,7 @@ void IsisMain() {
     Application::Log(defFile.FindGroup("ValidMeasure", Pvl::Traverse));
   }
 
-  //No List files - only Delete option was chosen
-  if(useDefaultOptions && deleteIgnored) {
-    for(int cp = cnet.Size() - 1; cp >= 0; cp --) {
-      if(cnet[cp].Ignore()) {
-        numMeasuresDeleted += cnet[cp].Size();
-        cnet.Delete(cp);
-        numPointsDeleted++;
-      }
-      else {
-        for(int cm = cnet[cp].Size() - 1; cm >= 0; cm --) {
-          if(cnet[cp][cm].Ignore()) {
-            cnet[cp].Delete(cm);
-            numMeasuresDeleted++;
-          }
-        }
-
-        // Check if the number of measures in the point is zero or
-        // there are few measures in the point and preserve flag is false.
-        if(((cnet[cp].Size() < 2 && !preservePoints) && cnet[cp].Type() != ControlPoint::Ground)
-            || cnet[cp].Size() == 0) {
-          numMeasuresDeleted += cnet[cp].Size();
-          cnet.Delete(cp);
-          numPointsDeleted++;
-        }
-      }
-    }
-  }
-
-  //log statistics
+  // Log statistics
   if(keepLog) {
     pvlLog += Isis::PvlKeyword("PointsDeleted", numPointsDeleted);
     pvlLog += Isis::PvlKeyword("MeasuresDeleted", numMeasuresDeleted);
@@ -204,10 +214,13 @@ void ProcessControlMeasures(std::string psFileName, ControlNet &pcCnet) {
       std::string serialNumber = pcCnet[cp][cm].CubeSerialNumber();
       if(snl.HasSerialNumber(serialNumber)) {
         pcCnet[cp][cm].SetIgnore(true);
+
+        if(pcCnet[cp][cm].IsReference())
+          pcCnet[cp].SetIgnore(true);
       }
 
       //also look for previously ignored control measures
-      if(deleteIgnored && pcCnet[cp][cm].Ignore()) {
+      if(deleteIgnored && pcCnet[cp][cm].Ignore() && !pcCnet[cp][cm].IsReference()) {
         pcCnet[cp].Delete(cm);
         numMeasuresDeleted++;
       }
@@ -246,10 +259,13 @@ void CheckAllMeasureValidity(ControlNet & cnet, std::string cubeList) {
 
       if(InvalidMeasure(cnet[cp][cm], serialNumbers.Filename(serialNumber))) {
         cnet[cp][cm].SetIgnore(true);
+
+        if(cnet[cp][cm].IsReference())
+          cnet[cp].SetIgnore(true);
       }
 
       //also look for previously ignored control measures
-      if(deleteIgnored && cnet[cp][cm].Ignore()) {
+      if(deleteIgnored && cnet[cp][cm].Ignore() && !cnet[cp][cm].IsReference()) {
         cnet[cp].Delete(cm);
         numMeasuresDeleted++;
       }
