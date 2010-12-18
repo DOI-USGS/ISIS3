90a4578d0645f9c5cb409d9ee5ef096dd6803b8a
Debbie Cook <dcook@usgs.gov>
2010-12-18 01:26:45 +0000

    merged system version with Ken Edmundson version
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1424 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/control/apps/jigsaw/jigsaw.cpp
+++ isis/src/control/apps/jigsaw/jigsaw.cpp
@@ -19,31 +19,37 @@ void IsisMain() {
   // Get the held list if entered and prep for bundle adjustment
   BundleAdjust *b = NULL;
 
-  if(ui.WasEntered("HELDLIST")) {
-    std::string heldList = ui.GetFilename("HELDLIST");
-    b = new BundleAdjust(cnetFile, cubeList, heldList);
-  }
-  else {
+//   if (ui.WasEntered("HELDLIST")) {
+//     std::string heldList = ui.GetFilename("HELDLIST");
+//     b = new BundleAdjust(cnetFile, cubeList, heldList);
+//   }
+//   else {
     b = new BundleAdjust(cnetFile, cubeList);
-  }
+//   }
+
+  if (ui.WasEntered("SC_SIGMAS"))
+    b->ReadSCSigmas(ui.GetFilename("SC_SIGMAS"));
 
   b->SetObservationMode(ui.GetBoolean("OBSERVATIONS"));
   b->SetSolutionMethod(ui.GetString("METHOD"));
   b->SetSolveRadii(ui.GetBoolean("RADIUS"));
+  b->SetErrorPropagation(ui.GetBoolean("ERRORPROPAGATION"));
+  b->SetOutlierRejection(ui.GetBoolean("OUTLIER_REJECTION"));
 
   b->SetCkDegree(ui.GetInteger("CKDEGREE"));
   b->SetSolveCamDegree(ui.GetInteger("SOLVEDEGREE"));
   std::string camsolve = ui.GetString("CAMSOLVE");
-  if(camsolve == "NONE") {
+
+  if (camsolve == "NONE") {
     b->SetSolveCmatrix(BundleAdjust::None);
   }
-  else if(camsolve == "ANGLES") {
+  else if (camsolve == "ANGLES") {
     b->SetSolveCmatrix(BundleAdjust::AnglesOnly);
   }
-  else if(camsolve == "VELOCITIES") {
+  else if (camsolve == "VELOCITIES") {
     b->SetSolveCmatrix(BundleAdjust::AnglesVelocity);
   }
-  else if(camsolve == "ACCELERATIONS") {
+  else if (camsolve == "ACCELERATIONS") {
     b->SetSolveCmatrix(BundleAdjust::AnglesVelocityAcceleration);
   }
   else {
@@ -66,38 +72,61 @@ void IsisMain() {
     b->SetSolveSpacecraftPosition(BundleAdjust::PositionVelocityAcceleration);
   }
 
+  // global parameter uncertainties
+  b->SetGlobalLatitudeAprioriSigma(ui.GetDouble("POINT_LATITUDE_SIGMA"));
+  b->SetGlobalLongitudeAprioriSigma(ui.GetDouble("POINT_LONGITUDE_SIGMA"));
+  b->SetGlobalRadiiAprioriSigma(ui.GetDouble("POINT_RADIUS_SIGMA"));
+
+  b->SetGlobalSpacecraftPositionAprioriSigma(ui.GetDouble("SPACECRAFT_POSITION_SIGMA"));
+  b->SetGlobalSpacecraftVelocityAprioriSigma(ui.GetDouble("SPACECRAFT_VELOCITY_SIGMA"));
+  b->SetGlobalSpacecraftAccelerationAprioriSigma(ui.GetDouble("SPACECRAFT_ACCELERATION_SIGMA"));
+
+  b->SetGlobalCameraAnglesAprioriSigma(ui.GetDouble("CAMERA_ANGLES_SIGMA"));
+  b->SetGlobalCameraAngularVelocityAprioriSigma(ui.GetDouble("CAMERA_ANGULAR_VELOCITY_SIGMA"));
+  b->SetGlobalCameraAngularAccelerationAprioriSigma(ui.GetDouble("CAMERA_ANGULAR_ACCELERATION_SIGMA"));
+
   // Check to make sure user entered something to adjust... Or can just points be in solution?
-  if(camsolve == "NONE"  &&  spsolve == "NONE") {
-    string msg = "Must either solve for camera pointing or spacecraft position";
-    throw iException::Message(Isis::iException::User, msg, _FILEINFO_);
-  }
+//   if (camsolve == "NONE"  &&  spsolve == "NONE") {
+//     string msg = "Must either solve for camera pointing or spacecraft position";
+//     throw iException::Message(Isis::iException::User, msg, _FILEINFO_);
+//   }
 
   double tol = ui.GetDouble("TOL");
-  int maxIterations = ui.GetInteger("MAXITS");
+
+  // set convergence threshold
+  b->SetConvergenceThreshold(ui.GetDouble("SIGMA0"));
+
+  // set maximum number of iterations
+  b->SetMaxIterations(ui.GetInteger("MAXITS"));
 
   // Bundle adjust the network
   try {
-    b->Solve(tol, maxIterations);
+
+    if(ui.GetString("METHOD") == "SPECIALK" )
+      b->SolveSpecialK();
+    else
+      b->Solve(tol);
+
     b->ControlNet()->Write(ui.GetFilename("ONET"));
     PvlGroup gp("JigsawResults");
 
     // Update the cube pointing if requested
-    if(ui.GetBoolean("UPDATE")) {
+    if (ui.GetBoolean("UPDATE")) {
 
-      for(int i = 0; i < b->Images(); i++) {
+      for (int i = 0; i < b->Images(); i++) {
         Process p;
         CubeAttributeInput inAtt;
         Cube *c = p.SetInputCube(b->Filename(i), inAtt, ReadWrite);
         //check for existing polygon, if exists delete it
-        if(c->Label()->HasObject("Polygon")) {
+        if (c->Label()->HasObject("Polygon")) {
           c->Label()->DeleteObject("Polygon");
         }
 
         //check for CameraStatistics Table, if exists, delete
-        for(int iobj = 0; iobj < c->Label()->Objects(); iobj++) {
+        for (int iobj = 0; iobj < c->Label()->Objects(); iobj++) {
           PvlObject obj = c->Label()->Object(iobj);
-          if(obj.Name() != "Table") continue;
-          if(obj["Name"][0] != iString("CameraStatistics")) continue;
+          if (obj.Name() != "Table") continue;
+          if (obj["Name"][0] != iString("CameraStatistics")) continue;
           c->Label()->DeleteObject(iobj);
           break;
         }
