674e5cc2b33e243d018bd2101ee30fbab71ec94e
Kris Becker <kbecker@usgs.gov>
2010-12-28 23:44:50 +0000

    Check in new Kernels class
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1467 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/base/objs/Kernels/Kernels.h
@@ -0,0 +1,185 @@
+#if !defined(Kernels_h)
+#define Kernels_h
+/**                                                                       
+ * @file                                                                  
+ * $Revision: 1.1 $
+ * $Date: 2009/06/26 23:39:21 $
+ * 
+ *   Unless noted otherwise, the portions of Isis written by the USGS are 
+ *   public domain. See individual third-party library and package descriptions 
+ *   for intellectual property information, user agreements, and related  
+ *   information.                                                         
+ *                                                                        
+ *   Although Isis has been used by the USGS, no warranty, expressed or   
+ *   implied, is made by the USGS as to the accuracy and functioning of such 
+ *   software and related material nor shall the fact of distribution     
+ *   constitute any such warranty, and no responsibility is assumed by the
+ *   USGS in connection therewith.                                        
+ *                                                                        
+ *   For additional information, launch                                   
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html                
+ *   in a browser or see the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.                                    
+ */                                                                       
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "Pvl.h"
+#include "Cube.h"
+#include "Camera.h"
+#include "CollectorMap.h"
+#include "iException.h"
+
+namespace Isis {
+
+  /**
+   * @brief Determine SPICE kernels defined in an ISIS file
+   * 
+   * This class determines all SPICE kernels associated with an ISIS cube file 
+   * and optionally/selectively loads them using the NAIF toolkit.  This creates
+   * the kernel pool as it was when spiceinit determined all the kernels and it 
+   * initialized the file for geometric operations. 
+   *  
+   * Note that because NAIF kernel management is global in nature, careful 
+   * thought and planning should preceed use of this class.  You can get 
+   * yourself into trouble if you take over management of kernels and 
+   * inadvertantly unload them without regard for use by other resources. 
+   * Methods are provided in this implementation to help minimize problems 
+   * related to management of this global resource. 
+   * 
+   * Note that ISIS caches some of the voluminous NAIF kernels, extracting only
+   * what is required from the SPK and CK (generally) kernels for a given 
+   * observation.  After this occurs, these kernels are no longer loaded by the
+   * ISIS Spice class hierarchy.  This class provides that environment so that
+   * further NAIF operations can occur, such as velocity vector and instrument
+   * timing manipulations. 
+   *  
+   * This class can be used instead of instantiating an ISIS Spice object.  The 
+   * recommended usage is as follows: 
+   *  
+   * @code 
+   *   Cube cube;
+   *   cube.Open("mycube.cub");
+   *   Kernels myKernels(cube);
+   *   myKernels.UpdateLoadStatus();  //  Ensures loaded kernels stay loaded
+   *   myKernels.Load();
+   *  
+   *    . . .
+   *  
+   *   // After manipulating the CK Spice data...
+   *   myKernels.UnLoad();
+   * @endcode 
+   *  
+   * The implications of this code segment are: 1) All the kernels pertinent to 
+   * an individual ISIS file are identified upon Kernels instantiation, 2) their 
+   * load status is determined using the UpdateLoadStatus() method, 3) kernels 
+   * already loaded are flagged as unmanaged, meaning they cannot be unloaded 
+   * unless management is an explicit "hostile takeover" provided via the 
+   * Manage() method (don't do it unless you absolutely have to), 4) after 
+   * checking load status, and kernels you load are managed by your 
+   * instantiation, and 5) when you UnLoad() or the object is destroyed, all 
+   * managed kernels are unloaded - unmanaged kernels are not unloaded. 
+   *  
+   * This class can be used in lieu of the Spice class with liberal use of the 
+   * UpdateLoadStatus() and Discover() methods of this class. 
+   *  
+   * It is possible to double load NAIF kernels, so be sure to maintain load 
+   * status via UpdateLoadStatus() and proper usage techniques. 
+   * 
+   * @ingroup Utility
+   * @author 2010-12-28 Kris Becker 
+   *  
+   * @internal 
+   */
+class Kernels {
+    public:
+      /** Default Constructor */
+      Kernels() { }
+      Kernels(const std::string &filename);
+      Kernels(Cube &cube);
+      Kernels(Pvl &pvl);
+      /** Destructor always unloads the kernels from the pool */
+      virtual ~Kernels() { UnLoad(); }
+
+      /** Returns the number of kernels found and/or loaded */
+      int size() const { return (_kernels.size());  }
+      int Missing() const;
+
+      void Init(Pvl &pvl);
+      bool Add(const std::string &kfile);
+
+      void Clear();
+      int Discover();
+
+      void Manage();
+      void UnManage();
+      bool IsManaged() const;
+
+      void InitializeNaifKernelPool();
+
+      int Load(const std::string &ktype);
+      int Load();
+
+      int UnLoad(const std::string &ktype);
+      int UnLoad();
+
+      int UpdateLoadStatus();
+
+      int Merge(const Kernels &other);
+
+      std::vector<std::string> getKernelTypes() const;
+      std::vector<std::string> getKernelList(const std::string &ktype = "") 
+                                             const;
+      std::vector<std::string> getLoadedList(const std::string &ktypes = "") 
+                                             const;
+      std::vector<std::string> getMissingList() const;
+
+      
+      /** Returns the ISIS camera model version number */
+      int CameraVersion() const { return (_camVersion); }
+
+  private:
+    struct KernelFile {          
+      std::string  pathname;
+      std::string  name;
+      std::string  fullpath;
+      bool         exists; 
+      std::string  ktype; 
+      mutable bool loaded; 
+      bool         managed;
+    };
+
+    typedef std::vector<KernelFile> KernelList;
+    KernelList _kernels;
+    int        _camVersion;
+
+    typedef std::vector<KernelFile *> KernelFileList;
+    typedef CollectorMap<std::string, KernelFileList> TypeList;
+
+    bool Load(KernelFile &kfile);
+    bool UnLoad(KernelFile &kfile);
+
+    std::vector<std::string> getTypes(const std::string &ktypes) const;
+    std::string resolveType(const std::string &kfile) const;
+    bool IsNaifType(const std::string &ktype) const;
+    KernelFile examine(const std::string &fname, const bool &manage = true) 
+                       const;
+    std::vector<KernelFile> findKernels(Pvl &pvl, const std::string &kname,
+                                       const bool &manage = true);
+    KernelFile *findByName(const std::string &kfile);
+    TypeList categorizeByType() const;
+     
+    void addKernels(const KernelList &klist);
+    std::string getKernelType(const std::string &kname) const; 
+    void loadKernel(const std::string &ktype = "");
+    int getCameraVersion(Pvl &pvl) const;
+
+};
+
+}     // namespace Isis
+#endif
+
+
---
+++ isis/src/base/objs/Kernels/unitTest.cpp
@@ -0,0 +1,179 @@
+#include "Preference.h"
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+
+#include "Kernels.h"
+
+//  Used extensively in Kernels interface
+typedef std::vector<std::string>  StrList;
+
+using namespace std;
+using namespace Isis;
+
+int main(int argc, char *argv[]) {
+  Isis::Preference::Preferences(true);
+  string inputFile = "$ISIS3DATA/mgs/testData/ab102401.lev2.cub";
+  if (--argc == 1) { inputFile = argv[1]; }
+
+  cout << "\n\nTesting Kernels class using file " << inputFile << "\n";
+
+  Kernels myKernels(inputFile);
+  cout << "\nList of kernels found - Total: " << myKernels.size() << "\n";
+  StrList kfiles = myKernels.getKernelList();
+  copy(kfiles.begin(), kfiles.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  cout << "\nTypes of kernels found\n";
+  StrList ktypes = myKernels.getKernelTypes();
+  copy(ktypes.begin(), ktypes.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  //  Test to see if we have any kernels loaded at all
+  Kernels query;
+  query.Discover();
+  cout << "\nInitial currently loaded kernel files = " << query.size() << "\n";
+  StrList kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  //  Load all the kernels
+  myKernels.Load();
+  query.Discover();
+  cout << "\nAfter LoadALL option, kernels loaded = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Unload and check for proper status
+  myKernels.UnLoad();
+  query.Discover();
+  cout << "\nUnLoading All, count after = " << query.size() << "\n";
+
+  //  Now load the DAF kernels after unloading
+  myKernels.Load("DAF");
+  query.Discover();
+  cout << "\nLoaded DAF kernels = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Load kernels needed for Time manipulation
+  myKernels.Load("LSK,SCLK");
+  myKernels.UnLoad("DAF");
+  query.Discover();
+  cout << "\nLoad LSK, SCLK for Time manip, unload DAF kernels = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Check double load behavior
+  Kernels clone;
+  clone.Merge(query);
+  clone.Manage();
+  clone.UnLoad();
+
+  myKernels.UpdateLoadStatus();
+  cout << "\nNumber loaded: " << myKernels.getLoadedList().size() << "\n";
+  myKernels.Load("LSK,SCLK");
+  // Load same files
+  clone.Load();
+  query.Discover();
+  cout << "\nCheck Double-Load of LSK, SCLK = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Unload each set
+  clone.UnLoad();
+  query.Discover();
+  cout << "\nUnload the cloned set = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+  clone.UnManage();
+
+  //  Load SPK set
+  myKernels.UnLoad();
+  myKernels.Load("LSK,FK,DAF,SPK");
+  query.Discover();
+  cout << "\nCheck SPK load  (LSK,FK,DAF,SPK)= " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Now unload SPKs, preserve LSK and load CK stuff
+  myKernels.UnLoad("DAF,SPK");
+  cout << "Unload DAF,SPK\n";
+  myKernels.Load("SCLK,IK,CK");
+  query.Discover();
+  cout << "\nCheck CK load  (SCLK,IK,CK) = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Now reload all and check
+  myKernels.Load("LSK,FK,SCLK,IK,CK");
+  query.Discover();
+  cout << "\nCheck CK reload  (LSK,FK,SCLK,IK,CK) = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  //  Clear the pool and start fresh.  Clear all instances and reinitialize NAIF
+  clone.Clear();
+  query.Clear();
+  myKernels.Clear();
+  myKernels.InitializeNaifKernelPool();
+
+  // Left two kernels open, ensure we have none left
+  query.Discover();
+  cout << "\n\nEnsure clean pool...Count: " << query.size() << "\n";
+
+  // Load a bogus file and check for missing
+  myKernels.Add("$base/kernels/lsk/dne.lsk");
+  cout << "\nLoad of bogus file, should have one missing: " << myKernels.Missing()
+       << "\n";
+  myKernels.Clear();
+
+  // Now add a set be hand
+  myKernels.Add("$base/kernels/lsk/naif0009.tls");
+  myKernels.Add("$base/kernels/spk/de405.bsp");
+  myKernels.Add("$clementine1/kernels/ck/clem_ulcn2005_type2_1sc.bc");
+  myKernels.Add("$clementine1/kernels/fk/clem_v11.tf");
+  myKernels.Add("$clementine1/kernels/sclk/dspse002.tsc");
+  myKernels.Add("$clementine1/kernels/spk/SPKMERGE_940219_940504_CLEMV001b.bsp");
+  myKernels.Add("$clementine1/kernels/iak/uvvisAddendum003.ti");
+
+  cout << "\n\nAdd Kernels directly - Count: " << myKernels.size() 
+       << ", Missing: "<< myKernels.Missing() << "\n";
+  cout << "\nList of kernels in object..\n";
+  kfiles = myKernels.getKernelList();
+  copy(kfiles.begin(), kfiles.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  cout << "\nList of kernel types\n";
+  ktypes = myKernels.getKernelTypes();
+  copy(ktypes.begin(), ktypes.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Find unknown types
+  kfiles = myKernels.getKernelList("UNKNOWN");
+  cout << "\nUnknown kernels in list: " << kfiles.size() << "\n";
+  copy(kfiles.begin(), kfiles.end(), ostream_iterator<std::string>(cout, "\n"));
+
+
+  // Load them all
+  myKernels.Load();
+  kloaded = myKernels.getLoadedList();
+  cout << "\nLoading all, total loaded: " << kloaded.size() << "\n";
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  //  Now double check list
+  query.Discover();
+  cout << "\nCheck Load Status = " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  // Unload SPK and CKs
+  myKernels.UnLoad("SPK,CK");
+  query.Discover();
+  cout << "\nUnload SPK,CK - Loaded: " << query.size() << "\n";
+  kloaded = query.getKernelList();
+  copy(kloaded.begin(), kloaded.end(), ostream_iterator<std::string>(cout, "\n"));
+
+  myKernels.UnLoad();
+  query.Discover();
+  cout << "\n\nAll Done - Should be 0 discovered: " << query.size() << "\n";
+  // All done...
+
+  return (0);
+}
