98a60c31443349d56f052b6c00af5a22263b5c89
Janet Barrett <jbarrett@usgs.gov>
2011-01-26 01:15:59 +0000

    Added new directory for Kaguya specific s/w
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@1620 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/kaguya/apps/kaguyatc2isis/kaguyatc2isis.cpp
@@ -0,0 +1,75 @@
+#include "Isis.h"
+
+#include <cstdio>
+#include <string>
+
+#include "ProcessImportPds.h"
+
+#include "UserInterface.h"
+#include "Filename.h"
+
+using namespace std;
+using namespace Isis;
+
+void IsisMain() {
+  ProcessImportPds p;
+  Pvl label;
+  UserInterface &ui = Application::GetUserInterface();
+
+  string labelFile = ui.GetFilename("FROM");
+  Filename inFile = ui.GetFilename("FROM");
+  iString id;
+  Pvl lab(inFile.Expanded());
+
+  try {
+    id = (string) lab.FindKeyword("DATA_SET_ID");
+  }
+  catch(iException &e) {
+    string msg = "Unable to read [DATA_SET_ID] from input file [" +
+                 inFile.Expanded() + "]";
+    throw iException::Message(iException::Io, msg, _FILEINFO_);
+  }
+
+  id.ConvertWhiteSpace();
+  id.Compress();
+  id.Trim(" ");
+  if(id != "TC_MAP") { 
+    string msg = "Input file [" + inFile.Expanded() + "] does not appear to be " +
+                 "in Kaguya Terrain Camera level 2 format. " +
+                 "DATA_SET_ID is [" + id + "]";
+    throw iException::Message(iException::Io, msg, _FILEINFO_);
+  }
+
+  p.SetPdsFile(labelFile, "", label);
+  Cube *outcube = p.SetOutputCube("TO");
+
+  p.SetOrganization(Isis::ProcessImport::BSQ);
+
+  p.StartProcess();
+
+  // Get the mapping labels
+  Pvl otherLabels;
+  p.TranslatePdsProjection(otherLabels);
+
+  // Get the directory where the generic pds2isis level 2 translation tables are.
+  PvlGroup dataDir(Preference::Preferences().FindGroup("DataDirectory"));
+  iString transDir = (string) dataDir["base"] + "/translations/";
+
+  // Translate the Archive group
+  Filename transFile(transDir + "pdsImageArchive.trn");
+  PvlTranslationManager archiveXlater(label, transFile.Expanded());
+  archiveXlater.Auto(otherLabels);
+
+  // Write the Archive and Mapping groups to the output cube label
+  outcube->PutGroup(otherLabels.FindGroup("Mapping"));
+  outcube->PutGroup(otherLabels.FindGroup("Archive"));
+   
+  // Add the BandBin group
+  PvlGroup bbin("BandBin");
+  bbin += PvlKeyword("FilterName", "BroadBand");
+  bbin += PvlKeyword("Center", "640nm");
+  bbin += PvlKeyword("Width", "420nm");
+  outcube->PutGroup(bbin);
+
+  p.EndProcess();
+}
