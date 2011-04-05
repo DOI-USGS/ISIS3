3b830f646de43e63ba1258705d35626b94ca0d65
Steven Lambright <slambright@usgs.gov>
2011-04-05 17:44:42 +0000

    Added progress
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2188 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/control/apps/cnetpvl2bin/cnetpvl2bin.cpp
+++ isis/src/control/apps/cnetpvl2bin/cnetpvl2bin.cpp
@@ -1,23 +1,16 @@
 #include "Isis.h"
 
-#include <sstream>
-
-#include "Pvl.h"
 #include "ControlNet.h"
-#include "iString.h"
+#include "Progress.h"
 
 using namespace Isis;
-using namespace std;
 
-void IsisMain(){
-
-  // Get user entered file name & mode 
+void IsisMain() {
+  // Get user entered file name & mode
   UserInterface &ui = Application::GetUserInterface();
+  Progress p;
 
-  ControlNet *cnet = new ControlNet;
-  cnet->ReadControl(ui.GetFilename("FROM"));
-  cnet->WritePB(ui.GetFilename("TO"));
-
-  delete cnet;
-
+  ControlNet cnet;
+  cnet.ReadControl(ui.GetFilename("FROM"), &p);
+  cnet.WritePB(ui.GetFilename("TO"));
 }
