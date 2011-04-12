f1672c2862b9e4cabd534a9ab2423ac5650587ad
Mackenzie Boyd <mboyd@usgs.gov>
2011-04-12 00:14:28 +0000

    Change parameter LONG to LON, removed duplicate code concerned withoption XY or LATLON
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2236 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/maplab/maplab.cpp
+++ isis/src/base/apps/maplab/maplab.cpp
@@ -42,103 +42,76 @@ void IsisMain() {
     throw iException::Message(iException::User, msg, _FILEINFO_);
   }
 
-  //Read in line and sample inputs
-  double line = ui.GetDouble("LINE");
-  double samp = ui.GetDouble("SAMPLE");
 
   // Get user entered option
   string option = ui.GetString("COORDINATES");
 
-  //Given x,y coordinates
+  double x = 0.0;
+  double y = 0.0;
+  Projection * proj = ProjectionFactory::Create(userMap, false);
   if(option == "XY") {
-    //find values for x and y at the origin (upperleftcorner)
-    double x = ui.GetDouble("X");
-    double y = ui.GetDouble("Y");
-    //Get Resolution and Scale
-    double res = 0.0;
-    double scale = 0.0;
-    Projection *proj = ProjectionFactory::Create(userMap, false);
-    if(mapGrp.HasKeyword("PixelResolution")) {
-      double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
-      res = mapGrp.FindKeyword("PixelResolution");
-      scale = (2.0 * Isis::PI * localRadius) / (360.0 * res);
-    }
-    else if(mapGrp.HasKeyword("Scale")) {
-      double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
-      scale = mapGrp.FindKeyword("Scale");
-      res = (2.0 * Isis::PI * localRadius) / (360.0 * scale);
-    }
-    else {
-      string msg = "The given MAP [" + userMap.Name() +
-                   "] does not have the PixelResolution or Scale keywords.";
-      throw iException::Message(iException::User, msg, _FILEINFO_);
-    }
-    x = x - res * (samp - 0.5);
-    y = y + res * (line - 0.5);
-    //add origen values to Mapping Group
-    mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerX", x, "meters"), Pvl::Replace);
-    mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerY", y, "meters"), Pvl::Replace);
-    if(not mapGrp.HasKeyword("PixelResolution")) {
-      mapGrp.AddKeyword(PvlKeyword("PixelResolution", res, "meters"));
-    }
-    if(not mapGrp.HasKeyword("Scale")) {
-      mapGrp.AddKeyword(PvlKeyword("Scale", scale, "pixels/degree"));
-    }
+    x = ui.GetDouble("X");
+    y = ui.GetDouble("Y");
   }
-  //Given latitude,longitude coordinates
+  else if(option == "LATLON") {
+    proj->SetGround(ui.GetDouble("LAT"), ui.GetDouble("LON"));
+    x = proj->XCoord();
+    y = proj->YCoord();
+  }
   else {
-    //get lat and lon from user interface
-    double lat = ui.GetDouble("LAT");
-    double lon = ui.GetDouble("LONG");
-    //create projection using input map
-    Projection *proj = ProjectionFactory::Create(userMap, false);
-    // feed lat, lon into projection
-    proj->SetGround(lat, lon);
-    //find origen values for x and y
-    double x = proj->XCoord();
-    double y = proj->YCoord();
-    //Get Resolution and Scale
-    double res = 0.0;
-    double scale = 0.0;
-    if(mapGrp.HasKeyword("PixelResolution")) {
-      double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
-      res = mapGrp.FindKeyword("PixelResolution");
-      scale = (2.0 * Isis::PI * localRadius) / (360.0 * res);
-    }
-    else if(mapGrp.HasKeyword("Scale")) {
-      double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
-      scale = mapGrp.FindKeyword("Scale");
-      res = (2.0 * Isis::PI * localRadius) / (360.0 * scale);
-    }
-    else {
-      string msg = "The given MAP[" + userMap.Name() +
-                   "] does not have the PixelResolution or Scale keywords.";
-      throw iException::Message(iException::User, msg, _FILEINFO_);
-    }
-    x = x - res * (samp - 0.5);
-    y = y + res * (line - 0.5);
-    //add origen values to Mapping Group
-    mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerX", x, "meters"), Pvl::Replace);
-    mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerY", y, "meters"), Pvl::Replace);
-    if(not mapGrp.HasKeyword("PixelResolution")) {
-      mapGrp.AddKeyword(PvlKeyword("PixelResolution", res, "meters"));
-    }
-    if(not mapGrp.HasKeyword("Scale")) {
-      mapGrp.AddKeyword(PvlKeyword("Scale", scale, "pixels/degree"));
-    }
+    string message = "Invalid option [" + option + "] for parameter COORDINATES";
+    throw iException::Message(iException::User, message, _FILEINFO_);
+  } 
+
+  double res = 0.0;
+  double scale = 0.0;
+
+  if(mapGrp.HasKeyword("PixelResolution")) {
+    double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
+    res = mapGrp.FindKeyword("PixelResolution");
+    scale = (2.0 * Isis::PI * localRadius) / (360.0 * res);
   }
+  else if(mapGrp.HasKeyword("Scale")) {
+    double localRadius = proj->LocalRadius(proj->TrueScaleLatitude());
+    scale = mapGrp.FindKeyword("Scale");
+    res = (2.0 * Isis::PI * localRadius) / (360.0 * scale);
+  }
+  else {
+    string msg = "The given MAP[" + userMap.Name() +
+                 "] does not have the PixelResolution or Scale keywords.";
+    throw iException::Message(iException::User, msg, _FILEINFO_);
+  }
+  
+  //Read in line and sample inputs
+  double line = ui.GetDouble("LINE");
+  double samp = ui.GetDouble("SAMPLE");
+  x = x - res * (samp - 0.5);
+  y = y + res * (line - 0.5);
+
+  //add origen values to Mapping Group
+  mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerX", x, "meters"), Pvl::Replace);
+  mapGrp.AddKeyword(PvlKeyword("UpperLeftCornerY", y, "meters"), Pvl::Replace);
+
+  if(!mapGrp.HasKeyword("PixelResolution")) {
+    mapGrp.AddKeyword(PvlKeyword("PixelResolution", res, "meters"));
+  }
+  if(!mapGrp.HasKeyword("Scale")) {
+    mapGrp.AddKeyword(PvlKeyword("Scale", scale, "pixels/degree"));
+  }
+
+
   // Output the mapping group used to the Gui session log
   Application::GuiLog(userMap);
   // Extract label from cube file
   Pvl *label = cube.Label();
   PvlObject &o = label->FindObject("IsisCube");
-  //Add Mapping Group to input cube
+  // Add Mapping Group to input cube
   if(o.HasGroup("Mapping")) {
     o.DeleteGroup("Mapping");
   }
   o.AddGroup(mapGrp);
 
-  //keep track of change to labels in history
+  // keep track of change to labels in history
   History hist = History("IsisCube");
   try {
     cube.Read(hist);
