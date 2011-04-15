02682de9834ab5a2f142a2ccf805bf7367205721
Sharmilla Prasad <sprasad@usgs.gov>
2011-04-15 17:23:09 +0000

    Ported the enlarge functionality to base/objs/Enlarge class
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2264 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/enlarge/enlarge.cpp
+++ isis/src/base/apps/enlarge/enlarge.cpp
@@ -1,15 +1,11 @@
 #include "Isis.h"
 
-#include "ProcessRubberSheet.h"
-#include "enlarge.h"
+#include "Enlarge.h"
 #include "iException.h"
-#include "AlphaCube.h"
-#include "SubArea.h"
+#include "ProcessRubberSheet.h"
 
 using namespace std;
 using namespace Isis;
-
-Cube cube;
 
 void IsisMain() {
   ProcessRubberSheet p;
@@ -23,22 +19,20 @@ void IsisMain() {
   int inb = icube->Bands();
 
   // Output samples and lines
-  int ons;
-  int onl;
+  int ons, onl;
 
   // Scaling factors
-  double sscale;
-  double lscale;
+  double sampleScale, lineScale;
 
   UserInterface &ui = Application::GetUserInterface();
   if(ui.GetString("MODE") == "SCALE") {
     // Retrieve the provided scaling factors
-    sscale = ui.GetDouble("SSCALE");
-    lscale = ui.GetDouble("LSCALE");
+    sampleScale = ui.GetDouble("SSCALE");
+    lineScale   = ui.GetDouble("LSCALE");
 
     // Calculate the output size. If there is a fractional pixel, round up
-    ons = (int)ceil(ins * sscale);
-    onl = (int)ceil(inl * lscale);
+    ons = (int)ceil(ins * sampleScale);
+    onl = (int)ceil(inl * lineScale);
   }
   else {
     // Retrieve the provided sample/line dimensions in the output
@@ -46,8 +40,8 @@ void IsisMain() {
     onl = ui.GetInteger("ONL");
 
     // Calculate the scaling factors
-    sscale = (double)ons / (double)ins;
-    lscale = (double)onl / (double)inl;
+    sampleScale = (double)ons / (double)ins;
+    lineScale   = (double)onl / (double)inl;
   }
 
   // Ensure that the calculated number of output samples and lines is greater
@@ -57,17 +51,7 @@ void IsisMain() {
     msg = msg + " to the input samples/lines.";
     throw iException::Message(iException::User, msg, _FILEINFO_);
   }
-
-  // Set up the transform object with the calculated scale and number of
-  // output pixels
-  Transform *transform = new Enlarge(sscale, lscale, ons, onl);
-
-  string from = ui.GetFilename("FROM");
-  cube.Open(from);
-
-  // Allocate the output file, the number of bands does not change in the output
-  Cube *ocube = p.SetOutputCube("TO", ons, onl, inb);
-
+  
   // Set up the interpolator
   Interpolator *interp;
   if(ui.GetString("INTERP") == "NEARESTNEIGHBOR") {
@@ -84,34 +68,22 @@ void IsisMain() {
                  ui.GetString("INTERP") + "]";
     throw iException::Message(iException::Programmer, msg, _FILEINFO_);
   }
+  
+  // Allocate the output file, the number of bands does not change in the output
+  Cube *ocube = p.SetOutputCube("TO", ons, onl, inb);
+
+  // Set up the transform object with the calculated scale and number of
+  // output pixels
+  Transform *transform = new Enlarge(icube, sampleScale, lineScale);
   p.StartProcess(*transform, *interp);
-
-  // Construct a label with the results
-  PvlGroup results("Results");
-  results += PvlKeyword("InputLines", inl);
-  results += PvlKeyword("InputSamples", ins);
-  results += PvlKeyword("StartingLine", "1");
-  results += PvlKeyword("StartingSample", "1");
-  results += PvlKeyword("EndingLine", inl);
-  results += PvlKeyword("EndingSample", ins);
-  results += PvlKeyword("LineIncrement", 1. / lscale);
-  results += PvlKeyword("SampleIncrement", 1. / sscale);
-  results += PvlKeyword("OutputLines", onl);
-  results += PvlKeyword("OutputSamples", ons);
-
-  // Update the Mapping, Instrument, and AlphaCube groups in the output
-  // cube label
-  SubArea s;
-  s.SetSubArea(cube.Lines(), cube.Samples(), 1, 1, cube.Lines(), cube.Samples(),
-               1. / lscale, 1. / sscale);
-  s.UpdateLabel(&cube, ocube, results);
-
+  PvlGroup resultsGrp = transform->UpdateOutputLabel(ocube);
+  
   p.EndProcess();
 
   delete transform;
   delete interp;
 
   // Write the results to the log
-  Application::Log(results);
+  Application::Log(resultsGrp);
 }
 
