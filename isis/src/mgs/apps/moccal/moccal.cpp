f59f7def38f28e0f49dcf8c524cc2b2f58b5a39a
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 00:48:18 +0000

    moccal.xml: Removed Mgs namespace scope.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2347 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/mgs/apps/moccal/moccal.cpp
+++ isis/src/mgs/apps/moccal/moccal.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 #include "Isis.h"
 #include "ProcessByLine.h"
 #include "SpecialPixel.h"
@@ -31,7 +49,7 @@ namespace gbl {
   vector<double> inLineAvg;     // Average of each input line
   vector<double> outLineAvg;    // Average of each output line
 
-  Mgs::MocLabels *moc;
+  MocLabels *moc;
 
   bool nullWago;
 }
@@ -44,7 +62,7 @@ void IsisMain() {
   // Setup the input and make sure it is a moc file
   UserInterface &ui = Application::GetUserInterface();
   Cube *icube = p.SetInputCube("FROM", OneBand);
-  gbl::moc = new Mgs::MocLabels(ui.GetFilename("FROM"));
+  gbl::moc = new MocLabels(ui.GetFilename("FROM"));
 
   // If it is already calibrated then complain
   if(icube->HasGroup("Radiometry")) {
