c9980c9c87b49e53fb4d5e08ad0cc4dcc6de9760
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 00:47:13 +0000

    LwirCamera.h: 2011-05-03 Jeannie Walldren - Added ShutterOpenCloseTimes() method. Updated unitTest to test for new methods. Updated documentation. Replaced Clementine namespace wrap with Isis namespace. Added Isis Disclaimer to files. Added NAIF error check to constructor.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2344 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/clementine/objs/LwirCamera/LwirCamera.cpp
+++ isis/src/clementine/objs/LwirCamera/LwirCamera.cpp
@@ -1,21 +1,52 @@
+/**
+ * @file
+ *
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for 
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software 
+ *   and related material nor shall the fact of distribution constitute any such 
+ *   warranty, and no responsibility is assumed by the USGS in connection 
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see 
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
 #include "LwirCamera.h"
+
 #include "CameraDetectorMap.h"
 #include "CameraFocalPlaneMap.h"
-#include "RadialDistortionMap.h"
 #include "CameraGroundMap.h"
 #include "CameraSkyMap.h"
 #include "iString.h"
 #include "iTime.h"
+#include "NaifStatus.h"
+#include "RadialDistortionMap.h"
 
 using namespace std;
-using namespace Isis;
-namespace Clementine {
+
+namespace Isis {
   /**
-   * Constructor
+   * Constructs a LwirCamera object using the image labels
+   *
+   * @param lab Pvl label from a Clementine LWIR image.
+   *
    * @internal
    *   @history 2009-01-16 Jeannie Walldren - Original Version
+   *   @history 2011-05-03 Jeannie Walldren - Added NAIF error check.  Added
+   *                          call to ShutterOpenCloseTimes() method. Changed
+   *                          centertime to add half exposure duration to start
+   *                          time to maintain consistency with other Clementine
+   *                          models. 
    */
   LwirCamera::LwirCamera(Pvl &lab) : FramingCamera(lab) {
+    NaifStatus::CheckErrors();
 
     // Get the camera characteristics
     iString filter = (string)(lab.FindGroup("BandBin", Pvl::Traverse))["FilterName"];
@@ -28,12 +59,22 @@ namespace Clementine {
     PvlGroup inst = lab.FindGroup("Instrument", Pvl::Traverse);
     string stime = inst["StartTime"];
 
+    // set variables startTime and exposureDuration
     double time;
     str2et_c(stime.c_str(), &time);
+    // divide exposure duration keyword value by 1000 to convert to seconds
+    double exposureDuration = ((double) inst["ExposureDuration"]) / 1000.0;
+    pair<iTime, iTime> shuttertimes = ShutterOpenCloseTimes(time, exposureDuration);
 
-    // Do not correct time for center of the exposure duration. This is because the kernels were built to accept the
-    // start times of the images.
-    //time += ((double)inst["ExposureDuration"] / 1000.0) / 2.0; // Add half exposure duration in milliseconds
+    /************************************************************************
+     * The following line was uncommented to maintain consistency within all
+     * clementine camera models. Not sure why the following was originally
+     * commented out:
+     * 2010-08-05 Jeannie Walldren
+     ***********************************************************************/
+    // Do not correct time for center of the exposure duration. This is because
+    // the kernels were built to accept the start times of the images.
+    iTime centerTime = shuttertimes.first.Et() + exposureDuration / 2.0; // Add half exposure duration
 
     // Setup detector map
     new CameraDetectorMap(this);
@@ -41,8 +82,11 @@ namespace Clementine {
     // Setup focal plane map
     CameraFocalPlaneMap *focalMap = new CameraFocalPlaneMap(this, NaifIkCode());
 
-    focalMap->SetDetectorOrigin(Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + "_BORESIGHT_SAMPLE"),
-                                Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + "_BORESIGHT_LINE"));
+    focalMap->SetDetectorOrigin(
+      Spice::GetDouble("INS" + (iString)(int)NaifIkCode() +
+                       "_BORESIGHT_SAMPLE"),
+      Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + 
+                       "_BORESIGHT_LINE"));
 
     // Setup distortion map
     new RadialDistortionMap(this, 0.0);
@@ -51,15 +95,53 @@ namespace Clementine {
     new CameraGroundMap(this);
     new CameraSkyMap(this);
 
-    SetTime(time);
+    SetTime(centerTime);
     LoadCache();
+    NaifStatus::CheckErrors();
+  }
+
+  /**
+   * Returns the shutter open and close times. The user should pass in the
+   * ExposureDuration keyword value, converted from milliseconds to seconds, and
+   * the StartTime keyword value, converted to ephemeris time. The StartTime
+   * keyword value from the labels represents the time at the start of the
+   * observation, as noted in the Clementine EDR image SIS. This method uses the
+   * FramingCamera class implementation, returning the given time value as the
+   * shutter open and the sum of the time value and exposure duration as the
+   * shutter close.
+   *
+   * @param exposureDuration ExposureDuration keyword value from the labels,
+   *                         converted to seconds.
+   * @param time The StartTime keyword value from the labels, converted to
+   *             ephemeris time.
+   *
+   * @return @b pair < @b iTime, @b iTime > The first value is the shutter
+   *         open time and the second is the shutter close time.
+   *
+   * @see http://pds-imaging.jpl.nasa.gov/documentation/clementine_edrsis.pdf
+   * @author 2011-05-03 Jeannie Walldren
+   * @internal
+   *   @history 2011-05-03 Jeannie Walldren - Original version.
+   */
+  pair<iTime, iTime> LwirCamera::ShutterOpenCloseTimes(double time,
+                                                       double exposureDuration) {
+    return FramingCamera::ShutterOpenCloseTimes(time, exposureDuration);
   }
 }
 
+
 /**
+ * This is the function that is called in order to instantiate a LwirCamera
+ * object. 
+ *
+ * @param lab Cube labels
+ *
+ * @return Isis::Camera* LwirCamera
  * @internal
  *   @history 2009-01-16 Jeannie Walldren - Original Version
+ *   @history 2011-05-03 Jeannie Walldren - Added documentation.  Removed
+ *            Clementine namespace.
  */
-extern "C" Camera *LwirCameraPlugin(Pvl &lab) {
-  return new Clementine::LwirCamera(lab);
+extern "C" Isis::Camera *LwirCameraPlugin(Isis::Pvl &lab) {
+  return new Isis::LwirCamera(lab);
 }
--- isis/src/clementine/objs/LwirCamera/LwirCamera.h
+++ isis/src/clementine/objs/LwirCamera/LwirCamera.h
@@ -1,36 +1,91 @@
 #ifndef LwirCamera_h
 #define LwirCamera_h
+/**
+ * @file 
+ *  
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 
 #include "FramingCamera.h"
 
-namespace Clementine {
+namespace Isis {
   /**
-   * @brief LWIR Camera class
+   * This is the camera model for the Clementine Long-Wavelength Infrared Camera
    *
-   * This is the camera class for Clementine's long wavelength
-   * infared camera.
-   *
+   * @ingroup SpiceInstrumentsAndCameras
    * @ingroup Clementine
+   *  
+   * @see 
+   *      http://astrogeology.usgs.gov/Projects/Clementine/nasaclem/sensors/lwir/lwir.html
+   * @see 
+   *      http://astrogeology.usgs.gov/Projects/Clementine/nasaclem/clemhome.html
+   * @see http://pds-imaging.jpl.nasa.gov/portal/clementine_mission.html 
+   * @see http://astrogeology.usgs.gov/Missions/Clementine
    *
    * @author  2009-01-16 Jeannie Walldren
    *
    * @internal
-   *   @history 2009-01-16 Jeannie Walldren - Original Version
+   *   @history 2009-01-22 Jeannie Walldren - Original Version
    *   @history 2009-08-28 Steven Lambright - Changed inheritance to no longer
-   *            inherit directly from Camera
+   *                          inherit directly from Camera.  Camera is now pure
+   *                          virtual, parent class is FramingCamera.
+   *   @history 2010-09-16 Steven Lambright - Updated unitTest to not use a DEM.
+   *   @history 2011-01-14 Travis Addair - Added new CK/SPK accessor methods,
+   *                          pure virtual in Camera, implemented in mission
+   *                          specific cameras.
+   *   @history 2011-02-09 Steven Lambright - Major changes to camera classes.
+   *   @history 2011-05-03 Jeannie Walldren - Added ShutterOpenCloseTimes()
+   *                          method. Updated unitTest to test for new methods.
+   *                          Updated documentation. Replaced Clementine
+   *                          namespace wrap with Isis namespace. Added Isis
+   *                          Disclaimer to files. Added NAIF error check to
+   *                          constructor.
+   *  
    */
-  class LwirCamera : public Isis::FramingCamera {
+  class LwirCamera : public FramingCamera {
     public:
-      LwirCamera(Isis::Pvl &lab);
+      LwirCamera(Pvl &lab);
+      //! Destroys the LwirCamera object
       ~LwirCamera() {};
+      virtual std::pair <iTime, iTime> ShutterOpenCloseTimes(double time,
+                                                             double exposureDuration);
 
-      /** CK Frame ID - Instrument Code from spacit run on CK */
+      /**
+       * CK frame ID -  - Instrument Code from spacit run on CK
+       *  
+       * @return @b int The appropriate instrument code for the "Camera-matrix" 
+       *         Kernel Frame ID
+       */
       virtual int CkFrameId() const { return (-40000); }
 
-      /** CK Reference ID - J2000 */
+      /** 
+       * CK Reference ID - J2000
+       * 
+       * @return @b int The appropriate instrument code for the "Camera-matrix"
+       *         Kernel Reference ID
+       */
       virtual int CkReferenceId() const { return (1); }
 
-      /** SPK Reference ID - J2000 */
+      /** 
+       * SPK Reference ID - J2000
+       * 
+       * @return @b int The appropriate instrument code for the Spacecraft
+       *         Kernel Reference ID
+       */
       virtual int SpkReferenceId() const { return (1); }
   };
 };
