7943bb4623ae64a55ea4a00047755cacc2b10792
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 00:49:11 +0000

    MocNarrowAngleSumming.h: 2011-05-03 Jeannie Walldren - Updated documentation. Removed Mgs namespace wrap inside Isis namespace. Added Isis Disclaimer to file.  Removed Mgs scope from unitTest and truth file.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2351 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/mgs/objs/MocNarrowAngleSumming/MocNarrowAngleSumming.h
+++ isis/src/mgs/objs/MocNarrowAngleSumming/MocNarrowAngleSumming.h
@@ -1,33 +1,75 @@
 #ifndef MocNarrowAngleSumming_h
 #define MocNarrowAngleSumming_h
+/**
+ * @file
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 
 namespace Isis {
-  namespace Mgs {
-    class MocNarrowAngleSumming {
-      public:
-        // constructors
-        MocNarrowAngleSumming(int csum, int ss) {
-          p_csum = csum;
-          p_ss = ((double) csum / 2.0) + 0.5 + (double)(ss - 1);
-        }
+  /**
+   * Mars Global Surveyor MOC narrow angle summing class. 
+   *
+   * @ingroup SpiceInstrumentsAndCameras
+   * @ingroup MarsGlobalSurveyor
+   *  
+   * @internal
+   *   @history 2011-05-03 Jeannie Walldren - Updated documentation. Removed Mgs
+   *            namespace wrap inside Isis namespace. Added Isis Disclaimer to
+   *            file.  Removed Mgs scope from unitTest and truth file.
+   */
+  class MocNarrowAngleSumming {
+    public:
+      /** 
+       * Constructs the MocNarrowAngleSumming object 
+       * @param csum 
+       * @param ss 
+       */
+      MocNarrowAngleSumming(int csum, int ss) {
+        p_csum = csum;
+        p_ss = ((double) csum / 2.0) + 0.5 + (double)(ss - 1);
+      }
 
-        // destructor
-        ~MocNarrowAngleSumming() {};
+      //! Destroys the MocNarrowAngleSumming object
+      ~MocNarrowAngleSumming() {};
 
-        inline double Detector(double sample) const {
-          return (sample - 1.0) * (double) p_csum + p_ss;
-        }
+      /**
+       * Given the sample value, this method computes the corresponding 
+       * detector. 
+       * @param sample 
+       * @return @b double Detector
+       */
+      inline double Detector(double sample) const {
+        return (sample - 1.0) * (double) p_csum + p_ss;
+      }
 
-        inline double Sample(double detector) const {
-          return (detector - p_ss) / ((double) p_csum) + 1.0;
-        }
+      /** 
+       * Given the detector value, this method computes the corresponding 
+       * sample. 
+       * @param detector 
+       * @return @b double Sample
+       */
+      inline double Sample(double detector) const {
+        return (detector - p_ss) / ((double) p_csum) + 1.0;
+      }
 
-      private:
-        int p_csum;
-        double p_ss;
+    private:
+      int p_csum;
+      double p_ss;
 
-    };
   };
 };
-
 #endif
--- isis/src/mgs/objs/MocNarrowAngleSumming/unitTest.cpp
+++ isis/src/mgs/objs/MocNarrowAngleSumming/unitTest.cpp
@@ -1,17 +1,36 @@
-using namespace std;
-
+/**
+ * @file
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 #include <iostream>
 #include <iomanip>
 
 #include "MocNarrowAngleSumming.h"
 #include "Preference.h"
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
   Isis::Preference::Preferences(true);
 
-  cout << "Unit test for Isis::Mgs::MocNarrowAngleSumming" << endl;
+  cout << "Unit test for Isis::MocNarrowAngleSumming" << endl;
 
   cout << "Testing csum=1, ss=1" << endl;
-  Isis::Mgs::MocNarrowAngleSumming s1(1, 1);
+  Isis::MocNarrowAngleSumming s1(1, 1);
   cout << s1.Detector(1.0) << endl;
   cout << s1.Detector(2.0) << endl;
   cout << s1.Detector(3.0) << endl;
@@ -21,7 +40,7 @@ int main(int argc, char *argv[]) {
   cout << endl;
 
   cout << "Testing csum=2, ss=1" << endl;
-  Isis::Mgs::MocNarrowAngleSumming s2(2, 1);
+  Isis::MocNarrowAngleSumming s2(2, 1);
   cout << s2.Detector(1.0) << endl;
   cout << s2.Detector(2.0) << endl;
   cout << s2.Detector(3.0) << endl;
@@ -31,7 +50,7 @@ int main(int argc, char *argv[]) {
   cout << endl;
 
   cout << "Testing csum=3, ss=10" << endl;
-  Isis::Mgs::MocNarrowAngleSumming s3(3, 10);
+  Isis::MocNarrowAngleSumming s3(3, 10);
   cout << s3.Detector(1.0) << endl;
   cout << s3.Detector(2.0) << endl;
   cout << s3.Detector(3.0) << endl;
