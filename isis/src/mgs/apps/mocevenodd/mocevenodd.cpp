06c2958027b0631a5047d0ecc073ac86cd6943a3
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 00:48:09 +0000

    mocevenodd.xml: Removed Mgs namespace scope.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2346 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/mgs/apps/mocevenodd/mocevenodd.cpp
+++ isis/src/mgs/apps/mocevenodd/mocevenodd.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 //
 //    mocevenodd.cpp
 //
@@ -32,7 +50,7 @@ void IsisMain() {
   // Make sure we have a moc cube
   ProcessByLine p;
   p.SetInputCube("FROM");
-  Mgs::MocLabels moc(Application::GetUserInterface().GetFilename("FROM"));
+  MocLabels moc(Application::GetUserInterface().GetFilename("FROM"));
 
   // It must have crosstrack summing of 1
   if(moc.CrosstrackSumming() != 1) {
