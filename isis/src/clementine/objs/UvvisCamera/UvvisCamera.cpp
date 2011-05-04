d6e001377a34c9f6c3d9cd45ab15cbadfbcf0d54
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 00:46:55 +0000

    UvvisCamera.h: 2011-05-03 Jeannie Walldren - Added ShutterOpenCloseTimes() method. Updated unitTest to test for new methods. Updated documentation. Replaced Clementine namespace wrap with Isis namespace. Added Isis Disclaimer to files. Added NAIF error check to constructor.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2343 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/clementine/objs/UvvisCamera/UvvisCamera.cpp
+++ isis/src/clementine/objs/UvvisCamera/UvvisCamera.cpp
@@ -1,16 +1,48 @@
+/**
+ * @file
+ *
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for 
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software 
+ *   and related material nor shall the fact of distribution constitute any such 
+ *   warranty, and no responsibility is assumed by the USGS in connection 
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see 
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
 #include "UvvisCamera.h"
+
 #include "CameraDetectorMap.h"
-#include "CameraFocalPlaneMap.h"
 #include "CameraDistortionMap.h"
+#include "CameraFocalPlaneMap.h"
 #include "CameraGroundMap.h"
 #include "CameraSkyMap.h"
 #include "iString.h"
 #include "iTime.h"
+#include "NaifStatus.h"
 
 using namespace std;
-using namespace Isis;
-namespace Clementine {
+
+namespace Isis {
+  /**
+   * Constructs a Clementine UV/VIS Model using the image labels.
+   *  
+   * @param lab Pvl label from a Clementine UVVIS image. 
+   *  
+   * @internal 
+   *   @history 2011-05-03 Jeannie Walldren - Added NAIF error check.  Added
+   *                          call to ShutterOpenCloseTimes() method.
+   */
   UvvisCamera::UvvisCamera(Pvl &lab) : FramingCamera(lab) {
+    NaifStatus::CheckErrors();
     // Get the camera characteristics
     SetFocalLength();
     SetPixelPitch();
@@ -19,9 +51,14 @@ namespace Clementine {
     PvlGroup inst = lab.FindGroup("Instrument", Pvl::Traverse);
     string stime = inst["StartTime"];
 
+    // set variables startTime and exposureDuration
     double time;
     str2et_c(stime.c_str(), &time);
-    time += ((double)inst["ExposureDuration"] / 1000.0) / 2.0; // Add half exposure duration in milliseconds
+    // divide exposure duration keyword value by 1000 to convert to seconds
+    double exposureDuration = ((double) inst["ExposureDuration"]) / 1000.0;
+    pair<iTime, iTime> shuttertimes = ShutterOpenCloseTimes(time, exposureDuration);
+    // Add half exposure duration to get time at center of image
+    iTime centerTime = shuttertimes.first.Et() + exposureDuration / 2.0;
 
     // Setup detector map
     new CameraDetectorMap(this);
@@ -29,8 +66,11 @@ namespace Clementine {
     // Setup focal plane map
     CameraFocalPlaneMap *focalMap = new CameraFocalPlaneMap(this, NaifIkCode());
 
-    focalMap->SetDetectorOrigin(Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + "_BORESIGHT_SAMPLE"),
-                                Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + "_BORESIGHT_LINE"));
+    focalMap->SetDetectorOrigin(
+      Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + 
+                       "_BORESIGHT_SAMPLE"),
+      Spice::GetDouble("INS" + (iString)(int)NaifIkCode() + 
+                       "_BORESIGHT_LINE"));
 
     // Setup distortion map
     new CameraDistortionMap(this);
@@ -39,11 +79,51 @@ namespace Clementine {
     new CameraGroundMap(this);
     new CameraSkyMap(this);
 
-    SetTime(time);
+    SetTime(centerTime);
     LoadCache();
+    NaifStatus::CheckErrors();
+  }
+
+  /**
+   * Returns the shutter open and close times. The user should pass in the
+   * ExposureDuration keyword value, converted to seconds, and the StartTime
+   * keyword value, converted to ephemeris time. The StartTime keyword value
+   * from the labels represents the time at the start of the observation, as
+   * noted in the Clementine EDR image SIS. This method uses the FramingCamera
+   * class implementation, returning the given time value as the shutter open
+   * and the sum of the time value and exposure duration as the shutter close.
+   *
+   * @param exposureDuration ExposureDuration keyword value from the labels,
+   *                         converted to seconds.
+   * @param time The StartTime keyword value from the labels, converted to
+   *             ephemeris time.
+   *
+   * @return @b pair < @b iTime, @b iTime > The first value is the shutter
+   *         open time and the second is the shutter close time.
+   *
+   * @see http://pds-imaging.jpl.nasa.gov/documentation/clementine_edrsis.pdf
+   * @author 2011-05-03 Jeannie Walldren
+   * @internal
+   *   @history 2011-05-03 Jeannie Walldren - Original version.
+   */
+  pair<iTime, iTime> UvvisCamera::ShutterOpenCloseTimes(double time,
+                                                        double exposureDuration) {
+    return FramingCamera::ShutterOpenCloseTimes(time, exposureDuration);
   }
 }
 
-extern "C" Camera *UvvisCameraPlugin(Pvl &lab) {
-  return new Clementine::UvvisCamera(lab);
+
+/**
+ * This is the function that is called in order to instantiate a UvvisCamera
+ * object.
+ *
+ * @param lab Cube labels
+ *
+ * @return Isis::Camera* UvvisCamera
+ * @internal 
+ *   @history 2011-05-03 Jeannie Walldren - Added documentation.  Removed
+ *            Clementine namespace.
+ */
+extern "C" Isis::Camera *UvvisCameraPlugin(Isis::Pvl &lab) {
+  return new Isis::UvvisCamera(lab);
 }
--- isis/src/clementine/objs/UvvisCamera/UvvisCamera.h
+++ isis/src/clementine/objs/UvvisCamera/UvvisCamera.h
@@ -1,40 +1,100 @@
 #ifndef UvvisCamera_h
 #define UvvisCamera_h
+/** 
+ * @file 
+ *  
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
 
 #include "FramingCamera.h"
 
-namespace Clementine {
+namespace Isis {
   /**
-   * @brief Camera class
+   * This is the camera model for the Clementine Ultraviolet/Visible Camera 
    *
-   * This is the camera class for the UvvisCamera
-   *
+   * @ingroup SpiceInstrumentsAndCameras
    * @ingroup Clementine
-   *
+   *  
+   * @see 
+   *      http://astrogeology.usgs.gov/Projects/Clementine/nasaclem/sensors/uvvis/uvvis.html
+   * @see 
+   *      http://astrogeology.usgs.gov/Projects/Clementine/nasaclem/clemhome.html
+   * @see http://pds-imaging.jpl.nasa.gov/portal/clementine_mission.html 
+   * @see http://astrogeology.usgs.gov/Missions/Clementine
+   *  
    * @author  2007-07-10 Tracie Sucharski
    *
    * @internal
-   *   @history 2007-07-10 Steven Lambright - Imported to Isis 3
-   *   @history 2007-07-11 Steven Koechle - casted NaifIkCode to int before
-   *             istring to fix Linux 32bit build error
-   *   @history 2008-08-08 Steven Lambright Made the unit test work with a Sensor
-   *            change. Also, now using the new LoadCache(...) method instead of
-   *            CreateCache(...).
+   *   @history 2007-01-11 Tracie Sucharski - Original version.
+   *   @history 2007-01-30 Elizabeth Ribelin - Renamed String to iString and
+   *                          Time to iTime.
+   *   @history 2007-07-10 Steven Lambright - Added unitTest.  Read boresight
+   *                          from addendum.  Added time to 1/2 exposure
+   *                          duration to get center time of image.
+   *   @history 2007-07-11 Steven Koechle - Casted NaifIkCode to int before
+   *                          istring to fix Linux 32bit build error
+   *   @history 2008-08-08 Steven Lambright - Made the unit test work with a
+   *                          Sensor change. Also, now using the new
+   *                          LoadCache(...) method instead of CreateCache(...).
    *   @history 2009-08-28 Steven Lambright - Changed inheritance to no longer
-   *            inherit directly from Camera
+   *                          inherit directly from Camera.  Camera is now pure
+   *                          virtual, parent class is FramingCamera.
+   *   @history 2010-09-16 Steven Lambright - Updated unitTest to not use a DEM.
+   *   @history 2011-01-14 Travis Addair - Added new CK/SPK accessor methods,
+   *                          pure virtual in Camera, implemented in mission
+   *                          specific cameras.
+   *   @history 2011-02-09 Steven Lambright - Major changes to camera classes.
+   *   @history 2011-05-03 Jeannie Walldren - Added ShutterOpenCloseTimes()
+   *                          method. Updated unitTest to test for new methods.
+   *                          Updated documentation. Replaced Clementine
+   *                          namespace wrap with Isis namespace. Added Isis
+   *                          Disclaimer to files. Added NAIF error check to
+   *                          constructor.
    */
-  class UvvisCamera : public Isis::FramingCamera {
+  class UvvisCamera : public FramingCamera {
     public:
-      UvvisCamera(Isis::Pvl &lab);
+      UvvisCamera(Pvl &lab);
+      //! Destroys the UvvisCamera object.
       ~UvvisCamera() {};
+      virtual std::pair <iTime, iTime> ShutterOpenCloseTimes(double time, 
+                                                             double exposureDuration);
 
-      /** CK Frame ID - Instrument Code from spacit run on CK */
+      /**
+       * CK frame ID -  - Instrument Code from spacit run on CK
+       *  
+       * @return @b int The appropriate instrument code for the "Camera-matrix" 
+       *         Kernel Frame ID
+       */
       virtual int CkFrameId() const { return (-40000); }
 
-      /** CK Reference ID - J2000 */
+      /** 
+       * CK Reference ID - J2000
+       * 
+       * @return @b int The appropriate instrument code for the "Camera-matrix"
+       *         Kernel Reference ID
+       */
       virtual int CkReferenceId() const { return (1); }
 
-      /** SPK Reference ID - J2000 */
+      /** 
+       * SPK Reference ID - J2000
+       * 
+       * @return @b int The appropriate instrument code for the Spacecraft
+       *         Kernel Reference ID
+       */
       virtual int SpkReferenceId() const { return (1); }
   };
 };
