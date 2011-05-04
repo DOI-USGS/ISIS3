c9eb9b5293cd98a61cf84d8cf01a818453d553cd
Jeannie Walldren <jwalldren@usgs.gov>
2011-05-04 01:04:11 +0000

    Sensor.h: 2011-05-03 Jeannie Walldren - Added Isis Disclaimer to files.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2379 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/Sensor/Sensor.h
+++ isis/src/base/objs/Sensor/Sensor.h
@@ -60,99 +60,100 @@ namespace Isis {
    * @author 2003-04-11 Jeff Anderson
    *
    * @internal
-   *  @history 2003-05-16 Stuart Sides -  Modified schema from astrogeology...
-   *                                      isis.astrogeology...
-   *  @history 2003-05-30 Jeff Anderson - Updated unitTest and truth to account
-   *                                      for precision due to optimization
-   *  @history 2003-10-16 Jeff Anderson - Added LoadEulerMounting and
-   *                                      LoadFrameMounting methods
-   *  @history 2003-11-26 Jeff Anderson - Modified FrameMounting methods to allow
-   *                                      for fixed frames or time dependent
-   *                                      frames
-   *  @history 2004-01-14 Jeff Anderson - Remove an unused constructor
-   *  @history 2004-01-20 Jeff Anderson - Added an option to the
-   *                                      SetUniversalGround method to eliminate
-   *                                      checks for points on the backside of the
-   *                                      target
-   *  @history 2004-02-18 Jeff Anderson - Fixed a problem with the FrameMounting
-   *                                      methods as the frame kernel is
-   *                                      unloaded if a cache is created.
-   *  @history 2004-02-23 Jeff Anderson - Fixed two bugs in the handling of DEMs.
-   *                                      Used universal lat/lon when appropriate
-   *                                      and the radius needed to be converted
-   *                                      to km when read from the DEM file.
-   *  @history 2005-02-15 Elizabeth Ribelin - Modified file to support Doxygen
-   *                                      documentation
-   *  @history 2005-02-24 Jeff Anderson - Added SlantDistance method and made
-   *                                      the constructor ignore the
-   *                                      ElevationModel keyword if it is null
-   *  @history 2005-02-25 Jeff Anderson - Added LocalSolarTime method
-   *  @history 2005-06-09 Elizabeth Ribelin - Added LoadEulerMounting method that
-   *                                      accepts a matrix as a parameter
-   *  @history 2005-08-24 Jeff Anderson - Make sure LocalSolarTime always return
-   *                                      positive hours
-   *  @history 2005-09-20 Jeff Anderson - Added tests for trying to intersect the
-   *                                      sky
-   *  @history 2006-03-31 Elizabeth Miller - Added SpacecraftAltitude &
-   *                                      SolarDistance methods
-   *  @history 2006-09-07 Debbie A. Cook - Changed back-of-planet test to use
-   *                                      emission angle instead of length
-   *                                      of vector to surface point
-   *  @history 2006-10-11 Jeff Anderson - Modified reading radius
-   *                                      from dem using bilinear
-   *                                      interpolation
-   *  @history 2006-10-12 Jeff Anderson - Modified the
-   *                                      SetLookDirection method
-   *                                      to use the DEM (if
-   *                                      available) when
-   *                                      computing a ground point
-   *  @history 2007-05-18 Jeff Anderson - Modify SpacecraftAltitude method
-   *                                      to use DEM
-   *  @history 2007-06-11 Debbie A. Cook - Added alternative  method that includes radius
-   *  @history 2007-08-24 Debbie A. Cook - Replaced references to p_sB since it was removed from Spice
-   *  @history 2007-11-27 Debbie A. Cook - Added overloaded method SetUniversalGround(lat, lon, radius)
-   *  @history 2008-05-21 Steven Lambright - CubeManager is now used to speed up DEM Cube I/O
-   *  @history 2008-06-18 Debbie A. Cook - Made DemRadius radius public instead of private and added
-   *                                       method HasElevationModel
-   *  @history 2008-08-06 Stuart Sides   - Modified SetLookDirection to better
-   *                                       handle oblique views. In the past it
-   *                                       would oscillate and run out of
-   *                                       iterations.
-   *  @history 2009-02-06 Debbie A. Cook - Changed the tolerance from 1e-6 to 1e-12 for dist**2 (mm)
-   *  @history 2009-02-06 Debbie A. Cook - Changed the tolerance back to 1e-6 (mm)
-   *  @history 2009-02-15 Debbie A. Cook - Added virtual Resolution method
-   *  @history 2009-06-30 Steven Lambright - Added IgnoreElevationModel and fixed
-   *                                       DemRadius
-   *  @history 2009-07-09 Debbie A. Cook - Corrected documentation on Resolution method
-   *  @history 2009-09-23  Tracie Sucharski - Convert negative longitudes
-   *                                      returned by reclat in SetLookDirection.
-   *  @history 2010-09-15 Janet Barrett - Modified the SetLookDirection method to use a new
-   *                                      algorithm for finding the intersection of a ray with
-   *                                      the DEM. This was required to take care of problems that
-   *                                      were encountered at the poles of global DEM files. The
-   *                                      algorithm that is being used was taken from "Intersection
-   *                                      between spacecraft viewing vectors and digital elevation
-   *                                      models" by N.A. Teanby.
-   *  @history 2010-11-09 Eric Hyer - moved some private members to protected
-   *                                      section for Camera
-   *  @history 2010-12-07 Steven Lambright - Added LocalRadius(lat,lon) and
-   *                                     removed a sqrt from SetLookDirection
-   *  @history 2011-01-26 Steven Lambright - The LocalRadius methods now return
-   *                                     Distance objects and added a
-   *                                     LocalRadius method that uses the
-   *                                     Latitude and Longitude classes.
-   *  @history 2011-02-08 Jeannie Walldren - Added method parameter documentation.
-   *  @history 2011-02-09 Steven Lambright & Debbie Cook - Refactored heavily to
-   *                                     use Latitude, Longitude, SurfacePoint,
-   *                                     and iTime where applicable. Optimized
-   *                                     SetLookDirection. These changes were
-   *                                     meant primarily for readability and
-   *                                     reducing error-proneness of the code.
-   *  @history 2011-03-28 Janet Barrett - Fixed the SetLookDirection routine so that it checks to
-   *                                     make sure that the projection of the DEM is in an 
-   *                                     Equatorial cylindrical projection before using the new ray 
-   *                                     tracing routine. The check for this was moved when other
-   *                                     code was added. This was causing a problem with footprintinit.
+   *   @history 2003-05-16 Stuart Sides -  Modified schema from astrogeology...
+   *                                       isis.astrogeology...
+   *   @history 2003-05-30 Jeff Anderson - Updated unitTest and truth to account
+   *                                       for precision due to optimization
+   *   @history 2003-10-16 Jeff Anderson - Added LoadEulerMounting and
+   *                                       LoadFrameMounting methods
+   *   @history 2003-11-26 Jeff Anderson - Modified FrameMounting methods to allow
+   *                                       for fixed frames or time dependent
+   *                                       frames
+   *   @history 2004-01-14 Jeff Anderson - Remove an unused constructor
+   *   @history 2004-01-20 Jeff Anderson - Added an option to the
+   *                                       SetUniversalGround method to eliminate
+   *                                       checks for points on the backside of the
+   *                                       target
+   *   @history 2004-02-18 Jeff Anderson - Fixed a problem with the FrameMounting
+   *                                       methods as the frame kernel is
+   *                                       unloaded if a cache is created.
+   *   @history 2004-02-23 Jeff Anderson - Fixed two bugs in the handling of DEMs.
+   *                                       Used universal lat/lon when appropriate
+   *                                       and the radius needed to be converted
+   *                                       to km when read from the DEM file.
+   *   @history 2005-02-15 Elizabeth Ribelin - Modified file to support Doxygen
+   *                                       documentation
+   *   @history 2005-02-24 Jeff Anderson - Added SlantDistance method and made
+   *                                       the constructor ignore the
+   *                                       ElevationModel keyword if it is null
+   *   @history 2005-02-25 Jeff Anderson - Added LocalSolarTime method
+   *   @history 2005-06-09 Elizabeth Ribelin - Added LoadEulerMounting method that
+   *                                       accepts a matrix as a parameter
+   *   @history 2005-08-24 Jeff Anderson - Make sure LocalSolarTime always return
+   *                                       positive hours
+   *   @history 2005-09-20 Jeff Anderson - Added tests for trying to intersect the
+   *                                       sky
+   *   @history 2006-03-31 Elizabeth Miller - Added SpacecraftAltitude &
+   *                                       SolarDistance methods
+   *   @history 2006-09-07 Debbie A. Cook - Changed back-of-planet test to use
+   *                                       emission angle instead of length
+   *                                       of vector to surface point
+   *   @history 2006-10-11 Jeff Anderson - Modified reading radius
+   *                                       from dem using bilinear
+   *                                       interpolation
+   *   @history 2006-10-12 Jeff Anderson - Modified the
+   *                                       SetLookDirection method
+   *                                       to use the DEM (if
+   *                                       available) when
+   *                                       computing a ground point
+   *   @history 2007-05-18 Jeff Anderson - Modify SpacecraftAltitude method
+   *                                       to use DEM
+   *   @history 2007-06-11 Debbie A. Cook - Added alternative  method that includes radius
+   *   @history 2007-08-24 Debbie A. Cook - Replaced references to p_sB since it was removed from Spice
+   *   @history 2007-11-27 Debbie A. Cook - Added overloaded method SetUniversalGround(lat, lon, radius)
+   *   @history 2008-05-21 Steven Lambright - CubeManager is now used to speed up DEM Cube I/O
+   *   @history 2008-06-18 Debbie A. Cook - Made DemRadius radius public instead of private and added
+   *                                        method HasElevationModel
+   *   @history 2008-08-06 Stuart Sides   - Modified SetLookDirection to better
+   *                                        handle oblique views. In the past it
+   *                                        would oscillate and run out of
+   *                                        iterations.
+   *   @history 2009-02-06 Debbie A. Cook - Changed the tolerance from 1e-6 to 1e-12 for dist**2 (mm)
+   *   @history 2009-02-06 Debbie A. Cook - Changed the tolerance back to 1e-6 (mm)
+   *   @history 2009-02-15 Debbie A. Cook - Added virtual Resolution method
+   *   @history 2009-06-30 Steven Lambright - Added IgnoreElevationModel and fixed
+   *                                        DemRadius
+   *   @history 2009-07-09 Debbie A. Cook - Corrected documentation on Resolution method
+   *   @history 2009-09-23  Tracie Sucharski - Convert negative longitudes
+   *                                       returned by reclat in SetLookDirection.
+   *   @history 2010-09-15 Janet Barrett - Modified the SetLookDirection method to use a new
+   *                                       algorithm for finding the intersection of a ray with
+   *                                       the DEM. This was required to take care of problems that
+   *                                       were encountered at the poles of global DEM files. The
+   *                                       algorithm that is being used was taken from "Intersection
+   *                                       between spacecraft viewing vectors and digital elevation
+   *                                       models" by N.A. Teanby.
+   *   @history 2010-11-09 Eric Hyer - moved some private members to protected
+   *                                       section for Camera
+   *   @history 2010-12-07 Steven Lambright - Added LocalRadius(lat,lon) and
+   *                                      removed a sqrt from SetLookDirection
+   *   @history 2011-01-26 Steven Lambright - The LocalRadius methods now return
+   *                                      Distance objects and added a
+   *                                      LocalRadius method that uses the
+   *                                      Latitude and Longitude classes.
+   *   @history 2011-02-08 Jeannie Walldren - Added method parameter documentation.
+   *   @history 2011-02-09 Steven Lambright & Debbie Cook - Refactored heavily to
+   *                                      use Latitude, Longitude, SurfacePoint,
+   *                                      and iTime where applicable. Optimized
+   *                                      SetLookDirection. These changes were
+   *                                      meant primarily for readability and
+   *                                      reducing error-proneness of the code.
+   *   @history 2011-03-28 Janet Barrett - Fixed the SetLookDirection routine so that it checks to
+   *                                      make sure that the projection of the DEM is in an 
+   *                                      Equatorial cylindrical projection before using the new ray 
+   *                                      tracing routine. The check for this was moved when other
+   *                                      code was added. This was causing a problem with footprintinit.
+   *   @history 2011-05-03 Jeannie Walldren - Added Isis Disclaimer to files.
    */
   class Sensor : public Spice {
     public:
