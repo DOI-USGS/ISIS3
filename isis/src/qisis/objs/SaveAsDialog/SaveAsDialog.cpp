7e287decc1c2edbbb15b926e9e58b65979eaed46
Sharmilla Prasad <sprasad@usgs.gov>
2011-05-11 17:23:22 +0000

    Widget to save(Save As) Isis cubes
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2422 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/qisis/objs/SaveAsDialog/SaveAsDialog.cpp
@@ -0,0 +1,135 @@
+#include "SaveAsDialog.h"
+
+#include <QFileDialog>
+#include <iostream>
+
+using namespace std;
+using namespace Isis;
+
+namespace Qisis {
+
+  /**
+   * Constructor - Displays FileDialog with different save options
+   * 
+   * @author Sharmila Prasad (5/11/2011)
+   * 
+   * @param pTitle      - Dialog Title
+   * @param pFilterList - Dialog Filter list 
+   * @param pDir        - Current Directory 
+   * @param pParent     - Parent widget
+   */
+  SaveAsDialog::SaveAsDialog(QString pTitle, QStringList &pFilterList, QDir &pDir, QWidget *pParent) :
+    FileDialog(pTitle, pFilterList, pDir, pParent), p_dir(pDir) {
+
+    this->setFileMode(QFileDialog::AnyFile);
+
+    // This returns a list of all the buttons in QFileDialog
+    QList<QPushButton *> allPButtons = this->findChildren<QPushButton *>();
+    // Edit the first (Open) button title.
+    allPButtons[0]->setText("&Save");
+    
+    /// Edit the second (Cancel) button title.
+    allPButtons[1]->setText("Close");
+
+    p_saveAsType = FullImage;
+    QHBoxLayout *hBoxLayout = new QHBoxLayout;
+    hBoxLayout->setAlignment(Qt::AlignLeft);
+    hBoxLayout->setSpacing(25);
+    hBoxLayout->setContentsMargins (25, 11, 25, 11 );
+    
+    // Button Group
+    QButtonGroup *exportOptionsGrp = new QButtonGroup();
+    exportOptionsGrp->setExclusive(true); // only one option can be clicked
+    
+    p_fullImage    = new QRadioButton("Full &Image", this);
+    connect(p_fullImage, SIGNAL(clicked(bool)), this, SLOT(setFullImage(bool)));
+    exportOptionsGrp->addButton(p_fullImage);
+    p_fullImage->setWhatsThis("Save the entire Image");
+    
+    p_exportAsIs    = new QRadioButton("Export &As Is", this);
+    connect(p_exportAsIs, SIGNAL(clicked(bool)), this, SLOT(setAsIs(bool)));
+    exportOptionsGrp->addButton(p_exportAsIs);
+    p_exportAsIs->setWhatsThis("Save the Image As Viewed");
+    
+    p_exportFullRes = new QRadioButton("Export Full &Res", this);
+    connect(p_exportFullRes, SIGNAL(clicked(bool)), this, SLOT(setFullResolution(bool)));
+    exportOptionsGrp->addButton(p_exportFullRes);
+    p_exportFullRes->setWhatsThis("Save the Image As Viewed with Full Resoultion");
+    
+    hBoxLayout->addWidget(p_exportAsIs);
+    hBoxLayout->addWidget(p_exportFullRes);
+    
+    p_fullImage->setEnabled(true);
+    p_exportAsIs->setEnabled(true);
+    p_exportFullRes->setEnabled(true);
+    
+    p_fullImage->setChecked(true);
+    
+    QLayout *dialogLayout = layout();
+    dialogLayout->addWidget(p_fullImage);
+    dialogLayout->addItem(hBoxLayout);
+    dialogLayout->setAlignment(hBoxLayout, Qt::AlignLeft );
+    setLayout(dialogLayout);
+  }
+
+  /**
+   * Get user chosen save type
+   * 
+   * @author Sharmila Prasad (5/11/2011)
+   * 
+   * @return int - Return user chosen save type
+   */
+  int SaveAsDialog::getSaveAsType()
+  {
+    if (p_exportAsIs->isChecked())
+      return ExportAsIs;
+    if(p_exportFullRes->isChecked())
+      return ExportFullRes;
+    return FullImage;
+  }
+  
+  /**
+   * Check FullImage radio button and if checked set the 
+   * saveAsType to FullImage 
+   * 
+   * @author Sharmila Prasad (5/11/2011)
+   * 
+   * @param pbChecked - Button Checked(true/false)
+   */
+  void SaveAsDialog::setFullImage(bool pbChecked)
+  {
+    if(pbChecked) {
+      p_saveAsType = FullImage;
+    }
+  }
+  
+  /**
+   * Check ExportAsIs radio button and if checked set the 
+   * saveAsType to ExportAsIs 
+   * 
+   * @author Sharmila Prasad (5/11/2011)
+   * 
+   * @param pbChecked - Button Checked(true/false)
+   */
+  void SaveAsDialog::setAsIs(bool pbChecked)
+  {
+    if(pbChecked) {
+      p_saveAsType = ExportAsIs;
+    }
+  }
+
+  /**
+   * Check ExportFullRes radio button and if checked set the 
+   * saveAsType to ExportFullRes 
+   * 
+   * @author Sharmila Prasad (5/11/2011)
+   * 
+   * @param pbChecked - Button Checked(true/false)
+   */
+  void SaveAsDialog::setFullResolution(bool pbChecked)
+  {
+    if(pbChecked) {
+      p_saveAsType = ExportFullRes;
+    }
+  }
+}
---
+++ isis/src/qisis/objs/SaveAsDialog/SaveAsDialog.h
@@ -0,0 +1,67 @@
+#ifndef SaveAsDialog_h
+#define SaveAsDialog_h
+
+/**
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+ 
+#include <QCheckBox>
+#include <QRadioButton>
+#include "FileDialog.h"
+
+namespace Qisis {
+
+  /**
+  * @brief Widget to save(Save As) Isis cubes(used in qview) to display the 
+  * FileDialog to select the output cube. This dialog additionally displays 
+  * radio buttons for choices FullImage, ExportAsIs, ExportFullRes. These choices 
+  * are located at the bottom of the dialog. 
+  *  
+  * FullImage     - copies the entire image into the user specified output file 
+  * ExportAsIs    - copies the image as displayed in the qview app window 
+  * ExportFullRes - copies the image as displayed in the qview app window but with 
+  *                 full resolution 
+  *
+  * @ingroup Visualization Tools
+  *
+  * @author Sharmila Prasad
+  *
+  * @internal
+  * @history 2011-05-11  Sharmila Prasad - Initial Version
+  */
+  class SaveAsDialog : public FileDialog {
+      Q_OBJECT
+    public:
+      SaveAsDialog(QString pTitle, QStringList &pFilterList, QDir &pDir, QWidget *pParent = 0);
+      enum saveAsType {FullImage, ExportAsIs, ExportFullRes};
+      int getSaveAsType();
+      
+    public slots:
+      void setFullImage(bool);
+      void setAsIs(bool);
+      void setFullResolution(bool);
+
+    private:
+      QDir &p_dir;//!<The directory to open the dialog with
+      QRadioButton *p_fullImage;     //!< FullImage Button
+      QRadioButton *p_exportAsIs;    //!< ExportAsIs Button
+      QRadioButton *p_exportFullRes; //!< ExportFullRes Button
+      saveAsType p_saveAsType;       //!< Current Save Type
+  };
+};
+
+#endif
