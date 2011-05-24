80fbbb765ca9800ba09593adb50603712a744c49
Sharmilla Prasad <sprasad@usgs.gov>
2011-05-24 00:39:46 +0000

    Opens a window in Gui Application as a helper to edit file
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2500 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/base/objs/Gui/GuiEditFile.cpp
@@ -0,0 +1,338 @@
+#include "Filename.h"
+#include "GuiEditFile.h"
+#include "UserInterface.h"
+
+#include <QDir>
+#include <QFileDialog>
+#include <QList>
+#include <QPushButton>
+#include <QStatusBar>
+#include <QTextEdit>
+#include <QTextStream>
+#include <QMenuBar>
+#include <QMessageBox>
+
+using namespace Isis;
+using namespace std;
+
+namespace Isis {
+  
+  // Singleton
+  GuiEditFile* GuiEditFile::m_instance = NULL;
+  
+  /**
+   * Creates a single instance of the GuiEditFile. If already an instance, 
+   * display the object 
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   * 
+   * @param pUI    - User Interface of parent app
+   * @param psFile - File to edit
+   */
+  void GuiEditFile::EditFile(UserInterface & pUI, std::string psFile) {
+    if (m_instance == NULL) {
+      m_instance = new GuiEditFile(pUI, psFile);
+    }
+    else {
+      m_instance->showWindow(psFile);
+    }
+  }
+  
+  /**
+   * Display the window if there is already an instance of this object
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   * 
+   * @param psFile - File to edit
+   */
+  void GuiEditFile::showWindow(std::string psFile) {
+    m_fileName = QString(psFile.c_str());
+    m_editWin->setWindowTitle (m_fileName);
+    OpenFile(m_fileName);
+    m_editWin->show();
+  }
+  
+  /**
+   * Constructor
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   * 
+   * @param pUI 
+   * @param psFile 
+   */
+  GuiEditFile::GuiEditFile(UserInterface & pUI, std::string psFile) {
+    // Init data members
+    m_parent   = pUI.TheGui();
+    m_fileName = QString(psFile.c_str());
+    m_editFile=NULL;
+    
+    // Main Window
+    m_editWin = new QMainWindow(m_parent, Qt::SubWindow);
+    m_editWin->setWindowTitle (m_fileName);
+    m_editWin->resize (400, 600);
+    
+    // Status bar
+    QStatusBar *statusBar = new QStatusBar(m_editWin);
+    m_editWin->setStatusBar(statusBar);
+  
+    // Menu bar
+    QMenuBar *menuBar = new QMenuBar(m_editWin);
+    QMenu *fileMenu = menuBar->addMenu("&File");
+    
+    // Action File-> Open
+    m_open = new QAction(menuBar);
+    m_open->setShortcut(Qt::CTRL + Qt::Key_O);
+    m_open->setText("&Open...");
+    m_open->setToolTip("Open File");
+    QString whatsThis = "Open a file to edit";
+    m_open->setWhatsThis(whatsThis);
+    connect(m_open, SIGNAL(activated()), this, SLOT(open()));
+    fileMenu->addAction(m_open);
+    
+    // Action File-> Save
+    m_save = new QAction(menuBar);
+    m_save->setShortcut(Qt::CTRL + Qt::Key_S);
+    m_save->setText("&Save...");
+    m_save->setToolTip("Save File");
+    m_save->setWhatsThis("Save the current file");
+    connect(m_save, SIGNAL(activated()), this, SLOT(saveFile()));
+    fileMenu->addAction(m_save);
+    
+    // Action File->Save As
+    m_saveAs = new QAction(menuBar);
+    m_saveAs->setText("Save &As...");
+    m_saveAs->setShortcut(Qt::CTRL + Qt::Key_A);
+    m_saveAs->setToolTip("Save As File");
+    m_saveAs->setWhatsThis("Save the current file into another file");
+    connect(m_saveAs, SIGNAL(activated()), this, SLOT(saveAs()));
+    fileMenu->addAction(m_saveAs);
+    
+    // Action File->Close
+    m_close = new QAction(menuBar);
+    m_close->setText("&Close...");
+    m_close->setShortcut(Qt::CTRL + Qt::Key_C);
+    m_close->setToolTip("Close File");
+    m_close->setWhatsThis("Close the current file");
+    connect(m_close, SIGNAL(activated()), this, SLOT(closeFile()));
+    fileMenu->addAction(m_close);
+    
+    // Action Exit
+    m_exit = menuBar->addAction("&Exit");
+    m_exit->setShortcut(Qt::CTRL + Qt::Key_E);
+    m_exit->setText("&Exit...");
+    m_exit->setToolTip("Exit");
+    m_exit->setWhatsThis("Exit the Editor");
+    //connect(m_exit, SIGNAL(activated()), m_editWin, SLOT(close()));
+    connect(m_exit, SIGNAL(activated()), this, SLOT(closeWin()));
+    
+    m_editWin->setMenuBar(menuBar);
+
+    // Text Edit
+    m_textChanged = false;
+    m_txtEdit = new QTextEdit(m_editWin);
+    m_txtEdit->setUndoRedoEnabled(true);
+    m_txtEdit->resize(400,500);
+    m_editWin->setCentralWidget(m_txtEdit);
+    if (m_fileName.length() > 0){
+      OpenFile(m_fileName);
+    }
+    connect(m_txtEdit, SIGNAL(textChanged()), this, SLOT(setTextChanged()));
+    
+    m_editWin->show(); 
+  }
+  
+  /**
+   * Destructor
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   */
+  GuiEditFile::~GuiEditFile() {
+    delete m_editFile;
+    m_editFile = NULL;
+    
+    delete m_txtEdit;
+    m_txtEdit = NULL;
+    
+    delete m_editWin;
+    m_editWin = NULL;
+    
+    delete m_instance;
+    m_instance = NULL;
+  }
+  
+  /**
+   * Action Exit - Close window and clear text editor
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   */
+  void GuiEditFile::closeWin() {
+    m_editWin->close();
+    m_txtEdit->clear();
+  }
+  
+  /**
+   * Flag to indicate text has changed
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   */
+  void GuiEditFile::setTextChanged() {
+    m_textChanged = true;
+  }
+
+  /**
+   * Action File->Open
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   */
+  void GuiEditFile::open() {
+    //Set up the list of filters that are default with this dialog.
+    //cerr << "setTextChanged=" << m_textChanged << endl;
+    if (m_textChanged) {
+      if(QMessageBox::question((QWidget *)parent(), tr("Save File?"),
+                               tr("Are you sure you want to save this file?"),
+                               tr("&Save"), tr("&Cancel"),
+                               QString(), 1, 0)) {
+        saveFile();
+      }
+    }
+    
+    QString sFilterList("All files (*)");
+    QDir currDir = QDir::current();
+    QString sOpen("Open");
+    QFileDialog  *fileDialog = new QFileDialog((QWidget *)parent(), sOpen, currDir.dirName(), sFilterList);
+    fileDialog->show();
+    connect(fileDialog, SIGNAL(fileSelected(QString)), this, SLOT(OpenFile(QString)));
+  }
+
+  /**
+   * Action File->Close, close the opened file
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   */
+  void GuiEditFile::closeFile(){
+    if (m_textChanged) {
+      if(QMessageBox::question((QWidget *)parent(), tr("Save File?"), 
+                               tr("Changes have been made to the file. Do you want to Save?"),
+                               QMessageBox::Save, QMessageBox::No) == QMessageBox::Save) {
+        saveFile();
+      }
+    }
+    m_editFile->close();
+    m_txtEdit->clear();
+    m_textChanged = false;
+    m_editWin->setWindowTitle(tr(""));
+  }
+  
+  /**
+   * Display the selected file
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   * 
+   * @param psOutFile 
+   */
+  void GuiEditFile::OpenFile(QString psOutFile){
+    //cerr << "Open File " << psOutFile.toStdString() << "\n";
+    if(psOutFile.isEmpty()){
+      QMessageBox::information((QWidget *)parent(), "Error", "No output file selected");
+      return;
+    }
+    
+    if (m_editFile != NULL) {
+      m_txtEdit->clear();
+      delete (m_editFile);
+    }
+    m_editFile = new QFile(psOutFile);
+    //m_editWin->setWindowTitle(Filename(psOutFile.toStdString()).Name().c_str());
+    windowTitle(psOutFile);
+    
+    if (m_editFile->open(QIODevice::ReadWrite)){
+      char buf[1024];
+      QString bufStr;
+      qint64 lineLength = m_editFile->readLine(buf, sizeof(buf));
+      while (lineLength != -1) {
+        bufStr = QString(buf);
+        //std::cerr << bufStr.toStdString();
+        bufStr.remove('\n'); // puts an additional new line
+        m_txtEdit->append(bufStr);
+        lineLength = m_editFile->readLine(buf, sizeof(buf));
+      }
+    }
+  }
+  
+  /**
+   * Action File->Save
+   * 
+   * @author Sharmila Prasad (5/20/2011)
+   */
+  void GuiEditFile::saveFile(){
+    if (m_editFile){
+      clearFile();
+      QTextStream out(m_editFile);
+      out << m_txtEdit->document()->toPlainText();
+      m_textChanged = false;
+    }
+  }
+  
+  /**
+   * Action File->Save As
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   */
+  void GuiEditFile::saveAs() {
+    QString sFilterList("All files (*)");
+    QDir currDir = QDir::current();
+    QString sOpen("Open");
+    QFileDialog  *saveAsDialog = new QFileDialog((QWidget *)parent(), sOpen, currDir.dirName(), sFilterList);
+
+    QList<QPushButton *> allPButtons = saveAsDialog->findChildren<QPushButton *>();
+    // Edit the first (Open) button title.
+    allPButtons[0]->setText("&Save");
+    // Edit the second (Cancel) button title.
+    allPButtons[1]->setText("&Close");
+    
+    saveAsDialog->show();
+    connect(saveAsDialog, SIGNAL(fileSelected(QString)), this, SLOT(saveAsFile(QString)));
+  }
+  
+  /**
+   * Copy the current file into user selected file
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   * 
+   * @param psNewFile 
+   */
+  void GuiEditFile::saveAsFile(QString psNewFile) {
+    m_editFile->close();
+    delete(m_editFile);
+    m_editFile = new QFile(psNewFile);
+    m_editFile->open(QFile::ReadWrite);
+    saveFile();
+    windowTitle(psNewFile);
+  }
+  
+  /**
+   * Delete the contents of the current file - especially if changes are made 
+   * and they are not to be saved.
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   */
+  void GuiEditFile::clearFile(){
+    if (m_editFile){
+      m_editFile->close();
+      //m_editFile->open(QIODevice::Truncate | QIODevice::ReadWrite);
+      m_editFile->open(QFile::ReadWrite | QFile::Truncate);
+    }
+  }
+  
+  /**
+   * Display only the base name of the file
+   * 
+   * @author Sharmila Prasad (5/23/2011)
+   * 
+   * @param psfile - current file
+   */
+  void GuiEditFile::windowTitle(QString & psfile) {
+    m_editWin->setWindowTitle(Filename(psfile.toStdString()).Name().c_str());
+  }
+  
+}
---
+++ isis/src/base/objs/Gui/GuiEditFile.h
@@ -0,0 +1,95 @@
+#ifndef _GuiEditFile_h_
+#define _GuiEditFile_h_
+
+#include <QAction>
+#include <QFile>
+#include <QObject>
+#include <QMainWindow>
+#include <QString>
+#include <QTextEdit>
+#include <QWidget>
+
+/**
+ *   Unless noted otherwise, the portions of Isis written by the USGS are
+ *   public domain. See individual third-party library and package descriptions
+ *   for intellectual property information, user agreements, and related
+ *   information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or
+ *   implied, is made by the USGS as to the accuracy and functioning of such
+ *   software and related material nor shall the fact of distribution
+ *   constitute any such warranty, and no responsibility is assumed by the
+ *   USGS in connection therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html
+ *   in a browser or see the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
+namespace Isis {
+  class UserInterface;
+ /**
+  * @brief  Opens a window in Gui Application to be able to 
+  * edit, save and create text files. Creates a <b>singleton</b> window 
+  * to edit files.
+  *  
+  * Example can be used to edit DefFiles in control net applications. 
+  * Refer to cnetedit Application
+  *
+  * @author Sharmila Prasad
+  *
+  * @internal
+  * @history 2011-05-21  Sharmila Prasad - Initial Version
+  */
+  
+  class GuiEditFile : public QObject {
+    Q_OBJECT
+    public:
+      //! Creates a single instance of the GuiEditFile
+      static void EditFile(UserInterface & pUI, std::string psFile="");
+
+      //! If there is already an instance of this object, then display the window
+      void showWindow(std::string psFile="");
+      
+      //! Destructor
+      ~GuiEditFile();
+      
+      //! Delete the contents of a file
+      void clearFile();
+
+    public slots:
+      void open();             //!< For action File->Open
+      void OpenFile(QString);  //!< To display the contents of an opened file
+      void setTextChanged();   //!< Indicator that the text has changed
+      void saveAs();           //!< For action File->Save As
+      void saveAsFile(QString);//!< Save the contents of text editor to another file
+      void saveFile();         //!< For action File->Save
+      void closeFile();        //!< For action File->Close
+      void closeWin();         //!< For action Exit (close the window)
+
+    private:
+      //! Constructor
+      GuiEditFile(UserInterface & pUI, std::string psFile="");
+      void windowTitle(QString & psfile); //!< display only the file base name
+      
+      static GuiEditFile* m_instance; //!< Instance of this object - singleton
+      QWidget *m_parent;              //!< Parent widget
+      QString m_fileName;             //!< Current file open
+      QMainWindow *m_editWin;         //!< Editor window
+      QTextEdit *m_txtEdit;           //!< Text Editor
+      QFile *m_editFile;              //!< File pointer to current file 
+      bool m_textChanged;             //!< Flag to indicate text changed
+      
+      //! Actioons
+      QAction *m_open;    //!< Action Open
+      QAction *m_save;    //!< Action Save
+      QAction *m_saveAs;  //!< Action Save As
+      QAction *m_close;   //!< Action Close
+      QAction *m_exit;    //!< Action Exit
+  };
+
+};
+#endif
+
