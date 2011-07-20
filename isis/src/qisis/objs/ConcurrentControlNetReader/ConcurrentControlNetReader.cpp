cd217d4ed6e4040b96ed608a3a6d3f6d7c6e597a
Eric Hyer <ehyer@usgs.gov>
2011-07-20 00:19:43 +0000

    Added class ConcurrentControlNetReader
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2822 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/qisis/objs/ConcurrentControlNetReader/ConcurrentControlNetReader.cpp
@@ -0,0 +1,257 @@
+/**
+ * @file
+ * $Revision: 1.9 $
+ * $Date: 2009/07/15 17:33:52 $
+ *
+ *   Unless noted otherwise, the portions of Isis written by the USGS are
+ *   public domain. See individual third-party library and package descriptions
+ *   for intellectual property information, user agreements, and related
+ *   information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or
+ *   implied, is made by the USGS as to the accuracy and functioning of such
+ *   software and related material nor shall the fact of distribution
+ *   constitute any such warranty, and no responsibility is assumed by the
+ *   USGS in connection therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html
+ *   in a browser or see the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
+#include "ConcurrentControlNetReader.h"
+
+#include <algorithm>
+#include <iostream>
+
+#include <QFuture>
+#include <QFutureWatcher>
+#include <QString>
+#include <QtConcurrentRun>
+#include <QtConcurrentMap>
+
+#include "ControlNet.h"
+#include "ControlNetFileV0002.pb.h"
+#include "ControlNetVersioner.h"
+#include "ControlPoint.h"
+#include "Distance.h"
+#include "Filename.h"
+#include "Projection.h"
+
+
+using std::cerr;
+using std::cout;
+using std::swap;
+
+
+namespace Isis {
+  
+  /**
+   * Allocates memory at construction instead of as needed
+   */
+  ConcurrentControlNetReader::ConcurrentControlNetReader() {
+    nullify();
+
+    m_readWatcher = new QFutureWatcher<LatestControlNetFile *>;
+    m_builderWatcher = new QFutureWatcher< QAtomicPointer< ControlNet > >;
+
+    connect(m_readWatcher, SIGNAL(finished()),
+        this, SLOT(startBuildingNetwork()));
+    connect(m_builderWatcher, SIGNAL(finished()),
+        this, SLOT(networkBuilt()));
+    connect(m_builderWatcher, SIGNAL(progressRangeChanged(int, int)),
+        this, SIGNAL(progressRangeChanged(int, int)));
+    connect(m_builderWatcher, SIGNAL(progressValueChanged(int)),
+        this, SIGNAL(progressValueChanged(int)));
+  }
+
+
+  /**
+   * This destructor will cancel all running threads and block.
+   */
+  ConcurrentControlNetReader::~ConcurrentControlNetReader() {
+    
+    if (m_readWatcher)
+    {
+      m_readWatcher->cancel();
+      m_readWatcher->waitForFinished();
+    }
+    
+    if (m_builderWatcher)
+    {
+      m_builderWatcher->cancel();
+      m_builderWatcher->waitForFinished();
+    }
+      
+    delete m_readWatcher;
+    m_readWatcher = NULL;
+
+    delete m_builderWatcher;
+    m_builderWatcher = NULL;
+
+    delete m_versionerFile; // should be NULL anyway
+    m_versionerFile = NULL;
+  }
+
+
+  /**
+   * @param filename The filename of the network to read
+   */
+  void ConcurrentControlNetReader::read(QString filename) {
+    QThreadPool::globalInstance()->setMaxThreadCount(1);
+    QFuture<LatestControlNetFile *> result =
+        QtConcurrent::run(ReadNetworkFunctor(filename));
+    m_readWatcher->setFuture(result);
+  }
+
+
+  /**
+   * Initializes members to NULL
+   */
+  void ConcurrentControlNetReader::nullify() {
+    m_readWatcher = NULL;
+    m_builderWatcher = NULL;
+    m_versionerFile = NULL;
+  }
+
+
+  /**
+   * Called when we have a binary container for the network, and in turn
+   * creates the network using QtConcurrent::mappedReduced()
+   */
+  void ConcurrentControlNetReader::startBuildingNetwork() {
+    m_versionerFile = m_readWatcher->result();
+    QString targetName = QString::fromStdString(
+        m_versionerFile->GetNetworkHeader().targetname());
+
+    QFuture< QAtomicPointer< ControlNet > > result;
+
+    result = QtConcurrent::mappedReduced(m_versionerFile->GetNetworkPoints(),
+        NetworkBuilder(targetName),
+        &NetworkBuilder::addToNetwork,
+        QtConcurrent::SequentialReduce |
+        QtConcurrent::OrderedReduce
+                                        );
+
+    m_builderWatcher->setFuture(result);
+  }
+
+
+  /**
+   * Called when the ControlNet is finished being populated with points.  This
+   * method sets the header data and finally emits networkReadFinished()
+   */
+  void ConcurrentControlNetReader::networkBuilt() {
+    ControlNet *net = m_builderWatcher->result();
+    ControlNetFileHeaderV0002 &header = m_versionerFile->GetNetworkHeader();
+
+    net->SetTarget(header.targetname());
+    net->SetDescription(header.description());
+    net->SetUserName(header.username());
+    net->SetCreatedDate(header.created());
+    net->SetNetworkId(header.networkid());
+    net->SetModifiedDate(header.lastmodified());
+
+    delete m_versionerFile;
+    m_versionerFile = NULL;
+
+    emit networkReadFinished(m_builderWatcher->result());
+  }
+
+
+  /**
+   * Sets up the ReadNetworkFunctor which provides the binary container for
+   * the NetworkBuilder.
+   * 
+   * @param someNetworkFilename filename of a ControlNet file.
+   */
+  ConcurrentControlNetReader::ReadNetworkFunctor::ReadNetworkFunctor(
+    QString someNetworkFilename) {
+    m_networkFilename = someNetworkFilename;
+  }
+
+
+  ConcurrentControlNetReader::ReadNetworkFunctor::~ReadNetworkFunctor() {}
+
+
+  LatestControlNetFile *
+  ConcurrentControlNetReader::ReadNetworkFunctor::operator()() const {
+    return ControlNetVersioner::Read(Filename(m_networkFilename.toStdString()));
+  }
+
+
+  ConcurrentControlNetReader::NetworkBuilder::NetworkBuilder(QString target) {
+    nullify();
+
+    PvlGroup pvlRadii = Projection::TargetRadii(target.toStdString());
+    m_majorRad = new Distance(pvlRadii["EquatorialRadius"], Distance::Meters);
+    m_minorRad = new Distance(pvlRadii["EquatorialRadius"], Distance::Meters);
+    m_polarRad = new Distance(pvlRadii["PolarRadius"], Distance::Meters);
+  }
+
+
+  ConcurrentControlNetReader::NetworkBuilder::NetworkBuilder(
+    NetworkBuilder const &other) {
+    nullify();
+
+    m_majorRad = new Distance(*other.m_majorRad);
+    m_minorRad = new Distance(*other.m_minorRad);
+    m_polarRad = new Distance(*other.m_polarRad);
+  }
+
+
+  ConcurrentControlNetReader::NetworkBuilder::~NetworkBuilder() {
+    if (m_majorRad) {
+      delete m_majorRad;
+      m_majorRad = NULL;
+    }
+
+    if (m_minorRad) {
+      delete m_minorRad;
+      m_minorRad = NULL;
+    }
+
+    if (m_polarRad) {
+      delete m_polarRad;
+      m_polarRad = NULL;
+    }
+  }
+
+
+  ControlPoint *ConcurrentControlNetReader::NetworkBuilder::operator()(
+    const ControlPointFileEntryV0002 &versionerPoint) const {
+    return new ControlPoint(versionerPoint, *m_majorRad, *m_minorRad,
+        *m_polarRad);
+  }
+
+
+  ConcurrentControlNetReader::NetworkBuilder &
+  ConcurrentControlNetReader::NetworkBuilder::operator=(
+    NetworkBuilder other) {
+    swap(*m_majorRad, *other.m_majorRad);
+    swap(*m_minorRad, *other.m_minorRad);
+    swap(*m_polarRad, *other.m_polarRad);
+
+    return *this;
+  }
+
+
+  void ConcurrentControlNetReader::NetworkBuilder::addToNetwork(
+    QAtomicPointer< ControlNet > & network,
+    ControlPoint *const &item) {
+    if (!network)
+      network = new ControlNet;
+
+    network->AddPoint(item);
+  }
+
+
+  void ConcurrentControlNetReader::NetworkBuilder::nullify() {
+    m_majorRad = NULL;
+    m_minorRad = NULL;
+    m_polarRad = NULL;
+  }
+}
+
---
+++ isis/src/qisis/objs/ConcurrentControlNetReader/ConcurrentControlNetReader.h
@@ -0,0 +1,147 @@
+#ifndef ConcurrentControlNetReader_h
+#define ConcurrentControlNetReader_h
+/**
+ * @file
+ * $Revision: 1.9 $
+ * $Date: 2009/07/15 17:33:52 $
+ *
+ *   Unless noted otherwise, the portions of Isis written by the USGS are
+ *   public domain. See individual third-party library and package descriptions
+ *   for intellectual property information, user agreements, and related
+ *   information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or
+ *   implied, is made by the USGS as to the accuracy and functioning of such
+ *   software and related material nor shall the fact of distribution
+ *   constitute any such warranty, and no responsibility is assumed by the
+ *   USGS in connection therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html
+ *   in a browser or see the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
+#include <QObject>
+#include <QString>
+
+#include "ControlNetFile.h"
+
+
+template<typename A> class QAtomicPointer;
+template<typename A> class QFutureWatcher;
+
+
+namespace Isis {
+  class ControlNet;
+  class ControlPoint;
+  class Distance;
+
+
+  /**
+   * @brief This reads a control network in the background
+   *
+   * This class provides progress for reading a ControlNet.  Reading is done
+   * in separate threads using QtConcurrent.  When reading large Pvl networks
+   * the progress will hang at zero percent for a while.
+   * 
+   * @author Eric Hyer, Steven Lambright, Jai Rideout
+   */
+  class ConcurrentControlNetReader : public QObject {
+      Q_OBJECT
+
+    public:
+      ConcurrentControlNetReader();
+      ~ConcurrentControlNetReader();
+      void read(QString filename);
+
+
+    signals:
+      void progressRangeChanged(int, int);
+      void progressValueChanged(int);
+      void networkReadFinished(ControlNet *);
+
+
+    private:
+      void nullify();
+
+
+    private slots:
+      void startBuildingNetwork();
+      void networkBuilt();
+
+
+    private:
+      //! provides SIGNALS / SLOTS for ReadNetworkFunctor
+      QFutureWatcher<LatestControlNetFile *> * m_readWatcher;
+      
+      //! provides SIGNALS / SLOTS for NetworkBuilder
+      QFutureWatcher< QAtomicPointer<ControlNet> > * m_builderWatcher;
+      
+      //! This is the binary container when reading a network
+      LatestControlNetFile *m_versionerFile;
+
+
+    private:
+      /**
+       * @brief This functor reads the network file to the binary container
+       *
+       * This class is designed to be called with QtConcurrent::run()
+       * 
+       * @author Eric Hyer, Steven Lambright, Jai Rideout
+       */
+      class ReadNetworkFunctor : public std::unary_function<
+          void, LatestControlNetFile *> {
+
+        public:
+          ReadNetworkFunctor(QString);
+          ~ReadNetworkFunctor();
+          LatestControlNetFile *operator()() const;
+
+
+        private:
+          //! needed by operator() to do call ControlNetVersioner::Read()
+          QString m_networkFilename;
+      };
+      
+
+      /**
+       * @brief This functor builds the points in a network
+       *
+       * This class is designed to be called with QtConcurrent::mappedReduced()
+       * 
+       * @author Eric Hyer, Steven Lambright, Jai Rideout
+       */
+      class NetworkBuilder : public std::unary_function<
+          const ControlPointFileEntryV0002 &, ControlPoint *> {
+
+        public:
+          NetworkBuilder(QString);
+          NetworkBuilder(NetworkBuilder const &);
+          ~NetworkBuilder();
+          ControlPoint * operator()(const ControlPointFileEntryV0002 &) const;
+          NetworkBuilder & operator=(NetworkBuilder);
+
+          static void addToNetwork(QAtomicPointer<ControlNet> &,
+              ControlPoint * const &);
+
+
+        private:
+          void nullify();
+
+
+        private:
+          //! Needed for construction of ControlPoints
+          Distance *m_majorRad;
+          
+          //! Needed for construction of ControlPoints
+          Distance *m_minorRad;
+          
+          //! Needed for construction of ControlPoints
+          Distance *m_polarRad;
+      };
+  };
+}
+
+#endif
