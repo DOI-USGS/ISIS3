e8f3c37c86d831d471cfb53bcbc88c42d2dc7af3
Tracie Sucharski <tsucharski@usgs.gov>
2011-08-01 20:42:41 +0000

    Added same bilinear interpolation scheme that the cameras use.  Fix app tests, documentation, examples.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@2925 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/control/apps/cnetnewradii/cnetnewradii.cpp
+++ isis/src/control/apps/cnetnewradii/cnetnewradii.cpp
@@ -8,8 +8,10 @@
 #include "Cube.h"
 #include "Filename.h"
 #include "iException.h"
+#include "Interpolator.h"
 #include "Latitude.h"
 #include "Longitude.h"
+#include "Portal.h"
 #include "SpecialPixel.h"
 #include "SurfacePoint.h"
 #include "UniversalGroundMap.h"
@@ -23,18 +25,29 @@ void IsisMain() {
   UserInterface &ui = Application::GetUserInterface();
   ControlNet cnet(ui.GetFilename("CNET"));
 
-  // Get input cube and get camera model for it
+  // Get input DEM cube and get ground map for it
   string from = ui.GetFilename("MODEL");
-  Cube cube;
-  cube.open(from);
+  Cube demCube;
+  demCube.open(from);
   UniversalGroundMap *ugm = NULL;
   try {
-    ugm = new UniversalGroundMap(cube);
+    ugm = new UniversalGroundMap(demCube);
   }
-  catch(iException e) {
-    iString msg = "Cannot initalize UniversalGroundMap for cube [" + from + "]";
+  catch (iException e) {
+    iString msg = "Cannot initalize UniversalGroundMap for DEM cube [" +
+                   from + "]";
     throw iException::Message(iException::User, msg, _FILEINFO_);
   }
+
+  //  Use bilinear interpolation to read radius from DEM
+  //   Use bilinear interpolation from dem
+  Interpolator *interp = new Interpolator(Interpolator::BiLinearType);;
+
+  //   Buffer used to read from the model
+  Portal *portal = new Portal(interp->Samples(), interp->Lines(),
+                              demCube.getPixelType(),
+                              interp->HotSample(), interp->HotLine());;
+
 
   GetLatLon newRadiiSource;
   iString getLatLon = iString(ui.GetAsString("GETLATLON")).UpCase();
@@ -52,12 +65,14 @@ void IsisMain() {
 
   int numSuccesses = 0;
   int numFailures = 0;
+  int numConstrainedFixed = 0;
   string failedIDs = "";
 
-  for(int i = 0; i < cnet.GetNumPoints() ; i++) {
+  for (int i = 0; i < cnet.GetNumPoints() ; i++) {
     ControlPoint *cp = cnet.GetPoint(i);
 
-    if(cp->GetType() == ControlPoint::Fixed) {
+    if (cp->GetType() != ControlPoint::Free) {
+      numConstrainedFixed++;
       // Create Brick on samp, line to get the dn value of the pixel
       SurfacePoint surfacePt;
       if (newRadiiSource == Adjusted)
@@ -66,23 +81,25 @@ void IsisMain() {
         surfacePt = cp->GetAprioriSurfacePoint();
       bool success = surfacePt.Valid();
 
-      if(success) {
+      if (success) {
         success = ugm->SetUniversalGround(surfacePt.GetLatitude().GetDegrees(),
-            surfacePt.GetLongitude().GetDegrees());
+                                          surfacePt.GetLongitude().GetDegrees());
       }
 
-      Brick b(1, 1, 1, cube.getPixelType());
-      if(success) {
-        b.SetBasePosition((int)ugm->Sample(), (int)ugm->Line(), 1);
-        cube.read(b);
-        success = !IsSpecial(b[0]);
+      double radius = 0.;
+      if (success) {
+        portal->SetPosition(ugm->Sample(), ugm->Line(), 1);
+        demCube.read(*portal);
+        radius = interp->Interpolate(ugm->Sample(), ugm->Line(),
+                                     portal->DoubleBuffer());
+        success = !IsSpecial(radius);
       }
 
       // if we are unable to calculate a valid Radius value for a point,
       // we will ignore this point and keep track of it
-      if(!success) {
+      if (!success) {
         numFailures++;
-        if(numFailures > 1) {
+        if (numFailures > 1) {
           failedIDs = failedIDs + ", ";
         }
         failedIDs = failedIDs + cp->GetId();
@@ -91,7 +108,7 @@ void IsisMain() {
       // otherwise, we will replace the computed radius value to the output control net
       else {
         numSuccesses++;
-        surfacePt.ResetLocalRadius(Distance(b[0], Distance::Meters));
+        surfacePt.ResetLocalRadius(Distance(radius, Distance::Meters));
         if (newRadiiSource == Adjusted)
           cp->SetAdjustedSurfacePoint(surfacePt);
         else if (newRadiiSource == Apriori)
@@ -102,30 +119,44 @@ void IsisMain() {
 
   delete ugm;
   ugm = NULL;
+  delete interp;
+  interp = NULL;
+  delete portal;
+  portal = NULL;
 
-  if(numSuccesses == 0) {
-    string msg = "No valid radii can be calculated. Verify that the DEM [" + ui.GetAsString("MODEL") + "] is valid.";
-    throw Isis::iException::Message(Isis::iException::User, msg, _FILEINFO_);
+  if (numSuccesses == 0) {
+    if (numConstrainedFixed == 0) {
+      string msg = "There were no Fixed or Constrained points in this network."
+          "  No radii were replaced.";
+      throw Isis::iException::Message(Isis::iException::User, msg, _FILEINFO_);
+    }
+    else {
+      string msg = "No valid radii can be calculated. Verify that the DEM [" +
+                       ui.GetAsString("MODEL") + "] is valid.";
+      throw Isis::iException::Message(Isis::iException::User, msg, _FILEINFO_);
+    }
   }
 
-  cnet.Write(ui.GetFilename("TO"));
+  cnet.Write(ui.GetFilename("ONET"));
 
   // Write results to Logs
   // Summary group is created with the counts of successes and failures
   PvlGroup summaryGroup = PvlGroup("Summary");
   summaryGroup.AddKeyword(PvlKeyword("Successes", numSuccesses));
   summaryGroup.AddKeyword(PvlKeyword("Failures", numFailures));
+  summaryGroup.AddKeyword(PvlKeyword("NumberFixedConstrainedPoints",
+                                      numConstrainedFixed));
 
   bool errorlog;
   Filename errorlogFile;
   // if a filename was entered, use it to create the log
-  if(ui.WasEntered("ERRORS")) {
+  if (ui.WasEntered("ERRORS")) {
     errorlog = true;
     errorlogFile = ui.GetFilename("ERRORS");
   }
   // if no filename was entered, but there were some failures,
   // create an error log named "failures" in the current directory
-  else if(numFailures > 0) {
+  else if (numFailures > 0) {
     errorlog = true;
     errorlogFile = "failures.log";
   }
@@ -135,16 +166,18 @@ void IsisMain() {
     errorlog = false;
   }
 
-  if(errorlog) {
+  if (errorlog) {
     // Write details in error log
     Pvl results;
     results.SetName("Results");
     results.AddGroup(summaryGroup);
-    if(numFailures > 0) {
+    if (numFailures > 0) {
       // if there are any failures, add comment to the summary log to alert user
-      summaryGroup.AddComment("Unable to calculate radius for all points. Point IDs for failures contained in [" + errorlogFile.Name() + "].");
+      summaryGroup.AddComment("Unable to calculate radius for all points. Point"
+              " IDs for failures contained in [" + errorlogFile.Name() + "].");
       PvlGroup failGroup = PvlGroup("Failures");
-      failGroup.AddComment("A point fails if we are unable to set universal ground or if the radius calculated is a special pixel value.");
+      failGroup.AddComment("A point fails if we are unable to set universal "
+               "ground or if the radius calculated is a special pixel value.");
       failGroup.AddKeyword(PvlKeyword("PointIDs", failedIDs));
       results.AddGroup(failGroup);
     }
