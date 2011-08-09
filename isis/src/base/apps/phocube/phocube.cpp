dcf2fc0c940f8a6f85607a41d6f4f60b655d21e3
Kris Becker <kbecker@usgs.gov>
2011-08-09 00:53:34 +0000

    Added DN, MORPHOLOGY and ALBEDO backplane options; made BandBin more label/camera model compliant; added allbands appTest;  modified default appTest to compute default option planes
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3016 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/phocube/phocube.cpp
+++ isis/src/base/apps/phocube/phocube.cpp
@@ -7,6 +7,7 @@
 #include "SpecialPixel.h"
 #include "iException.h"
 
+#include <cmath>
 
 using namespace std;
 using namespace Isis;
@@ -15,6 +16,7 @@ using namespace Isis;
 Camera *cam;
 int nbands;
 
+bool dn;
 bool phase; 
 bool emission;
 bool incidence;
@@ -32,8 +34,32 @@ bool spacecraftAzimuth;
 bool offnadirAngle;
 bool subSpacecraftGroundAzimuth;
 bool subSolarGroundAzimuth;
+bool morphology;
+bool albedo;
 
+void phocubeDN(Buffer &in, Buffer &out);
 void phocube(Buffer &out);
+
+
+// Function to create a keyword with same values of a specified count
+template <typename T> PvlKeyword makeKey(const std::string &name, 
+                                         const int &nvals, 
+                                         const T &value);
+
+// Structure containing new mosaic planes
+struct MosData {
+  MosData() :  m_morph(Null), m_albedo(Null) {  }
+  double m_morph;
+  double m_albedo;
+};
+
+// Computes the special MORPHOLOGY and ALBEDO planes
+MosData *getMosaicIndicies(Camera &camera, MosData &md);
+// Updates BandBin keyword
+void UpdateBandKey(const std::string &keyname, PvlGroup &bb, const int &nvals, 
+                   const std::string &default_value = "Null");
+
+
 
 void IsisMain() {
   // Get the camera information
@@ -48,6 +74,7 @@ void IsisMain() {
   UserInterface &ui = Application::GetUserInterface();
 
   nbands = 0;
+  if((dn = ui.GetBoolean("DN"))) nbands++;
   if((phase = ui.GetBoolean("PHASE"))) nbands++;
   if((emission = ui.GetBoolean("EMISSION"))) nbands++;
   if((incidence = ui.GetBoolean("INCIDENCE"))) nbands++;
@@ -65,15 +92,25 @@ void IsisMain() {
   if((offnadirAngle = ui.GetBoolean("OFFNADIRANGLE"))) nbands++;
   if((subSpacecraftGroundAzimuth = ui.GetBoolean("SUBSPACECRAFTGROUNDAZIMUTH"))) nbands++;
   if((subSolarGroundAzimuth = ui.GetBoolean("SUBSOLARGROUNDAZIMUTH"))) nbands++;
+  if ((morphology = ui.GetBoolean("MORPHOLOGY"))) nbands++; 
+  if ((albedo = ui.GetBoolean("ALBEDO"))) nbands++; 
 
   if(nbands < 1) {
     string message = "At least one photometry parameter must be entered"
-                     "[PHASE, EMISSION, INCIDENCE, LATITUDE, LONGITUDE]";
+                     "[PHASE, EMISSION, INCIDENCE, LATITUDE, LONGITUDE...]";
     throw iException::Message(iException::User, message, _FILEINFO_);
+  }
+
+  // Retrieve the orignal values from the input cube band
+  PvlGroup &mybb = icube->getGroup("BandBin");
+  string bname("DN");
+  if ( mybb.HasKeyword("Name") ) {
+    bname = mybb["Name"][0];
   }
 
   // Create a bandbin group for the output label
   PvlKeyword name("Name");
+  if (dn) name += bname;
   if(phase) name += "Phase Angle";
   if(emission) name += "Emission Angle";
   if(incidence) name += "Incidence Angle";
@@ -91,46 +128,87 @@ void IsisMain() {
   if(offnadirAngle) name += "OffNadir Angle";
   if(subSpacecraftGroundAzimuth) name += "Sub Spacecraft Ground Azimuth";
   if(subSolarGroundAzimuth) name += "Sub Solar Ground Azimuth";
-  PvlGroup bandBin("BandBin");
-  bandBin += name;
+  if (morphology) name += "Morphology";
+  if (albedo) name += "Albedo";
+
 
   // Create the output cube.  Note we add the input cube to expedite propagation
-  // of input cube elements (label, blobs, etc...).  It *must* be cleared
-  // prior to systematic processing.
+  // of input cube elements (label, blobs, etc...).  It will be cleared
+  // prior to systematic processing only if the DN option is not selected.
+  // If DN is chosen by the user, then we propagate the input buffer with a 
+  // different function - one that accepts both input and output buffers.
   (void) p.SetInputCube("FROM", OneBand);
-  Cube *ocube = p.SetOutputCube("TO", icube->getSampleCount(), icube->getLineCount(), nbands);
+  Cube *ocube = p.SetOutputCube("TO", icube->getSampleCount(), 
+                                icube->getLineCount(), nbands);
   p.SetBrickSize(64, 64, nbands);
-  p.ClearInputCubes();     // Toss the input file as stated above
 
-  // Start the processing
-  p.StartProcess(phocube);
+  if (dn) {
+    // Process with input and output buffers
+    p.StartProcess(phocubeDN);
+  }
+  else {
+    // Toss the input file as stated above
+    p.ClearInputCubes();   
+
+    // Start the processing
+    p.StartProcess(phocube);
+  }
 
   // Add the bandbin group to the output label.  If a BandBin group already
   // exists, remove all existing keywords and add the keywords for this app.
   // Otherwise, just put the group in.
   PvlObject &cobj = ocube->getLabel()->FindObject("IsisCube");
-  if(cobj.HasGroup("BandBin")) {
-    PvlGroup &bb = cobj.FindGroup("BandBin");
-    bb.Clear();
-    PvlContainer::PvlKeywordIterator k = bandBin.Begin();
-    while(k != bandBin.End()) {
-      bb += *k;
-      ++k;
-    }
+  if(!cobj.HasGroup("BandBin")) {
+    cobj.AddGroup(PvlGroup("BandBin"));
   }
-  else {
-    ocube->putGroup(bandBin);
+
+  PvlGroup &bb = cobj.FindGroup("BandBin");
+  bb.AddKeyword(name, PvlContainer::Replace);
+  int nvals = name.Size();
+  UpdateBandKey("Center", bb, nvals, "1.0");
+
+  if ( bb.HasKeyword("OriginalBand") ) {
+    UpdateBandKey("OriginalBand", bb, nvals, "1.0");
   }
+
+  if ( bb.HasKeyword("Number") ) {
+    UpdateBandKey("Number", bb, nvals, "1.0");
+  }
+
+  UpdateBandKey("Width", bb, nvals, "1.0");
+
+
 
   p.EndProcess();
 }
 
 
+//  This propagates the input plane to the output plane, then passes it off to
+//  the general routine
+void phocubeDN(Buffer &in, Buffer &out) {
+  for (int i = 0 ; i < in.size() ; i++) {
+    out[i] = in[i];
+  }
+  phocube(out);
+}
+
+
+//  Computes all the geometric properties for the output buffer.  Certain
+//  knowledge of the buffers size is assumed below, so ensure the buffer
+//  is still of the expected size.
 void phocube(Buffer &out) {
+
+
+  // If the DN option is selected, it is already added by the phocubeDN
+  // function.  We must compute the offset to start at the second band.
+  int skipDN = (dn) ? 64 * 64   :  0;
 
   for(int i = 0; i < 64; i++) {
     for(int j = 0; j < 64; j++) {
-      int index = i * 64 + j;
+
+      MosData mosd, *p_mosd(0);  // For special mosaic angles
+
+      int index = i * 64 + j + skipDN;
       double samp = out.Sample(index);
       double line = out.Line(index);
       cam->SetImage(samp, line);
@@ -221,6 +299,20 @@ void phocube(Buffer &out) {
               cam->UniversalLongitude(), sslat, sslon);
           index += 64 * 64;
         }
+
+        // Special Mosaic indexes
+        if (morphology) {
+          if (!p_mosd) { p_mosd = getMosaicIndicies(*cam, mosd); }
+          out[index] = mosd.m_morph;
+          index += 64 * 64;
+        }
+
+        if (albedo) {
+          if (!p_mosd) { p_mosd = getMosaicIndicies(*cam, mosd); }
+          out[index] = mosd.m_albedo;
+          index += 64 * 64;
+        }
+
       }
       // Trim outerspace
       else {
@@ -232,3 +324,70 @@ void phocube(Buffer &out) {
     }
   }
 }
+
+
+// Function to create a keyword with same values of a specified count
+template <typename T> 
+  PvlKeyword makeKey(const std::string &name, const int &nvals, 
+                     const T &value) {
+    PvlKeyword key(name);
+    for (int i = 0 ; i < nvals ; i++) {
+      key += value;
+    }
+    return (key);
+  }
+
+
+// Computes the special MORPHOLOGY and ALBEDO planes
+MosData *getMosaicIndicies(Camera &camera, MosData &md) {
+  const double Epsilon(1.0E-8);
+  Angle myphase;
+  Angle myincidence;
+  Angle myemission;
+  bool mysuccess;
+  camera.LocalPhotometricAngles(myphase, myincidence, myemission, mysuccess);
+  if (!mysuccess) {
+    myemission.SetDegrees(camera.EmissionAngle());
+    myincidence.SetDegrees(camera.IncidenceAngle());
+  }
+  double res = camera.PixelResolution();
+  if (fabs(res) < Epsilon) res = Epsilon;
+
+  md = MosData();  // Nullifies the data
+  if (myemission.Valid()) {
+    // Compute MORPHOLOGY
+    double cose = cos(myemission.GetRadians());
+    if (fabs(cose) < Epsilon) cose = Epsilon;
+    // Convert resolution to units of KM
+    md.m_morph = (res / 1000.0) / cose;
+
+    if (myincidence.Valid()) {
+      // Compute ALBEDO
+      double cosi = cos(myincidence.GetRadians());
+      if (fabs(cosi) < Epsilon) cosi = Epsilon;
+      //  Convert resolution to KM
+      md.m_albedo = (res / 1000.0 ) * ( (1.0 / cose) + (1.0 / cosi) );  
+    }
+  }
+
+  return (&md);
+}
+
+
+//  Updates existing BandBin keywords with additional values to ensure
+//  label compilancy (which should support Camera models).  It checks for the
+//  existance of the keyword and uses its (assumed) first value to set nvals
+//  values to a constant.  If the keyword doesn't exist, it uses the default
+//  value.
+void UpdateBandKey(const std::string &keyname, PvlGroup &bb, const int &nvals, 
+                   const std::string &default_value) {
+
+  string defVal(default_value);
+  if ( bb.HasKeyword(keyname) ) {
+    defVal = bb[keyname][0];
+  }
+
+  bb.AddKeyword(makeKey(keyname, nvals, defVal), PvlContainer::Replace);
+  return;
+}
+
