619037182cee87abb207727e7b9033317d49862e
Travis Addair <taddair@usgs.gov>
2011-08-31 22:33:22 +0000

    Changed to allow ranges of a single DN to fix #400.  Also refactored code to reduce duplication, and updated stylistic elements (partially) to align with new standards.  Added new make command to test this new addition.  Updated documentation to include new functionality, and explain that all ranges are fully inclusive.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3142 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/specpix/specpix.cpp
+++ isis/src/base/apps/specpix/specpix.cpp
@@ -28,14 +28,17 @@ struct spRange {
   double max;
   SpecPix specPix;
 };
-vector <spRange> rngList;
-int numRange;
 
-// Line processing routine
+
 void specpix(Buffer &in, Buffer &out);
+bool descending(const spRange &r1, const spRange &r2);
+void addRange(string minName, string maxName, SpecPix pixel);
 
-bool Descending(const spRange &r1, const spRange &r2);
+
+vector <spRange> rangeList;
+int numRange;
 int nnull, nlis, nlrs, nhis, nhrs;
+
 
 void IsisMain() {
   // We will be processing by line
@@ -47,67 +50,12 @@ void IsisMain() {
   p.SetInputCube("FROM");
   p.SetOutputCube("TO");
 
-  spRange temp;
   // Read range values from user
-  UserInterface &ui = Application::GetUserInterface();
-
-  temp.min = 0;
-  temp.max = 0;
-  if(ui.WasEntered("NULLMIN")) temp.min = ui.GetDouble("NULLMIN");
-  if(ui.WasEntered("NULLMAX")) temp.max = ui.GetDouble("NULLMAX");
-
-  if(temp.min != 0 || temp.max != 0) {
-    nnull = 0;
-    temp.specPix = NULLP;
-    rngList.push_back(temp);
-    temp.min = 0;
-    temp.max = 0;
-    temp.specPix = NONE;
-  }
-
-  if(ui.WasEntered("LRSMIN")) temp.min = ui.GetDouble("LRSMIN");
-  if(ui.WasEntered("LRSMAX")) temp.max = ui.GetDouble("LRSMAX");
-  if(temp.min != 0 || temp.max != 0) {
-    nlrs = 0;
-    temp.specPix = LRS;
-    rngList.push_back(temp);
-    temp.min = 0;
-    temp.max = 0;
-    temp.specPix = NONE;
-  }
-
-  if(ui.WasEntered("HRSMIN")) temp.min = ui.GetDouble("HRSMIN");
-  if(ui.WasEntered("HRSMAX")) temp.max = ui.GetDouble("HRSMAX");
-  if(temp.min != 0 || temp.max != 0) {
-    nhrs = 0;
-    temp.specPix = HRS;
-    rngList.push_back(temp);
-    temp.min = 0;
-    temp.max = 0;
-    temp.specPix = NONE;
-  }
-
-  if(ui.WasEntered("LISMIN")) temp.min = ui.GetDouble("LISMIN");
-  if(ui.WasEntered("LISMAX")) temp.max = ui.GetDouble("LISMAX");
-  if(temp.min != 0 || temp.max != 0) {
-    nlis = 0;
-    temp.specPix = LIS;
-    rngList.push_back(temp);
-    temp.min = 0;
-    temp.max = 0;
-    temp.specPix = NONE;
-  }
-
-  if(ui.WasEntered("HISMIN")) temp.min = ui.GetDouble("HISMIN");
-  if(ui.WasEntered("HISMAX")) temp.max = ui.GetDouble("HISMAX");
-  if(temp.min != 0 || temp.max != 0) {
-    nhis = 0;
-    temp.specPix = HIS;
-    rngList.push_back(temp);
-    temp.min = 0;
-    temp.max = 0;
-    temp.specPix = NONE;
-  }
+  addRange("NULLMIN", "NULLMAX", NULLP);
+  addRange("LRSMIN", "LRSMAX", LRS);
+  addRange("HRSMIN", "HRSMAX", HRS);
+  addRange("LISMIN", "LISMAX", LIS);
+  addRange("HISMIN", "HISMAX", HIS);
 
   //  If more than one range was entered,
   //  make sure there is no overlap in ranges between differing special
@@ -115,11 +63,11 @@ void IsisMain() {
   //  Then compare each min to the max in the next set of ranges.  If
   //  the min is less than the next max, there is overlap between those
   //  two sets of ranges.
-  numRange = rngList.size();
+  numRange = rangeList.size();
   if(numRange > 1) {
     vector <spRange> sortList(numRange);
-    copy(rngList.begin(), rngList.end(), sortList.begin());
-    sort(sortList.begin(), sortList.end(), Descending);
+    copy(rangeList.begin(), rangeList.end(), sortList.begin());
+    sort(sortList.begin(), sortList.end(), descending);
     for(int i = 0; i < numRange - 1; i++) {
       if(sortList[i].min < sortList[i+1].max) {
         //  We have overlap
@@ -129,7 +77,7 @@ void IsisMain() {
       }
     }
     //  Copy sorted list back to original vector.
-    copy(sortList.begin(), sortList.end(), rngList.begin());
+    copy(sortList.begin(), sortList.end(), rangeList.begin());
   }
 
   // Start the processing
@@ -138,7 +86,7 @@ void IsisMain() {
 
   // Erase vector, otherwise if specpix is re-run it adds new
   // ranges to the end of the last run.
-  rngList.erase(rngList.begin(), rngList.end());
+  rangeList.clear();
 
   //  Print out number of values changed
   PvlGroup results("Results");
@@ -162,37 +110,37 @@ void specpix(Buffer &in, Buffer &out) {
   for(int i = 0; i < in.size(); i++) {
     out[i] = in[i];
     for(int rng = 0; rng < numRange; rng++) {
-      switch(rngList[rng].specPix) {
+      switch(rangeList[rng].specPix) {
         case NULLP:
-          if(in[i] >= rngList[rng].min && in[i] <= rngList[rng].max) {
+          if(in[i] >= rangeList[rng].min && in[i] <= rangeList[rng].max) {
             out[i] = NULL8;
             nnull++;
           }
           break;
 
         case LRS:
-          if(in[i] >= rngList[rng].min && in[i] <= rngList[rng].max) {
+          if(in[i] >= rangeList[rng].min && in[i] <= rangeList[rng].max) {
             out[i] = LOW_REPR_SAT8;
             nlrs++;
           }
           break;
 
         case HRS:
-          if(in[i] >= rngList[rng].min && in[i] <= rngList[rng].max) {
+          if(in[i] >= rangeList[rng].min && in[i] <= rangeList[rng].max) {
             out[i] = HIGH_REPR_SAT8;
             nhrs++;
           }
           break;
 
         case LIS:
-          if(in[i] >= rngList[rng].min && in[i] <= rngList[rng].max) {
+          if(in[i] >= rangeList[rng].min && in[i] <= rangeList[rng].max) {
             out[i] = LOW_INSTR_SAT8;
             nlis++;
           }
           break;
 
         case HIS:
-          if(in[i] >= rngList[rng].min && in[i] <= rngList[rng].max) {
+          if(in[i] >= rangeList[rng].min && in[i] <= rangeList[rng].max) {
             out[i] = HIGH_INSTR_SAT8;
             nhis++;
           }
@@ -205,10 +153,22 @@ void specpix(Buffer &in, Buffer &out) {
   }
 }
 
-bool Descending(const spRange &r1, const spRange &r2) {
-  if(r1.min > r2.min) {
-    return true;
-  }
-  else
-    return false;
+
+bool descending(const spRange &r1, const spRange &r2) {
+  return r1.min > r2.min;
 }
+
+
+void addRange(string minName, string maxName, SpecPix pixel) {
+  UserInterface &ui = Application::GetUserInterface();
+
+  if (ui.WasEntered(minName) && ui.WasEntered(maxName)) {
+    spRange temp;
+    temp.min = ui.GetDouble(minName);
+    temp.max = ui.GetDouble(maxName);
+    temp.specPix = pixel;
+
+    rangeList.push_back(temp);
+  }
+}
+
