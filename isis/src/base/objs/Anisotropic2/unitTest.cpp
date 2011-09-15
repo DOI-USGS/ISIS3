b9feca6e15bb5849cfa85e7c334498597c6c27af
Janet Barrett <jbarrett@usgs.gov>
2011-09-15 03:19:16 +0000

    Added shadow parameter to CalcAtmEffect method
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3204 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/Anisotropic2/unitTest.cpp
+++ isis/src/base/objs/Anisotropic2/unitTest.cpp
@@ -17,6 +17,7 @@ int main() {
   double pstd;
   double trans;
   double trans0;
+  double transs;
   double sbar;
 
   std::cout << "UNIT TEST for Anisotropic2 atmospheric function" << std::endl << std::endl;
@@ -44,7 +45,7 @@ int main() {
     AtmosModel *am = AtmosModelFactory::Create(pvl, *pm);
 
     am->SetStandardConditions(true);
-    am->CalcAtmEffect(0.0, 0.0, 0.0, &pstd, &trans, &trans0, &sbar);
+    am->CalcAtmEffect(0.0, 0.0, 0.0, &pstd, &trans, &trans0, &sbar, &transs);
     am->SetStandardConditions(false);
 
     std::cout << "Test phase=0.0, incidence=0.0, emission=0.0 (standard conditions) ..." << std::endl;
@@ -54,7 +55,7 @@ int main() {
     std::cout << "Sbar = " << sbar << std::endl << std::endl;
 
     am->SetAtmosTau(.28);
-    am->CalcAtmEffect(86.7226722, 51.7002388, 38.9414439, &pstd, &trans, &trans0, &sbar);
+    am->CalcAtmEffect(86.7226722, 51.7002388, 38.9414439, &pstd, &trans, &trans0, &sbar, &transs);
 
     std::cout << "Test phase=86.7226722, incidence=51.7002388, emission=38.9414439 ..." << std::endl;
     std::cout << "Pstd = " << pstd << std::endl;
@@ -62,7 +63,7 @@ int main() {
     std::cout << "Trans0 = " << trans0 << std::endl;
     std::cout << "Sbar = " << sbar << std::endl << std::endl;
 
-    am->CalcAtmEffect(180.0, 90.0, 90.0, &pstd, &trans, &trans0, &sbar);
+    am->CalcAtmEffect(180.0, 90.0, 90.0, &pstd, &trans, &trans0, &sbar, &transs);
 
     std::cout << "Test phase=180.0, incidence=90.0, emission=90.0 ..." << std::endl;
     std::cout << "Pstd = " << pstd << std::endl;
