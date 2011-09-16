04f2d6356647e3c8ef5710998950529b5451e171
Janet Barrett <jbarrett@usgs.gov>
2011-09-16 16:36:53 +0000

    Fixed unitTest because of change to RadarSlantRangeMap class ComputeA method
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3233 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/lro/objs/MiniRF/unitTest.cpp
+++ isis/src/lro/objs/MiniRF/unitTest.cpp
@@ -40,8 +40,8 @@ int main(void) {
     // These should be lat/lon at center of image. To obtain these numbers for a new cube/camera,
     // set both the known lat and known lon to zero and copy the unit test output "Latitude off by: "
     // and "Longitude off by: " values directly into these variables.
-    double knownLat = 85.5973849707835512;
-    double knownLon = 264.7360862944493078;
+    double knownLat = 85.5973879396895398;
+    double knownLon = 264.7361454607174664;
 
     Pvl p("$chan1/testData/FSR_CDR_LV1_01801_0R.cub");
     Camera *cam = Isis::CameraFactory::Create(p);
