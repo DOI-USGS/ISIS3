b773970aec799d56a300d513db5e9a9f7bf61090
Eric Hyer <ehyer@usgs.gov>
2011-10-16 00:36:28 +0000

    merged Tab from photoclin branch
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3603 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/qisis/objs/Tab/Tab.cpp
+++ isis/src/qisis/objs/Tab/Tab.cpp
@@ -1,109 +1,124 @@
+#include "IsisDebug.h"
+
 #include "Tab.h"
 
-#include <cstddef>
+//#include <iostream>
 
 #include <QWidget>
 #include <QStylePainter>
 
-namespace Isis {
-  //! constructs a Tab
-  Tab::Tab(QWidget *associatedWidget, QWidget *parent) :
-    QToolButton(parent) {
-    this->associatedWidget = NULL;
-    this->associatedWidget = associatedWidget;
+#include "iException.h"
 
+
+namespace Isis
+{
+
+  //! constructs a Tab
+  Tab::Tab(QWidget * associatedWidget, QWidget * parent) : QAction(parent)
+  {
+    this->associatedWidget = associatedWidget;
+  
     position = 0;
     radioGroup = 0;
     selectedStatus = false;
-
-    connect(this, SIGNAL(clicked()), this, SLOT(handleTriggered()));
+  
+    setCheckable(true);
+  
+    connect(this, SIGNAL(triggered()), this, SLOT(handleTriggered()));
+    
+    ASSERT_PTR(associatedWidget);
   }
-
-
-  //! copy constructs a Tab
-  Tab::Tab(const Tab &other) {
-    associatedWidget = NULL;
-
-    associatedWidget = other.associatedWidget;
-    position = other.position;
-    radioGroup = other.radioGroup;
-    selectedStatus = other.selectedStatus;
-  }
-
-
+  
+  
   //! destructs a Tab
-  Tab::~Tab() {
+  Tab::~Tab()
+  {
     associatedWidget = NULL;
   }
-
-
+  
+  
   //! set the position of the Tab within a TabBar
-  void Tab::setPosition(const int &newPosition) {
+  void Tab::setPosition(const int & newPosition)
+  {
     position = newPosition;
   }
-
-
+  
+  
   //! get the position of the Tab within a TabBar
-  const int &Tab::getPosition() const {
+  const int & Tab::getPosition() const
+  {
     return position;
   }
-
-
+  
+  
   /**
-  * Tabs which share a radio group have the property that only only one Tab
-  * in the group can be selected at a time.
-  *
-  * @param newRadioGroup The new radio group to which the Tab should belong
-  */
-  void Tab::setRadioGroup(const int &newRadioGroup) {
+   * Tabs which share a radio group have the property that only only one Tab
+   * in the group can be selected at a time.
+   *
+   * @param newRadioGroup The new radio group to which the Tab should belong
+   */
+  void Tab::setRadioGroup(const int & newRadioGroup)
+  {
     radioGroup = newRadioGroup;
   }
-
-
+  
+  
   /**
-  * Tabs which share a radio group have the property that only only one Tab
-  * in the group can be selected at a time.
-  *
-  * @returns The radio group which this Tab belongs to
-  */
-  const int &Tab::getRadioGroup() const {
+   * Tabs which share a radio group have the property that only only one Tab
+   * in the group can be selected at a time.
+   *
+   * @returns The radio group which this Tab belongs to
+   */
+  const int & Tab::getRadioGroup() const
+  {
     return radioGroup;
   }
-
-
+  
+  
   /**
-  * A selected Tab will look visually pressed and have its associatedWidget
-  * visible.  A Tab which is not selected will look like a normal button and its
-  * associatedWidget will be hidden.
-  *
-  * @param newStatus True if the Tab should be selected, false otherwise
-  */
-  void Tab::setSelected(bool newStatus) {
-    setDown(newStatus);
+   * A selected Tab will look visually pressed and have its associatedWidget
+   * visible.  A Tab which is not selected will look like a normal button and
+   * its associatedWidget will be hidden.
+   *
+   * @param newStatus True if the Tab should be selected, false otherwise
+   */
+  void Tab::setSelected(bool newStatus)
+  {
+    if (associatedWidget == NULL)
+    {
+      QString msg = "Tab::setSelected called but can't show or hide the "
+          "associatedWidget because it is NULL!";
+      throw iException::Message(iException::Programmer, msg.toStdString(),
+          _FILEINFO_);
+    } 
+    setChecked(newStatus);
     newStatus ? associatedWidget->show() : associatedWidget->hide();
     selectedStatus = newStatus;
   }
-
-
+  
+  
   /**
-  * A selected Tab will look visually pressed and have its associatedWidget
-  * visible.  A Tab which is not selected will look like a normal button and its
-  * associatedWidget will be hidden.
-  *
-  * @returns True if the Tab is selected, false otherwise
-  */
-  bool Tab::isSelected() {
+   * A selected Tab will look visually pressed and have its associatedWidget
+   * visible.  A Tab which is not selected will look like a normal button and
+   * its associatedWidget will be hidden.
+   *
+   * @returns True if the Tab is selected, false otherwise
+   */
+  bool Tab::isSelected()
+  {
     return selectedStatus;
   }
-
-
+  
+  
   /**
-  * This SLOT is executed when the Tab is clicked, and emits its own clicked
-  * SIGNAL (which contains its index) to the TabBar.  The TabBar can then
-  * use this index to determine which Tab was clicked (TabBars store their Tabs
-  * in a QVector).
-  */
-  void Tab::handleTriggered() {
+   * This SLOT is executed when the Tab is clicked, and emits its own clicked
+   * SIGNAL (which contains its index) to the TabBar.  The TabBar can then
+   * use this index to determine which Tab was clicked (TabBars store their Tabs
+   * in a QVector).
+   */
+  void Tab::handleTriggered()
+  {
     emit clicked(position);
   }
+  
 }
--- isis/src/qisis/objs/Tab/Tab.h
+++ isis/src/qisis/objs/Tab/Tab.h
@@ -23,62 +23,65 @@
  *   http://www.usgs.gov/privacy.html.
  */
 
-
-// QToolButton is the parent of this class, and should be the ONLY include
+// QAction is the parent of this class, and should be the ONLY include
 // in this file.  Do not add any other includes to this header file!
-#include <QToolButton>
+#include <QAction>
 
 class QWidget;
-class QPaintEvent;
 
-namespace Isis {
+namespace Isis
+{
   /**
-   * A Tab is simply a QToolButton which shows or hides some other QWidget,
-   * which we call associatedWidget.  The Tab does not own this widget, it
-   * just stores a pointer so that it can set the widget to be visible or
-   * invisible.  Tabs are toggle buttons which stay down when clicked, and then
-   * come back up if clicked again.  If a Tab is down then its associatedWidget
-   * is visible.  If a Tab is up then its associatedWidget is invisible.  Tabs
-   * are designed to be added to TabBars, which are special QToolBars that can
-   * handle the management and storing of Tabs.  Tabs have a "radioGroup" to
-   * which they belong, which is just an int.  Tabs in the same TabBar which
-   * have the same radioGroup number have the property such that only one of the
-   * Tabs can be selected at a time.  Note that the radioGroup value is only
-   * used if the TabBar for which the Tab resides has a radioStyle() of true,
-   * which is false by default.  See TabBar for more details about radioStyle.
+   * A Tab is a QAction which shows or hides some other QWidget, which we call
+   * associatedWidget.  The Tab does not own this widget, it just stores a
+   * pointer so that it can set the widget to be visible or invisible.  Tabs
+   * are toggleable.  If a Tab is down then its associatedWidget is visible
+   * If a Tab is up then its associatedWidget is invisible.  Tabs are designed
+   * to be added to TabBars, which are special QToolBars that can handle the
+   * management and storing of Tabs.  Tabs have a "radioGroup" to which they
+   * belong, which is just an int.  Tabs in the same TabBar which have the same
+   * radioGroup number have the property such that only one of the Tabs can be
+   * selected at a time.  Note that the radioGroup value is only used if the
+   * TabBar for which the Tab resides has a radioStyle() of true, which is
+   * false by default.  See TabBar for more details about radioStyle.
    *
    * @author Eric Hyer
    *
    * @internal
    * @history 2010-05-06 Eric Hyer - Original Version
+   * @history 2010-09-23 Eric Hyer - Parent class now QAction, not QToolButton
+   *                               - Fixed namespace issue
    */
-  class Tab : public QToolButton {
+  class Tab : public QAction
+  {
       Q_OBJECT
 
     public:
-      Tab(QWidget *associatedWidget, QWidget *parent = 0);
-      Tab(const Tab &other);
+      Tab(QWidget * associatedWidget, QWidget * parent = 0);
 
       virtual ~Tab();
 
-      void setPosition(const int &newPosition);
-      const int &getPosition() const;
-      void setRadioGroup(const int &newRadioGroup);
-      const int &getRadioGroup() const;
+      void setPosition(const int & newPosition);
+      const int & getPosition() const;
+      void setRadioGroup(const int & newRadioGroup);
+      const int & getRadioGroup() const;
       void setSelected(bool newStatus);
       bool isSelected();
-
 
     signals:
       void clicked(const int &);
 
-
+      
+    private:
+      Tab(const Tab & other);
+      const Tab & operator=(Tab);
+      
+      
     private slots:
       void handleTriggered();
 
-
     private:
-      QWidget *associatedWidget;
+      QWidget * associatedWidget;
       int position;
       int radioGroup;
       int location;
