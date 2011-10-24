efc8f2f90677475e83398f2dc5e1b5198e833757
James Alexander Crough <jcrough@usgs.gov>
2011-10-24 22:54:58 +0000

    Updated the updateCassiniKernels script to take variable paths (i.e. from different ISIS versions), created a startup.py script to read the csh startup, created an isis3Startup.py which can set the isis version from Python)
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3647 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/scripts/isis3Startup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+from optparse import OptionParser
+
+def setisis():
+  #Try to see if ISISROOT is set...
+  try:
+    ISISROOT = os.environ["ISISROOT"]
+  except:
+    ISISROOT = None
+
+  #... and if it's not give it a default value
+  if ISISROOT is None:
+    ISISROOT = "/usgs/pkgs/isis3/isis"
+    os.environ['ISISROOT'] = ISISROOT
+
+  #Check for the ISIS3DATA directory. If it does not exist use a default
+  if os.path.exists("%s/../data" % (ISISROOT)):
+    os.environ['ISIS3DATA'] = "%s/../data" % (ISISROOT)
+  else:
+    os.environ['ISIS3DATA'] = "/usgs/cpkgs/isis3/data"
+  
+  #Check for the ISIS3TESTDATA directory. If it does not exist use a default
+  if os.path.exists("%s/../testData" % (ISISROOT)):
+    os.environ['ISIS3TESTDATA'] = "%s/../testData" % ISISROOT
+  else:
+    os.environ['ISIS3TESTDATA'] = "/usgs/cpkgs/isis3/testData"
+
+  #If PATH is not set, just set it to a default location. Else append
+  #the isis path to the end of the current path
+  try:
+    environ['PATH']
+  except:
+    os.environ['PATH'] = "%s/bin" % (os.environ['PATH'])
+  else:
+    os.environ['PATH'] = "%s:%s/bin" % (os.environ['PATH'], ISISROOT)
+
+  os.environ['QT_PLUGIN_PATH'] = "%s/3rdParty/plugins" % (ISISROOT)
+
+if __name__ == "__main__":
+  setisis()
