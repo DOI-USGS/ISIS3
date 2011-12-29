324110edcc99408960dd3c1770ebf067c02c0bd9
Sharmilla Prasad <sprasad@usgs.gov>
2011-12-29 16:40:54 +0000

    fixed problem in converting from double to int
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3806 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/mro/apps/hijitter/hijitter.cpp
+++ isis/src/mro/apps/hijitter/hijitter.cpp
@@ -476,10 +476,11 @@ void GetCropLines(const string inFile, double eTime1, double eTime2, int & line1
   iTime etTime = cam->Spice::CacheStartTime();
   double etStart = etTime.Et();
   double lineRate = ((LineScanCameraDetectorMap*) cam->DetectorMap())->LineRate();
+  lineRate += 0.5;
 
   int imgLines = inCube->getLineCount();
-  line1 = (eTime1 - etStart) / lineRate + 0.5;
-  line2 = (eTime2 - etStart) / lineRate + 0.5;
+  line1 = int ((eTime1 - etStart) / lineRate);
+  line2 = int ((eTime2 - etStart) / lineRate);
 
   if (line2 > imgLines)
     numLines = imgLines - line1 + 1;
