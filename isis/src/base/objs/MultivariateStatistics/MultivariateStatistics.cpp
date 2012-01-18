727772dd757d9b6ac38aeb65642a2d4f1224d369
Steven Lambright <slambright@usgs.gov>
2012-01-18 19:46:00 +0000

    Minor compile warning fix, references #613
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@3884 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/MultivariateStatistics/MultivariateStatistics.cpp
+++ isis/src/base/objs/MultivariateStatistics/MultivariateStatistics.cpp
@@ -93,7 +93,7 @@ namespace Isis {
       p_sumxy += x * y * count;
       p_validPixels += count;
 
-      for (int i = 0; i < count; i++) {
+      for (unsigned int i = 0; i < count; i++) {
         p_x.AddData(x);
         p_y.AddData(y);
       }
