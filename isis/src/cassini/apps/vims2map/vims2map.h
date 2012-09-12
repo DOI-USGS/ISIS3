3d471a0dd78306def0007dfe4973d2cd85a649d2
Tracie Sucharski <tsucharski@usgs.gov>
2012-09-12 15:36:41 +0000

    Resurrected vims2map from r3911, updated for changes to IException,iString and FileName.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@4690 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/cassini/apps/vims2map/vims2map.h
@@ -0,0 +1,28 @@
+#ifndef vims2map_h
+#define vims2map_h
+
+#include "Transform.h"
+
+/**
+ * @author 2008-02-13 Stacy Alley
+ *
+ * @internal
+ */
+class vims2map : public Isis::Transform {
+  private:
+    Isis::Camera *p_incam;
+    Isis::Projection *p_outmap;
+
+  public:
+    // constructor
+    vims2map(const int inputSamples, const int inputLines, Isis::Camera *incam,
+             const int outputSamples, const int outputLines, Isis::Projection *outmap,
+             bool trim);
+
+    // destructor
+    ~vims2map() {};
+
+
+};
+
+#endif
