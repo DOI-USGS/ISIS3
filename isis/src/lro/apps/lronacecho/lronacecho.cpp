b05d52c969a9f5f3e95d5f30c383bd473fa23901
Stuart Sides <ssides@usgs.gov>
2013-06-07 21:57:32 +0000

    Updated with code from LROC team. Fixes 1674.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@5198 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/lro/apps/lronacecho/lronacecho.cpp
+++ isis/src/lro/apps/lronacecho/lronacecho.cpp
@@ -3,12 +3,14 @@
 #include "ProcessByLine.h"
 #include "SpecialPixel.h"
 
+
 using namespace std;
 using namespace Isis;
 
 void correct(Buffer &in, Buffer &out);
 
-double g_delta;
+double g_delta, g_halfDelta, g_smoothingCosntant;
+int g_step, g_smoothingRows;
 
 bool g_isLeft, g_isSummed;
 
@@ -22,8 +24,9 @@ void IsisMain() {
   p.SetInputCube("FROM");
   p.SetOutputCube("TO");
 
-  // Get the coefficients
+  // Get the coefficient
   g_delta = ui.GetDouble("DELTA");
+  g_halfDelta = g_delta/2.0;
 
   Pvl lab(ui.GetFileName("FROM"));
   PvlGroup &inst = lab.findGroup("Instrument", Pvl::Traverse);
@@ -31,7 +34,7 @@ void IsisMain() {
   // Check if it is a NAC image
   QString instId = inst["InstrumentId"];
   if (instId != "NACL" && instId != "NACR") {
-    string msg = "This is not a NAC image.  lrocnaccal requires a NAC image.";
+    string msg = "This is not a NAC image. lrocnacecho requires a NAC image.";
     throw IException(IException::User, msg, _FILEINFO_);
   }
 
@@ -40,46 +43,102 @@ void IsisMain() {
   else
     g_isLeft = false;
 
-  if ((int) inst["SpatialSumming"] == 1)
+  if ((int) inst["SpatialSumming"] == 1){
     g_isSummed = false;
-  else
+    g_step = 2;
+  }
+  else {
     g_isSummed = true;
+    g_step = 1;
+  }
+
+  // Make sure that we aren't passed in a scaled or cropped cub
+  if(lab.findObject("IsisCube").hasGroup("AlphaCube")) {
+    QString msg = "This application can not be run on any image that has been geometrically transformed (i.e. scaled, rotated, sheared, or reflected) or cropped.";
+    throw IException(IException::User, msg, _FILEINFO_);
+  }
+
+  // Pre calulate a constant for the correction's smooth turn on
+  g_smoothingRows = ui.GetInteger("SMOOTHINGROWS");
+  if (g_isSummed)
+      g_smoothingRows /= 2;
+  g_smoothingCosntant = PI/(double)g_smoothingRows;
 
   p.ProcessCube(correct, false);
 }
 
-// Unary routine
 void correct(Buffer &in, Buffer &out) {
-  // Loop for each pixel in the line
-  int step = 2;
-  if (g_isSummed)
-    step = 1;
+  double delta = 0; //<! A delta value used for 'ramping up' the correction at the edge of the image  
 
   if (g_isLeft) {
-    out[0] = in[0];
-    out[1] = in[1];
-    for(int i = step; i < in.size(); i++) {
-      if(IsSpecial(in[i]) || IsSpecial(in[i-step])) {
+    for(int i = 0; i < in.size(); i++) {
+      if(IsSpecial(in[i]) || IsSpecial(in[i-g_step])) {
         out[i] = in[i];
       }
+      // Image starts at Sample 42 for native images
+      // Image starts at Sample 21 for summed images
+      else if (i >= 21*g_step && i <= (21*g_step) + g_smoothingRows ){
+          delta = ((g_halfDelta * (1 - cos(g_smoothingCosntant * (i - 21*g_step) ))));
+          //Apply the 'slow' correction
+          out[i] = in[i] - (delta * out[i-g_step]);
+      }
       else {
-        out[i] = in[i] - g_delta * out[i-step];
+          //Apply the 'nominal' correction
+          out[i] = in[i] - g_delta * out[i-g_step];
       }
     }
   }
   else {
-    out[in.size()-1] = in[in.size()-1];
-    out[in.size()-2] = in[in.size()-2];
-    for(int i = in.size() - 1 - step; i >= 0; i--) {
-      if(IsSpecial(in[i]) || IsSpecial(in[i+step])) {
-        out[i] = in[i];
-      }
-      else {
-        out[i] = in[i] - g_delta * out[i+step];
-      }
+    for(int i = (in.size() - 1); i >= 0; i--) {
+
+        if(IsSpecial(in[i]) || IsSpecial(in[i+g_step])) {
+            out[i] = in[i];
+        }
+
+        // Image starts at Sample 5024 for native images
+        // Image starts at Sample 2512 for summed images
+        else if ((i <=  (2512 * g_step) && (i >= (2512 * g_step) - g_smoothingRows ))){
+            delta = (g_halfDelta * (1 - cos(g_smoothingCosntant * ((in.size() - i)  - (21*g_step) ))));
+            //Apply the 'slow' correction
+            out[i] = in[i] - (delta * out[i+g_step]);
+        }
+        else {
+            out[i] = in[i] - g_delta * out[i+g_step];
+        }
     }
   }
-  //we then normalize the row
-  for(int i = 0; i < out.size(); i++)
-      out[i] = (out[i] * (1 + g_delta));
+
+  // We must now normalize the entire line
+  if (g_isLeft) {
+      for(int i = 0; i < out.size(); i++){
+          if(IsSpecial(in[i]) || IsSpecial(in[i-g_step])) {
+              out[i] = in[i];
+          }
+          // Image starts at Sample 42 for native images
+          // Image starts at Sample 21 for summed images
+          else if (i >= 21*g_step && i <= (21*g_step) + g_smoothingRows ){
+              delta = ((g_halfDelta * (1 - cos(g_smoothingCosntant * (i - 21*g_step) ))));
+              out[i] = (out[i] * (1 + delta));
+          }
+          else {
+              out[i] = (out[i] * (1 + g_delta));
+          }
+      }
+  }
+  else {
+      for(int i = (in.size() - 1); i >= 0; i--) {
+          if(IsSpecial(in[i]) || IsSpecial(in[i+g_step])) {
+              out[i] = in[i];
+          }
+          // Image starts at Sample 5024 for native images
+          // Image starts at Sample 2512 for summed images
+          else if ((i <=  (2512 * g_step) && (i >= (2512 * g_step) - g_smoothingRows ))){
+              delta = (g_halfDelta * (1 - cos(g_smoothingCosntant * ((in.size() - i)  - (21*g_step) ))));
+              out[i] = (out[i] * (1 + delta));
+          }
+          else {
+              out[i] = (out[i] * (1 + g_delta));
+          }
+      }
+  }
 }
