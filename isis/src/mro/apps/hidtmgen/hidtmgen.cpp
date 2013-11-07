36c87d960e6e4aba796c76f6c7e775bbc3495897
Jeannie Backer <jwbacker@usgs.gov>
2013-11-07 17:27:34 +0000

    PROG: Fixed merge from revision 5558.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@5576 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/mro/apps/hidtmgen/hidtmgen.cpp
+++ isis/src/mro/apps/hidtmgen/hidtmgen.cpp
@@ -1,6 +1,7 @@
 #include "Isis.h"
 
 #include <QDir>
+#include <QString>
 
 #include "Cube.h"
 #include "FileName.h"
@@ -29,9 +30,9 @@ char findScaleLetter(double scale);
 
 // Main
 void IsisMain() {
-  UserInterface & ui = Application::GetUserInterface();
+  UserInterface &ui = Application::GetUserInterface();
   ProcessExportPds p;
-  Cube * inCube = p.SetInputCube("DTM");
+  Cube *inCube = p.SetInputCube("DTM");
 
   // Verify HiRISE
   if (inCube->bandCount() > 1 ||
@@ -45,19 +46,19 @@ void IsisMain() {
   double max = DBL_MAX;
 
   // Setup output type for DTM, default 32 bit
-  if(ui.GetString("DTMBITTYPE") == "8BIT") {
+  if (ui.GetString("DTMBITTYPE") == "8BIT") {
     p.SetOutputType(Isis::UnsignedByte);
     min = 0.0;
     max = 255.0;
     setRangeAndPixels(ui, p, min, max, BOTH);
   }
-  else if(ui.GetString("DTMBITTYPE") == "S16BIT") {
+  else if (ui.GetString("DTMBITTYPE") == "S16BIT") {
     p.SetOutputType(Isis::SignedWord);
     min = -32768.0;
     max = 32767.0;
     setRangeAndPixels(ui, p, min, max, NEG);
   }
-  else if(ui.GetString("DTMBITTYPE") == "U16BIT") {
+  else if (ui.GetString("DTMBITTYPE") == "U16BIT") {
     p.SetOutputType(Isis::UnsignedWord);
     min = 0.0;
     max = 65535.0;
@@ -74,15 +75,17 @@ void IsisMain() {
   }
 
   // Set Endian for all output
-  if(ui.GetString("ENDIAN") == "MSB")
+  if (ui.GetString("ENDIAN") == "MSB") {
     p.SetOutputEndian(Isis::Msb);
-  else if(ui.GetString("ENDIAN") == "LSB")
+  }
+  else if (ui.GetString("ENDIAN") == "LSB") {
     p.SetOutputEndian(Isis::Lsb);
+  }
 
   p.SetExportType(ProcessExportPds::Fixed);
 
   // These are our output labels, will be modifying heavily
-  Pvl & pdsLabel = p.StandardPdsLabel(ProcessExportPds::Image);
+  Pvl &pdsLabel = p.StandardPdsLabel(ProcessExportPds::Image);
 
   double northAzimuth = 0.0;
   double newRadius = 0.0;
@@ -90,7 +93,7 @@ void IsisMain() {
 
   // Do not use this after calling AddObject on pdsLabel, there is a note
   // at that location.
-  PvlObject & mapping = pdsLabel.findObject("IMAGE_MAP_PROJECTION");
+  PvlObject &mapping = pdsLabel.findObject("IMAGE_MAP_PROJECTION");
 
   // Set data for VIEWING_PARAMETERS keyword
   // Also set radius values
@@ -100,7 +103,7 @@ void IsisMain() {
     isEquirectangular = true;
 
     // Get projection and use radius for new radius
-    TProjection * proj = (TProjection *) ProjectionFactory::CreateFromCube(*inCube);
+    Projection *proj = ProjectionFactory::CreateFromCube(*inCube);
     newRadius = proj->LocalRadius((double)mapping["CENTER_LATITUDE"]);
 
     // Convert radius to KM
@@ -150,14 +153,14 @@ void IsisMain() {
 
   // Create and then add object, this will be used later in orthos as well.
   PvlObject viewingParameters("VIEWING_PARAMETERS");
-  viewingParameters.addKeyword(PvlKeyword("NORTH_AZIMUTH",toString(northAzimuth),"DEG"));
+  viewingParameters.addKeyword(PvlKeyword("NORTH_AZIMUTH", toString(northAzimuth), "DEG"));
 
   pdsLabel.addObject(viewingParameters);
 
   // This is our replacement for mapping
-  PvlObject & projection = pdsLabel.findObject("IMAGE_MAP_PROJECTION");
+  PvlObject &projection = pdsLabel.findObject("IMAGE_MAP_PROJECTION");
   projection.addKeyword(
-      PvlKeyword("^DATA_SET_MAP_PROJECTION","DSMAP.CAT"));
+      PvlKeyword("^DATA_SET_MAP_PROJECTION", "DSMAP.CAT"));
 
   // Delete unneeded keywords in object
   projection.deleteKeyword("FIRST_STANDARD_PARALLEL");
@@ -175,19 +178,19 @@ void IsisMain() {
   Pvl format;
 
   // Have to do things this way to get an array of values
-  PvlKeyword validMin("VALID_MINIMUM","REAL");
+  PvlKeyword validMin("VALID_MINIMUM", "REAL");
   validMin += "2";
   format.addKeyword(validMin);
-  PvlKeyword validMax("VALID_MAXIMUM","REAL");
+  PvlKeyword validMax("VALID_MAXIMUM", "REAL");
   validMax += "2";
   format.addKeyword(validMax);
 
-  format.addKeyword(PvlKeyword("SAMPLE_BIT_MASK","BINARY"));
+  format.addKeyword(PvlKeyword("SAMPLE_BIT_MASK", "BINARY"));
   // Aiming for MISSING_CONSTANT = (HEX, 4); Two separate items.
-  PvlKeyword mc("MISSING_CONSTANT","HEX");
+  PvlKeyword mc("MISSING_CONSTANT", "HEX");
   mc += "4";
   format.addKeyword(mc);
-  PvlFormat * form = pdsLabel.format();
+  PvlFormat *form = pdsLabel.format();
   form->add(format);
   pdsLabel.setFormat(form);
 
@@ -200,10 +203,10 @@ void IsisMain() {
   pdsLabel.findObject("IMAGE").addKeyword(PvlKeyword("NOTE", note));
 
   // Four labels that are pretty set in stone
-  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_NAME","MARS RECONNAISSANCE ORBITER"));
-  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_ID","MRO"));
-  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_NAME","HIGH RESOLUTION IMAGING SCIENCE EXPERIMENT"));
-  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_ID","HIRISE"));
+  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_NAME", "MARS RECONNAISSANCE ORBITER"));
+  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_ID", "MRO"));
+  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_NAME", "HIGH RESOLUTION IMAGING SCIENCE EXPERIMENT"));
+  pdsLabel.addKeyword(PvlKeyword("INSTRUMENT_ID", "HIRISE"));
 
   Pvl inputParameters(ui.GetFileName("PARAMSPVL"));
 
@@ -212,8 +215,9 @@ void IsisMain() {
   pdsLabel.addKeyword(inputParameters["DATA_SET_NAME"]);
   pdsLabel.addKeyword(inputParameters["PRODUCER_INSTITUTION_NAME"]);
   pdsLabel.addKeyword(inputParameters["PRODUCER_ID"]);
+
   if (ui.WasEntered("PRODUCER_FULL_NAME")) {
-    pdsLabel.addKeyword(PvlKeyword("PRODUCER_FULL_NAME",ui.GetString("PRODUCER_FULL_NAME")));
+    pdsLabel.addKeyword(PvlKeyword("PRODUCER_FULL_NAME", ui.GetString("PRODUCER_FULL_NAME")));
   }
   else {
     pdsLabel.addKeyword(inputParameters["PRODUCER_FULL_NAME"]);
@@ -226,10 +230,10 @@ void IsisMain() {
 
   QString vers; // Version string, will be used in orthos
   if (!defaultNames) {
-    IString pId = ui.GetFileName("DTMTO");
-    pId.substr(0, pId.find_first_of('.')+1);
-    pdsLabel.addKeyword(PvlKeyword("PRODUCT_ID",ui.GetString("DTM_PRODUCT_ID")));
-    pdsLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID",ui.GetString("SOURCE_PRODUCT_ID")));
+    QString pId = ui.GetFileName("DTMTO");
+    pId.left(pId.indexOf( QChar('.')) + 1 );
+    pdsLabel.addKeyword(PvlKeyword("PRODUCT_ID", ui.GetString("DTM_PRODUCT_ID")));
+    pdsLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID", ui.GetString("SOURCE_PRODUCT_ID")));
   }
   else {
     // Set projection letter
@@ -240,75 +244,91 @@ void IsisMain() {
     pId += findScaleLetter(projection.findKeyword("MAP_SCALE"));
 
     // Product ids of orthos
-    pId += ortho1.baseName().mid(3,12);
-    pId += ortho3.baseName().mid(3,12);
+    pId += ortho1.baseName().mid(3, 12);
+    pId += ortho3.baseName().mid(3, 12);
     QString producing = inputParameters["PRODUCING_INSTITUTION"][0];
     if (producing.size() > 1) {
-      QString msg = "PRODUCTING_INSTITUTION value [" + producing + "] must be a single character";
+      QString msg = "PRODUCING_INSTITUTION value [" + producing + "] must be a single character";
       throw IException(IException::User, msg, _FILEINFO_);
     }
     pId += "_" + producing;
 
-    double version = ui.GetDouble("PRODUCT_VERSION_ID");
+    double version = 0.0;
+    if (ui.WasEntered("PRODUCT_VERSION_ID")) {
+      version = ui.GetDouble("PRODUCT_VERSION_ID");
+    }
+    else {
+      version = toDouble(inputParameters["PRODUCT_VERSION_ID"]);
+    }
 
     // Format the version for the output name:
     // Only important thing to note is that a #.0 number is converted to 0#
-    // for the name, otherwise, its is predictable, always 2 greatest numbers:
+    // for the name, otherwise, it is predictable, always 2 greatest numbers:
     // ##.
     // >10, takes first two digits
     // The number found here is used in ortho images as well.
     if (version >= 10.0) {
-      vers = IString(IString(version).substr(0,2)).ToQt();
+      vers = toString(version).left(2);
     }
     else if (version >= 1.0) {
-      vers = IString(version).ToQt();
+      vers = toString(version);
       bool wasInt = false;
       // Checking for integer values, if so, make #.0 into 0#
       // necessary because in DTMgen version 1.0 corresponded to a 01 in names.
       if (vers.size() == 3) {
         if (vers.at(2) == '0') {
-          vers = "0" + IString(vers.mid(0,1)).ToQt();
+          vers = "0" + vers.left(1);
           wasInt = true;
         }
       }
       // Wasn't int, make #.# into ##
       if (!wasInt) {
-        vers = IString(IString(version).Remove(".")).ToQt();
-        if (vers.size() > 2) vers = vers.mid(0,2);
+        vers = toString(version).remove(QChar('.'));
+        if (vers.size() > 2) {
+          vers = vers.left(2);
+        }
       }
     }
     // 0 - <1, if 0.#, is 0#, is 0.#####, is first two ##
     else if (version >= 0.001) { // Any less and we get E... not dealing with that.
-      vers = IString(IString(version).Remove(".")).ToQt();
+      vers = toString(version).remove(QChar('.'));
       int nonZero = vers.lastIndexOf('0');
-      if (vers.size() > 2) vers = vers.mid(nonZero-1,2);
+      if (vers.size() > 2) vers = vers.mid(nonZero-1, 2);
     }
     // It was negative, or something else crazy?
     else {
-      string msg = "Version number [" + IString(version) + "] is invalid";
+      QString msg = "Version number [" + toString(version) + "] is invalid";
       throw IException(IException::User, msg, _FILEINFO_);
     }
 
     // Finish off name string
     pId += vers;
-    pdsLabel.addKeyword(PvlKeyword("PRODUCT_ID",pId));
+    pdsLabel.addKeyword(PvlKeyword("PRODUCT_ID", pId));
 
     // Source product ids
-    QString orthoS1 = ortho1.baseName().mid(0,15);
-    QString orthoS3 = ortho3.baseName().mid(0,15);
-    QString sourceId = "("+orthoS1+","+orthoS3+")"; // (######_####,######_####)
-    pdsLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID",sourceId));
+    QString orthoS1 = ortho1.baseName().left(15);
+    QString orthoS3 = ortho3.baseName().left(15);
+    PvlKeyword sourceProductId("SOURCE_PRODUCT_ID");
+    sourceProductId.addValue(orthoS1);
+    sourceProductId.addValue(orthoS3);
+    pdsLabel.addKeyword(sourceProductId);
   } // End scope of defaultNames true
 
-  pdsLabel.addKeyword(PvlKeyword("PRODUCT_VERSION_ID",ui.GetAsString("PRODUCT_VERSION_ID")));
-  pdsLabel.addKeyword(PvlKeyword("RATIONALE_DESC",ui.GetString("RATIONALE_DESC")));
+  if (ui.WasEntered("PRODUCT_VERSION_ID")) {
+    pdsLabel.addKeyword(PvlKeyword("PRODUCT_VERSION_ID", ui.GetAsString("PRODUCT_VERSION_ID")));
+  }
+  else {
+    pdsLabel.addKeyword(inputParameters["PRODUCT_VERSION_ID"]);
+  }
+
+  pdsLabel.addKeyword(PvlKeyword("RATIONALE_DESC", ui.GetString("RATIONALE_DESC")));
   pdsLabel.addKeyword(inputParameters["SOFTWARE_NAME"]);
 
   // Label records should always be 1, my example didn't included it, so we won't.
   pdsLabel.deleteKeyword("LABEL_RECORDS");
 
   // Delete or change unneeded keywords in image group
-  PvlObject & image = pdsLabel.findObject("IMAGE");
+  PvlObject &image = pdsLabel.findObject("IMAGE");
   image.findKeyword("CORE_NULL").setName("MISSING_CONSTANT");
   image.deleteKeyword("BAND_STORAGE_TYPE");
   image.deleteKeyword("CORE_LOW_REPR_SATURATION");
@@ -317,9 +337,9 @@ void IsisMain() {
   image.deleteKeyword("CORE_HIGH_INSTR_SATURATION");
 
   // Create statistics and add to image group
-  Statistics * stat = inCube->statistics();
-  image.addKeyword(PvlKeyword("VALID_MINIMUM",toString(stat->Minimum())));
-  image.addKeyword(PvlKeyword("VALID_MAXIMUM",toString(stat->Maximum())));
+  Statistics *stat = inCube->statistics();
+  image.addKeyword(PvlKeyword("VALID_MINIMUM", toString(stat->Minimum())));
+  image.addKeyword(PvlKeyword("VALID_MAXIMUM", toString(stat->Maximum())));
 
   // The output directory, will be used for ortho images as well
   FileName outDir = FileName(ui.GetString("OUTPUTDIR"));
@@ -357,19 +377,19 @@ void IsisMain() {
 
   p.ClearInputCubes();
 
-  if(ui.GetString("ORTHOBITTYPE") == "8BIT") {
+  if (ui.GetString("ORTHOBITTYPE") == "8BIT") {
     p.SetOutputType(Isis::UnsignedByte);
     min = 0.0;
     max = 255.0;
     setRangeAndPixels(ui, p, min, max, BOTH);
   }
-  else if(ui.GetString("ORTHOBITTYPE") == "S16BIT") {
+  else if (ui.GetString("ORTHOBITTYPE") == "S16BIT") {
     p.SetOutputType(Isis::SignedWord);
     min = -32768.0;
     max = 32767.0;
     setRangeAndPixels(ui, p, min, max, NEG);
   }
-  else if(ui.GetString("ORTHOBITTYPE") == "U16BIT") {
+  else if (ui.GetString("ORTHOBITTYPE") == "U16BIT") {
     p.SetOutputType(Isis::UnsignedWord);
     min = 0.0;
     max = 65535.0;
@@ -388,10 +408,10 @@ void IsisMain() {
   // Loop through 4 ortho images
   for (int i = 0; i < 4; /*4 = Number of ortho images*/ i++) {
     QString ortho("ORTHO" + toString(i + 1));
-    Cube * orthoInCube = p.SetInputCube(ortho);
+    Cube *orthoInCube = p.SetInputCube(ortho);
 
-    Pvl & orthoLabel = p.StandardPdsLabel(ProcessExportPds::Image);
-    PvlObject & orthoMap = orthoLabel.findObject("IMAGE_MAP_PROJECTION");
+    Pvl &orthoLabel = p.StandardPdsLabel(ProcessExportPds::Image);
+    PvlObject &orthoMap = orthoLabel.findObject("IMAGE_MAP_PROJECTION");
 
     QString orthoOut;
     if (!defaultNames) {
@@ -409,26 +429,26 @@ void IsisMain() {
       orthoOut += findScaleLetter(orthoMap.findKeyword("MAP_SCALE"));
 
       // continue building name
-      orthoOut += ortho1.baseName().mid(3,12);
-      orthoOut += ortho3.baseName().mid(3,12);
+      orthoOut += ortho1.baseName().mid(3, 12);
+      orthoOut += ortho3.baseName().mid(3, 12);
       QString producing = inputParameters["PRODUCING_INSTITUTION"][0];
       if (producing.size() > 1) {
-        QString msg = "PRODUCTING_INSTITUTION value [" + producing + "] must be a single character";
+        QString msg = "PRODUCING_INSTITUTION value [" + producing + "] must be a single character";
         throw IException(IException::User, msg, _FILEINFO_);
       }
       orthoOut += "_" + producing;
 
       // Found the version above
       orthoOut += vers;
-      orthoLabel.addKeyword(PvlKeyword("PRODUCT_ID",orthoOut));
+      orthoLabel.addKeyword(PvlKeyword("PRODUCT_ID", orthoOut));
     }
 
     orthoMap.addKeyword(
-        PvlKeyword("^DATA_SET_MAP_PROJECTION","DSMAP.CAT"));
+        PvlKeyword("^DATA_SET_MAP_PROJECTION", "DSMAP.CAT"));
 
     if (isEquirectangular) {
       // Get projection and use radius for new radius
-      TProjection * proj = (TProjection *) ProjectionFactory::CreateFromCube(*orthoInCube);
+      TProjection *proj = (TProjection *) ProjectionFactory::CreateFromCube(*orthoInCube);
       newRadius = proj->LocalRadius((double)orthoMap["CENTER_LATITUDE"]);
 
       // Convert radius to KM
@@ -453,21 +473,23 @@ void IsisMain() {
     }
 
     if (!defaultNames) {
-      orthoLabel.addKeyword(PvlKeyword("PRODUCT_ID",ui.GetString(ortho+"_PRODUCT_ID")));
-      orthoLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID",ui.GetString("SOURCE_PRODUCT_ID")));
+      orthoLabel.addKeyword(PvlKeyword("PRODUCT_ID", ui.GetString(ortho+"_PRODUCT_ID")));
+      orthoLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID", ui.GetString("SOURCE_PRODUCT_ID")));
     }
     else {
-      QString orthoS1 = ortho1.baseName().mid(0,15);
-      QString orthoS3 = ortho3.baseName().mid(0,15);
-      QString sourceId = "("+orthoS1+","+orthoS3+")"; // (######_####,######_####)
-      orthoLabel.addKeyword(PvlKeyword("SOURCE_PRODUCT_ID",sourceId));
+      QString orthoS1 = ortho1.baseName().left(15);
+      QString orthoS3 = ortho3.baseName().left(15);
+      PvlKeyword sourceProductId("SOURCE_PRODUCT_ID");
+      sourceProductId.addValue(orthoS1);
+      sourceProductId.addValue(orthoS3);
+      orthoLabel.addKeyword(sourceProductId);
     }
 
     // Four labels that are pretty set in stone
-    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_NAME","MARS RECONNAISSANCE ORBITER"));
-    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_ID","MRO"));
-    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_NAME","HIGH RESOLUTION IMAGING SCIENCE EXPERIMENT"));
-    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_ID","HIRISE"));
+    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_NAME", "MARS RECONNAISSANCE ORBITER"));
+    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_HOST_ID", "MRO"));
+    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_NAME", "HIGH RESOLUTION IMAGING SCIENCE EXPERIMENT"));
+    orthoLabel.addKeyword(PvlKeyword("INSTRUMENT_ID", "HIRISE"));
 
     // These come from the user, mostly, and are in the order they appear in the output
     // labels.
@@ -477,13 +499,20 @@ void IsisMain() {
     orthoLabel.addKeyword(inputParameters["PRODUCER_ID"]);
 
     if (ui.WasEntered("PRODUCER_FULL_NAME")) {
-      orthoLabel.addKeyword(PvlKeyword("PRODUCER_FULL_NAME",ui.GetString("PRODUCER_FULL_NAME")));
+      orthoLabel.addKeyword(PvlKeyword("PRODUCER_FULL_NAME", ui.GetString("PRODUCER_FULL_NAME")));
     }
     else {
       orthoLabel.addKeyword(inputParameters["PRODUCER_FULL_NAME"]);
     }
-    orthoLabel.addKeyword(PvlKeyword("PRODUCT_VERSION_ID",ui.GetAsString("PRODUCT_VERSION_ID")));
-    orthoLabel.addKeyword(PvlKeyword("RATIONALE_DESC",ui.GetString("RATIONALE_DESC")));
+
+    if (ui.WasEntered("PRODUCT_VERSION_ID")) {
+      orthoLabel.addKeyword(PvlKeyword("PRODUCT_VERSION_ID", ui.GetAsString("PRODUCT_VERSION_ID")));
+    }
+    else {
+      orthoLabel.addKeyword(inputParameters["PRODUCT_VERSION_ID"]);
+    }
+
+    orthoLabel.addKeyword(PvlKeyword("RATIONALE_DESC", ui.GetString("RATIONALE_DESC")));
     orthoLabel.addKeyword(inputParameters["SOFTWARE_NAME"]);
 
     // Long ago we made a VIEWING_PARAMETERS object, it still is correct
@@ -546,37 +575,37 @@ char findScaleLetter(double scale) {
 
 //Sets up special pixels and valid pixel ranges
 void setRangeAndPixels(UserInterface &ui, ProcessExportPds &p, double &min, double &max, Pixtype ptype) {
-  if(ptype == NEG) {
-    if(ui.GetBoolean("NULL")) {
+  if (ptype == NEG) {
+    if (ui.GetBoolean("NULL")) {
       p.SetOutputNull(min++);
     }
-    if(ui.GetBoolean("LRS")) {
+    if (ui.GetBoolean("LRS")) {
       p.SetOutputLrs(min++);
     }
-    if(ui.GetBoolean("LIS")) {
+    if (ui.GetBoolean("LIS")) {
       p.SetOutputLis(min++);
     }
-    if(ui.GetBoolean("HIS")) {
+    if (ui.GetBoolean("HIS")) {
       p.SetOutputHis(min++);
     }
-    if(ui.GetBoolean("HRS")) {
+    if (ui.GetBoolean("HRS")) {
       p.SetOutputHrs(min++);
     }
   }
-  else if(ptype == BOTH) {
-    if(ui.GetBoolean("NULL")) {
+  else if (ptype == BOTH) {
+    if (ui.GetBoolean("NULL")) {
       p.SetOutputNull(min++);
     }
-    if(ui.GetBoolean("LRS")) {
+    if (ui.GetBoolean("LRS")) {
       p.SetOutputLrs(min++);
     }
-    if(ui.GetBoolean("LIS")) {
+    if (ui.GetBoolean("LIS")) {
       p.SetOutputLis(min++);
     }
-    if(ui.GetBoolean("HRS")) {
+    if (ui.GetBoolean("HRS")) {
       p.SetOutputHrs(max--);
     }
-    if(ui.GetBoolean("HIS")) {
+    if (ui.GetBoolean("HIS")) {
       p.SetOutputHis(max--);
     }
   }
