783d44916179c5aa17a3f2be6d1db8a7c0567ab7
Janet Barrett <jbarrett@usgs.gov>
2013-12-12 16:27:39 +0000

    A new program, mimap2isis, was created to import Kaguya MI MAP file into ISIS3
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@5629 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/kaguya/apps/mimap2isis/mimap2isis.cpp
@@ -0,0 +1,90 @@
+#include "Isis.h"
+#include "ProcessImportPds.h"
+
+#include "UserInterface.h"
+#include "FileName.h"
+
+#include <QFile>
+
+using namespace std;
+using namespace Isis;
+
+void IsisMain() {
+  ProcessImportPds p, ptmp;
+  Pvl label;
+  UserInterface &ui = Application::GetUserInterface();
+
+  QString labelFile = ui.GetFileName("FROM");
+  QString imageFile("");
+  if(ui.WasEntered("IMAGE")) {
+    imageFile = ui.GetFileName("IMAGE");
+  }
+
+  // The Kaguya MI MAP files have an incorrect SAMPLE_PROJECTION_OFFSET
+  // keyword value in their labels. The following code creates a temporary
+  // detached PDS label with the correct (negated) keyword value.
+  ptmp.SetPdsFile(labelFile, imageFile, label);
+  PvlObject obj = label.findObject("IMAGE_MAP_PROJECTION");
+  double soff = toDouble(obj.findKeyword("SAMPLE_PROJECTION_OFFSET")[0]);
+  soff = -soff;
+  label.findObject("IMAGE_MAP_PROJECTION").addKeyword(PvlKeyword("SAMPLE_PROJECTION_OFFSET",toString(soff)),Pvl::Replace);
+  FileName tempFileName = FileName::createTempFile("TEMPORARYlabel.pvl").name();
+  QString fn(tempFileName.expanded());
+  label.write(fn);
+  p.SetPdsFile(fn, labelFile, label);
+  QFile::remove(fn);
+
+  Cube *ocube = p.SetOutputCube("TO");
+
+  // Get user entered special pixel ranges
+  if(ui.GetBoolean("SETNULLRANGE")) {
+    p.SetNull(ui.GetDouble("NULLMIN"), ui.GetDouble("NULLMAX"));
+  }
+  if(ui.GetBoolean("SETHRSRANGE")) {
+    p.SetHRS(ui.GetDouble("HRSMIN"), ui.GetDouble("HRSMAX"));
+  }
+  if(ui.GetBoolean("SETHISRANGE")) {
+    p.SetHIS(ui.GetDouble("HISMIN"), ui.GetDouble("HISMAX"));
+  }
+  if(ui.GetBoolean("SETLRSRANGE")) {
+    p.SetLRS(ui.GetDouble("LRSMIN"), ui.GetDouble("LRSMAX"));
+  }
+  if(ui.GetBoolean("SETLISRANGE")) {
+    p.SetLIS(ui.GetDouble("LISMIN"), ui.GetDouble("LISMAX"));
+  }
+
+  // Export the cube
+  p.StartProcess();
+
+  // Get as many of the other labels as we can
+  Pvl otherLabels;
+  p.TranslatePdsProjection(otherLabels);
+  if(p.IsIsis2()) {
+    p.TranslateIsis2Labels(otherLabels);
+  }
+  else {
+    p.TranslatePdsLabels(otherLabels);
+  }
+
+  if(otherLabels.hasGroup("Mapping") &&
+      (otherLabels.findGroup("Mapping").keywords() > 0)) {
+    ocube->putGroup(otherLabels.findGroup("Mapping"));
+  }
+  if(otherLabels.hasGroup("Instrument") &&
+      (otherLabels.findGroup("Instrument").keywords() > 0)) {
+    ocube->putGroup(otherLabels.findGroup("Instrument"));
+  }
+  if(otherLabels.hasGroup("BandBin") &&
+      (otherLabels.findGroup("BandBin").keywords() > 0)) {
+    ocube->putGroup(otherLabels.findGroup("BandBin"));
+  }
+  if(otherLabels.hasGroup("Archive") &&
+      (otherLabels.findGroup("Archive").keywords() > 0)) {
+    ocube->putGroup(otherLabels.findGroup("Archive"));
+  }
+
+  p.EndProcess();
+
+  return;
+}
+
