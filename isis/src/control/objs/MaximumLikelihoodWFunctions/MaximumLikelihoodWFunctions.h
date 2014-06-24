8c651b5a99c4f59c294c22e8fe7e5b667fa780ce
Jeannie Backer <jwbacker@usgs.gov>
2014-06-24 01:28:08 +0000

    ISIS coding standards fixes.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/branches/ipce@5872 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/control/objs/MaximumLikelihoodWFunctions/MaximumLikelihoodWFunctions.h
+++ isis/src/control/objs/MaximumLikelihoodWFunctions/MaximumLikelihoodWFunctions.h
@@ -26,15 +26,19 @@
 
 namespace Isis {
   /**
-   * @brief Class provides maximum likelihood estimation functions for robust parameter estimation e.g. in bundle adjustment.
+   * @brief Class provides maximum likelihood estimation functions for robust parameter estimation, 
+   *        e.g. in bundle adjustment.
    *
-   * A maximum likelihood estimation W function provides a scheme for 're-weighting' observations so that measures with
-   * large residuals have reduced or negligible effect on the solution.  There are many such functions available, a few
-   * have been programmed into this class.  See enum Model documentation for specifics of the estimation models.
-   * References:  Zhangs,  "Parameter Estimation: A Tutorial with Application to Conic Fitting"
-   *              Koch, "Parameter Estimation and Hypothesis Testing in Linear Systems" 2nd edition chapter 3.8
-   *              Manual of Photogrammetry 5th edition chapter 2.2 (particularly 2.2.6)
-   *              Chen "Robust Regression with Projection Based M-estimators"
+   * A maximum likelihood estimation W function provides a scheme for 're-weighting' observations so 
+   * that measures with large residuals have reduced or negligible effect on the solution.  There 
+   * are many such functions available, a few have been programmed into this class.  See enum Model 
+   * documentation for specifics of the estimation models. 
+   *  
+   * References:  Zhangs,  "Parameter Estimation: A Tutorial with Application to Conic Fitting" 
+   *              Koch, "Parameter Estimation and Hypothesis Testing in Linear Systems"
+   *                    2nd edition, chapter 3.8
+   *              Manual of Photogrammetry, 5th edition, chapter 2.2 (particularly 2.2.6)
+   *              Chen, "Robust Regression with Projection Based M-estimators"
    *
    * @ingroup ControlNetwork
    *
@@ -43,6 +47,8 @@ namespace Isis {
    *
    * @internal
    *   @history 2012-03-23 Orrin Thomas -- Original Version
+   *   @history 2014-06-23 Jeannie Backer - Moved method implementation to cpp file and other ISIS
+   *                           coding standards fixes.
    */
   class MaximumLikelihoodWFunctions {
   public:
@@ -52,57 +58,67 @@ namespace Isis {
      */
     enum Model {
 
-      /**  According to Zhang (Parameter Estimation: A Tutorial with application to conic fitting) "[Huber's]
-       *    estimator is so satisfactory this is has been recommended for almost all situations; very
-       *    rarely has it been found to be inferior to some other function."  Its one deficiency is the
-       *    discontinuous second derivative which cause rare diffeculites.  No measures are totally disregarded.
+      /**  According to Zhang (Parameter Estimation: A Tutorial with application to conic fitting)
+       *   "[Huber's] estimator is so satisfactory this is has been recommended for almost all
+       *   situations; very rarely has it been found to be inferior to some other function."  Its
+       *   one deficiency is the discontinuous second derivative which cause rare diffeculites.
+       *   No measures are totally disregarded.
        *    http://research.microsoft.com/en-us/um/people/zhang/Papers/ZhangIVC-97-01.pdf
        */
       Huber,     
      
-      /** A modification to Huber's method propsed by William J.J. Rey in Introduction to Robust and Quasi-Robust 
-       *    Statistical Methods. Springer, Berlin, Heidelberg, 1983.  It has similiar properties to the Huber,
-       *    but with a continuous second derivative.  This comes at the cost of being somewhat more computationally
-       *    expernsive.  No measures are totally disregarded.
+      /** A modification to Huber's method propsed by William J.J. Rey in Introduction to Robust
+       *  and Quasi-Robust Statistical Methods. Springer, Berlin, Heidelberg, 1983.  It has similiar
+       *  properties to the Huber, but with a continuous second derivative.  This comes at the cost
+       *  of being somewhat more computationally expernsive.  No measures are totally disregarded.
        *    http://research.microsoft.com/en-us/um/people/zhang/Papers/ZhangIVC-97-01.pdf
        */
       HuberModified,  
 
-      /** The Welsch method aggresively discounts measures with large resiudals.  Residuals two times greater
-       *    than the tweaking constant are all but ignored.  This method can be risky to use (at least at first)
-       *    because it does not gaurantee a unique solution.  And if sufficient measures are effectively 
-       *    'removed' by the weighting, the system can become singular.  The manual of photogrammetry recommended 
-       *    using it for clean up after convergeance or near convergeance had been optained with a more stable
-       *    method (such as Huber's).
+      /** The Welsch method aggresively discounts measures with large resiudals.  Residuals two
+       *  times greater than the tweaking constant are all but ignored.  This method can be risky to
+       *  use (at least at first) because it does not gaurantee a unique solution.  And if
+       *  sufficient measures are effectively 'removed' by the weighting, the system can become
+       *  singular.  The manual of photogrammetry recommended using it for clean up after
+       *  convergeance or near convergence had been optained with a more stable method
+       *  (such as Huber's).
        *    http://research.microsoft.com/en-us/um/people/zhang/Papers/ZhangIVC-97-01.pdf
        */
       Welsch,         
 
-      /** The Chen method was found in "Robust Regression with Projection Based M-estimators"  Chen, et. al.,  though
-       *    Chen does not take credit as the author.  It was of interest because he seemed to present its use as 
-       *    expected in systems with large numbers of outliers, and because of it's unique properties.  It is
-       *    exceptionally aggresive.  Residuals less than the tweaking constant generally have MORE influence than 
-       *    in standard least squares (or any other estimation function I've studied), and residuals larger than
-       *    the tweaking function are totaly discounted.
+      /** The Chen method was found in "Robust Regression with Projection Based M-estimators"  Chen,
+       *  et. al.,  though Chen does not take credit as the author.  It was of interest because he
+       *  seemed to present its use as expected in systems with large numbers of outliers, and
+       *  because of it's unique properties.  It is exceptionally aggresive.  Residuals less than
+       *  the tweaking constant generally have MORE influence than in standard least squares
+       *  (or any other estimation function I've studied), and residuals larger than the tweaking
+       *  function are totaly discounted.
        */
-      Chen            
+      Chen
     };
 
-    MaximumLikelihoodWFunctions() {this->setModel(Huber);}; //choose Model and define the tweaking constant
-    MaximumLikelihoodWFunctions(Model modelSelection, double tweakingConstant); //choose Model and define the tweaking constant
-    MaximumLikelihoodWFunctions(Model modelSelection);                          //choose Model and and use the default tweaking constant
-    ~MaximumLikelihoodWFunctions() {}; //empty destructor
-  
-    bool setModel(Model modelSelection, double tweakingConstant); //choose Model and define the tweaking constant
-    bool setModel(Model modelSelection);  //choose Model and use default tweaking constant
-    bool setTweakingConstant(double tweakingConstant); //leave model type unaltered and change tweaking constant
-    bool setTweakingConstantDefault();  //reset tweaking constant to the default for the current model
+    MaximumLikelihoodWFunctions();
+    MaximumLikelihoodWFunctions(Model modelSelection, double tweakingConstant);
+    MaximumLikelihoodWFunctions(Model modelSelection);
+    ~MaximumLikelihoodWFunctions();
 
-    //the W functions provide an additional weighting factor W which is used to 're-weight' each observation dynamically during an adjustment, the scalar functions provide access to various flavors of this scalar (as a function of the residual divided by the residuals sigma)
-    double sqrtWeightScaler(double residualZScore); //it is often convient to use square roots of weights when building normals, this function provides the scaler for the square root of the weight directly
-  
-    double tweakingConstantQuantile();  //returns which quantile of the residuals is recommended to use as the tweaking constant, this varies as a function of the model being employed
+    bool setModel(Model modelSelection, double tweakingConstant);
+    bool setModel(Model modelSelection);
+    bool setTweakingConstant(double tweakingConstant);
+    bool setTweakingConstantDefault();
 
+    // the W functions provide an additional weighting factor W which is used
+    // to 're-weight' each observation dynamically during an adjustment, the
+    // scalar functions provide access to various flavors of this scalar (as
+    // a function of the residual divided by the residuals sigma)
+
+    double sqrtWeightScaler(double residualZScore); //it is often convient to use square roots of 
+                                                    //weights when building normals, this function
+                                                    // provides the scaler for the square root of
+                                                    // the weight directly
+    double tweakingConstantQuantile(); // returns which quantile of the residuals is recommended to
+                                       // use as the tweaking constant, this varies as a function of
+                                       // the model being employed
     double tweakingConstant();
 
     void maximumLikelihoodModel(char *model);
@@ -110,6 +126,7 @@ namespace Isis {
     void weightedResidualCutoff(char *cutoff);
 
   private:
+
     /** Tweaking constant for the maximum likelihood estimation function
       *   Suggested values are available for each method: setTweakingConstantDefault()
       *   They can also be manually adjusted: setTweakingConstant(double tweakingConstant)
@@ -123,12 +140,12 @@ namespace Isis {
     double m_PI;   //PI
 
     /** Which maximum likelihood model is being used
-     */   
+     */
     Model m_model; //selected maximum likelihood estimation model
 
-
-    double weightScaler(double residualZScore);     //This directly provides the scaler for the weight (instead of the radical weight), thus it provides sqrtWeightScaler^2
-
+    double weightScaler(double residualZScore); // This directly provides the scaler for the weight
+                                                // (instead of the radical weight), thus it provides
+                                                // sqrtWeightScaler^2
     double huber(double residualZScore);
     double huberModified(double residualZScore);
     double welsch(double residualZScore);
