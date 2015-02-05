1e608d149b6ca6c4d9bc77c48427caa1ba20349a
Moses Milazzo <moses@usgs.gov>
2015-02-05 18:29:14 +0000

    Updated version numbers within mdiscal. This resolves #02179.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@6042 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/messenger/apps/mdiscal/mdiscal.cpp
+++ isis/src/messenger/apps/mdiscal/mdiscal.cpp
@@ -67,12 +67,12 @@ void Calibrate(vector<Buffer *>& in, vector<Buffer *>& out);
 void IsisMain() {
 
   const QString mdiscal_program = "mdiscal";
-  const QString mdiscal_version = "1.4";
+  const QString mdiscal_version = "1.5";
   const QString mdiscal_revision = "$Revision$";
   QString mdiscal_runtime = Application::DateTime();
 
   // Specify the version of the CDR generated
-  const int cdr_version = 4;
+  const int cdr_version = 5;
 
   // We will be processing by column in case of a linear dark current fit. This will make the
   //   calibration a one pass system in this case, rather than two.
