55dabf2f04d0507399abae5af38bd2b45246f619
Tyler Wilson <tjwilson@usgs.gov>
2015-09-01 23:50:59 +0000

    Corrected history entry for KaguyaMiCameraDistortionMap ref #2283
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@6338 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/kaguya/objs/KaguyaMiCamera/KaguyaMiCameraDistortionMap.h
+++ isis/src/kaguya/objs/KaguyaMiCamera/KaguyaMiCameraDistortionMap.h
@@ -1,8 +1,8 @@
 #ifndef KaguyaMiCameraDistortionMap_h
 #define KaguyaMiCameraDistortionMap_h
-/** 
- * @file 
- *  
+/**
+ * @file
+ *
  *   Unless noted otherwise, the portions of Isis written by the USGS are public
  *   domain. See individual third-party library and package descriptions for
  *   intellectual property information,user agreements, and related information.
@@ -25,7 +25,7 @@
 
 namespace Isis {
 
-  /** 
+  /**
    *  Distort/undistort focal plane coordinates
    *
    * Creates a map for adding/removing optical distortions
@@ -38,9 +38,11 @@ namespace Isis {
    *
    * @author 2012-06-20 Orrin Thomas
    * @internal
-   *   @history 2020-06-20 Orrin Thomas - Original Version
-   *   @history 2012-07-06 Debbie A. Cook, Updated Spice members to be more compliant with Isis 
+   *   @history 2012-06-20 Orrin Thomas - Original Version
+   *   @history 2012-07-06 Debbie A. Cook, Updated Spice members to be more compliant with Isis
    *                          coding standards. References #972.
+   *   @history 2012-08-24 Tyler J. Wilson, Changed the date of the first internal
+   *                          history entry to 2012-06-20 (originally 2020-06-20)
    */
   class KaguyaMiCameraDistortionMap : public CameraDistortionMap {
     public:
