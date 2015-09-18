6e4675c435f3b501a1f0d5f14f5b5c2aabb6323b
Tyler Wilson <tjwilson@usgs.gov>
2015-09-18 17:51:36 +0000

    Added a timeout event handler to edrget (ref#2289)
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@6369 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/edrget/ftpget.h
+++ isis/src/base/apps/edrget/ftpget.h
@@ -3,6 +3,7 @@
 
 #include <QFile>
 #include <QFtp>
+#include <QTimer>
 #include "Progress.h"
 
 class QUrl;
@@ -18,7 +19,7 @@ namespace Isis {
     public:
       FtpGet(QObject *parent = 0);
 
-      bool getFile(const QUrl &url, QString topath);
+      bool getFile(const QUrl &url, QString topath,int timeout);
 
       bool error() const {
         return p_error;
@@ -29,16 +30,29 @@ namespace Isis {
     signals:
       void done();
 
+      //tjw
+      void dataTransferProgress(qint64, qint64);
+
+
     private slots:
       void ftpDone(bool error);
       void ftpProgress(qint64 done, qint64 total);
 
+      //tjw
+      void ftpTimeout();
+
+
     private:
+
       QFtp p_ftp;
       QFile p_file;
       bool p_error;
       int p_lastDone;
       Progress p_progress;
+
+
+      int p_timeOut;
+      QTimer p_timer;
 
   };
 }
