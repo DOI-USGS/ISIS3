3d786bf3b0fa2271fa3543075df6a61183b33c3f
Tyler Wilson <tjwilson@usgs.gov>
2015-09-30 17:10:49 +0000

    isis2fits app no longer crashes with 'failed to convert string' error msg (Ref#2272)
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@6382 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/apps/isis2fits/isis2fits.cpp
+++ isis/src/base/apps/isis2fits/isis2fits.cpp
@@ -19,11 +19,12 @@ using namespace std;
 using namespace Isis;
 
 // Global variables
-int headerBytes = 0;
+
+int g_headerBytes = 0;
 
 QString FitsKeyword(QString keyword, bool isVal, QString value, QString unit = "");
 
-QString WritePvl(QString fitsKey, QString group, IString key, Cube *icube, bool isString);
+QString WritePvl(QString fitsKey, QString group, QString key, Cube *icube, bool isString);
 
 // Main program
 void IsisMain() {
@@ -34,16 +35,16 @@ void IsisMain() {
   Cube *icube = p.SetInputCube("FROM");
 
   // Conform to the Big-Endian format for FITS
-  if(IsLsb()) p.SetOutputEndian(Isis::Msb);
+  if (IsLsb()) p.SetOutputEndian(Isis::Msb);
 
   // Generate the name of the fits file and open it
   UserInterface &ui = Application::GetUserInterface();
 
   // specify the bits per pixel
   QString bitpix;
-  if(ui.GetString("BITTYPE") == "8BIT") bitpix = "8";
-  else if(ui.GetString("BITTYPE") == "16BIT") bitpix = "16";
-  else if(ui.GetString("BITTYPE") == "32BIT") bitpix = "-32";
+  if (ui.GetString("BITTYPE") == "8BIT") bitpix = "8";
+  else if (ui.GetString("BITTYPE") == "16BIT") bitpix = "16";
+  else if (ui.GetString("BITTYPE") == "32BIT") bitpix = "-32";
   else {
     QString msg = "Pixel type of [" + ui.GetString("BITTYPE") + "] is unsupported";
     throw IException(IException::User, msg, _FILEINFO_);
@@ -51,20 +52,20 @@ void IsisMain() {
 
   //  Determine bit size and calculate number of bytes to write
   //  for each line.
-  if(bitpix == "8") p.SetOutputType(Isis::UnsignedByte);
-  if(bitpix == "16") p.SetOutputType(Isis::SignedWord);
-  if(bitpix == "-32") p.SetOutputType(Isis::Real);
+  if (bitpix == "8") p.SetOutputType(Isis::UnsignedByte);
+  if (bitpix == "16") p.SetOutputType(Isis::SignedWord);
+  if (bitpix == "-32") p.SetOutputType(Isis::Real);
 
   // determine core base and multiplier, set up the stretch
   PvlGroup pix = icube->label()->findObject("IsisCube").findObject("Core").findGroup("Pixels");
   double scale = toDouble(pix["Multiplier"][0]);
   double base = toDouble(pix["Base"][0]);
 
-  if(ui.GetString("STRETCH") != "NONE" && bitpix != "-32") {
-    if(ui.GetString("STRETCH") == "LINEAR") {
+  if (ui.GetString("STRETCH") != "NONE" && bitpix != "-32") {
+    if (ui.GetString("STRETCH") == "LINEAR") {
       p.SetInputRange();
     }
-    else if(ui.GetString("STRETCH") == "MANUAL") {
+    else if (ui.GetString("STRETCH") == "MANUAL") {
       p.SetInputRange(ui.GetDouble("MINIMUM"), ui.GetDouble("MAXIMUM"));
     }
 
@@ -82,9 +83,7 @@ void IsisMain() {
   }
 
 
-  //////////////////////////////////////////
-  // Write the minimal fits header        //
-  //////////////////////////////////////////
+  // Write the minimal fits header   
   QString header;
 
   // specify that this file conforms to simple fits standard
@@ -96,7 +95,7 @@ void IsisMain() {
 
   // specify the number of data axes (2: samples by lines)
   int axes = 2;
-  if(icube->bandCount() > 1) {
+  if (icube->bandCount() > 1) {
     axes = 3;
   }
 
@@ -108,7 +107,7 @@ void IsisMain() {
   // specify the limit on data axis 2 (number of lines)
   header += FitsKeyword("NAXIS2", true, toString(icube->lineCount()));
 
-  if(axes == 3) {
+  if (axes == 3) {
     header += FitsKeyword("NAXIS3", true, toString(icube->bandCount()));
   }
 
@@ -117,23 +116,23 @@ void IsisMain() {
   header += FitsKeyword("BSCALE", true, toString(scale));
 
   // Sky and All cases
-  if(ui.GetString("INFO") == "SKY" || ui.GetString("INFO") == "ALL") {
-    IString msg = "cube has not been skymapped";
+  if (ui.GetString("INFO") == "SKY" || ui.GetString("INFO") == "ALL") {
+
+    //tjw:  IString msg changed to QString
+    QString msg = "cube has not been skymapped";
     PvlGroup map;
 
-    if(icube->hasGroup("mapping")) {
+    if (icube->hasGroup("mapping")) {
       map = icube->group("mapping");
       msg = (QString)map["targetname"];
     }
     // If we have sky we want it
-    if(msg == "Sky") {
+    if (msg == "Sky") {
       double midRa = 0, midDec = 0;
 
-      midRa = ((double)map["MaximumLongitude"] +
-               (double)map["MinimumLongitude"]) / 2;
+      midRa = ((double)map["MaximumLongitude"] + (double)map["MinimumLongitude"]) / 2;
 
-      midDec = ((double)map["MaximumLatitude"] +
-                (double)map["MinimumLatitude"]) / 2;
+      midDec = ((double)map["MaximumLatitude"] + (double)map["MinimumLatitude"]) / 2;
 
       header += FitsKeyword("OBJCTRA", true, toString(midRa));
 
@@ -142,26 +141,33 @@ void IsisMain() {
 
     }
 
-    if(ui.GetString("INFO") == "ALL") {
-      header += WritePvl("INSTRUME", "Instrument", "InstrumentId", icube, true);
-      header += WritePvl("OBSERVER", "Instrument", "SpacecraftName", icube, true);
-      header += WritePvl("OBJECT  ", "Instrument", "TargetName", icube, true);
+    //tjw:  All strings in this block  placed inside QString constructors
+    if (ui.GetString("INFO") == "ALL") {
+      header += WritePvl(QString("INSTRUME"), QString("Instrument"),
+                         QString("InstrumentId"), icube, true);
+      header += WritePvl(QString("OBSERVER"), QString("Instrument"),
+                         QString("SpacecraftName"), icube, true);
+      header += WritePvl(QString("OBJECT  "), QString("Instrument"),
+                         QString("TargetName"), icube, true);
       // StartTime is sometimes middle of the exposure and somtimes beginning,
       // so StopTime can't be calculated off of exposure reliably.
-      header += WritePvl("DATE-OBS", "Instrument", "StartTime", icube, true);
+      header += WritePvl(QString("DATE-OBS"), QString("Instrument"),
+                         QString("StartTime"), icube, true);
       // Some cameras don't have StopTime
-      if(icube->hasGroup("Instrument")) {
+      if (icube->hasGroup("Instrument")) {
         PvlGroup inst = icube->group("Instrument");
-        if(inst.hasKeyword("StopTime")) {
-          header += WritePvl("TIME_END", "Instrument", "StopTime", icube, true);
+        if (inst.hasKeyword("StopTime")) {
+          header += WritePvl(QString("TIME_END"), QString("Instrument"),
+                             QString("StopTime"), icube, true);
         }
-        if(inst.hasKeyword("ExposureDuration")) {
-          header += WritePvl("EXPTIME", "Instrument", "ExposureDuration", icube, false);
+        if (inst.hasKeyword("ExposureDuration")) {
+          header += WritePvl(QString("EXPTIME"), QString("Instrument"),
+                             QString("ExposureDuration"), icube, false);
         }
       }
     }
     // If we were set on SKY and Sky doesn't exist
-    else if(msg != "Sky") {
+    else if (msg != "Sky") {
       throw IException(IException::User, msg, _FILEINFO_);
     }
   }
@@ -171,13 +177,13 @@ void IsisMain() {
 
   // fill the rest of the fits header with space so to conform with the fits header
   // size of 2880 bytes
-  for(int i = header.length() % 2880 ; i < 2880 ; i++) header += " ";
+  for (int i = header.length() % 2880 ; i < 2880 ; i++) header += " ";
 
   // open the cube for writing
   QString to = ui.GetFileName("TO", "fits");
   ofstream fout;
   fout.open(to.toAscii().data(), ios::out | ios::binary);
-  if(!fout.is_open()) {
+  if (!fout.is_open()) {
     QString msg = "Cannot open fits output file";
     throw IException(IException::Programmer, msg, _FILEINFO_);
   }
@@ -190,7 +196,7 @@ void IsisMain() {
   // Finish off data area to a number n % 2880 == 0 is true
   // 2880 is the size of the data blocks
   int count = 2880 - (fout.tellp() % 2880);
-  for(int i = 0; i < count; i++) {
+  for (int i = 0; i < count; i++) {
     // Write nul characters as needed. ascii 0, hex 00...
     fout.write("\0", 1);
   }
@@ -200,45 +206,46 @@ void IsisMain() {
 
 QString FitsKeyword(QString key, bool isValue, QString value, QString unit) {
   // pad the keyword with space
-  for(int i = key.length() ; i < 8 ; i++)
+  for (int i = key.length() ; i < 8 ; i++)
     key += " ";
 
   // add the value indicator (or lack thereof)
-  if(isValue)
+  if (isValue)
     key += "= ";
   else
     key += "  ";
 
   // right-justify the value
-  if(value.length() < 70) {
+  if (value.length() < 70) {
     // pad the left part of the value with space
-    for(int i =  value.length() ; i < 20 ; i++)
+    for (int i =  value.length() ; i < 20 ; i++)
       key += " ";
 
     // add the actual value
     key += value;
-    if(isValue) {
+    if (isValue) {
       key += " / ";
-      if(unit != "") {
+      if (unit != "") {
         key += "[" + unit + "]";
       }
     }
 
     // finish the line by padding the rest of the space
-    for(int i =  key.length() ; i < 80 ; i++)
+    for (int i =  key.length() ; i < 80 ; i++)
       key += " ";
   }
 
   // record the total length of the header
-  headerBytes += key.length();
+  g_headerBytes += key.length();
   return key;
 }
 
-QString WritePvl(QString fitsKey, QString group, IString key, Cube *icube, bool isString) {
-  if(icube->hasGroup(group)) {
+//tjw:  IString key -> QString key
+QString WritePvl(QString fitsKey, QString group, QString key, Cube *icube, bool isString) {
+  if (icube->hasGroup(group)) {
     PvlGroup theGroup = icube->group(group);
     QString name = (QString)theGroup[key];
-    if(isString) {
+    if (isString) {
       name = "'" + name + "'";
     }
     QString unit = theGroup[key].unit();
