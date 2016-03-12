5200f8e5b2031829d9ff430a0fe4d8c32450cf66
Jeannie Backer <jwbacker@usgs.gov>
2016-03-12 06:40:54 +0000

    Added cnetcombinept program to the ISIS package. Fixes #3870
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@6631 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/control/apps/cnetcombinept/ControlPointCloudPt.h
@@ -0,0 +1,310 @@
+#ifndef ControlPointCloudPt_h
+#define ControlPointCloudPt_h
+/**
+ * @file
+ * $Revision: 1.0 $ 
+ * $Date: 2014/02/27 18:49:25 $ 
+ *
+ *   Unless noted otherwise, the portions of Isis written by the USGS are public
+ *   domain. See individual third-party library and package descriptions for
+ *   intellectual property information,user agreements, and related information.
+ *
+ *   Although Isis has been used by the USGS, no warranty, expressed or implied,
+ *   is made by the USGS as to the accuracy and functioning of such software
+ *   and related material nor shall the fact of distribution constitute any such
+ *   warranty, and no responsibility is assumed by the USGS in connection
+ *   therewith.
+ *
+ *   For additional information, launch
+ *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html in a browser or see
+ *   the Privacy &amp; Disclaimers page on the Isis website,
+ *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
+ *   http://www.usgs.gov/privacy.html.
+ */
+
+#include <cmath>
+
+#include <QExplicitlySharedDataPointer>
+#include <QSharedData>
+#include <QVector>
+#include <QtGlobal>
+
+#include "ControlPoint.h"
+#include "ControlMeasure.h"
+
+
+namespace Isis {
+
+/**
+ * @brief 3-D ControlPoint class for use in PointCloud datasets
+ *  
+ *  The ControlPoint container is required to not change its content for the
+ *  duration of use of the nanoflann kd-tree built from the points.
+ *  
+ * @author  2015-10-11 Kris Becker
+ *  
+ * @internal 
+ *   @history 2015-10-11 Kris Becker - Original Version 
+ */
+
+class ControlPointCloudPt {
+  public:
+    enum CoordinateType { Image, Ground };
+    enum Ownership { Shared,      // Indicates creator of ControlPoint retains
+                                  // ownership. take() provides a unique clone
+                                  // of ControlPoint.
+                     Exclusive    // Creator gives ownership to ControlPointCloudPt
+                                  // take() will give up pointer interally.
+                   };
+
+    ControlPointCloudPt() : m_xyz(), m_type(Image), m_serialno(),
+                            m_data( new ControlPointData() ),
+                            m_merged() {
+      m_data->getImageCoordinates(m_xyz); 
+    }
+
+    ControlPointCloudPt(ControlPoint *point, const CoordinateType &ptype,
+                        const Ownership owner,
+                        const QString &serialno = "", 
+                        const double &weight = 1.0) : 
+                        m_xyz( ), m_type(ptype), m_serialno(serialno),
+                        m_data(new ControlPointData(point, owner, weight)),
+                        m_merged() {
+      if ( Image == ptype ) {
+        m_data->setReference(serialno);
+        if ( !selectImageCoordinates() ) { 
+          m_data->disable(); 
+        }
+      }
+      else {  //  ( Ground == ptype )
+        if ( !selectGroundCoordinates() ) { 
+          m_data->disable(); 
+        }
+      }
+    }
+
+    virtual ~ControlPointCloudPt() { }
+
+    inline bool isValid() const {
+      return ( !m_data->isDisabled() );
+    }
+
+    inline void disable() {
+      return ( m_data->disable() );
+    }
+
+    inline int size() const {
+      return ( m_data->size() );
+    }
+
+    Ownership owner() const {
+      return ( m_data->m_owner );
+    }
+
+    ControlPoint *take() const {
+       return ( m_data->take() );
+    }
+
+    inline bool selectGroundCoordinates() {
+      m_type = Ground;
+      return ( m_data->getGroundCoordinates(m_xyz) );
+    }
+
+    inline bool selectImageCoordinates() {
+      m_type = Image;
+      return ( m_data->getImageCoordinates(m_xyz) );
+    }
+
+    inline CoordinateType getCoordinateType() const {
+      return ( m_type );
+    }
+
+    inline QString id() const {
+      return ( m_data->m_point->GetId() );
+    }
+
+    inline const ControlPoint *getPoint() const {
+      return ( m_data->m_point );
+    }
+
+    inline ControlPoint &getPoint() {
+      return ( *m_data->m_point );
+    }
+
+    inline QString getSerialNumber() const {
+      return ( m_serialno );
+    }
+
+    inline ControlMeasure *getMeasure(const QString &serialno) const {
+      if ( !isValid() ) { return (0); }
+      if (m_data->m_point->HasSerialNumber(serialno) ) {
+        return ( m_data->m_point->GetMeasure(serialno) );
+      }
+
+      return (0);
+    }
+
+    // Convenient retrieval coordinates
+    inline double x() const { return ( m_xyz[0] ); }
+    inline double y() const { return ( m_xyz[1] ); }
+    inline double z() const { return ( m_xyz[2] ); }
+    inline double w() const { return ( m_xyz[3] ); }
+    inline const double *array() const { return ( m_xyz ); }
+
+    inline bool operator==(const ControlPointCloudPt &other) const {
+      return ( m_data->m_point == other.m_data->m_point );
+    }
+
+    inline bool operator!=(const ControlPointCloudPt &other) const {
+      return ( m_data->m_point != other.m_data->m_point );
+    }
+
+  private:
+    /** 
+     *  Shared ControlPoint data pointer
+     * @author  2015-10-11 Kris Becker
+     *  
+     * @internal 
+     *   @history 2015-10-11 Kris Becker - Original Version 
+     */
+    class ControlPointData : public QSharedData {
+      typedef ControlPointCloudPt::Ownership Ownership;
+      public:
+        ControlPointData() : QSharedData(), m_point(0), m_reference(0), 
+                             m_weight(1.0), m_initial(0),
+                             m_owner(Exclusive){ }
+        ControlPointData(ControlPoint *point, const Ownership owner, 
+                         const double weight = 1.0) :
+                         QSharedData(), m_point(point), 
+                         m_reference(point->GetRefMeasure()), 
+                         m_weight(weight),
+                         m_initial(point->GetNumValidMeasures()), 
+                         m_owner(owner) { }
+        ControlPointData(const ControlPointData &other) : QSharedData(other),
+                         m_point(other.m_point),
+                         m_reference(other.m_reference),
+                         m_weight(other.m_weight),
+                         m_initial(other.m_initial),
+                         m_owner(Shared) { }
+        ~ControlPointData() { 
+          if ( Exclusive == m_owner ) {
+             delete (m_point);
+             m_point = 0;
+          }
+        }
+
+        inline int size() const {
+          if ( 0 == m_point ) { return (0); }
+          return ( m_point->GetNumValidMeasures() );
+        }
+
+        inline bool setReference(const QString &serialno) {
+          if ( 0 == m_point ) { return ( false); }
+          m_reference = 0;
+
+          if ( serialno.isEmpty() ) {
+            m_reference = m_point->GetRefMeasure();
+          }
+          else {
+            if ( m_point->HasSerialNumber(serialno) ) {
+              m_reference = m_point->GetMeasure(serialno);
+            }
+          }
+
+          return ( 0 != m_reference );
+        }
+
+        /** This approach allows use of 2 and 3 dimensional Euclidean
+         *  distances */
+        inline bool getImageCoordinates(double coords[4]) const {
+          (void) getNoPointData(coords);
+          if ( 0 == m_reference ) { return (false); }
+          if ( 0 == m_point ) { return ( false ); }
+
+          // ControlMeasure *refm = m_point->GetRefMeasure();
+          coords[0] = m_reference->GetSample();
+          coords[1] = m_reference->GetLine();
+          coords[2] = 0.0;
+          coords[3] = m_weight;
+          return ( true );
+        }
+
+
+        /** This approach assumes 3 dimensional Euclidean distances */
+        inline bool getGroundCoordinates(double coords[4]) const {
+          (void) getNoPointData(coords);
+          if ( 0 == m_point ) { return ( false ); }
+
+          // Always get the best surface point (2015-10-27)
+          SurfacePoint surfpt = m_point->GetBestSurfacePoint();
+          if ( !surfpt.Valid() ) { return (false); }
+
+          // Get the location and convert to meters!
+          surfpt.ToNaifArray(&coords[0]);
+          coords[0] *= 1000.0;
+          coords[1] *= 1000.0;
+          coords[2] *= 1000.0;
+          return (true);
+        }
+
+        inline bool isDisabled() const {
+          if ( 0 == m_point ) return ( true );
+          return (
+                   m_point->IsInvalid()  || 
+                   m_point->IsIgnored() || 
+                   m_point->IsRejected() ||
+                   m_point->IsEditLocked()  ||
+                   ( 0 == m_reference )
+                   ); 
+        }
+
+        inline void disable() {
+          if ( !isDisabled() ) {
+            m_point->SetIgnored( true );
+          }
+          return;
+        }
+
+        ControlPoint *take() {
+          ControlPoint *p(m_point); 
+          if ( 0 == m_point ) {  return (p); }
+
+          // If someone else owns the point, clone it
+          if ( Shared == m_owner ) {
+            p = new ControlPoint(*m_point);
+          }
+          else {
+            // Exclusive == Ownership
+            // Relinquish ownership
+            m_point = 0;
+          }
+
+          return ( p );
+        }
+
+        // Data....
+        ControlPoint          *m_point; 
+        ControlMeasure        *m_reference;
+        double                m_weight;
+        int                   m_initial;
+        Ownership             m_owner;
+
+
+      private:
+        inline bool getNoPointData(double coords[4]) const {
+          coords[0] = coords[1] = coords[2] = 0.0; 
+          coords[3] = m_weight;
+          return ( false );
+        }
+    };
+
+    // Variables...
+    double                                  m_xyz[4];
+    CoordinateType                          m_type;
+    QString                                 m_serialno;
+    QExplicitlySharedDataPointer<ControlPointData> m_data;
+    QList<ControlPointCloudPt>              m_merged;
+};
+
+};  // namespace Isis
+#endif
---
+++ isis/src/control/apps/cnetcombinept/cnetcombinept.cpp
@@ -0,0 +1,306 @@
+#include "Isis.h"
+
+#include <QFile>
+#include <QtGlobal>
+#include <QTextStream>
+#include <QScopedPointer>
+
+// boost library
+#include <boost/foreach.hpp>
+
+#include "nanoflann/nanoflann.hpp"
+
+#include "ControlNet.h"
+#include "ControlPointCloudPt.h"
+#include "ControlPointMerger.h"
+#include "FileList.h"
+#include "FileName.h"
+#include "IString.h"
+#include "PointCloud.h"
+#include "PointCloudTree.h"
+#include "ProcessByLine.h"
+#include "Progress.h"
+#include "SerialNumber.h"
+#include "SpecialPixel.h"
+#include "SurfacePoint.h"
+#include "IException.h"
+
+using namespace std;
+using namespace Isis;
+
+/** Format an incoming 3-point vector to a string */
+inline QString formatVector(const double *v, const QString &sep = "") {
+  QString s(sep);
+  QString data;
+  for ( int i = 0 ; i < 3 ; i++) {
+    data += (s +  QString::number(v[i]));
+    s = ",";
+  }
+  return (data);
+}
+
+// Control network manager
+typedef QList<QSharedPointer<ControlNet> >    ControlNetList;
+
+// ControlNet point cloud types. Dist3d distance works for all types.
+typedef ControlPointCloudPt         PointType;
+typedef Dist3d<ControlPointCloudPt> DistanceType;
+
+typedef PointCloud<PointType, DistanceType>             CNetPointCloud;
+typedef PointCloudTree<PointType, DistanceType>         CNetPointCloudTree;
+typedef PointCloudSearchResult<PointType, DistanceType> ResultType;
+
+void IsisMain() {
+ 
+  // We will be processing by line
+  ProcessByLine p;
+  UserInterface &ui = Application::GetUserInterface();
+
+  QStringList cnetfiles;
+  int nBase(0);
+  if ( ui.WasEntered("CNETBASE") ) {
+    cnetfiles.append(ui.GetAsString("CNETBASE")); 
+    nBase++;
+  }
+
+  int nFrom(0);
+  if ( ui.WasEntered("CNETFROM") ) {
+    cnetfiles.append(ui.GetAsString("CNETFROM")); 
+    nFrom++;
+  }
+
+  int nList(0);
+  if ( ui.WasEntered("CNETLIST") ) {
+    FileList list_o_nets(ui.GetFileName("CNETLIST"));
+    if (list_o_nets.size() < 1) {
+      QString mess = "The file provided for CNETLIST, [";
+      mess += ui.GetAsString("CNETLIST");
+      mess += "] is empty.";
+      throw IException(IException::User, mess, _FILEINFO_);
+    }
+    BOOST_FOREACH ( FileName cfile, list_o_nets ) {
+      cnetfiles.append( cfile.original() );
+      nList++;
+    }
+  }
+
+  // Check for any files at all
+  int totalNetFiles = nBase + nFrom + nList;
+  if ( totalNetFiles <= 0 ) {
+    QString mess = "No input networks files provided!";
+    throw IException(IException::User, mess, _FILEINFO_);
+  }
+
+  // Ok, if we end up with one net, it must be entered in CNETBASE.
+  if ( (cnetfiles.size() == 1) && (1 != nBase) ) {
+    QString mess = "It appears you are attempting to merge points from a "
+                   "single network, which is fine, but it must be specified "
+                   "in CNETBASE. Try again if that is your intent!";
+    throw IException(IException::User, mess, _FILEINFO_);
+  }
+
+  // Now establish the type of processing mode we are dealing with. Can be 
+  // IMAGE or GROUND.
+  QString cmode = ui.GetString("MODE").toLower();
+  ControlPointCloudPt::CoordinateType ctype = ( "image" == cmode) ? 
+                                                ControlPointCloudPt::Image :
+                                                ControlPointCloudPt::Ground ;
+ 
+  // If image node is requested, get the image to use as a reference
+  QString serialno;
+  if ( "image" == cmode ) {
+    if ( ui.WasEntered("REFERENCE")) {
+      serialno = SerialNumber::Compose(ui.GetAsString("REFERENCE")); 
+    }
+  }
+
+  // Create the point cloud container and load the control networks
+  QScopedPointer<CNetPointCloud> cloud(new CNetPointCloud()); 
+  // ControlNetList cnetlist;
+
+  // Collect some stuff from input nets for the output net
+  QString netid;
+  QString target;
+  QString description;
+  BigInt allPoints(0);
+  BOOST_FOREACH ( QString cfile, cnetfiles ) {
+    std::cout << "\nLoading " << cfile << "...\n";
+    Progress c_progress;
+    QScopedPointer<ControlNet> cnet( new ControlNet(cfile, &c_progress) );
+    if ( netid.isEmpty() )       { netid = cnet->GetNetworkId(); }
+    if ( target.isEmpty() )      { target = cnet->GetTarget(); }
+    if ( description.isEmpty() ) { description = cnet->Description(); }
+
+    // Get all control points by taking ownership from the control net
+    int npoints(0);
+    QList<ControlPoint *> points = cnet->take();
+    BOOST_FOREACH ( ControlPoint *point, points) {
+      ControlPointCloudPt cpt(point, ctype, ControlPointCloudPt::Exclusive, serialno);
+      if ( cpt.isValid() ) { 
+        cloud->addPoint(cpt); 
+        npoints++;
+      }
+    }
+    std::cout << "Added " << npoints << " of " << points.size() << "\n";
+    allPoints += points.size();
+
+    // Instead of having to save the ControlNet instances, we take ownership
+    // of all the points from ControlNet and turn it over to the cloud...
+    // 
+    //  cnetlist.append( QSharedPointer<ControlNet> ( cnet.take() ) );
+  }
+  std::cout << "\nTotal " << cloud->size() << " of " << allPoints << "\n";
+
+  // Set up conditions for search 
+  double image_tolerance  = ui.GetDouble("IMAGETOL");
+  double ground_tolerance = ui.GetDouble("GROUNDTOL");
+  double search_distance  = ui.GetDouble("DISTANCE");
+  double search_radius_sq = search_distance * search_distance;
+
+  std::cout << "\nCreating cloud kd-tree..\n";
+  int kd_nodes = ui.GetInteger("KDNODES");
+  CNetPointCloudTree cloud_t( cloud.take(), kd_nodes );
+
+  // Get the cloud back to use in subsequent processing
+  const CNetPointCloud &v_cloud = cloud_t.cloud();
+  Progress progress;
+  progress.SetText("merging");
+  progress.SetMaximumSteps( v_cloud.size() );
+  progress.CheckStatus();
+
+
+  //  Run through all valid points. Note they may be invalided as processing
+  //  is done through mergers, so validity must be checked!!!
+  BigInt nfound(0);
+  BigInt nMerged(0);
+  for (int n = 0 ; n < v_cloud.size() ; n++) {
+    const ControlPointCloudPt &point = v_cloud.point(n);
+    if ( point.isValid() ) {
+      ResultType results = cloud_t.radius_query(point, search_radius_sq); 
+      nfound += results.size();
+      ControlPointMerger merger(image_tolerance, ground_tolerance);
+      results.forEachPair(merger);
+      nMerged += merger.merge();
+    }
+    progress.CheckStatus();
+  }
+
+  // Now check all remaining points by ignoring all points with less than
+  // MINMEASURES.
+  BigInt nMinMeasures(0);
+  bool setaprioribest = ui.GetBoolean("SETAPRIORIBEST");
+  QString rejectedmeasures = ui.GetString("REJECTEDMEASURES").toLower();
+  bool ignoreRejected = ( "ignore" == rejectedmeasures);
+  bool removeRejected = ( "remove" == rejectedmeasures);
+
+  int minmeasures = ui.GetInteger("MINMEASURES");
+  bool savemins = ui.GetBoolean("SAVEMINS");
+
+  // Now create the output control network
+  BigInt oPoints(0);
+  BigInt nRejected(0);
+
+  ControlNet cnet;
+  if ( ui.WasEntered("NETWORKID") ) {
+    netid = ui.GetString("NETWORKID");
+  }
+
+  cnet.SetNetworkId(netid); 
+  cnet.SetUserName(Application::UserName());
+
+  if ( ui.WasEntered("DESCRIPTION") ) {  
+    description = ui.GetString("DESCRIPTION"); 
+  }
+
+  cnet.SetDescription(description); 
+  cnet.SetCreatedDate(Application::DateTime());
+  cnet.SetTarget( target );
+
+  // Gotta transfer all points/measures
+  for ( int i = 0 ; i < v_cloud.size() ; i++) {
+    ControlPointCloudPt point = v_cloud.point(i);
+
+    if ( point.isValid() ) {
+      
+      // Set up a reference for convenience
+      ControlPoint &cp = point.getPoint();
+
+      // Processes measures if requested
+      if ( true == removeRejected ) {
+
+        QList<ControlMeasure *> measures = cp.getMeasures( false );
+        ControlMeasure *refm = cp.GetRefMeasure();
+        bool removedRef = ( 0 == refm ) ? true : false;
+
+        BOOST_FOREACH ( ControlMeasure *m, measures) {
+          if ( m->IsRejected() ) {  
+            if ( (0 != refm) && (*m == *refm)) { 
+              removedRef = true; 
+              refm = 0;
+            }
+            cp.Delete(m); 
+            nRejected++;
+          }
+        }
+
+        // If we removed the reference, simply set to the first measure
+        if ( removedRef && (cp.GetNumValidMeasures() > 0 ) ) {
+          cp.SetRefMeasure(0);
+        }
+      }
+      // Set rejected points to ignore
+      else if ( true == ignoreRejected ) {
+        QList<ControlMeasure *> measures = cp.getMeasures( false );
+        BOOST_FOREACH ( ControlMeasure *m, measures) {
+          if ( m->IsRejected() ) {  
+            m->SetIgnored( true ); 
+            nRejected++;
+          }
+        }
+      }
+
+      // Check to see if we want to reset the apriori surface to the best
+      // available measure.
+      if ( true == setaprioribest) {
+        cp.SetAdjustedSurfacePoint(cp.GetBestSurfacePoint());
+      }
+
+      //  Now save point if valid
+      if (point.size() < minmeasures) {
+        point.disable();
+        nMinMeasures++;
+        // Check if we are to save points that are less than the valid minimum
+        // measures.
+        if ( true == savemins ) {
+          cnet.AddPoint( point.take() ); 
+          oPoints++;
+        }
+      }
+      else {
+        cnet.AddPoint( point.take() ); 
+        oPoints++;
+      }
+    }
+  }
+
+
+  // Write the resulting control network to the specfied ONET file
+  if ( ui.WasEntered("ONET") ) {
+    cnet.Write( ui.GetAsString("ONET") );
+  }
+
+  // Write out a report
+  int pMerged = v_cloud.size() - oPoints;
+  PvlGroup summary("Summary");
+  summary += PvlKeyword("TotalInputPoints",  toString(v_cloud.size()));
+  summary += PvlKeyword("TotalOutputPoints", toString(oPoints));
+  summary += PvlKeyword("PointsEliminated",  toString(pMerged));
+  summary += PvlKeyword("PointsEvaluated",   toString(nfound));
+  summary += PvlKeyword("RejectedMeasures",  toString(nRejected));
+  summary += PvlKeyword("MeasuresMerged",    toString(nMerged));
+  summary += PvlKeyword("MinimumMeasures",   toString(nMinMeasures));
+  Application::Log(summary);
+
+  p.EndProcess();
+}
+
