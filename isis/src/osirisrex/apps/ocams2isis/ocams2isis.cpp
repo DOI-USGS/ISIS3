1dd4791d1f646c4b84e51ee257bf27dd2fe6d982
Jeannie Backer <jwbacker@usgs.gov>
2016-12-24 22:10:20 +0000

    Added OSIRIS-REx ingestion program to ISIS. References #4570.
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@7309 41f8697f-d340-4b68-9986-7bafba869bb8

---
+++ isis/src/osirisrex/apps/ocams2isis/ocams2isis.cpp
@@ -0,0 +1,81 @@
+#include "Isis.h"
+
+#include <fstream>
+#include <iostream>
+
+#include <QString>
+
+#include "Cube.h"
+#include "IException.h"
+#include "IString.h"
+#include "OriginalLabel.h"
+#include "ProcessBySample.h"
+#include "ProcessImportFits.h"
+#include "Pvl.h"
+#include "PvlGroup.h"
+#include "PvlKeyword.h"
+#include "UserInterface.h"
+
+using namespace std;
+using namespace Isis;
+
+void IsisMain() {
+
+  UserInterface &ui = Application::GetUserInterface();
+
+  ProcessImportFits importFits;
+
+  importFits.setFitsFile(FileName(ui.GetFileName("FROM")));
+
+  importFits.setProcessFileStructure(0);
+
+  Cube *output = importFits.SetOutputCube("TO");
+
+  // Get the directory where the OSIRIS-REx translation tables are.
+  PvlGroup dataDir(Preference::Preferences().findGroup("DataDirectory"));
+  QString transDir = (QString) dataDir["OsirisRex"] + "/translations/";
+
+  // Temp storage of translated labels
+  Pvl outLabel;
+
+  // Get the FITS label
+  Pvl fitsLabel;
+  fitsLabel.addGroup(importFits.fitsLabel(0));
+
+  // Create an Instrument group
+  FileName insTransFile(transDir + "ocamsInstrument_fit.trn");
+  PvlTranslationManager insXlater(fitsLabel, insTransFile.expanded());
+  insXlater.Auto(outLabel);
+  output->putGroup(outLabel.findGroup("Instrument", Pvl::Traverse));
+
+  // Create a Band Bin group
+  FileName bandTransFile(transDir + "ocamsBandBin_fit.trn");
+  PvlTranslationManager bandBinXlater(fitsLabel, bandTransFile.expanded());
+  bandBinXlater.Auto(outLabel);
+  output->putGroup(outLabel.findGroup("BandBin", Pvl::Traverse));
+
+  // Create an Archive group
+  // OREx team reduced the output keywords, so this was commented out
+  // Remove if all goes well with next release
+  //FileName archiveTransFile(transDir + "ocamsArchive_fit.trn");
+  //PvlTranslationManager archiveXlater(fitsLabel, archiveTransFile.expanded());
+  //archiveXlater.Auto(outLabel);
+  //output->putGroup(outLabel.findGroup("Archive", Pvl::Traverse));
+
+  // Create a Kernels group
+  FileName kernelsTransFile(transDir + "ocamsKernels_fit.trn");
+  PvlTranslationManager kernelsXlater(fitsLabel, kernelsTransFile.expanded());
+  kernelsXlater.Auto(outLabel);
+  output->putGroup(outLabel.findGroup("Kernels", Pvl::Traverse));
+
+  // Save the input FITS label in the Cube original labels
+  Pvl pvl;
+  pvl += importFits.fitsLabel(0);
+  OriginalLabel originals(pvl);
+  output->write(originals);
+
+  // Convert the image data
+  //importFits.StartProcess(Convert);
+  importFits.StartProcess();
+  importFits.Finalize();
+}
