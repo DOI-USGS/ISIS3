dca362be61a568f6a18228c2eefc64e2f2a9444b
Kelvin Rodriguez <krodriguez@usgs.gov>
2017-01-14 00:30:32 +0000

    PROG: Fixed namespace error, refrences #4575
    git-svn-id: http://subversion.wr.usgs.gov/repos/prog/isis3/trunk@7358 41f8697f-d340-4b68-9986-7bafba869bb8

--- isis/src/base/objs/Isis/Isis.h
+++ isis/src/base/objs/Isis/Isis.h
@@ -121,7 +121,7 @@ int main(int argc, char *argv[]) {
 #endif
 
   // Add the plugin directory so QT looks at the Isis plugin dir
-  FileName qtpluginpath("$ISISROOT/3rdParty/plugins");
+  Isis::FileName qtpluginpath("$ISISROOT/3rdParty/plugins");
   QCoreApplication::addLibraryPath(qtpluginpath.expanded());
 
   Isis::Application *app = new Isis::Application(argc, argv);
