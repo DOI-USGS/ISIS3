2206afe45f7bb93e54e15b62672eeceae43831ac
Jesse Mapel <jmapel@usgs.gov>
2018-01-19 16:11:13 -0700

    Removed references to ControlCubeGraphNode in ControlNetStatistics

--- isis/src/control/objs/ControlNetStatistics/ControlNetStatistics.cpp
+++ isis/src/control/objs/ControlNetStatistics/ControlNetStatistics.cpp
@@ -15,7 +15,6 @@
 #include "ControlPoint.h"
 #include "ControlMeasure.h"
 #include "ControlMeasureLogData.h"
-#include "ControlCubeGraphNode.h"
 #include "Cube.h"
 #include "CubeManager.h"
 #include "FileName.h"
@@ -219,15 +218,15 @@ namespace Isis {
     CubeManager cubeMgr;
     cubeMgr.SetNumOpenCubes(50);
 
-    mCubeGraphNodes = mCNet->GetCubeGraphNodes();
+    QList<QString> cnetSerials = mCNet->GetCubeSerials();
 
     if (mProgress != NULL) {
       mProgress->SetText("Generating Image Stats.....");
-      mProgress->SetMaximumSteps(mCubeGraphNodes.size());
+      mProgress->SetMaximumSteps(cnetSerials.size());
       mProgress->CheckStatus();
     }
 
-    foreach (ControlCubeGraphNode * node, mCubeGraphNodes) {
+    foreach (QString sn, cnetSerials) {
       geos::geom::CoordinateSequence * ptCoordinates =
           new geos::geom::CoordinateArraySequence();
 
@@ -235,7 +234,6 @@ namespace Isis {
       vector<double> imgStats(numImageStats, 0);
 
       // Open the cube to get the dimensions
-      QString sn = node->getSerialNumber();
       Cube *cube = cubeMgr.OpenCube(mSerialNumList.fileName(sn));
 
       mSerialNumMap[sn] = true;
@@ -245,7 +243,7 @@ namespace Isis {
       imgStats[imgLines]   = cube->lineCount();
       double cubeArea      = imgStats[imgSamples] * imgStats[imgLines];
 
-      QList< ControlMeasure * > measures = node->getMeasures();
+      QList< ControlMeasure * > measures = mCNet->GetMeasuresInCube(sn);
 
       // Populate pts with a list of control points
       if (!measures.isEmpty()) {
@@ -360,7 +358,7 @@ namespace Isis {
       QString msg = QObject::tr("Error writing to file: [%1]").arg(psImageFile);
       throw IException(IException::Io, msg, _FILEINFO_);
     }
-    ostm.close(); 
+    ostm.close();
   }
 
 
@@ -613,4 +611,3 @@ namespace Isis {
     }
   }
 }
-
