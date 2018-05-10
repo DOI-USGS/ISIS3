25ad33d7402a0636b87da31458d3739df0732e72
Makayla Shepherd <mshepherd@usgs.gov>
2018-05-10 13:36:57 -0700

    Revert "Added oblique resolutions to caminfo and the advanced track tool of qview and qnet. Fixes #4100. "

--- isis/src/qisis/objs/AdvancedTrackTool/AdvancedTrackTool.h
+++ isis/src/qisis/objs/AdvancedTrackTool/AdvancedTrackTool.h
@@ -24,9 +24,6 @@
 
 // The only includes allowed in this file are the direct parents of this class!
 #include "Tool.h"
-#include <QList>
-#include <QString>
-using namespace std;
 
 class QAction;
 
@@ -85,12 +82,6 @@ namespace Isis {
    *                          before it attempts to record a point so that a table is created
    *                          to record the point into so that the first recorded point is drawn.
    *                          Fixes #5143.
-   *  @history 2018-03-07 Kaitlyn Lee - Added columns for oblique pixel, sample, line,
-   *                          and detector resolutions. Added checkBoxItems and loop to add
-   *                          the elments to the AdvancedTrackTool, instead of hardcoded method
-   *                          calls. Instead of using the enum, I added a method getIndex()
-   *                          that calculates what column the element should be added to.
-   *                          Fixes #4100.
    */
   class AdvancedTrackTool : public Tool {
       Q_OBJECT
@@ -100,7 +91,6 @@ namespace Isis {
       void addTo(QMenu *menu);
       void addToPermanent(QToolBar *perm);
       bool eventFilter(QObject *o, QEvent *e);
-      int getIndex(QString keyword);
 
     public slots:
       virtual void mouseMove(QPoint p);
@@ -133,66 +123,67 @@ namespace Isis {
       void writeSettings();
       QString settingsFilePath() const;
 
-      // Used to store information about each check box to later add to the table
-      // New entries can be added anywhere in the QList.
-      // Format: QList<QString>({<Header>, <onByDefault>, <menuText>, <toolTip>}) <<
-      // If a toolTip is not needed, use "".
-      QList<QList<QString> > checkBoxItems = QList< QList<QString> >() <<
-        QList<QString>({"Id", "false", "Id", ""}) <<
-        QList<QString>({"Sample:Line", "true", "Sample:Line", ""}) <<
-        QList<QString>({"Band", "false", "Band", ""}) <<
-        QList<QString>({"Pixel", "true", "Pixel", ""}) <<
-        QList<QString>({"Planetocentric Latitude", "true", "Planetocentric Lat", ""}) <<
-        QList<QString>({"Planetographic Latitude", "false", "Planetographic Lat", ""}) <<
-        QList<QString>({"360 Positive East Longitude", "true", "360 East Longitude", ""}) <<
-        QList<QString>({"360 Positive West Longitude", "false", "360 West Longitude", ""}) <<
-        QList<QString>({"180 Positive East Longitude", "true", "180 East Longitude", ""}) <<
-        QList<QString>({"180 Positive West Longitude", "false", "180 West Longitude", ""}) <<
-        QList<QString>({"Projected X:Projected Y", "false", "Projected X:Projected Y",
-                         "X and Y values for a projected image"}) <<
-        QList<QString>({"Local Radius", "false", "Radius", ""}) <<
-        QList<QString>({"Point X:Point Y:Point Z", "false", "XYZ",
-                         "The X, Y, and Z of surface intersection in body-fixed coordinates"}) <<
-        QList<QString>({"Right Ascension:Declination", "false", "Ra:Dec",
-                         "Right Ascension and Declination"}) <<
-        QList<QString>({"Resolution", "false", "Resolution", ""}) <<
-        QList<QString>({"Oblique Pixel Resolution", "false", "Oblique Pixel Res", ""}) <<
-        QList<QString>({"Oblique Sample Resolution", "false", "Oblique Sample Res", ""}) <<
-        QList<QString>({"Oblique Line Resolution", "false", "Oblique Line Res", ""}) <<
-        QList<QString>({"Oblique Detector Resolution", "false", "Oblique Detector Res", ""}) <<
-        QList<QString>({"Phase", "false", "Phase", ""}) <<
-        QList<QString>({"Incidence", "false", "Incidence", ""}) <<
-        QList<QString>({"Emission", "false", "Emission", ""}) <<
-        QList<QString>({"LocalIncidence", "false", "LocalIncidence", ""}) <<
-        QList<QString>({"LocalEmission", "false", "LocalEmission", ""}) <<
-        QList<QString>({"North Azimuth", "false", "North Azimuth", ""}) <<
-        QList<QString>({"Sun Azimuth", "false", "Sun Azimuth", ""}) <<
-        QList<QString>({"Solar Longitude", "false", "Solar Longitude", ""}) <<
-        QList<QString>({"Spacecraft X:Spacecraft Y:Spacecraft Z", "false", "Spacecraft Position",
-                           "The X, Y, and Z of the spacecraft position"}) <<
-        QList<QString>({"Spacecraft Azimuth", "false", "Spacecraft Azimuth", ""}) <<
-        QList<QString>({"Slant Distance", "false", "Slant Distance", ""}) <<
-        QList<QString>({"Focal Plane X:Focal Plane Y", "false", "Focal Plane X:Y", ""}) <<
-        QList<QString>({"Undistorted Focal X:Undistorted Focal Y:Undistorted Focal Z",
-                           "false", "Undistorted Focal Plane X:Y:Z", ""}) <<
-        QList<QString>({"Ephemeris Time", "false", "Ephemeris iTime", ""}) <<
-        QList<QString>({"Local Solar Time", "false", "Local Solar iTime", ""}) <<
-        QList<QString>({"UTC", "false", "UTC", "Internal time in UTC format"}) <<
-        QList<QString>({"Path", "false", "Path", ""}) <<
-        QList<QString>({"FileName", "false", "FileName", ""}) <<
-        QList<QString>({"Serial Number", "false", "Serial Number", ""}) <<
-        QList<QString>({"Track Mosaic Index", "false", "Track Mosaic Index", ""}) <<
-        QList<QString>({"Track Mosaic FileName", "false", "Track Mosaic FileName", ""}) <<
-        QList<QString>({"Track Mosaic Serial Number", "false", "Track Mosaic Serial Number", ""}) <<
-        QList<QString>({"Notes", "false", "Notes", ""});
-
+      /**
+       * Enum for column values
+       */
+      enum {
+        ID,                     //!< The record ID
+        SAMPLE,                 //!< The current sample
+        LINE,                   //!< The current line
+        BAND,                   //!< The current band
+        PIXEL,                  //!< The current pixel
+        PLANETOCENTRIC_LAT,     //!< The planetocentric latitude for this point
+        PLANETOGRAPHIC_LAT,     //!< The planetographic latitude for this point
+        EAST_LON_360,           //!< The 360 east longitude for this point
+        WEST_LON_360,           //!< The 360 west longitude for this point
+        EAST_LON_180,           //!< The 180 east longitude for this point
+        WEST_LON_180,           //!< The 180 west longitude for this point
+        PROJECTED_X,            //!< Projected X value for valid projections
+        PROJECTED_Y,            //!< Projected Y value for valid projections
+        RADIUS,                 //!< The radius for this point
+        POINT_X,                //!< The x value for this point
+        POINT_Y,                //!< The y value for this point
+        POINT_Z,                //!< The z value for this point
+        RIGHT_ASCENSION,        //!< The right ascension for this point
+        DECLINATION,            //!< The declination for this point
+        RESOLUTION,             //!< The resoultion for this point
+        PHASE,                  //!< The phase for this point
+        INCIDENCE,              //!< The incidence for this point
+        EMISSION,               //!< The emission for this point
+        LOCAL_INCIDENCE,        //!< The local incidence for this point
+        LOCAL_EMISSION,         //!< The local emission for this point
+        NORTH_AZIMUTH,          //!< The north azimuth for this cube
+        SUN_AZIMUTH,            //!< The sun azimuth for this cube
+        SOLAR_LON,              //!< The solar longitude for this point
+        SPACECRAFT_X,           //!< The spacecraft x position for this cube
+        SPACECRAFT_Y,           //!< The spacecraft y position for this cube
+        SPACECRAFT_Z,           //!< The spacecraft z position for this cube
+        SPACECRAFT_AZIMUTH,     //!< The spacecraft azimuth for this cube
+        SLANT,                  //!< The slant for this cube
+        DISTORTED_FOCAL_X,      //!< The x of the distorted focal plane
+        DISTORTED_FOCAL_Y,      //!< The y of the distorted focal plane
+        UNDISTORTED_FOCAL_X,    //!< The x of the undistorted focal plane
+        UNDISTORTED_FOCAL_Y,    //!< The y of the undistorted focal plane
+        UNDISTORTED_FOCAL_Z,    //!< The z of the undistorted focal plane
+        EPHEMERIS_TIME,         //!< The ephemeris time for this cube
+        SOLAR_TIME,             //!< The local solar time for this cube
+        UTC,                    //!< The UTC for this cube
+        PATH,                   //!< The path for this cube
+        FILENAME,               //!< The filename for this cube
+        SERIAL_NUMBER,          //!< The serial number for this cube
+        TRACK_MOSAIC_INDEX,     //!< Track the origin of the Mosaic, display the zero based index
+        TRACK_MOSAIC_FILENAME,  //!< Track the origin of the Mosaic, display file name
+        TRACK_MOSAIC_SERIAL_NUM,//!< Track the origin of the Mosaic, display file name
+        NOTES                   //!< Any notes for this record
+      };
       QAction *p_action;                   //!< Action to bring up the track tool
       int p_numRows;                       //!< The number of rows in the table
       int p_id;                            //!< The record id
-      TableMainWindow *p_tableWin;         //!< The table window
+      TableMainWindow *p_tableWin;  //!< The table window
       bool m_showHelpOnStart;              //!< True to show dialog When tool is started
 
   };
+
 };
 
 #endif
