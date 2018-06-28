9d3d5edf720fc6b3c7ed031c19f8498d9525ba1b
ihumphrey <ihumphrey@usgs.gov>
2018-06-28 14:53:20 -0700

    Revert "Removed the weighting tab from JigsawSetupDialog"

--- isis/src/qisis/objs/JigsawSetupDialog/JigsawSetupDialog.h
+++ isis/src/qisis/objs/JigsawSetupDialog/JigsawSetupDialog.h
@@ -85,7 +85,6 @@ namespace Isis {
    *                           invalid a priori sigma values whenever an a priori sigma values changes.
    *                           If any value is invalid, the OK and Apply Settings buttons are disabled
    *                           until all the a priori sigma values are valid again. References #497.
-   *   @history 2018-06-28 Makayla Shepherd - Removed the weighting tab.
    */
   class JigsawSetupDialog : public QDialog {
     Q_OBJECT
