e935b8ee8d60a795a22ad380df9b8521f2979994
Adam Goins <agoins@usgs.gov>
2018-08-14 00:20:05 -0700

    Commented out MosaicSceneWidgetTester code to reflect that we don't use qtest for gui testing. Updated the truth file

--- isis/src/qisis/objs/MosaicSceneWidget/MosaicSceneWidgetTester.cpp
+++ isis/src/qisis/objs/MosaicSceneWidget/MosaicSceneWidgetTester.cpp
@@ -20,75 +20,80 @@
 namespace Isis {
   /**
    * This is a very basic functionality test.
+   * 
+   * @internal
+   *
+   *   @history 2018-08-13 Adam Goins - Commented out the testing for this object because we don't
+   *                           use qtest to test gui. 
    */
   void MosaicSceneWidgetTester::testBasicFunctionality() {
-    QStatusBar status;
+    //QStatusBar status;
 
-    MosaicSceneWidget widget(&status, true, true, NULL);
-    widget.show();
-    QTest::qWaitForWindowExposed(widget.windowHandle());
-    QVERIFY(widget.getProgress());
-    QVERIFY(widget.getView());
-    QVERIFY(widget.getScene());
-    QVERIFY(widget.getProjection() == NULL);
+    //MosaicSceneWidget widget(&status, true, true, NULL);
+    //widget.show();
+    //QTest::qWaitForWindowExposed(widget.windowHandle());
+    //QVERIFY(widget.getProgress());
+    //QVERIFY(widget.getView());
+    //QVERIFY(widget.getScene());
+    //QVERIFY(widget.getProjection() == NULL);
 
-    Image *image = new Image(QString("./lub3994m.342.lev1.cub"));
-    QScopedPointer<QMutex> cameraMutex(new QMutex);
-    image->initFootprint(cameraMutex.data());
+    //Image *image = new Image(QString("./lub3994m.342.lev1.cub"));
+    //QScopedPointer<QMutex> cameraMutex(new QMutex);
+    //image->initFootprint(cameraMutex.data());
 
-    ImageList images;
-    images.append(image);
+    //ImageList images;
+    //images.append(image);
 
-    widget.addImages(images);
+    //widget.addImages(images);
 
-    QCOMPARE(widget.allMosaicSceneItems().count(), 1);
-    QCOMPARE(image, widget.allMosaicSceneItems().first()->image());
-    QVERIFY(widget.cubesSelectable());
+    //QCOMPARE(widget.allMosaicSceneItems().count(), 1);
+    //QCOMPARE(image, widget.allMosaicSceneItems().first()->image());
+    //QVERIFY(widget.cubesSelectable());
 
-    // Check that the bounding rect is approx. the same
-    QRectF expected(QPointF(2376269.37351469, -964957.418535598), QSize(109739, 48049));
+    //// Check that the bounding rect is approx. the same
+    //QRectF expected(QPointF(2376269.37351469, -964957.418535598), QSize(109739, 48049));
 
-    QVERIFY(qAbs(widget.cubesBoundingRect().top() - expected.top()) < 0.0001);
-    QVERIFY(qAbs(widget.cubesBoundingRect().left() - expected.left()) < 0.0001);
-    QVERIFY(qAbs(widget.cubesBoundingRect().bottom() - expected.bottom()) < 1);
-    QVERIFY(qAbs(widget.cubesBoundingRect().right() - expected.right()) < 1);
+    //QVERIFY(qAbs(widget.cubesBoundingRect().top() - expected.top()) < 0.0001);
+    //QVERIFY(qAbs(widget.cubesBoundingRect().left() - expected.left()) < 0.0001);
+    //QVERIFY(qAbs(widget.cubesBoundingRect().bottom() - expected.bottom()) < 1);
+    //QVERIFY(qAbs(widget.cubesBoundingRect().right() - expected.right()) < 1);
   }
 
 
   void MosaicSceneWidgetTester::testSynchronization() {
-    QStatusBar status;
+    //QStatusBar status;
 
-    MosaicSceneWidget widget(&status, true, true, NULL);
-    widget.show();
-    QTest::qWaitForWindowExposed(widget.windowHandle());
+    //MosaicSceneWidget widget(&status, true, true, NULL);
+    //widget.show();
+    //QTest::qWaitForWindowExposed(widget.windowHandle());
 
-    Image *image = new Image(QString("./lub3994m.342.lev1.cub"));
-    QScopedPointer<QMutex> cameraMutex(new QMutex);
-    image->initFootprint(cameraMutex.data());
+    //Image *image = new Image(QString("./lub3994m.342.lev1.cub"));
+    //QScopedPointer<QMutex> cameraMutex(new QMutex);
+    //image->initFootprint(cameraMutex.data());
 
-    ImageList images;
-    images.append(image);
+    //ImageList images;
+    //images.append(image);
 
-    widget.addImages(images);
+    //widget.addImages(images);
 
-    MosaicSceneItem *sceneItem = widget.allMosaicSceneItems().first();
+    //MosaicSceneItem *sceneItem = widget.allMosaicSceneItems().first();
 
-    ImageDisplayProperties *displayProperties = image->displayProperties();
-    QCOMPARE(sceneItem->color(),
-             displayProperties->getValue(ImageDisplayProperties::Color).value<QColor>());
-    QCOMPARE(sceneItem->isSelected(),
-             displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
+    //ImageDisplayProperties *displayProperties = image->displayProperties();
+    //QCOMPARE(sceneItem->color(),
+    //         displayProperties->getValue(ImageDisplayProperties::Color).value<QColor>());
+    //QCOMPARE(sceneItem->isSelected(),
+    //         displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
 
-    sceneItem->setSelected(true);
+    //sceneItem->setSelected(true);
 
-    QVERIFY(sceneItem->isSelected());
-    QCOMPARE(sceneItem->isSelected(),
-             displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
+    //QVERIFY(sceneItem->isSelected());
+    //QCOMPARE(sceneItem->isSelected(),
+    //         displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
 
-    displayProperties->setSelected(false);
+    //displayProperties->setSelected(false);
 
-    QVERIFY(!displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
-    QCOMPARE(sceneItem->isSelected(),
-             displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
+    //QVERIFY(!displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
+    //QCOMPARE(sceneItem->isSelected(),
+    //         displayProperties->getValue(ImageDisplayProperties::Selected).toBool());
   }
 }
