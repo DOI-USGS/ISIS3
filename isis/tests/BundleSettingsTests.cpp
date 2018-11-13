65d6338aab47f291e68ca1fd8b8f3aedcfe22460
Jesse Mapel <jmapel@usgs.gov>
2018-11-12 20:10:01 -0700

    Added more tests

--- isis/tests/BundleSettingsTests.cpp
+++ isis/tests/BundleSettingsTests.cpp
@@ -1,6 +1,19 @@
 #include "BundleSettings.h"
 
+#include <algorithm>
+
+#include <QString>
+#include <QList>
+
+#include "BundleObservationSolveSettings.h"
+
 #include <gtest/gtest.h>
+
+bool observationSettingsComparison(
+      BundleObservationSolveSettings a,
+      BundleObservationSolveSettings b) {
+  return (a.instrumentId() == b.instrumentId());
+}
 
 class BoolTest : public ::testing::TestWithParam<bool> {
   // Intentionally empty
@@ -12,6 +25,50 @@ TEST_P(BoolTest, validateNetwork) {
   EXPECT_EQ(GetParam(), testSettings.validateNetwork());
 }
 
+TEST_P(BoolTest, outlierRejection) {
+  BundleSettings testSettings;
+  testSettings.setOutlierRejection(GetParam());
+  EXPECT_EQ(GetParam(), testSettings.outlierRejection());
+}
+
 INSTANTIATE_TEST_CASE_P(BundleSettings,
                         BoolTest,
                         ::testing::Bool());
+
+TEST(BundleSettings, outlierRejectionMultiplier) {
+  BundleSettings testSettings;
+  testSettings.setOutlierRejection(true, 8.0);
+  EXPECT_EQ(8.0, testSettings.outlierRejectionMultiplier());
+}
+
+TEST(BundleSettings, observationSolveSettings) {
+  BundleObservationSolveSettings firstObsSettings;
+  BundleObservationSolveSettings secondObsSettings;
+  QString firstInstrument("First Instrument");
+  QString secondInstrument("Second Instrument");
+  QString firstObservationNumber("First Observation");
+  QString secondObservationNumber("Second Observation");
+  firstObsSettings.setInstrumentId(firstInstrument);
+  secondObsSettings.setInstrumentId(secondInstrument);
+  firstObsSettings.addObservationNumber(firstObservationNumber);
+  secondObsSettings.addObservationNumber(secondObservationNumber);
+  QList<BundleObservationSolveSettings> optionsList = {
+        firstObsSettings,
+        secondObsSettings};
+
+  BundleSettings testSettings;
+  testSettings.setObservationSolveOptions(optionsList);
+
+  EXPECT_EQ(testSettings.numberSolveSettings(), optionsList.size());
+  EXPECT_TRUE(std::equal(
+        optionsList.begin(),
+        optionsList.end(),
+        testSettings.observationSolveSettings().begin(),
+        observationSettingsComparison));
+  EXPECT_TRUE(observationSettingsComparison(
+        testSettings.observationSolveSettings(secondObservationNumber),
+        secondObsSettings));
+  EXPECT_TRUE(observationSettingsComparison(
+        testSettings.observationSolveSettings(1),
+        secondObsSettings));
+}
