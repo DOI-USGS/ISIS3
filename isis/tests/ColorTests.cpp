ec670134716170215e792abfc9e929425ee44807
Jesse Mapel <jam826@nau.edu>
2018-11-21 10:18:06 -0700

    Add Utilities, clean up testing (#604)
    * Update CMakeLists.txt with version 3.6.0 (patch 1)
    
    * Update cmake/CMakeLists.txt library version
    
    * Updated the Installation instructions based on user feedback.  Also fixed a problem with the isis3VarInit.py script.
    
    * Updates made in accordance with requests made on this PR by other developers.
    
    * Fix tgo cassis unit test correctly
    
    * Fix tgo cassis unit test correctly (#544)
    
    * Fixed two bug with AutoReg. One occurs with one dimensional pattern cubes and the other causes the revious registration sample/line to be returned on sub-pixel registration failure.
    
    * Fix tgo cassis camera truth again (#554)
    
    * Update docsys Makefile to support wwwdoc (#559)
    
    The documentation is installed under `docs`, not `doc`, so this file has been updated.
    `setisis isis3.6.0` and running `make wwwdoc` in `$ISISROOT/../isis/src/docsys` will properly sync the documentation to our web server.
    
    * Update isis3VarInit.py
    
    Unnecessary 'touch'ing, the '>' redirect will create the file if it doesn't exist.
    Also, the first redirect should be the single '>'.  Otherwise, multiple runs of this program will continue appending to these files.  They still work, its just a lot of setting and unsetting for no good reason.
    
    * Fixed isis3Startup scripts
    
    * Removed unneeded LineEquation include
    
    * Make this program more pythonic (#589)
    
    * Updated documentation, user interaction, and made the file and directory
    handling more 'pythonic'.
    
    * Correcting for PR comments.
    
    * Fixed broken links in the Installation instructions due to the hyperlinks to our GitHub wiki changing.
    
    * Removed unused Parabola class
    
    * Removed old license, added new unlicense (#594)
    
    * Fixed issue with the License change (#599)
    
    * Removed old license, added new unlicense
    
    * Fixed cmake looking for license in the wrong place
    
    * Added TestUtilities and used in Color tests
    
    * Fixed an issue where app xmls were not being copied
    
    * Tweaked IException test failure messages

--- isis/tests/ColorTests.cpp
+++ isis/tests/ColorTests.cpp
@@ -2,56 +2,60 @@
 
 #include "Color.h"
 #include "IException.h"
+#include "TestUtilities.h"
 
 #include <QDebug>
 #include <QColor>
 
 
 namespace Isis{
-  
-  
+
+
   class QStringQColorPair: public ::testing::TestWithParam<std::pair<QString, QColor> > {
     // Intentionally left empty
   };
-  
-  
+
+
   class InvalidColorString: public ::testing::TestWithParam<QString> {
   // Intentionally left empty
   };
 
 
   TEST_P (QStringQColorPair, ValidColorToString) {
-    EXPECT_EQ(GetParam().first, Color::toRGBAString(GetParam().second));
+    EXPECT_PRED_FORMAT2(AssertQStringsEqual, GetParam().first, Color::toRGBAString(GetParam().second));
   }
-  
-  
+
+
   TEST (ColorTests, InvalidColorToString) {
+    QString message = "Can not convert an invalid color to an RGBA string.  "
+                      "There is no string representation of an invalid color";
     try {
       Color::toRGBAString(QColor());
       FAIL() << "Expected an IException";
     }
     catch(IException &e) {
-      EXPECT_TRUE(e.toString().toLatin1().contains("Can not convert an invalid color to an RGBA "
-                                                   "string.  There is no string representation of "
-                                                   "an invalid color")) << e.toString().toStdString();
+      EXPECT_PRED_FORMAT3(
+            AssertIException,
+            e,
+            message,
+            IException::Unknown);
     }
     catch(...) {
-      FAIL() << "Expected error message: \"Can not convert an invalid color to an RGBA string.  "
-                "There is no string representation of an invalid color.\"";
+      FAIL() << "Expected error message: \"" << message.toStdString() << "\"";
     }
   }
-  
-  
+
+
   TEST_P (QStringQColorPair, ValidStringToColor) {
     EXPECT_EQ(GetParam().second, Color::fromRGBAString(GetParam().first));
   }
-  
-  
+
+
   TEST_P (InvalidColorString, InvalidStringToColor) {
     EXPECT_EQ(QColor(QColor::Invalid), Color::fromRGBAString(GetParam()));
   }
-  
-  
+
+
   TEST_P (QStringQColorPair, ValidColorRGBAFormat) {
     EXPECT_TRUE(Color::colorRGBAFormat().exactMatch(GetParam().first));
   }
@@ -60,11 +64,11 @@ namespace Isis{
   TEST_P (InvalidColorString, InvalidColorRGBAFormat) {
     EXPECT_FALSE(Color::colorRGBAFormat().exactMatch(GetParam()));
   }
-  
-  
-  INSTANTIATE_TEST_CASE_P (QStringQColorPairInstantiation, 
-                           QStringQColorPair, 
-                           ::testing::Values(std::make_pair(QString("#000000ff"), QColor(0, 0, 0)), 
+
+
+  INSTANTIATE_TEST_CASE_P (Color,
+                           QStringQColorPair,
+                           ::testing::Values(std::make_pair(QString("#000000ff"), QColor(0, 0, 0)),
                                             std::make_pair(QString("#00000000"), QColor(0, 0, 0, 0)),
                                             std::make_pair(QString("#ff000000"), QColor(255, 0, 0, 0)),
                                             std::make_pair(QString("#00ff0000"), QColor(0, 255, 0, 0)),
@@ -72,10 +76,10 @@ namespace Isis{
                                             std::make_pair(QString("#000000ff"), QColor(0, 0, 0, 255)),
                                             std::make_pair(QString("#ffffffff"), QColor(255, 255, 255, 255)),
                                             std::make_pair(QString("#0a141e28"), QColor(10, 20, 30, 40))));
-  
-  
-  INSTANTIATE_TEST_CASE_P (InvalidColorStringInstantiation, 
-                           InvalidColorString, 
+
+
+  INSTANTIATE_TEST_CASE_P (Color,
+                           InvalidColorString,
                            ::testing::Values(QString("#rrggbbaa"),
                                             QString(" 00112233"),
                                             QString(""),
