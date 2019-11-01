b83803385f367e7d2449387b836a68fa82ac857c
Tyler Thatcher <tdt62@nau.edu>
2019-11-01 14:18:19 -0700

    Jenkins

--- build.groovy
+++ build.groovy
@@ -55,7 +55,7 @@ node("${env.OS.toLowerCase()}") {
         env.STAGE_STATUS = "Checking out ISIS"
         checkout scm
         isisEnv.add("ISISROOT=${pwd()}/build")
-        // cmakeFlags.add("-DCMAKE_INSTALL_PREFIX=${pwd()}/install")
+        cmakeFlags.add("-DCMAKE_INSTALL_PREFIX=${pwd()}/install")
     }
 
     stage("Create environment") {
