ce8d1fbfdd2c167c2b0b2107129b13052ac36e11
Adam Paquette <acpaquette@usgs.gov>
2019-11-20 16:11:40 -0700

    Moved things around

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,9 +2,6 @@
 
 pipeline
 {
-    def rootDir = pwd()
-    def build_script = load "${rootDir}/script.groovy"
-
     stages {
         stage ("Checkout") {
             env.STAGE_STATUS = "Checking out ISIS"
@@ -12,7 +9,13 @@ pipeline
             isisEnv.add("ISISROOT=${pwd()}/build")
             cmakeFlags.add("-DCMAKE_INSTALL_PREFIX=${pwd()}/install")
         }
+        stage ("Create Environment") {
+            steps {
+                def rootDir = pwd()
+                def build_script = load "${rootDir}/script.groovy"
 
-        build_script.myFunc(centos)
+                build_script.myFunc(centos)
+            }
+        }
     }
 }
--- script.groovy
+++ script.groovy
@@ -1,19 +1,17 @@
 // vim: ft=groovy
 
 def myFunc(machine) {
-    stage("Create environment") {
-        env.STAGE_STATUS = "Creating conda environment"
-        sh '''
-            # Use the conda cache running on the Jenkins host
-            conda config --set channel_alias http://dmz-jenkins.wr.usgs.gov
-            conda config --set always_yes True
-            conda create -n isis python=3
-        '''
+    env.STAGE_STATUS = "Creating conda environment"
+    sh '''
+        # Use the conda cache running on the Jenkins host
+        conda config --set channel_alias http://dmz-jenkins.wr.usgs.gov
+        conda config --set always_yes True
+        conda create -n isis python=3
+    '''
 
-        if (machine.toLowerCase() == "centos") {
-            sh 'conda env update -n isis_${machine} -f environment_gcc4.yml --prune'
-        } else {
-            sh 'conda env update -n isis_${machine} -f environment.yml --prune'
-        }
+    if (machine.toLowerCase() == "centos") {
+        sh 'conda env update -n isis_${machine} -f environment_gcc4.yml --prune'
+    } else {
+        sh 'conda env update -n isis_${machine} -f environment.yml --prune'
     }
 }
