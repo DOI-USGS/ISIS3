73d86a5fa957c49f29143c7310b0a2f0ef22be71
Adam Paquette <acpaquette@usgs.gov>
2019-11-20 16:06:32 -0700

    Attempt to build centos env

--- script.groovy
+++ script.groovy
@@ -1,10 +1,19 @@
 // vim: ft=groovy
 
-def myFunc() {
-    println("IN MY FUNCTION")
-    checkout scm
+def myFunc(machine) {
+    stage("Create environment") {
+        env.STAGE_STATUS = "Creating conda environment"
+        sh '''
+            # Use the conda cache running on the Jenkins host
+            conda config --set channel_alias http://dmz-jenkins.wr.usgs.gov
+            conda config --set always_yes True
+            conda create -n isis python=3
+        '''
+
+        if (machine.toLowerCase() == "centos") {
+            sh 'conda env update -n isis_${machine} -f environment_gcc4.yml --prune'
+        } else {
+            sh 'conda env update -n isis_${machine} -f environment.yml --prune'
+        }
+    }
 }
-
-checkout scm
-
-return this
