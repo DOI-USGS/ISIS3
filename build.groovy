214dc93e190c3c42c787ffbc289aa44c6fd2272b
Jesse Mapel <jmapel@usgs.gov>
2019-12-10 12:16:31 -0700

    Added debug line to output Anaconda environment

--- build.groovy
+++ build.groovy
@@ -88,6 +88,7 @@ node("${env.OS.toLowerCase()}") {
                         source activate isis
                         echo `ls ../`
                         echo `pwd`
+                        conda list
                         cmake -GNinja ${cmakeFlags.join(' ')} ../isis
                         ninja -j4 install
                         python ../isis/scripts/isis3VarInit.py --data-dir ${env.ISIS3DATA} --test-dir ${env.ISIS3TESTDATA}
