a33b33618a8dd269eb3a90ed4a71949578e31ed2
Kelvin <kelvinrr@icloud.com>
2019-12-20 00:32:31 -0700

    added basic Spice test

--- isis/src/base/objs/Spice/Spice.cpp
+++ isis/src/base/objs/Spice/Spice.cpp
@@ -80,7 +80,7 @@ namespace Isis {
     Pvl &lab = *cube.label();
     PvlGroup kernels = lab.findGroup("Kernels", Pvl::Traverse);
     bool hasTables = (kernels["TargetPosition"][0] == "Table");
-    init(cube, !hasTables);
+    init(lab, !hasTables);
   }
 
   /**
@@ -90,7 +90,11 @@ namespace Isis {
    * @param noTables Indicates the use of tables.
    */
   Spice::Spice(Cube &cube, bool noTables) {  
-    init(cube, noTables);
+    init(*cube.label(), noTables);
+  }
+  
+  Spice::Spice(Pvl &lab, json isd) {
+    init(lab, true, isd);
   }
 
   /**
@@ -106,9 +110,9 @@ namespace Isis {
    * @internal
    *   @history 2011-02-08 Jeannie Walldren - Initialize pointers to null.
    */
-  void Spice::init(Cube &cube, bool noTables) {
+  void Spice::init(Pvl &lab, bool noTables, json isd) {
     NaifStatus::CheckErrors();
-
+    
     // Initialize members
     m_solarLongitude = new Longitude;
     m_et = NULL;
@@ -142,8 +146,6 @@ namespace Isis {
 
     // m_sky = false;
     
-    Pvl &lab = *cube.label();
-   
     // Get the kernel group and load main kernels
     PvlGroup kernels = lab.findGroup("Kernels", Pvl::Traverse);
 
@@ -166,7 +168,6 @@ namespace Isis {
     m_usingNaif = !lab.hasObject("NaifKeywords") || noTables;
     m_usingAle = false; 
 
-    json isd; 
     //  Modified  to load planetary ephemeris SPKs before s/c SPKs since some
     //  missions (e.g., MESSENGER) may augment the s/c SPK with new planet
     //  ephemerides. (2008-02-27 (KJB))
@@ -178,15 +179,18 @@ namespace Isis {
           QString msg = "Falling back to ISIS generation of nadir pointing";
           throw IException(IException::Programmer, msg, _FILEINFO_);
         }
+        
+        if (isd == NULL){
+          // try using ALE
+          std::ostringstream kernel_pvl;
+          kernel_pvl << kernels;
 
-        // try using ALE
-        std::ostringstream kernel_pvl;
-        kernel_pvl << kernels;
+          json props;
+          props["kernels"] = kernel_pvl.str();
 
-        json props;
-        props["kernels"] = kernel_pvl.str();
+          isd = ale::load(lab.fileName().toStdString(), props.dump(), "isis");
+        }
 
-        isd = ale::load(cube.fileName().toStdString(), props.dump(), "isis");
         json aleNaifKeywords = isd["NaifKeywords"];
         m_naifKeywords = new PvlObject("NaifKeywords", aleNaifKeywords);
         
@@ -230,7 +234,6 @@ namespace Isis {
           load(kernels["Extra"], noTables);
         }
       }
-
      
       // Moved the construction of the Target after the NAIF kenels have been loaded or the 
       // NAIF keywords have been pulled from the cube labels, so we can find target body codes 
@@ -367,7 +370,7 @@ namespace Isis {
     m_sunPosition = new SpicePosition(10, m_target->naifBodyCode());
 
     // Check to see if we have nadir pointing that needs to be computed &
-    // See if we have table blobs to load
+    // See if we have table blobs to load 
     if (kernels["TargetPosition"][0].toUpper() == "TABLE") {
       Table t("SunPosition", lab.fileName(), lab);
       m_sunPosition->LoadCache(t);
@@ -387,7 +390,6 @@ namespace Isis {
       m_bodyRotation->LoadCache(isd["BodyRotation"]);
       solarLongitude();
     }
-
     //  We can't assume InstrumentPointing & InstrumentPosition exist, old
     //  files may be around with the old keywords, SpacecraftPointing &
     //  SpacecraftPosition.  The old keywords were in existance before the
@@ -433,8 +435,7 @@ namespace Isis {
     }
     
     NaifStatus::CheckErrors();
-  }
-
+  } 
 
   /**
    * Loads/furnishes NAIF kernel(s)
@@ -1342,10 +1343,14 @@ namespace Isis {
   
   double Spice::sunToBodyDist() const {
     std::vector<double> sunPosition = m_sunPosition->Coordinate();
-    std::vector<double> moonRotation = m_bodyRotation->Matrix();
-        
+    std::vector<double> bodyRotation = m_bodyRotation->Matrix();
+    
+    for(size_t i = 0; i < bodyRotation.size(); i++) {
+      std::cout << bodyRotation[i] << std::endl;
+    } 
+
     double sunPosFromTarget[3];
-    mxv_c(&moonRotation[0], &sunPosition[0], sunPosFromTarget);
+    mxv_c(&bodyRotation[0], &sunPosition[0], sunPosFromTarget);
         
     return vnorm_c(sunPosFromTarget);  
   }
