25de7c982b3c640fc348fcb51bb73aaf2ac4ad97
Stuart Sides <ssides@usgs.gov>
2020-01-31 14:34:58 -0700

    Initial PR for Europa Clipper ingest, models, jitter correction (#3661)
    * Add initial files needed for EIS/clipper (#3636)
    
    * First add of jitterfit application
    
    * Initial commit of jitterfit
    
    * Clipper (#3656)
    
    * Try again
    
    * Try again
    
    * Documentation improvements
    
    * Fixed issues from first review
    
    * Initial ClipperNacRollingShutterCameraModel and associated classes PR (#3657)
    
    * Adds NthOrderPolynomial class needed for jitterfit (for EIS) to ISIS
    
    * Remove incorrect documentation
    
    * Update unitTest output
    
    * Remove unnecessary Makefile
    
    * Adds NthOrderPolynomial class to ISIS needed for jitterfit for EIS (#3639)
    
    * Adds NthOrderPolynomial class needed for jitterfit (for EIS) to ISIS
    
    * Remove incorrect documentation
    
    * Update unitTest output
    
    * Remove unnecessary Makefile
    
    * Adds ingestion application eis2isis to ISIS (#3638)
    
    * Initial commit of eis2isis
    
    * Remove references to EISBlob in comments and remove unnecessary Makefiles
    
    * Initial EIS Camera model commit with associated classes
    
    * Convert initial RSCameraDetectorMap unit test to a gtest
    
    * Changes in response to review comments: add checks to make sure tables and keywords exist before setting
    
    * Updated with old-style unit tests for camera models
    
    * Clipper (#3658)
    
    * Try again
    
    * Try again
    
    * Documentation improvements
    
    * Fixed issues from first review
    
    * Add more documentation
    
    * Fixed compile warning
    
    * Fix typo.
    
    Co-authored-by: Kristin <kberry@usgs.gov>
    
    * Adds Europa Clipper WAC Framing Camera Model - initial. (#3660)
    
    * Adds NthOrderPolynomial class needed for jitterfit (for EIS) to ISIS
    
    * Remove incorrect documentation
    
    * Update unitTest output
    
    * Remove unnecessary Makefile
    
    * Adds NthOrderPolynomial class to ISIS needed for jitterfit for EIS (#3639)
    
    * Adds NthOrderPolynomial class needed for jitterfit (for EIS) to ISIS
    
    * Remove incorrect documentation
    
    * Update unitTest output
    
    * Remove unnecessary Makefile
    
    * Adds ingestion application eis2isis to ISIS (#3638)
    
    * Initial commit of eis2isis
    
    * Remove references to EISBlob in comments and remove unnecessary Makefiles
    
    * Initial checkin of WAC camera model and associated changes to eis2isis.
    
    * add truthdata
    
    Co-authored-by: Kristin <kberry@usgs.gov>

---
+++ isis/src/base/objs/NthOrderPolynomial/unitTest.cpp
@@ -0,0 +1,56 @@
+#include "NthOrderPolynomial.h"
+#include <iostream>
+#include "Preference.h"
+
+using namespace Isis;
+using namespace std;
+
+int main() {
+  Preference::Preferences(true);
+
+  NthOrderPolynomial b(3);
+  vector<double> coefs;
+  coefs.push_back(0.5);
+  coefs.push_back(0.5);
+  coefs.push_back(0.5);
+  b.SetCoefficients(coefs);
+
+  cout << "Name   = " << b.Name() << endl;
+  cout << "Ncoefs = " << b.Coefficients() << endl;
+  cout << "Vars   = " << b.Variables() << endl;
+  for(int i = 0; i < b.Coefficients(); i++) {
+    cout << b.Coefficient(i) << endl;
+  }
+
+  cout << "---" << endl;
+  vector<double> vars;
+  vars.push_back(2.0);
+  vars.push_back(3.0);
+  cout << b.Evaluate(vars) << endl;
+  for(int i = 0; i < b.Coefficients(); i++) {
+    cout << b.Term(i) << endl;
+  }
+
+  cout << "---" << endl;
+  vars[0] = 1.0;
+  vars[1] = -2.0;
+  cout << b.Evaluate(vars) << endl;
+  for(int i = 0; i < b.Coefficients(); i++) {
+    cout << b.Term(i) << endl;
+  }
+
+  NthOrderPolynomial c(6);
+  cout << "--------" << endl;
+  cout << "Name   = " << c.Name() << endl;
+  cout << "Ncoefs = " << c.Coefficients() << endl;
+  cout << "Vars   = " << c.Variables() << endl;
+  coefs.push_back(1.0);
+  coefs.push_back(1.0);
+  coefs.push_back(1.0);
+
+  c.SetCoefficients(coefs);
+  cout << c.Evaluate(vars) << endl;
+  for(int i = 0; i < c.Coefficients(); i++) {
+    cout << c.Term(i) << endl;
+  }
+}
