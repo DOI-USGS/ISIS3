1ed0037d3e0e51871ba41947de1c491b8aa850b3
Kaitlyn Lee <kdl222@nau.edu>
2020-02-13 15:01:19 -0700

    Added default test case.

--- isis/tests/CamptTests.cpp
+++ isis/tests/CamptTests.cpp
@@ -1,7 +1,9 @@
 #include <QTemporaryFile>
-#include "campt.h"
 
+#include "campt.h"
 #include "Fixtures.h"
+#include "Pvl.h"
+#include "PvlGroup.h"
 #include "TestUtilities.h"
 
 #include "gmock/gmock.h"
@@ -10,7 +12,7 @@ using namespace Isis;
 
 QString CAMPT_XML = FileName("$ISISROOT/bin/xml/campt.xml").expanded();
 
-TEST_F(TestCube, BadColumnError) {
+TEST_F(DefaultCube, BadColumnError) {
   // set up bad coordinates file
   std::ofstream of;
   QTemporaryFile badList;
@@ -40,7 +42,7 @@ TEST_F(TestCube, BadColumnError) {
 }
 
 
-TEST_F(TestCube, FlatFileError) {
+TEST_F(DefaultCube, FlatFileError) {
   // configure UserInterface arguments for flat file error
   QVector<QString> args = {"format=flat"};
   UserInterface options(CAMPT_XML, args);
@@ -57,4 +59,28 @@ TEST_F(TestCube, FlatFileError) {
   catch(...) {
     FAIL() << "Expected an IException with message: \"Flat file must have a name.\"";
   }
+}
+
+TEST_F(DefaultCube, DefaultParameters) {
+  QVector<QString> args = {};
+  UserInterface options(CAMPT_XML, args);
+  Pvl appLog;
+
+  campt(testCube, options, &appLog);
+  PvlGroup groundPoint = appLog.findGroup("GroundPoint");
+
+  EXPECT_PRED_FORMAT2(AssertQStringsEqual, appLog.findKeyword("Filename"), ".cub");
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Sample"), 602.0);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Line"), 528.0);
+  EXPECT_PRED_FORMAT2(AssertQStringsEqual, appLog.findKeyword("PixelValue"), "Null");
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("RightAscension"), 310.2070335306);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Declination"), -46.327246785573);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PlanetocentricLatitude"), 10.181441241544);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PlanetographicLatitude"), 10.299790241741);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveEast360Longitude"), 255.89292858176);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveEast180Longitude"), -104.10707141824);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveWest360Longitude"), 104.10707141824);
+  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveWest180Longitude"), 104.10707141824);
+
+  std::cout<<appLog<<std::endl;
 }
