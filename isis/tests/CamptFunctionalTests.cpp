f7ee1d4061e8b5777f7b829a0163d6cf1f74ed5f
Kaitlyn Lee <kdl222@nau.edu>
2020-02-18 10:16:14 -0700

    Changed expect_double_eq to expect_near.

--- isis/tests/CamptFunctionalTests.cpp
+++ isis/tests/CamptFunctionalTests.cpp
@@ -72,68 +72,68 @@ TEST_F(DefaultCube, FunctionalTestCamptDefaultParameters) {
   EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Sample"), 602.0);
   EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Line"), 528.0);
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, groundPoint.findKeyword("PixelValue"), "Null");
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("RightAscension"), 310.2070335306);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Declination"), -46.327246785573);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PlanetocentricLatitude"), 10.181441241544);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PlanetographicLatitude"), 10.299790241741);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveEast360Longitude"), 255.89292858176);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveEast180Longitude"), -104.10707141824);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveWest360Longitude"), 104.10707141824);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("PositiveWest180Longitude"), 104.10707141824);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("RightAscension"), 310.2070335306, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("Declination"), -46.327246785573, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PlanetocentricLatitude"), 10.181441241544, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PlanetographicLatitude"), 10.299790241741, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PositiveEast360Longitude"), 255.89292858176, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PositiveEast180Longitude"), -104.10707141824, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PositiveWest360Longitude"), 104.10707141824, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("PositiveWest180Longitude"), 104.10707141824, 1e-8);
   
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[0]), -818.59644749774);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[1]), -3257.2675597135);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[2]),  603.17640797124);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[0]), -818.59644749774, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[1]), -3257.2675597135, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("BodyFixedCoordinate")[2]),  603.17640797124, 1e-8);
 
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("LocalRadius"), 3412288.6569795);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SampleResolution"), 18.904248467739);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("LineResolution"), 18.904248467739);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("ObliqueDetectorResolution"), 19.336214219327);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("ObliquePixelResolution"), 19.336214219327);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("ObliqueLineResolution"), 19.336214219327);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("ObliqueSampleResolution"), 19.336214219327);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("LocalRadius"), 3412288.6569795, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SampleResolution"), 18.904248467739, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("LineResolution"), 18.904248467739, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("ObliqueDetectorResolution"), 19.336214219327, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("ObliquePixelResolution"), 19.336214219327, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("ObliqueLineResolution"), 19.336214219327, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("ObliqueSampleResolution"), 19.336214219327, 1e-8);
 
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SpacecraftPosition")[0]), -1152.8979327717);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SpacecraftPosition")[1]), -3930.9421518203);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SpacecraftPosition")[2]),  728.14118380775);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SpacecraftPosition")[0]), -1152.8979327717, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SpacecraftPosition")[1]), -3930.9421518203, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SpacecraftPosition")[2]),  728.14118380775, 1e-8);
 
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SpacecraftAzimuth"), 240.08514246657);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SlantDistance"), 762.37204454685);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("TargetCenterDistance"), 4160.7294345949);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSpacecraftLatitude"), 10.078847382918);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSpacecraftLongitude"), 253.65422317887);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SpacecraftAltitude"), 753.22374841704);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("OffNadirAngle"), 9.9273765143684);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSpacecraftGroundAzimuth"), 267.5318718687);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SpacecraftAzimuth"), 240.08514246657, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SlantDistance"), 762.37204454685, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("TargetCenterDistance"), 4160.7294345949, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSpacecraftLatitude"), 10.078847382918, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSpacecraftLongitude"), 253.65422317887, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SpacecraftAltitude"), 753.22374841704, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("OffNadirAngle"), 9.9273765143684, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSpacecraftGroundAzimuth"), 267.5318718687, 1e-8);
 
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SunPosition")[0]),  147591102.63158);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SunPosition")[1]), -127854342.1274);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("SunPosition")[2]), -81844199.02275);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SunPosition")[0]),  147591102.63158, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SunPosition")[1]), -127854342.1274, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("SunPosition")[2]), -81844199.02275, 1e-8);
 
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSolarAzimuth"), 92.033828156965);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SolarDistance"), 1.4153000672557);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSolarLatitude"), -22.740326163641);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSolarLongitude"), 319.09846558533);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SubSolarGroundAzimuth"), 118.87356333938);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Phase"), 80.528381932125);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Incidence"), 70.127983116628);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("Emission"), 12.133564327344);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("NorthAzimuth"), 332.65918493997);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSolarAzimuth"), 92.033828156965, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SolarDistance"), 1.4153000672557, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSolarLatitude"), -22.740326163641, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSolarLongitude"), 319.09846558533, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SubSolarGroundAzimuth"), 118.87356333938, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("Phase"), 80.528381932125, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("Incidence"), 70.127983116628, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("Emission"), 12.133564327344, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("NorthAzimuth"), 332.65918493997, 1e-8);
   
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("EphemerisTime"), -709401200.26114);
-  EXPECT_TRUE( (QString) groundPoint.findKeyword("UTC") == "1977-07-09T20:05:51.5549999");
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("LocalSolarTime"), 7.7862975330952);
-  EXPECT_DOUBLE_EQ( (double) groundPoint.findKeyword("SolarLongitude"), 294.73518830595);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("EphemerisTime"), -709401200.26114, 1e-8);
+  EXPECT_PRED_FORMAT2(AssertQStringsEqual, groundPoint.findKeyword("UTC"), "1977-07-09T20:05:51.5549999");
+  EXPECT_NEAR( (double) groundPoint.findKeyword("LocalSolarTime"), 7.7862975330952, 1e-8);
+  EXPECT_NEAR( (double) groundPoint.findKeyword("SolarLongitude"), 294.73518830595, 1e-8);
 
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[0]),  0.43850176257802);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[1]),  0.88365594846443);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[2]), -0.16391573737569);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[0]),  0.43850176257802, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[1]),  0.88365594846443, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionBodyFixed")[2]), -0.16391573737569, 1e-8);
 
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[0]),  0.44577814515745);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[1]), -0.52737586689974);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[2]), -0.72329561059897);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[0]),  0.44577814515745, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[1]), -0.52737586689974, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionJ2000")[2]), -0.72329561059897, 1e-8);
 
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionCamera")[0]), -1.27447324380581e-04);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionCamera")[1]),  2.5816511718707e-05);
-  EXPECT_DOUBLE_EQ( toDouble(groundPoint.findKeyword("LookDirectionCamera")[2]),  0.99999999154535);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionCamera")[0]), -1.27447324380581e-04, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionCamera")[1]),  2.5816511718707e-05, 1e-8);
+  EXPECT_NEAR( toDouble(groundPoint.findKeyword("LookDirectionCamera")[2]),  0.99999999154535, 1e-8);
 }
