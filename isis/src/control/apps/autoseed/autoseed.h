fbbf2bb6658693e399c7bb353e5ce6eeb6ff422c
Adam Paquette <acpaquette@usgs.gov>
2020-02-19 16:24:30 -0700

    Moved enum into the namespace

--- isis/src/control/apps/autoseed/autoseed.h
+++ isis/src/control/apps/autoseed/autoseed.h
@@ -5,12 +5,12 @@
 #include "Pvl.h"
 #include "UserInterface.h"
 
-enum SeedDomain {
-  XY,
-  SampleLine
-};
-
 namespace Isis {
+  enum SeedDomain {
+    XY,
+    SampleLine
+  };
+
   extern void autoseed(UserInterface &ui,
                        Pvl *log = nullptr);
   extern void autoseed(UserInterface &ui, SerialNumberList &serialNumbers, ControlNet *precnet = nullptr, Pvl *log=nullptr);
