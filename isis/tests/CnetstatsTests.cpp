bec998f97a2c9fcdd11150eedba59f1937d3934e
Kaitlyn Lee <kdl222@nau.edu>
2020-02-20 13:19:54 -0700

    Added more tests.

--- isis/tests/CnetstatsTests.cpp
+++ isis/tests/CnetstatsTests.cpp
@@ -1,5 +1,9 @@
 #include "cnetstats.h"
 
+#include <QTemporaryFile>
+#include <QTextStream>
+#include <QStringList>
+
 #include "Fixtures.h"
 #include "PvlGroup.h"
 #include "TestUtilities.h"
@@ -61,4 +65,138 @@ TEST_F(ThreeImageNetwork, FunctionalTestCnetstatsDefault) {
   EXPECT_NEAR( (double) cnetSummary.findKeyword("MinConvexHullRatio"), 0.40388096067313, 1e-10);
   EXPECT_NEAR( (double) cnetSummary.findKeyword("MaxConvexHullRatio"), 0.60732301576372, 1e-10);
   EXPECT_NEAR( (double) cnetSummary.findKeyword("AvgConvexHullRatio"), 0.47889221036267, 1e-10);
+}
+
+TEST_F(ThreeImageNetwork, FunctionalTestCnetstatsImageStats) {
+  // Setup output file
+  QTemporaryFile statsFile;
+  ASSERT_TRUE(statsFile.open());
+
+  QVector<QString> args = {"create_image_stats=yes", "image_stats_file=" + statsFile.fileName()};
+  UserInterface options(APP_XML, args);
+  Pvl log;
+  QString serialNumList = cubeListTempPath.fileName();
+
+  cnetstats(*network, serialNumList, options, &log);
+
+  // No need to check specific values because the ControlNetStatistics unit test should check them.
+  QTextStream stream(&statsFile);
+  while(!stream.atEnd()) {
+    QString line = stream.readLine();
+    QStringList values = line.split(",");
+    ASSERT_DOUBLE_EQ(values.size(), 9);
+  }
+}
+
+TEST_F(ThreeImageNetwork, FunctionalTestCnetstatsPointStats) {
+  // Setup output file
+  QTemporaryFile statsFile;
+  ASSERT_TRUE(statsFile.open());
+
+  QVector<QString> args = {"create_point_stats=yes", "point_stats_file=" + statsFile.fileName()};
+  UserInterface options(APP_XML, args);
+  Pvl log;
+  QString serialNumList = cubeListTempPath.fileName();
+
+  cnetstats(*network, serialNumList, options, &log);
+
+  // No need to check specific values because the ControlNetStatistics unit test should check them.
+  int lineNumber = 0;
+  QTextStream stream(&statsFile);
+  while(!stream.atEnd()) {
+    QString line = stream.readLine();
+    QStringList values = line.split(",");
+
+    if(lineNumber == 0) {
+      // The output adds a comma at the end of the first line, increasing the size by 1.
+      // This is a bug in ControlNetStatistics. Once that is fixed, this test wil fail.
+      ASSERT_DOUBLE_EQ(values.size() - 1, 8);
+    }
+    else {
+      ASSERT_DOUBLE_EQ(values.size(), 8);
+    }
+    lineNumber++;
+  }
+}
+
+TEST_F(ThreeImageNetwork, FunctionalTestCnetstatsCubeFilter) {
+  // Setup def file
+  QTemporaryFile defFile("XXXXXX.def");
+  ASSERT_TRUE(defFile.open());
+
+  std::ofstream ofstream;
+  ofstream.open(defFile.fileName().toStdString());
+  ofstream << "Object = Filters\n\tGroup = Cube_NumPoints\n\t\tGreaterThan = 15\n\tEndGroup\nEndObject";
+  ofstream.close();
+
+  // Setup output flat file
+  QTemporaryFile flatFile;
+  ASSERT_TRUE(flatFile.open());
+
+  QVector<QString> args = {"filter=yes", 
+                           "deffile=" + defFile.fileName(), 
+                           "flatfile=" + flatFile.fileName()};
+  UserInterface options(APP_XML, args);
+  Pvl log;
+  QString serialNumList = cubeListTempPath.fileName();
+
+  cnetstats(*network, serialNumList, options, &log);
+
+  // No need to check specific values because the ControlNetFilter unit test should check them.
+  int lineNumber = 0;
+  QTextStream stream(&flatFile);
+  while(!stream.atEnd()) {
+    QString line = stream.readLine();
+    QStringList values = line.split(",");
+
+    if(lineNumber == 0) {
+      // The output adds a comma at the end of the first line, increasing the size by 1.
+      // This is a bug in ControlNetFilter. Once that is fixed, this test wil fail.
+      ASSERT_DOUBLE_EQ(values.size() - 1, 9);
+    }
+    else {
+      ASSERT_DOUBLE_EQ(values.size(), 9);
+    }
+    lineNumber++;
+  }
+  // Make sure the filter was applied
+  ASSERT_DOUBLE_EQ(lineNumber, 2);
+}
+
+TEST_F(ThreeImageNetwork, FunctionalTestCnetstatsPointFilter) {
+  // Setup def file
+  QTemporaryFile defFile("XXXXXX.def");
+  ASSERT_TRUE(defFile.open());
+
+  std::ofstream ofstream;
+  ofstream.open(defFile.fileName().toStdString());
+  ofstream << "Object = Filters\n\tGroup = Point_NumMeasures\n\t\tLessThan = 2\n\tEndGroup\nEndObject";
+  ofstream.close();
+
+  // Setup output flat file
+  QTemporaryFile flatFile;
+  ASSERT_TRUE(flatFile.open());
+
+  QVector<QString> args = {"filter=yes", 
+                           "deffile=" + defFile.fileName(), 
+                           "flatfile=" + flatFile.fileName()};
+  UserInterface options(APP_XML, args);
+  Pvl log;
+  QString serialNumList = cubeListTempPath.fileName();
+
+  cnetstats(*network, serialNumList, options, &log);
+
+  // No need to check specific values because the ControlNetFilter unit test should check them.
+  int lineNumber = 0;
+  QTextStream stream(&flatFile);
+  while(!stream.atEnd()) {
+    QString line = stream.readLine();
+    QStringList values = line.split(",");
+    std::cout<< line << std::endl;
+
+    ASSERT_DOUBLE_EQ(values.size(), 13);
+    lineNumber++;
+  }
+  // Make sure the filter was applied
+  ASSERT_DOUBLE_EQ(lineNumber, 15);
 }
