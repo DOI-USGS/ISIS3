6e640bbdf1bd2c5c5f360edd7535e4188999062c
Austin Sanders <arsanders@usgs.gov>
2020-02-21 09:41:41 -0700

    Added docstring.

--- isis/src/control/apps/cnetpvl2bin/cnetpvl2bin.cpp
+++ isis/src/control/apps/cnetpvl2bin/cnetpvl2bin.cpp
@@ -10,7 +10,21 @@ namespace Isis{
     cnetpvl2bin(cnet, ui, progress);
   }
 
+
+  /**
+    * Give a control network and criteria passed in through the UI, write the
+    * control network to a binary file.
+    *
+    * @param cnet A control network object.
+    *
+    * @param ui UserInterface file generated using the cnetpvl2bin file.
+    *
+    * @param progress A Progress object through which the UI can access progress
+    *                 reports.
+
+  */
   void cnetpvl2bin(ControlNet &cnet, UserInterface &ui, Progress *progress) {
+
     progress->SetText("Writing Control Network...");
     progress->SetMaximumSteps(1);
     progress->CheckStatus();
