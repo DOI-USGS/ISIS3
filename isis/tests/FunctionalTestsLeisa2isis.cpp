b6ad70b481128b7d89c6155b78ee4900c0f559c9
Kelvin Rodriguez <kr788@nau.edu>
2020-02-24 13:01:03 -0700

    fixed building errors (#3707)

--- isis/tests/FunctionalTestsLeisa2isis.cpp
+++ isis/tests/FunctionalTestsLeisa2isis.cpp
@@ -63,6 +63,7 @@ TEST(leisa2isisTest, leisa2isisTestDefault) {
 
   // BandBin Group
   // Check size, first, 2 middle, and last values? Enough?
+  PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
   ASSERT_EQ(bandbin["Center"].size(), 256);
   ASSERT_EQ(bandbin["Width"].size(), 256);
   ASSERT_EQ(bandbin["OriginalBand"].size(), 256);
