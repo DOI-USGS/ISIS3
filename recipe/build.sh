b02ad983d571d7d1c683757e07d51ce327b52a20
Lauren Adoram-Kershner <42873279+ladoramkershner@users.noreply.github.com>
2020-03-04 14:13:56 -0700

    Updates for 3.10.1 (#3730)
    * Fixes ddd2isis to support updated uvflat files (#3719)
    
    * Fixes ddd2isis to support updated uvflat files
    
    * Added history comment
    
    * Fix for QMOS not updating lat/lon ranges correctly.  (#3714)
    
    * qqqqqqmoooooosssssss
    
    * added doc string
    
    * Prep for 3.10.1
    
    Co-authored-by: acpaquette <acp263@nau.edu>
    Co-authored-by: Kelvin Rodriguez <kr788@nau.edu>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 mkdir build
 cd build
+export ISISROOT=$PWD
 cmake -GNinja -DJP2KFLAG=ON -Dpybindings=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX -Disis3Data=/usgs/cpkgs/isis3/data -Disis3TestData=/usgs/cpkgs/isis3/testData ../isis
 ninja install
