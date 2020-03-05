6f24c218b83fcf0803cdc0a715bf51560cf61e12
Adam Paquette <acpaquette@usgs.gov>
2020-03-05 16:48:16 -0700

    Moved cam2map gui helpers into the main.cpp

--- isis/src/base/apps/cam2map/cam2map.cpp
+++ isis/src/base/apps/cam2map/cam2map.cpp
@@ -1,6 +1,5 @@
-#define GUIHELPERS
-
 #include "cam2map.h"
+
 #include "Camera.h"
 #include "IException.h"
 #include "IString.h"
@@ -14,22 +13,6 @@
 using namespace std;
 
 namespace Isis {
-
-  void printMap();
-  void loadMapRes();
-  void loadCameraRes();
-  void loadMapRange();
-  void loadCameraRange();
-
-  map <QString, void *> GuiHelpers() {
-    map <QString, void *> helper;
-    helper ["PrintMap"] = (void *) printMap;
-    helper ["LoadMapRes"] = (void *) loadMapRes;
-    helper ["LoadCameraRes"] = (void *) loadCameraRes;
-    helper ["LoadMapRange"] = (void *) loadMapRange;
-    helper ["LoadCameraRange"] = (void *) loadCameraRange;
-    return helper;
-  }
 
   // Global variables
   void bandChange(const int band);
@@ -260,10 +243,11 @@ namespace Isis {
 
     // Output the mapping group used to the Gui session log
     PvlGroup cleanMapping = outmap->Mapping();
-    Application::GuiLog(cleanMapping);
 
     // Allocate the output cube and add the mapping labels
-    Cube *ocube = p.SetOutputCube("TO", samples, lines, icube->bandCount());
+    QString fname = ui.GetFileName("TO");
+    Isis::CubeAttributeOutput &atts = ui.GetOutputAttribute("TO");
+    Cube *ocube = p.SetOutputCube(fname, atts, samples, lines, icube->bandCount());
 
     ocube->putGroup(cleanMapping);
 
@@ -573,142 +557,5 @@ namespace Isis {
 
   void bandChange(const int band) {
     incam->SetBand(band);
-  }
-
-  // Helper function to print out mapfile to session log
-  void printMap() {
-    UserInterface &ui = Application::GetUserInterface();
-
-    // Get mapping group from map file
-    Pvl userMap;
-    userMap.read(ui.GetFileName("MAP"));
-    PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
-
-    //Write map file out to the log
-    Application::GuiLog(userGrp);
-  }
-
-  // Helper function to get mapping resolution.
-  void loadMapRes() {
-    UserInterface &ui = Application::GetUserInterface();
-
-    // Get mapping group from map file
-    Pvl userMap;
-    userMap.read(ui.GetFileName("MAP"));
-    PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
-
-    // Set resolution
-    if (userGrp.hasKeyword("Scale")) {
-      ui.Clear("RESOLUTION");
-      ui.PutDouble("RESOLUTION", userGrp["Scale"]);
-      ui.Clear("PIXRES");
-      ui.PutAsString("PIXRES", "PPD");
-    }
-    else if (userGrp.hasKeyword("PixelResolution")) {
-      ui.Clear("RESOLUTION");
-      ui.PutDouble("RESOLUTION", userGrp["PixelResolution"]);
-      ui.Clear("PIXRES");
-      ui.PutAsString("PIXRES", "MPP");
-    }
-    else {
-      QString msg = "No resolution value found in [" + ui.GetFileName("MAP") + "]";
-      throw IException(IException::User, msg, _FILEINFO_);
-    }
-  }
-
-  //Helper function to get camera resolution.
-  void loadCameraRes() {
-    UserInterface &ui = Application::GetUserInterface();
-    QString file = ui.GetFileName("FROM");
-
-    // Open the input cube, get the camera object, and the cam map projection
-    Cube c;
-    c.open(file);
-    Camera *cam = c.camera();
-    Pvl camMap;
-    cam->BasicMapping(camMap);
-    PvlGroup &camGrp = camMap.findGroup("Mapping");
-
-    ui.Clear("RESOLUTION");
-    ui.PutDouble("RESOLUTION", camGrp["PixelResolution"]);
-
-    ui.Clear("PIXRES");
-    ui.PutAsString("PIXRES", "MPP");
-  }
-
-  //Helper function to get ground range from map file.
-  void loadMapRange() {
-    UserInterface &ui = Application::GetUserInterface();
-
-    // Get map file
-    Pvl userMap;
-    userMap.read(ui.GetFileName("MAP"));
-    PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
-
-    // Set ground range keywords that are found in mapfile
-    int count = 0;
-    ui.Clear("MINLAT");
-    ui.Clear("MAXLAT");
-    ui.Clear("MINLON");
-    ui.Clear("MAXLON");
-    if (userGrp.hasKeyword("MinimumLatitude")) {
-      ui.PutDouble("MINLAT", userGrp["MinimumLatitude"]);
-      count++;
-    }
-    if (userGrp.hasKeyword("MaximumLatitude")) {
-      ui.PutDouble("MAXLAT", userGrp["MaximumLatitude"]);
-      count++;
-    }
-    if (userGrp.hasKeyword("MinimumLongitude")) {
-      ui.PutDouble("MINLON", userGrp["MinimumLongitude"]);
-      count++;
-    }
-    if (userGrp.hasKeyword("MaximumLongitude")) {
-      ui.PutDouble("MAXLON", userGrp["MaximumLongitude"]);
-      count++;
-    }
-
-    // Set default ground range param to map
-    ui.Clear("DEFAULTRANGE");
-    ui.PutAsString("DEFAULTRANGE", "MAP");
-
-    if (count < 4) {
-      QString msg = "One or more of the values for the ground range was not found";
-      msg += " in [" + ui.GetFileName("MAP") + "]";
-      throw IException(IException::User, msg, _FILEINFO_);
-    }
-  }
-
-  //Helper function to load camera range.
-  void loadCameraRange() {
-    UserInterface &ui = Application::GetUserInterface();
-    QString file = ui.GetFileName("FROM");
-
-    // Get the map projection file provided by the user
-    Pvl userMap;
-    userMap.read(ui.GetFileName("MAP"));
-
-    // Open the input cube, get the camera object, and the cam map projection
-    Cube c;
-    c.open(file);
-    Camera *cam = c.camera();
-
-    // Make the target info match the user mapfile
-    double minlat, maxlat, minlon, maxlon;
-    cam->GroundRange(minlat, maxlat, minlon, maxlon, userMap);
-
-    // Set ground range parameters in UI
-    ui.Clear("MINLAT");
-    ui.PutDouble("MINLAT", minlat);
-    ui.Clear("MAXLAT");
-    ui.PutDouble("MAXLAT", maxlat);
-    ui.Clear("MINLON");
-    ui.PutDouble("MINLON", minlon);
-    ui.Clear("MAXLON");
-    ui.PutDouble("MAXLON", maxlon);
-
-    // Set default ground range param to camera
-    ui.Clear("DEFAULTRANGE");
-    ui.PutAsString("DEFAULTRANGE", "CAMERA");
   }
 }
--- isis/src/base/apps/cam2map/main.cpp
+++ isis/src/base/apps/cam2map/main.cpp
@@ -1,3 +1,4 @@
+#define GUIHELPERS
 #include "Isis.h"
 
 #include "cam2map.h"
@@ -15,6 +16,22 @@
 using namespace std;
 using namespace Isis;
 
+void printMap();
+void loadMapRes();
+void loadCameraRes();
+void loadMapRange();
+void loadCameraRange();
+
+map <QString, void *> GuiHelpers() {
+  map <QString, void *> helper;
+  helper ["PrintMap"] = (void *) printMap;
+  helper ["LoadMapRes"] = (void *) loadMapRes;
+  helper ["LoadCameraRes"] = (void *) loadCameraRes;
+  helper ["LoadMapRange"] = (void *) loadMapRange;
+  helper ["LoadCameraRange"] = (void *) loadCameraRange;
+  return helper;
+}
+
 void IsisMain() {
   UserInterface &ui = Application::GetUserInterface();
   Pvl appLog;
@@ -31,4 +48,141 @@ void IsisMain() {
   for (auto grpIt = appLog.beginGroup(); grpIt!= appLog.endGroup(); grpIt++) {
     Application::Log(*grpIt);
   }
+}
+
+// Helper function to print out mapfile to session log
+void printMap() {
+  UserInterface &ui = Application::GetUserInterface();
+
+  // Get mapping group from map file
+  Pvl userMap;
+  userMap.read(ui.GetFileName("MAP"));
+  PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
+
+  //Write map file out to the log
+  Application::GuiLog(userGrp);
+}
+
+// Helper function to get mapping resolution.
+void loadMapRes() {
+  UserInterface &ui = Application::GetUserInterface();
+
+  // Get mapping group from map file
+  Pvl userMap;
+  userMap.read(ui.GetFileName("MAP"));
+  PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
+
+  // Set resolution
+  if (userGrp.hasKeyword("Scale")) {
+    ui.Clear("RESOLUTION");
+    ui.PutDouble("RESOLUTION", userGrp["Scale"]);
+    ui.Clear("PIXRES");
+    ui.PutAsString("PIXRES", "PPD");
+  }
+  else if (userGrp.hasKeyword("PixelResolution")) {
+    ui.Clear("RESOLUTION");
+    ui.PutDouble("RESOLUTION", userGrp["PixelResolution"]);
+    ui.Clear("PIXRES");
+    ui.PutAsString("PIXRES", "MPP");
+  }
+  else {
+    QString msg = "No resolution value found in [" + ui.GetFileName("MAP") + "]";
+    throw IException(IException::User, msg, _FILEINFO_);
+  }
+}
+
+//Helper function to get camera resolution.
+void loadCameraRes() {
+  UserInterface &ui = Application::GetUserInterface();
+  QString file = ui.GetFileName("FROM");
+
+  // Open the input cube, get the camera object, and the cam map projection
+  Cube c;
+  c.open(file);
+  Camera *cam = c.camera();
+  Pvl camMap;
+  cam->BasicMapping(camMap);
+  PvlGroup &camGrp = camMap.findGroup("Mapping");
+
+  ui.Clear("RESOLUTION");
+  ui.PutDouble("RESOLUTION", camGrp["PixelResolution"]);
+
+  ui.Clear("PIXRES");
+  ui.PutAsString("PIXRES", "MPP");
+}
+
+//Helper function to get ground range from map file.
+void loadMapRange() {
+  UserInterface &ui = Application::GetUserInterface();
+
+  // Get map file
+  Pvl userMap;
+  userMap.read(ui.GetFileName("MAP"));
+  PvlGroup &userGrp = userMap.findGroup("Mapping", Pvl::Traverse);
+
+  // Set ground range keywords that are found in mapfile
+  int count = 0;
+  ui.Clear("MINLAT");
+  ui.Clear("MAXLAT");
+  ui.Clear("MINLON");
+  ui.Clear("MAXLON");
+  if (userGrp.hasKeyword("MinimumLatitude")) {
+    ui.PutDouble("MINLAT", userGrp["MinimumLatitude"]);
+    count++;
+  }
+  if (userGrp.hasKeyword("MaximumLatitude")) {
+    ui.PutDouble("MAXLAT", userGrp["MaximumLatitude"]);
+    count++;
+  }
+  if (userGrp.hasKeyword("MinimumLongitude")) {
+    ui.PutDouble("MINLON", userGrp["MinimumLongitude"]);
+    count++;
+  }
+  if (userGrp.hasKeyword("MaximumLongitude")) {
+    ui.PutDouble("MAXLON", userGrp["MaximumLongitude"]);
+    count++;
+  }
+
+  // Set default ground range param to map
+  ui.Clear("DEFAULTRANGE");
+  ui.PutAsString("DEFAULTRANGE", "MAP");
+
+  if (count < 4) {
+    QString msg = "One or more of the values for the ground range was not found";
+    msg += " in [" + ui.GetFileName("MAP") + "]";
+    throw IException(IException::User, msg, _FILEINFO_);
+  }
+}
+
+//Helper function to load camera range.
+void loadCameraRange() {
+  UserInterface &ui = Application::GetUserInterface();
+  QString file = ui.GetFileName("FROM");
+
+  // Get the map projection file provided by the user
+  Pvl userMap;
+  userMap.read(ui.GetFileName("MAP"));
+
+  // Open the input cube, get the camera object, and the cam map projection
+  Cube c;
+  c.open(file);
+  Camera *cam = c.camera();
+
+  // Make the target info match the user mapfile
+  double minlat, maxlat, minlon, maxlon;
+  cam->GroundRange(minlat, maxlat, minlon, maxlon, userMap);
+
+  // Set ground range parameters in UI
+  ui.Clear("MINLAT");
+  ui.PutDouble("MINLAT", minlat);
+  ui.Clear("MAXLAT");
+  ui.PutDouble("MAXLAT", maxlat);
+  ui.Clear("MINLON");
+  ui.PutDouble("MINLON", minlon);
+  ui.Clear("MAXLON");
+  ui.PutDouble("MAXLON", maxlon);
+
+  // Set default ground range param to camera
+  ui.Clear("DEFAULTRANGE");
+  ui.PutAsString("DEFAULTRANGE", "CAMERA");
 }
