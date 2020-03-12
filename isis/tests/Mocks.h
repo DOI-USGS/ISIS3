7ff3aa329e96ee5a6542ff5393e7839abea8caee
Austin Sanders <arsanders@usgs.gov>
2020-03-11 17:03:40 -0700

    Added initial mock file.

--- isis/tests/Mocks.h
+++ isis/tests/Mocks.h
@@ -5,7 +5,11 @@
 
 #include "Cube.h"
 #include "Camera.h"
+#include "Interpolator.h"
+#include "Process.h"
+#include "ProcessRubberSheet.h"
 #include "TProjection.h"
+#include "Transform.h"
 
 using namespace Isis;
 
@@ -58,5 +62,23 @@ class MockTProjection : public TProjection {
     MOCK_METHOD(QString, Version, (), (const override));
     // MOCK_METHOD(int, SpkReferenceId, (), (const override));
 };
+
+class MockProcessRubberSheet : public ProcessRubberSheet {
+public:
+  MOCK_METHOD(void, StartProcess, (Transform &trans, Interpolator &interp), (override));
+  MOCK_METHOD(Cube*, SetOutputCube, (const QString &fname,
+                                     const Isis::CubeAttributeOutput &att,
+                                     const int ns, const int nl,
+                                     const int nb), (override));
+  MOCK_METHOD(void, SetInputCube, (Cube *inCube), (override));
+  MOCK_METHOD(void, processPatchTransform, (Transform &trans, Interpolator &interp), (override));
+  MOCK_METHOD(void, setPatchParameters, (int startSample, int startLine, int samples,
+                                         int lines, int sampleIncrement, int lineIncrement), (override));
+  MOCK_METHOD(void, forceTile, (double Samp, double Line));
+  MOCK_METHOD(void, SetTiling, (long long start, long long end));
+  MOCK_METHOD(void, EndProcess, (), (override));
+  MOCK_METHOD(void, BandChange, (void (*funct)(const int band)), (override));
+};
+
 
 #endif
