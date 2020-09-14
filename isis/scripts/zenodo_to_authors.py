92201e1cec43663c981403c3c42f37f33016bf85
Kristin <kberry@usgs.gov>
2020-09-14 10:43:27 -0700

    update based on more feedback

--- isis/scripts/zenodo_to_authors.py
+++ isis/scripts/zenodo_to_authors.py
@@ -6,33 +6,21 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser(description="Generate an AUTHORS.rst file from a .zenodo.json file")
-parser.add_argument("zenodo_input_file", nargs='?', type=str, 
-                    help="The file path to the .zenodo.json file.", default="$ISISROOT/../.zenodo.json")
-parser.add_argument("authors_output_file", nargs='?', type=str, 
-                    help="The path for the output AUTHORS.rst file.", default="$ISISROOT/../AUTHORS.rst")
+parser.add_argument("zenodo_input_file", type=str, help="The file path to the .zenodo.json file.")
+parser.add_argument("authors_output_file", type=str, help="The path for the output AUTHORS.rst file.")
 
 args = parser.parse_args()
 
 zenodo_path = args.zenodo_input_file
 authors_path = args.authors_output_file
 
-# Files to open
-if (("$ISISROOT" in args.zenodo_input_file) or ("$ISISROOT" in args.authors_output_file)):
-  if ("ISISROOT" in os.environ):
-    zenodo_path = os.path.expandvars(args.zenodo_input_file)
-    authors_path = os.path.expandvars(args.authors_output_file)
-  else:
-    print("Please set $ISISROOT and re-run this script or specify the input and output files.")
-    exit()
-
 # Read .zenodo.json file:
-lines = []
 try: 
   with open(zenodo_path, 'r') as zenodo_file:
     parsed_json = json.load(zenodo_file)
 except: 
-  print("{} could not be opened as the input file.".format(zenodo_path))
-  exit()
+  print("{} could not be read as the input file.".format(zenodo_path))
+  raise
 
 output_lines = ["ISIS3 Contributors", "==================\n"]
 
@@ -45,6 +33,10 @@ output_lines.append("\n-----")
 output_lines.append("This list was generated from the .zenodo.json file by running python zenodo_to_authors.py.")
 
 # Write to AUTHORS.rst file
-with open(authors_path, 'w') as authors_file:
-   authors_file.write('\n'.join(output_lines))
+try:
+  with open(authors_path, 'w') as authors_file:
+    authors_file.write('\n'.join(output_lines))
+except:
+  print("{} could not be opened as the output file.".format(authors_path))
+  raise
 
