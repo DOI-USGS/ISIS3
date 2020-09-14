114d0b22b6bae88f403a48d2267890b0b0cd5d18
Kristin <kberry@usgs.gov>
2020-09-14 09:35:09 -0700

    Removed json error in .zenodo.json and update script based on feedback

--- isis/scripts/zenodo_to_authors.py
+++ isis/scripts/zenodo_to_authors.py
@@ -1,29 +1,45 @@
 # Uses the .zenodo.json file to generate an AUTHORS.rst file. 
-# No arguments are necessary. Just set $ISISROOT and run: python zenodo_to_authors.py
+# No arguments are necessary if $ISISROOT is set.
 
 import os
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="Generate an AUTHORS.rst file from a .zenodo.json file")
+parser.add_argument("zenodo_input_file", nargs='?', type=str, 
+                    help="The file path to the .zenodo.json file.", default="$ISISROOT/../.zenodo.json")
+parser.add_argument("authors_output_file", nargs='?', type=str, 
+                    help="The path for the output AUTHORS.rst file.", default="$ISISROOT/../AUTHORS.rst")
+
+args = parser.parse_args()
+
+zenodo_path = args.zenodo_input_file
+authors_path = args.authors_output_file
 
 # Files to open
-if "ISISROOT" in os.environ:
-    zenodo_path = os.path.expandvars("$ISISROOT/../.zenodo.json")
-    authors_path = os.path.expandvars("$ISISROOT/../AUTHORS.rst")
-else:
-    print("Please set $ISISROOT and re-run this script.")
+if (("$ISISROOT" in args.zenodo_input_file) or ("$ISISROOT" in args.authors_output_file)):
+  if ("ISISROOT" in os.environ):
+    zenodo_path = os.path.expandvars(args.zenodo_input_file)
+    authors_path = os.path.expandvars(args.authors_output_file)
+  else:
+    print("Please set $ISISROOT and re-run this script or specify the input and output files.")
     exit()
 
 # Read .zenodo.json file:
 lines = []
-with open(zenodo_path, 'r') as zenodo_file:
-   lines = zenodo_file.readlines() 
-  
+try: 
+  with open(zenodo_path, 'r') as zenodo_file:
+    parsed_json = json.load(zenodo_file)
+except: 
+  print("{} could not be opened as the input file.".format(zenodo_path))
+  exit()
+
 output_lines = ["ISIS3 Contributors", "==================\n"]
 
-for line in lines:
-    # Pull the names out of the zenodo file and reformat 
-    if "\"name\"" in line:
-        name = line.strip().split(":")[1].strip("\", ")
-        last, first = name.split(",")
-        output_lines.append("- {} {}".format(first.strip(),last.strip()))
+for elt in parsed_json['creators']:
+  name = elt['name']
+  last, first = name.split(",")
+  output_lines.append("- {} {}".format(first.strip(),last.strip()))
 
 output_lines.append("\n-----")
 output_lines.append("This list was generated from the .zenodo.json file by running python zenodo_to_authors.py.")
