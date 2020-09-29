4f6fc5929ac2e96fe62be8d68335e559bc77fcd9
Adam Paquette <acpaquette@usgs.gov>
2020-09-29 10:23:37 -0700

    Updated cube list name

--- isis/tests/FuntionalTestsFindImageOverlaps.cpp
+++ isis/tests/FuntionalTestsFindImageOverlaps.cpp
@@ -67,7 +67,7 @@ TEST_F(ThreeImageNetwork, FunctionalTestFindImageOverlapsNoOverlap) {
   cube1->close();
   cube2->close();
 
-  QString cubeListPath = tempDir.path() + "/cubes.lis";
+  QString cubeListPath = tempDir.path() + "/newCubes.lis";
   cubes.write(cubeListPath);
   QVector<QString> args = {"from=" + cubeListPath, "overlapList=" + tempDir.path() + "/overlaps.txt"};
   UserInterface options(APP_XML, args);
