dc14e8a8eced3cc56bc65796a5d236367df4d687
Kelvin <kelvinrr@icloud.com>
2020-10-19 12:52:26 -0700

    opened window for more cosistant tests and removed a rx point for faster tests

--- isis/tests/FunctionalTestsApolloFindRx.cpp
+++ isis/tests/FunctionalTestsApolloFindRx.cpp
@@ -17,20 +17,16 @@ TEST_F(LargeCube, FunctionalTestApolloFindRxDefault) {
   PvlKeyword samples = PvlKeyword("Sample", "200");
   samples += "400"; 
   samples += "600"; 
-  samples += "800"; 
 
   PvlKeyword lines = PvlKeyword("Line", "200");
   lines += "400"; 
   lines += "600"; 
-  lines += "800"; 
 
   PvlKeyword types = PvlKeyword("Type", "5");
   types += "5"; 
   types += "5"; 
-  types += "5"; 
 
   PvlKeyword valid = PvlKeyword("Valid", "1");
-  valid += "1"; 
   valid += "1"; 
   valid += "1"; 
 
@@ -59,7 +55,7 @@ TEST_F(LargeCube, FunctionalTestApolloFindRxDefault) {
   testCube->reopen("r");
   
   QTemporaryDir prefix;
-  QVector<QString> args = {"tolerance=0.5", "patternsize=101", "deltax=2", "deltay=2"};
+  QVector<QString> args = {"tolerance=0.5", "patternsize=201"};
 
   UserInterface options(APP_XML, args);
   try {
@@ -73,22 +69,19 @@ TEST_F(LargeCube, FunctionalTestApolloFindRxDefault) {
 
   PvlGroup newReseaus = newLab.findObject("IsisCube").findGroup("Reseaus");
   PvlKeyword testKeyword = newReseaus.findKeyword("Line");
-  EXPECT_NEAR(testKeyword[0].toDouble(), 198.8, 0.0001);
-  EXPECT_NEAR(testKeyword[1].toDouble(), 388.8, 0.0001);
-  EXPECT_NEAR(testKeyword[2].toDouble(), 580.8, 0.0001);
-  EXPECT_NEAR(testKeyword[3].toDouble(), 744.8, 0.0001);
+  EXPECT_NEAR(testKeyword[0].toDouble(), 100.8141, 0.0001);
+  EXPECT_NEAR(testKeyword[1].toDouble(), 192.8, 0.0001);
+  EXPECT_NEAR(testKeyword[2].toDouble(), 275.8, 0.0001);
    
   testKeyword = newReseaus.findKeyword("Sample");
-  EXPECT_NEAR(testKeyword[0].toDouble(), 198.8, 0.0001);
-  EXPECT_NEAR(testKeyword[1].toDouble(), 388.8, 0.0001);
-  EXPECT_NEAR(testKeyword[2].toDouble(), 569.8, 0.0001);
-  EXPECT_NEAR(testKeyword[3].toDouble(), 742.8, 0.0001);
+  EXPECT_NEAR(testKeyword[0].toDouble(), 100.8141, 0.0001);
+  EXPECT_NEAR(testKeyword[1].toDouble(), 192.8, 0.0001);
+  EXPECT_NEAR(testKeyword[2].toDouble(), 167.8, 0.0001);
   
   testKeyword = newReseaus.findKeyword("Valid");
   EXPECT_EQ(testKeyword[0].toInt(), 1);
   EXPECT_EQ(testKeyword[1].toInt(), 1);
   EXPECT_EQ(testKeyword[2].toInt(), 1);
-  EXPECT_EQ(testKeyword[3].toInt(), 1);
 
   EXPECT_PRED_FORMAT2(AssertQStringsEqual, newReseaus.findKeyword("Status"), "Refined");
 
