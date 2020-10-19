29a5745fd5e9f25c0d620e027ee8e9acf9c0b4bc
Kelvin <kelvinrr@icloud.com>
2020-10-19 15:41:13 -0700

    swapped header funcs

--- isis/src/mro/apps/hicrop/hicrop.h
+++ isis/src/mro/apps/hicrop/hicrop.h
@@ -5,8 +5,8 @@
 #include "UserInterface.h"
 
 namespace Isis{
-  extern void hicrop(Cube *cube, UserInterface &ui, Pvl *log=nullptr);
   extern void hicrop(UserInterface &ui, Pvl *log=nullptr);
+  extern void hicrop(Cube *cube, UserInterface &ui, Pvl *log=nullptr);
 }
 
 #endif
