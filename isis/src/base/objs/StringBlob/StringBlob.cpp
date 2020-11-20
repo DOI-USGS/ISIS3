96086f643d10997a1ffe935f4851c4e39f19d70a
Jesse Mapel <jmapel@usgs.gov>
2020-11-20 08:25:14 -0700

    Updated StringBlob override methods

--- isis/src/base/objs/StringBlob/StringBlob.cpp
+++ isis/src/base/objs/StringBlob/StringBlob.cpp
@@ -1,6 +1,7 @@
 /**
  */
 #include <string>
+#include <QByteArray>
 #include "StringBlob.h"
 #include "Application.h"
 
@@ -41,22 +42,44 @@ namespace Isis {
    * Prepare to write string to output cube
    */
   void StringBlob::WriteInit() {
-    p_nbytes = m_string.size();
+    int bytes = m_string.size();
+
+    char *temp = p_buffer;
+    p_buffer = new char[p_nbytes+bytes];
+    if (temp != NULL) memcpy(p_buffer, temp, p_nbytes);
+    const char *ptr = m_string.c_str();
+    memcpy(&p_buffer[p_nbytes], (void *)ptr, bytes);
+    p_nbytes += bytes;
+
+    if (temp != NULL) delete [] temp;
   }
 
-
   /**
-   * Writes blob data to a stream
+   * Read binary data from an input stream into the string.
    *
-   * @param stream Output steam blob data will be written to
+   * @param stream The input stream to read from.
    *
-   * @throws IException::Io - Error writing data to stream
+   * @throws IException::Io - Error reading data from stream
    */
-  void StringBlob::WriteData(std::fstream &os) {
-    os.write(m_string.c_str(), p_nbytes);
-    if (!os.good()) {
-      QString msg = "Error writing data to " + p_type + " [" + p_blobName + "]";
+  void Blob::ReadData(std::istream &stream) {
+    // Read the binary data
+    if (p_buffer != NULL) delete [] p_buffer;
+    p_buffer = new char[p_nbytes];
+
+    streampos sbyte = p_startByte - 1;
+    stream.seekg(sbyte, std::ios::beg);
+    if (!stream.good()) {
+      QString msg = "Error preparing to read data from " + p_type +
+                   " [" + p_blobName + "]";
       throw IException(IException::Io, msg, _FILEINFO_);
     }
+
+    stream.read(p_buffer, p_nbytes);
+    if (!stream.good()) {
+      QString msg = "Error reading data from " + p_type + " [" + p_blobName + "]";
+      throw IException(IException::Io, msg, _FILEINFO_);
+    }
+
+    m_string = QByteArray(p_buffer, p_nbytes);
   }
 }
