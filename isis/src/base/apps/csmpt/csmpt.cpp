35673ecee3307279bb4cf69f15cbacc63710a1f4
Jesse Mapel <jmapel@usgs.gov>
2020-11-24 17:22:34 -0700

    Addressed PR comments

--- isis/src/base/apps/csmpt/csmpt.cpp
+++ isis/src/base/apps/csmpt/csmpt.cpp
@@ -1,9 +1,15 @@
+/** This is free and unencumbered software released into the public domain.
+
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
 #include "csmpt.h"
 
 #include <QList>
 #include <QString>
 #include <QStringList>
-#include <QLibrary>
 
 #include "csm/Model.h"
 #include "csm/RasterGM.h"
