405c9e81ea01fdbd7f6eea333adc28a2602557d7
Tim Giroux <tgiroux@contractor.usgs.gov>
2020-11-30 13:12:33 -0700

    add gtest file

--- isis/tests/FunctionalTestsMapmos.cpp
+++ isis/tests/FunctionalTestsMapmos.cpp
@@ -15,24 +15,22 @@ static QString APP_XML = FileName("$ISISROOT/bin/xml/mapmos.xml").expanded();
 
 TEST_F(ThreeImageNetwork, FunctionalTestMapmosDefault) {
   QTemporaryDir prefix;
-  QVector<QString> args = { "FROM=" + cube1->fileName(),
+  QVector<QString> args = { "FROM=" + cube1map->fileName(),
                             "MOSAIC=" + prefix.path() + "/mapmosOut.cub",
                             "create=true",
                             "track=true",
                             "priority=BAND",
                             "criteria=LESSER",
-                            "minlat=true",
-                            "maxlat=true",
-                            "minlon=true",
-                            "maxlon=true",
+                            "minlat=0",
+                            "maxlat=1",
+                            "minlon=0",
+                            "maxlon=1",
                             "highsaturation=true",
                             "lowsaturation=true",
                             "null=true" };
   UserInterface options(APP_XML, args);
   Pvl appLog;
 
-
-  cube1->addCachingAlgorithm(new UniqueIOCachingAlgorithm(2));
   mapmos(options, &appLog);
 
 }
