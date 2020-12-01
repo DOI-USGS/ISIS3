ad84f8d114158a671833a7db4f09dd8334ffc868
Kelvin <kelvinrr@icloud.com>
2020-12-01 13:13:10 -0700

    added ring automos tests

---
+++ isis/src/base/apps/ringsautomos/ringsautomos/ringsautomos.h
@@ -0,0 +1,11 @@
+#ifndef ringsautomos_h 
+#define ringsautomos_h
+
+#include "Cube.h"
+#include "UserInterface.h"
+
+namespace Isis{
+  extern void ringsautomos(UserInterface &ui, Pvl *log=nullptr);
+}
+
+#endif
