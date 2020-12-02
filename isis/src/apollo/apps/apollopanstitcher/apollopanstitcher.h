37564f1764c0737a5f1e79eefd23d1626b267899
Adam Paquette <acpaquette@usgs.gov>
2020-12-02 13:13:46 -0700

    Updated apollopanstitcher app format

---
+++ isis/src/apollo/apps/apollopanstitcher/apollopanstitcher.h
@@ -0,0 +1,11 @@
+#ifndef apollopanstitcher_h
+#define apollopanstitcher_h
+
+#include "Pvl.h"
+#include "UserInterface.h"
+
+namespace Isis{
+  extern void apolloPanSticher(UserInterface &ui);
+}
+
+#endif
