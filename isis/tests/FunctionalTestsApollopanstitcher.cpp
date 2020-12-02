dbdc1db6c993b89bd3e1e4a70d2bd3a9fafb30bf
Adam Paquette <acpaquette@usgs.gov>
2020-12-02 14:11:57 -0700

    Initial stab at the apollopanstitcher gtest

---
+++ isis/tests/FunctionalTestsApollopanstitcher.cpp
@@ -0,0 +1,35 @@
+#include <QTemporaryDir>
+#include <memory>
+
+#include "apollopanstitcher.h"
+#include "Fixtures.h"
+#include "Pvl.h"
+#include "PvlGroup.h"
+#include "TestUtilities.h"
+#include "Histogram.h"
+#include "Endian.h"
+#include "PixelType.h"
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "Fixtures.h"
+
+using namespace Isis;
+
+static QString APP_XML = FileName("$ISISROOT/bin/xml/apollopanstitcher.xml").expanded();
+
+TEST_F(TempTestingFiles, FunctionalTestsApollopanstitcherDefault) {
+  QVector<QString> args = {"file_base=$ISISTESTDATA/isis/src/apollo/apps/apollopanstitcher/tsts/default/input/AS15_P_0177R10",
+                           "to=" + tempDir.path() + "/reduced8.cub",
+                           "microns=50"};
+
+  UserInterface options(APP_XML, args);
+  try {
+   apolloPanStitcher(options);
+  }
+  catch (IException &e) {
+    FAIL() << "Unable to stitcher apollo images: " << e.toString().toStdString().c_str() << std::endl;
+  }
+  Cube outputCube(options.GetFileName("TO"));
+}
