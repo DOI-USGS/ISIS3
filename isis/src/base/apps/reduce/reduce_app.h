09b072aeca6b687606d1bcc556c2dfa79bfbe8e6
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2020-12-07 16:57:59 -0700

    removed comment per PR feedback

--- isis/src/base/apps/reduce/reduce_app.h
+++ isis/src/base/apps/reduce/reduce_app.h
@@ -1,4 +1,4 @@
-#ifndef reduceapp_h // Change this to your app name in all lower case suffixed with _h (e.g. campt_h, cam2map_h etc.)
+#ifndef reduceapp_h
 #define reduceapp_h
 
 #include "UserInterface.h"
