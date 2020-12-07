d3dba87dfd94dc903eb4eaea32b9b994210f481a
Jesse Mapel <jmapel@usgs.gov>
2020-12-06 21:00:04 -0700

    Added geometric model stuff to alternate

--- isis/tests/AlternativeTestCsmModel.cpp
+++ isis/tests/AlternativeTestCsmModel.cpp
@@ -4,8 +4,27 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
+const std::string AlternativeTestCsmModel::SENSOR_MODEL_NAME = "TestCsmModelName";
+const std::vector<std::string> AlternativeTestCsmModel::PARAM_NAMES = {
+  "TestParam1",
+  "TestParam2"
+};
+const std::vector<std::string> AlternativeTestCsmModel::PARAM_UNITS = {
+  "m",
+  "rad"
+};
+const std::vector<csm::param::Type> AlternativeTestCsmModel::PARAM_TYPES = {
+  csm::param::FICTITIOUS,
+  csm::param::REAL
+};
+const std::vector<csm::SharingCriteria> AlternativeTestCsmModel::PARAM_SHARING_CRITERIA = {
+  csm::SharingCriteria(),
+  csm::SharingCriteria()
+};
+
 AlternativeTestCsmModel::AlternativeTestCsmModel() {
   m_modelState = "AlternativeTestCsmModel_ModelState";
+  m_param_values.resize(AlternativeTestCsmModel::PARAM_NAMES.size(), 0.0);
 };
 
 AlternativeTestCsmModel::~AlternativeTestCsmModel() {
@@ -84,4 +103,110 @@ std::string AlternativeTestCsmModel::constructStateFromIsd(const csm::Isd isd){
   state["test_param_two"] = parsedIsd.at("test_param_two");
   state["test_param_three"] = parsedIsd.at("test_param_three");
   return state.dump();
+}
+
+csm::EcefCoord AlternativeTestCsmModel::getReferencePoint() const {
+  return csm::EcefCoord(0.0, 0.0, 0.0);
+}
+
+void AlternativeTestCsmModel::setReferencePoint(const csm::EcefCoord& groundPt) {
+  // do nothing for test
+}
+
+int AlternativeTestCsmModel::getNumParameters() const {
+  return m_param_values.size();
+}
+
+std::string AlternativeTestCsmModel::getParameterName(int index) const {
+  return AlternativeTestCsmModel::PARAM_NAMES[index];
+}
+
+
+std::string AlternativeTestCsmModel::getParameterUnits(int index) const {
+  return AlternativeTestCsmModel::PARAM_UNITS[index];
+}
+
+bool AlternativeTestCsmModel::hasShareableParameters() const {
+  return false;
+}
+
+bool AlternativeTestCsmModel::isParameterShareable(int index) const {
+  return false;
+}
+
+csm::SharingCriteria AlternativeTestCsmModel::getParameterSharingCriteria(int index) const {
+  return AlternativeTestCsmModel::PARAM_SHARING_CRITERIA[index];
+}
+
+double AlternativeTestCsmModel::getParameterValue(int index) const {
+  return m_param_values[index];
+}
+
+void AlternativeTestCsmModel::setParameterValue(int index, double value) {
+  m_param_values[index] = value;
+}
+
+csm::param::Type AlternativeTestCsmModel::getParameterType(int index) const {
+  return AlternativeTestCsmModel::PARAM_TYPES[index];
+}
+
+void AlternativeTestCsmModel::setParameterType(int index, csm::param::Type pType) {
+  // do nothing for test
+}
+
+double AlternativeTestCsmModel::getParameterCovariance(int index1,
+                                            int index2) const {
+  // default to identity covariance matrix
+  if (index1 == index2) {
+    return 1.0;
+  }
+  return 0.0;
+                              }
+void AlternativeTestCsmModel::setParameterCovariance(int index1,
+                                          int index2,
+                                          double covariance) {
+  // do nothing for test
+}
+
+int AlternativeTestCsmModel::getNumGeometricCorrectionSwitches() const {
+  return 0;
+}
+
+std::string AlternativeTestCsmModel::getGeometricCorrectionName(int index) const {
+  throw csm::Error(csm::Error::INDEX_OUT_OF_RANGE, "Index out of range.",
+                   "AlternativeTestCsmModel::getGeometricCorrectionName");
+}
+
+void AlternativeTestCsmModel::setGeometricCorrectionSwitch(int index,
+                                  bool value,
+                                  csm::param::Type pType) {
+  throw csm::Error(csm::Error::INDEX_OUT_OF_RANGE, "Index out of range.",
+                  "AlternativeTestCsmModel::setGeometricCorrectionSwitch");
+}
+
+bool AlternativeTestCsmModel::getGeometricCorrectionSwitch(int index) const {
+  throw csm::Error(csm::Error::INDEX_OUT_OF_RANGE, "Index out of range.",
+                   "AlternativeTestCsmModel::getGeometricCorrectionSwitch");
+}
+
+std::vector<double> AlternativeTestCsmModel::getCrossCovarianceMatrix(
+      const csm::GeometricModel& comparisonModel,
+      csm::param::Set pSet,
+      const csm::GeometricModel::GeometricModelList& otherModels) const {
+  const std::vector<int>& rowIndices = getParameterSetIndices(pSet);
+  size_t numRows = rowIndices.size();
+  const std::vector<int>& colIndices = comparisonModel.getParameterSetIndices(pSet);
+  size_t numCols = colIndices.size();
+  std::vector<double> covariance(numRows * numCols, 0.0);
+
+  if (&comparisonModel == this) {
+    for (size_t rowIndex = 0; rowIndex <  numRows; numRows++) {
+      for (size_t colIndex = 0; colIndex < numCols; colIndex++) {
+        covariance[rowIndex * numCols + colIndex] = getParameterCovariance(rowIndices[rowIndex],
+                                                                           colIndices[colIndex]);
+      }
+    }
+  }
+
+  return covariance;
 }
