7b6fe52ff540fc9b26dcd105b065daff4bbff632
Rebecca Leggett <roboticsgirl60@gmail.com>
2020-12-11 11:21:45 -0700

    moved main.cpp code into caminfo.cpp

---
+++ isis/src/base/apps/caminfo/caminfo.h
@@ -0,0 +1,12 @@
+#ifndef caminfo_h
+#define caminfo_h
+
+#include "UserInterface.h"
+
+namespace Isis{
+    void CamInfo(UserInterface &ui);
+
+    void CamInfo(Cube incube, UserInterface &ui);
+}
+
+#endif
