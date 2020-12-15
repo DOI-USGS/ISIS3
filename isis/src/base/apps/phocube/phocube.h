b4046165bfe7ac1d5450f43430ee6b405f70075d
Kaitlyn Lee <kdl222@nau.edu>
2020-12-15 09:29:11 -0700

    Converted phocube

---
+++ isis/src/base/apps/phocube/phocube.h
@@ -0,0 +1,12 @@
+#ifndef grid_h
+#define grid_h
+
+#include "Cube.h"
+#include "UserInterface.h"
+
+namespace Isis{
+  extern void phocube(Cube *icube, UserInterface &ui);
+  extern void phocube(UserInterface &ui);
+}
+
+#endif
