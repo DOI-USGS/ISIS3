87c8432b2c21f60610204b41a378f5c24464b6ca
robotprogrammer22 <46390033+robotprogrammer22@users.noreply.github.com>
2020-12-15 21:15:28 -0700

    Stretch functional tests (#4154)
    * stretch updates
    
    * pull request changes
    
    * modified setoutputcube to remove seg fault
    
    * merge with dev + fixed bug
    
    * updating test files
    
    * modified stretch app
    
    * updated mvix
    
    * modified setoutputcube functions with null ui parameter
    
    * modified stretch test and reverted changes involving setoutputcube
    
    * modified setoutputcube function call
    
    * made new setoutputcube for stretch
    
    * added +1 back to user interface, removed duplicate test
    
    * pull request changes
    
    Co-authored-by: Kelvin <kelvinrr@icloud.com>

--- isis/src/base/objs/Process/Process.h
+++ isis/src/base/objs/Process/Process.h
@@ -241,8 +241,11 @@ namespace Isis {
 
 
       virtual Isis::Cube *SetOutputCube(const QString &parameter);
+      virtual Isis::Cube *SetOutputCubeStretch(const QString &parameter, UserInterface *ui=nullptr);
       virtual Isis::Cube *SetOutputCube(const QString &parameter, const int nsamps,
                                 const int nlines, const int nbands = 1);
+      virtual Isis::Cube *SetOutputCubeStretch(const QString &parameter, const int nsamps,
+                                const int nlines, const int nbands = 1, UserInterface *ui=nullptr);
       virtual Isis::Cube *SetOutputCube(const QString &fname,
                                 const Isis::CubeAttributeOutput &att,
                                 const int nsamps, const int nlines,
