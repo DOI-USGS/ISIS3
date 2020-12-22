290d28056c3bedecad2b775c92536f2cb6d18727
ssides <ssides@usgs.gov>
2020-12-22 07:56:29 -0700

    add template file

--- isis/src/base/apps/topds4/topds4.cpp
+++ isis/src/base/apps/topds4/topds4.cpp
@@ -29,14 +29,18 @@ namespace Isis {
 
 
     json data;
+    data["IsisCube"]["Core"]["Dimensions"]["Samples"] = "1023";
+    data["IsisCube"]["Core"]["Dimensions"]["Lines"] = "1024";
     data["name"] = "world";
     render("Hello {{ name }}!", data); // Returns std::string "Hello world!"
     render_to(std::cout, "Hello {{ name }}!", data); // Writes "Hello world!" to stream
 
+    Environment env;
+    std::string inputTemplate = ui.GetFileName("TEMPLATE").toStdString();
+    std::string result = env.render_file(inputTemplate, data);
+    std::cout << result << std::endl;
 
-
-
-    std::cout << label << std::endl;
+    //std::cout << label << std::endl;
     return label.findGroup("Dimensions", Pvl::Traverse);
   }
 }
