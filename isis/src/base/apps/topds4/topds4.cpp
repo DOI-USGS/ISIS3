fbd20fcbc089e6a450b76f611f9de8f089f04037
Jesse Mapel <jmapel@usgs.gov>
2020-12-30 15:00:32 -0700

    Added callbacks and tests

--- isis/src/base/apps/topds4/topds4.cpp
+++ isis/src/base/apps/topds4/topds4.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <time.h>
 
 #include <inja/inja.hpp>
+
+#include "md5wrapper.h"
 
 #include "topds4.h"
 
@@ -26,17 +29,47 @@ namespace Isis {
 
     Pvl &label = *cube->label();
 
+    Environment renderEnv;
 
+    // Call back functions
+    /**
+     * Renders to the current UTC time formatted as YYYY-MM-DDTHH:MM:SS
+     */
+    renderEnv.add_callback("currentTime", 0, [](Arguments& args) {
+      time_t startTime = time(NULL);
+      struct tm *tmbuf = gmtime(&startTime);
+      char timestr[80];
+      strftime(timestr, 80, "%Y-%m-%dT%H:%M:%S", tmbuf);
+      return string(timestr);
+    });
+
+    /**
+     * Renders to the filename of the output img
+     */
+    renderEnv.add_callback("imageFileName", 0, [cube](Arguments& args) {
+      QString cubeFilename = cube->fileName().split("/").back();
+      return (cubeFilename.split(".")[0] + ".img").toStdString();
+    });
+
+    /**
+     * Renders to the MD5 hash for the input cube
+     */
+    renderEnv.add_callback("md5Hash", 0, [cube](Arguments& args) {
+      md5wrapper md5;
+      return md5.getHashFromFile(cube->fileName()).toStdString();
+    });
 
     json data;
     data["name"] = "world";
-    render("Hello {{ name }}!", data); // Returns std::string "Hello world!"
-    render_to(std::cout, "Hello {{ name }}!", data); // Writes "Hello world!" to stream
+    renderEnv.render("Hello {{ name }}!", data); // Returns std::string "Hello world!"
+    renderEnv.write(renderEnv.parse("{{ currentTime() }}\n"), data, ui.GetFileName("TO").toStdString());
+    // renderEnv.write(renderEnv.parse("{{ imageFileName() }}\n"), data, ui.GetFileName("TO").toStdString());
+    // renderEnv.write(renderEnv.parse("{{ md5Hash() }}\n"), data, ui.GetFileName("TO").toStdString());
+    renderEnv.render_to(std::cout, renderEnv.parse("{{ currentTime() }}\n"), data);
+    renderEnv.render_to(std::cout, renderEnv.parse("{{ imageFileName() }}\n"), data);
+    renderEnv.render_to(std::cout, renderEnv.parse("{{ md5Hash() }}\n"), data);
 
-
-
-
-    std::cout << label << std::endl;
+    // std::cout << label << std::endl;
     return label.findGroup("Dimensions", Pvl::Traverse);
   }
 }
