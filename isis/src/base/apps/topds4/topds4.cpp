cc2cbb79dc743311676989eb1755ba4febba63cd
Jesse Mapel <jmapel@usgs.gov>
2021-01-04 22:22:09 -0700

    Normalized topds4 and added tests (#4234)
    * Removed data source args and added app log
    
    * Added simple tests for topds4
    
    * Moved line

--- isis/src/base/apps/topds4/topds4.cpp
+++ isis/src/base/apps/topds4/topds4.cpp
@@ -19,53 +19,41 @@ using json = nlohmann::json;
 
 namespace Isis {
 
-
-
-  PvlGroup topds4(UserInterface &ui) {
+  void topds4(UserInterface &ui, Pvl *log) {
     Cube *icube = new Cube();
     icube->open(ui.GetFileName("FROM"));
-    return topds4(icube, ui);
+    topds4(icube, ui);
   }
 
-  PvlGroup topds4(Cube *icube, UserInterface &ui) {
+  void topds4(Cube *icube, UserInterface &ui, Pvl *log) {
 
     Process p;
     p.SetInputCube(icube);
 
     json dataSource;
 
-    // We will need the main label even if it is not used as a template data source
     Pvl &cubeLabel = *icube->label();
 
     // Add the input cube PVL label to template engine data
-    // *** TODO: make sure this label is inside a unique JSON element so nothing in it can
-    // conflict with other data sources
-    if (ui.GetBoolean("MAINLABEL")) {
-      dataSource["MainLabel"].update(pvlToJSON(cubeLabel));
-    }
+    dataSource["MainLabel"].update(pvlToJSON(cubeLabel));
 
     // Add the original label (from an ingestion app) to the template engine data
     // Wrap it in an OriginalLabel so existing elements don't get overwritten
-    // *** TODO: make sure this label is inside a unique JSON element so nothing in it can
-    // conflict with other data sources
-    if (ui.GetBoolean("ORIGINALLABEL")) {
-      if (cubeLabel.hasObject("OriginalLabel")) {
-        OriginalLabel origBlob;
-        icube->read(origBlob);
-        Pvl origLabel;
-        origLabel = origBlob.ReturnLabels();
-        dataSource["OriginalLabel"].update(pvlToJSON(origLabel));
-      }
-      else if (cubeLabel.hasObject("OriginalXmlLabel")) {
-        // get the xml label and add it to the template data
-      }
+    if (cubeLabel.hasObject("OriginalLabel")) {
+      OriginalLabel origBlob;
+      icube->read(origBlob);
+      Pvl origLabel;
+      origLabel = origBlob.ReturnLabels();
+      dataSource["OriginalLabel"].update(pvlToJSON(origLabel));
+    }
+    else if (cubeLabel.hasObject("OriginalXmlLabel")) {
+      // get the xml label and add it to the template data
     }
 
-    std::cout << "=================================" << std::endl;
+    std::cout << "=============Data================" << std::endl;
     std::cout << dataSource.dump(4) << std::endl;
     std::cout << "=================================" << std::endl;
 
-    std::string inputTemplate = ui.GetFileName("TEMPLATE").toStdString();
 
     Environment env;
     // Call back functions
@@ -95,15 +83,17 @@ namespace Isis {
       md5wrapper md5;
       return md5.getHashFromFile(icube->fileName()).toStdString();
     });
+
+    std::string inputTemplate = ui.GetFileName("TEMPLATE").toStdString();
+
     std::string result = env.render_file(inputTemplate, dataSource);
 
     std::ofstream outFile(ui.GetFileName("TO").toStdString());
     outFile << result;
     outFile.close();
 
+    std::cout << "============Result===============" << std::endl;
     std::cout << result << std::endl;
-
-    // TODO: return something useful
-    return cubeLabel.findGroup("Dimensions", Pvl::Traverse);
+    std::cout << "=================================" << std::endl;
   }
 }
