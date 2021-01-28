caec64be25fb1c8c9b5579d2431c05b5f85b4ef9
Jesse Mapel <jmapel@usgs.gov>
2021-01-28 12:38:02 -0700

    Changed psuedoinverse to Armadillo SVD

--- isis/tests/LinearAlgebraTests.cpp
+++ isis/tests/LinearAlgebraTests.cpp
@@ -45,3 +45,36 @@ TEST(LinearAlgebraTest, PsuedoinverseLeftInverse) {
 
   EXPECT_TRUE(LinearAlgebra::isIdentity(LinearAlgebra::multiply(inverse, mat)));
 }
+
+TEST(LinearAlgebraTest, PsuedoinverseSingular) {
+  LinearAlgebra::Matrix mat(3,2);
+  mat(0,0) = 1.0;
+  mat(0,1) = 2.0;
+  mat(1,0) = 3.0;
+  mat(1,1) = 2.0;
+  mat(2,0) = 4.0;
+  mat(2,1) = 6.0;
+
+  LinearAlgebra::Matrix inverse = LinearAlgebra::psuedoinverse(mat);
+
+  // Checks based on properties of the psuedoinverse
+  // M is the matrix
+  // M^+ is the psuedo inverse of M
+  // M^T is the transpose of M
+
+  // Check M * M^+ * M - M = 0
+  LinearAlgebra::Matrix firstProd = LinearAlgebra::multiply(mat, LinearAlgebra::multiply(inverse, mat));
+  EXPECT_TRUE(LinearAlgebra::isZero(firstProd - mat));
+
+  // Check M^+ * M * M^+ - M^+ = 0
+  LinearAlgebra::Matrix secondProd = LinearAlgebra::multiply(inverse, LinearAlgebra::multiply(mat, inverse));
+  EXPECT_TRUE(LinearAlgebra::isZero(secondProd - inverse));
+
+  // Check (M * M^+)^T = M * M^+
+  LinearAlgebra::Matrix thirdProd = LinearAlgebra::multiply(mat, inverse);
+  EXPECT_TRUE(LinearAlgebra::isZero(LinearAlgebra::transpose(thirdProd) - thirdProd));
+
+  // Check (M^+ * M)^T = M^+ * M
+  LinearAlgebra::Matrix fourthProd = LinearAlgebra::multiply(inverse, mat);
+  EXPECT_TRUE(LinearAlgebra::isZero(LinearAlgebra::transpose(fourthProd) - fourthProd));
+}
