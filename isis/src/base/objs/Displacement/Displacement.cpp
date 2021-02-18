552bf0748098cedd918206f48766b128da2ed34a
Kaitlyn Lee <kdl222@nau.edu>
2021-02-17 17:40:05 -0700

    Fixed error

--- isis/src/base/objs/Displacement/Displacement.cpp
+++ isis/src/base/objs/Displacement/Displacement.cpp
@@ -3,8 +3,7 @@ The authors of ISIS do not claim copyright on the contents of this file.
 For more details about the LICENSE terms and the AUTHORS, you will
 find files of those names at the top level of this repository. **/
 
-/* SPDX-License-Identifier: CC0-1.0 */ http://www.usgs.gov/privacy.html.
-
+/* SPDX-License-Identifier: CC0-1.0 */
 #include "Displacement.h"
 
 #include "Distance.h"
