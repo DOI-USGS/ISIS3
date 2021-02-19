1523fc5c64dc841a7e9c74b6943f0268bcaf56df
Kaitlyn Lee <kdl222@nau.edu>
2021-02-18 21:19:30 -0700

    Updated disclaimer in base objects (#4294)
    * Updated disclaimer in base objects.
    
    * Fixed error

--- isis/src/base/objs/Isis/IsisDebug.h
+++ isis/src/base/objs/Isis/IsisDebug.h
@@ -1,5 +1,11 @@
 #ifndef IsisDebug_h
 #define IsisDebug_h
+/** This is free and unencumbered software released into the public domain.
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
 
 #ifdef __GTHREADS
 #error *****IsisDebug.h MUST be included before any system header files!*****
--- isis/src/base/objs/PvlFlatMap/PvlFlatMap.h
+++ isis/src/base/objs/PvlFlatMap/PvlFlatMap.h
@@ -1,29 +1,11 @@
 #ifndef PvlFlatMap_h
 #define PvlFlatMap_h
-/**
- * @file                                                                  
- * $Revision: 6187 $ 
- * $Date: 2015-05-11 17:31:51 -0700 (Mon, 11 May 2015) $ 
- * $Id: PvlFlatMap.h 6187 2015-05-12 00:31:51Z kbecker@GS.DOI.NET $
- *
- *   Unless noted otherwise, the portions of Isis written by the USGS are
- *   public domain. See individual third-party library and package descriptions
- *   for intellectual property information, user agreements, and related
- *   information.
- *
- *   Although Isis has been used by the USGS, no warranty, expressed or
- *   implied, is made by the USGS as to the accuracy and functioning of such
- *   software and related material nor shall the fact of distribution
- *   constitute any such warranty, and no responsibility is assumed by the
- *   USGS in connection therewith.
- *
- *   For additional information, launch
- *   $ISISROOT/doc//documents/Disclaimers/Disclaimers.html
- *   in a browser or see the Privacy &amp; Disclaimers page on the Isis website,
- *   http://isis.astrogeology.usgs.gov, and the USGS privacy and disclaimers on
- *   http://www.usgs.gov/privacy.html.
- */
+/** This is free and unencumbered software released into the public domain.
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
 
+/* SPDX-License-Identifier: CC0-1.0 */
 // Qt library
 #include <QMap>
 #include <QString>
