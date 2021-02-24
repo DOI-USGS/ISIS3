58e3c77a1332b76c72bb58b3189153bcf7c8f721
Rebecca Leggett <roboticsgirl60@gmail.com>
2021-02-24 16:45:32 -0700

    changed crop input and ouput function

--- isis/tests/FunctionalTestsCrop.cpp
+++ isis/tests/FunctionalTestsCrop.cpp
@@ -26,14 +26,14 @@ TEST_F(LargeCube, FunctionalTestCropDefault) {
     FAIL() << "Unable to open image: " << e.what() << std::endl;
   }
 
-  /*Cube oCube(outCubeFileName, "r");
+  Cube oCube(outCubeFileName, "r");
 
   Histogram *oCubeStats = oCube.histogram();
 
-  EXPECT_DOUBLE_EQ(oCubeStats->Average(), 505.25);
-  EXPECT_DOUBLE_EQ(oCubeStats->Sum(), 50525);
-  EXPECT_DOUBLE_EQ(oCubeStats->ValidPixels(), 100);
-  EXPECT_DOUBLE_EQ(oCubeStats->StandardDeviation(), 302.16673352386897);*/
+  EXPECT_NEAR(oCubeStats->Average(), 798.5, 0.01);
+  EXPECT_DOUBLE_EQ(oCubeStats->Sum(), 15970000);
+  EXPECT_DOUBLE_EQ(oCubeStats->ValidPixels(), 20000);
+  EXPECT_NEAR(oCubeStats->StandardDeviation(), 57.735748638374318, 0.0000000001);
 }
 
 TEST_F(DefaultCube, FunctionalTestCropSkip1) {
@@ -50,14 +50,14 @@ TEST_F(DefaultCube, FunctionalTestCropSkip1) {
     FAIL() << "Unable to open image: " << e.what() << std::endl;
 }
 
-  /*Cube oCube(outCubeFileName, "r");
+  Cube oCube(outCubeFileName, "r");
 
   Histogram *oCubeStats = oCube.histogram();
 
   EXPECT_DOUBLE_EQ(oCubeStats->Average(), 505.25);
   EXPECT_DOUBLE_EQ(oCubeStats->Sum(), 50525);
   EXPECT_DOUBLE_EQ(oCubeStats->ValidPixels(), 100);
-  EXPECT_DOUBLE_EQ(oCubeStats->StandardDeviation(), 302.16673352386897);*/
+  EXPECT_DOUBLE_EQ(oCubeStats->StandardDeviation(), 302.16673352386897);
 }
 
 TEST_F(DefaultCube, FunctionalTestCropSkip2) {
