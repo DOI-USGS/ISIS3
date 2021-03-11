cb1a5e277033e7fe8fe994c6c2f109eff36e66ef
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2021-03-11 15:09:39 -0700

    Lrowac2isis App and Test Conversion (#4322)
    * Lrowac2isis Tests
    
    * Lrowac2isis Test Conversion
    
    * Fixed segfault issues with history.
    
    * Fixes lro_module_test Error
    
    * Whitespace correction.
    
    * Addressed PR feedback.
    
    * Lrowac2isis conversions.
    
    * Updates to tests
    
    * Revisions to lrowac2isis tests and app.

--- isis/src/lro/apps/lrowac2isis/lrowac2isis.h
+++ isis/src/lro/apps/lrowac2isis/lrowac2isis.h
@@ -1,3 +1,6 @@
+#ifndef lrowac2isis_h
+#define lrowac2isis_h
+
 /** This is free and unencumbered software released into the public domain.
 The authors of ISIS do not claim copyright on the contents of this file.
 For more details about the LICENSE terms and the AUTHORS, you will
@@ -5,14 +8,10 @@ find files of those names at the top level of this repository. **/
 
 /* SPDX-License-Identifier: CC0-1.0 */
 
-#include "Isis.h"
+#include "UserInterface.h"
 
-#include "Application.h"
-#include "lrowac2isis.h"
-
-using namespace Isis;
-
-void IsisMain() {
-  UserInterface &ui = Application::GetUserInterface();
-  lrowac2isis(ui);
+namespace Isis{
+  extern void lrowac2isis(UserInterface &ui);
 }
+
+#endif
