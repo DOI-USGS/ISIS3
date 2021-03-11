790fd009d1fe0d3177e7cd5e86446f61ab3db34b
Tim Giroux <62255438+tgiroux@users.noreply.github.com>
2021-03-11 09:44:03 -0700

    Eis2isis gtest (#4339)
    * basic test layoud
    
    * 4 working tests and a crop notebook
    
    * change expects to asserts

--- isis/src/clipper/apps/eis2isis/eis2isis.h
+++ isis/src/clipper/apps/eis2isis/eis2isis.h
@@ -1,3 +1,6 @@
+#ifndef eis2isis_h
+#define eis2isis_h
+
 /** This is free and unencumbered software released into the public domain.
 
 The authors of ISIS do not claim copyright on the contents of this file.
@@ -6,14 +9,10 @@ find files of those names at the top level of this repository. **/
 
 /* SPDX-License-Identifier: CC0-1.0 */
 
-#include "Isis.h"
 #include "UserInterface.h"
-#include "eis2isis.h"
 
-using namespace std;
-using namespace Isis;
-
-void IsisMain() {
-  UserInterface &ui = Application::GetUserInterface();
-  eis2isis(ui);
+namespace Isis {
+  extern void eis2isis(UserInterface &ui);
 }
+
+#endif
