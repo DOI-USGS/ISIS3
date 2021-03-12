646bc92c21b9790cb5142a0b81a78608791e3577
AustinSanders <arsanders@usgs.gov>
2021-03-12 14:32:07 -0700

    Initial core deployment (#4355)

---
+++ recipe/build_core.sh
@@ -0,0 +1,7 @@
+mkdir build_core install_core
+cd build_core
+export ISISROOT=$PWD
+cmake -GNinja -DBUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -DISIS_BUILD_SWIG=ON -DCMAKE_INSTALL_PREFIX=../install_core ../isis/src/core
+ninja core && ninja install
+cd swig/python/
+python setup.py install
