aeb2cb941bc4394d4a08e690dbfe309f6ba6c62f
Amy Stamile <astamile@contractor.usgs.gov>
2021-03-29 08:25:47 -0700

    Addressed PR feedback

--- isis/tests/OsirisRexOCamsCameraTests.cpp
+++ isis/tests/OsirisRexOCamsCameraTests.cpp
@@ -81,12 +81,11 @@ TEST_F(OsirisRexCube, SamplingCam) {
 
   const PvlGroup &inst = testCube->label()->findGroup("Instrument", Pvl::Traverse);
   double exposureDuration = ((double) inst["ExposureDuration"])/1000;
-  QString stime = inst["StartTime"];
-  double et; // StartTime keyword is the center exposure time
-  str2et_c(stime.toLatin1().data(), &et);
+  cam->SetImage (0.5, 0.5);
+  double et = cam->time().Et();
   pair <iTime, iTime> shuttertimes = cam->ShutterOpenCloseTimes(et, exposureDuration);
-  EXPECT_NEAR(shuttertimes.first.Et(), 600694634.18428946, 6E-14);
-  EXPECT_NEAR(shuttertimes.second.Et(), 600694634.28428948, 6E-14);
+  EXPECT_NEAR(shuttertimes.first.Et(), 502476937.73296136, 1e-14);
+  EXPECT_NEAR(shuttertimes.second.Et(), 502476937.83296138, 1e-14);
 
   EXPECT_TRUE(cam->SetImage(5, 5));
   EXPECT_NEAR(cam->UniversalLatitude(), 9.26486, 0.0001);
