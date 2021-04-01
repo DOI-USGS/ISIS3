602a3cdafa0d8b152b163c776ab6a4d2b7d59cb5
Amy Stamile <astamile@contractor.usgs.gov>
2021-04-01 14:01:41 -0700

    Sumspice test conversions

--- isis/tests/FunctionalTestsSumspice.cpp
+++ isis/tests/FunctionalTestsSumspice.cpp
@@ -1,35 +1,148 @@
-#include "sumspice.h"
+#include <QTemporaryDir>
+#include <QFile>
 
+#include "sumspice.h"
 #include "Fixtures.h"
 #include "TestUtilities.h"
 #include "UserInterface.h"
 #include "Histogram.h"
+#include "iTime.h"
 
 #include "gmock/gmock.h"
 
 using namespace Isis;
+using ::testing::HasSubstr;
 
 static QString APP_XML = FileName("$ISISROOT/bin/xml/sumspice.xml").expanded();
 
-TEST_F(DefaultCube, FunctionalTestSumspiceDefault) {
-  QString singleCubeListPath = tempDir.path() + "/newCubeList.lis";
-  QString toLogPath = tempDir.path() + "/sumspiceUpdateClocksToLog.txt";
-  QString sumFileListPath = tempDir.path() + "/sumfilelist.txt";
-  FileList singleCubeList;
-  singleCubeList.append(testCube->fileName());
-  singleCubeList.write(singleCubeListPath);
-  QString outPath = tempDir.path() + "/mosaic.cub";
+TEST(Sumspice, FunctionalTestSumspiceTimeUpdate) {
+  QTemporaryDir prefix;
+  QString tempDest = prefix.path() + "/sumspiceTEMP.cub";
+  QString cubeFileName = "data/sumspice/st_2395699394_v.lev0.cub";
+  QFile::copy(cubeFileName, tempDest);
 
-  QVector<QString> args = {"fromlist=" + singleCubeListPath,
-                           "sumfilelist= data/sumspice/sumfilelist.txt", //+ sumFileListPath,
+  QVector<QString> args = {"from=" + tempDest,
+                           "sumfile=data/sumspice/N2395699394.SUM",
                            "sumtime=start",
-                           "update=times",
-                           "timediff=1.0",
+                           "update=times"
+                          };
+  UserInterface options(APP_XML, args);
+  sumspice(options);
+
+  Cube cube(tempDest);
+  Pvl *isisLabel = cube.label();
+
+  // Instrument Group
+  PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
+  EXPECT_EQ(inst["SpacecraftClockStartCount"][0].toStdString(), "1/2395694869:238");
+  EXPECT_EQ(inst["SpacecraftClockStopCount"][0].toStdString(), "1/2395694872:183");
+  EXPECT_EQ(inst["StartTime"][0].toStdString(), "2005-09-21T10:44:07.352");
+  EXPECT_EQ(inst["StopTime"][0].toStdString(), "2005-09-21T10:44:07.439");
+
+  // SumTimeHistory Group
+  PvlGroup &sumTime = isisLabel->findGroup("SumTimeHistory", Pvl::Traverse);
+  EXPECT_EQ(sumTime["SUMFILE"][0].toStdString(), "N2395699394");
+  EXPECT_EQ(sumTime["SpacecraftClockStartCount"][0].toDouble(), 2395694888);
+  EXPECT_EQ(sumTime["SpacecraftClockStopCount"][0].toDouble(), 2395695365);
+  EXPECT_EQ(sumTime["StartTime"][0].toStdString(), "2005-09-21T10:44:07");
+  EXPECT_EQ(sumTime["StopTime"][0].toStdString(), "2005-09-21T10:44:07");
+
+
+}
+
+TEST(Sumspice, FunctionalTestSumspiceSpiceUpdate) {
+  QTemporaryDir prefix;
+  QString tempDest = prefix.path() + "/sumspiceTEMP.cub";
+  QString toLogPath = prefix.path() + "/log.txt";
+  QString cubeFileName = "data/sumspice/st_2395699394_updatedtime.cub";
+  QFile::copy(cubeFileName, tempDest);
+
+  QVector<QString> args = {"from=" + tempDest,
+                           "sumfile=data/sumspice/N2395699394.SUM",
+                           "sumtime=start",
+                           "update=spice",
                            "tolog=" + toLogPath
                           };
   UserInterface options(APP_XML, args);
   sumspice(options);
 
+  Cube cube(tempDest);
 
+  // InstrumentPointing Table
+  Table ptTable = cube.readTable("InstrumentPointing");
+  EXPECT_DOUBLE_EQ(ptTable.Label()["CkTableStartTime"], 180571511.57789);
+  EXPECT_DOUBLE_EQ(ptTable.Label()["CkTableEndTime"], 180571511.57789);
 
+  // InstrumentPosition Table
+  Table posTable = cube.readTable("InstrumentPosition");
+  EXPECT_DOUBLE_EQ(posTable.Label()["SpkTableStartTime"], 180571511.57789);
+  EXPECT_DOUBLE_EQ(posTable.Label()["SpkTableEndTime"], 180571511.57789);
+
+  // SunPosition Table
+  Table sunTable = cube.readTable("SunPosition");
+  EXPECT_DOUBLE_EQ(sunTable.Label()["SpkTableStartTime"], 180571511.57789);
+  EXPECT_DOUBLE_EQ(sunTable.Label()["SpkTableEndTime"], 180571511.57789);
+
+  // BodyRotation Table
+  Table bodyTable = cube.readTable("BodyRotation");
+  EXPECT_DOUBLE_EQ(bodyTable.Label().findKeyword("CkTableStartTime"), 180571511.57789);
+  EXPECT_DOUBLE_EQ(bodyTable.Label().findKeyword("CkTableEndTime"), 180571511.57789);
+
+  CSVReader::CSVAxis csvLine;
+  CSVReader header = CSVReader(toLogPath, false, 0, ',', false, true);
+  csvLine = header.getRow(0);
+  EXPECT_EQ(csvLine[0].toStdString(), "Filename");
+  EXPECT_EQ(csvLine[1].toStdString(), "SUMFILE");
+  EXPECT_EQ(csvLine[2].toStdString(), "SumTime");
+  EXPECT_EQ(csvLine[3].toStdString(), "Update");
+  EXPECT_EQ(csvLine[4].toStdString(), "CubeSumDeltaTime");
+  EXPECT_EQ(csvLine[5].toStdString(), " ExposureTime");
+  EXPECT_EQ(csvLine[6].toStdString(), "CubeStartTime");
+  EXPECT_EQ(csvLine[7].toStdString(), "CubeCenterTime");
+  EXPECT_EQ(csvLine[8].toStdString(), "CubeStopTime");
+  EXPECT_EQ(csvLine[9].toStdString(), "SumStartTime");
+  EXPECT_EQ(csvLine[10].toStdString(), "SumCenterTime");
+  EXPECT_EQ(csvLine[11].toStdString(), "SumStopTime");
+
+  csvLine = header.getRow(1);
+  EXPECT_EQ(csvLine[1].toStdString(), "N2395699394");
+  EXPECT_EQ(csvLine[2].toStdString(), "start");
+  EXPECT_EQ(csvLine[3].toStdString(), "spice");
+  EXPECT_EQ(csvLine[4].toDouble(), 2.3841858e-07);
+  EXPECT_EQ(csvLine[5].toDouble(), 0.087);
+  EXPECT_EQ(csvLine[6].toStdString(), "2005-09-21T10:44:07.3519998");
+  EXPECT_EQ(csvLine[7].toStdString(), "2005-09-21T10:44:07.3954998");
+  EXPECT_EQ(csvLine[8].toStdString(), "2005-09-21T10:44:07.4390337");
+  EXPECT_EQ(csvLine[9].toStdString(), "2005-09-21T10:44:07.352");
+  EXPECT_EQ(csvLine[10].toStdString(), "2005-09-21T10:44:07.3955");
+  EXPECT_EQ(csvLine[11].toStdString(), "2005-09-21T10:44:07.439");
+}
+
+TEST(Sumspice, FunctionalTestSumspiceNoCubeError) {
+  QVector<QString> args = {"sumfile=data/sumspice/N2395699394.SUM"};
+  UserInterface options(APP_XML, args);
+
+  try{
+    sumspice(options);
+    FAIL() << "Should throw an exception" << std::endl;
+  }
+
+  catch (IException &e){
+    EXPECT_THAT(e.what(), HasSubstr("User must provide either an input cube file or an input cube file list"));
+  }
+}
+
+TEST(Sumspice, FunctionalTestSumspiceNoSumError) {
+  QTemporaryDir prefix;
+  QVector<QString> args = {"from=" + prefix.path() + "/sumspiceTEMP.cub"};
+  UserInterface options(APP_XML, args);
+
+  try{
+    sumspice(options);
+    FAIL() << "Should throw an exception" << std::endl;
+  }
+
+  catch (IException &e){
+    EXPECT_THAT(e.what(), HasSubstr("User must provide either a sum file or a sum file list."));
+  }
 }
