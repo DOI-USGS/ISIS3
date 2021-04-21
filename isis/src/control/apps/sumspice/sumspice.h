95c149e9708b708631bbd239eb50c05d673a8ff3
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2021-04-21 10:52:38 -0700

    Sumspice App and Test Conversions (#4389)
    * Sumspice test/app conversion
    
    * Sumspice test conversions
    
    * Addressed PR feedback
    
    * Added tests for updating position and pointing.

---
+++ isis/src/control/apps/sumspice/sumspice.h
@@ -0,0 +1,11 @@
+#ifndef sumspice_h
+#define sumspice_h
+
+#include "Cube.h"
+#include "UserInterface.h"
+
+namespace Isis{
+  extern void sumspice(UserInterface &ui, Pvl *log=nullptr);
+}
+
+#endif
