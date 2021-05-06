f563524c7f01e8fba1443eaedf5ab1e60ff11325
Tim Giroux <62255438+tgiroux@users.noreply.github.com>
2021-05-06 10:42:17 -0700

    Findgaps gtests (#4422)
    * first few test cases, needs work
    
    * two working tests
    
    * add test for correlation tolerance
    
    * improve test data creation code

--- isis/src/base/apps/findgaps/findgaps.h
+++ isis/src/base/apps/findgaps/findgaps.h
@@ -1,3 +1,6 @@
+#ifndef findgaps_h
+#define findgaps_h
+
 /** This is free and unencumbered software released into the public domain.
 
 The authors of ISIS do not claim copyright on the contents of this file.
@@ -6,14 +9,10 @@ find files of those names at the top level of this repository. **/
 
 /* SPDX-License-Identifier: CC0-1.0 */
 
-#include "Isis.h"
-#include "findgaps.h"
-#include "Application.h"
+#include "UserInterface.h"
 
-using namespace std;
-using namespace Isis;
-
-void IsisMain() {
-  UserInterface &ui = Application::GetUserInterface();
-  findgaps(ui);
+namespace Isis{
+  extern void findgaps(UserInterface &ui);
 }
+
+#endif
