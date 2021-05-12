4f539a238da93ccc819516ac80cf1cdc34e28923
ssides <ssides@usgs.gov>
2021-05-12 09:13:13 -0700

    GEOS 3.9 ImagePolygon

--- isis/src/base/objs/ImagePolygon/ImagePolygon.h
+++ isis/src/base/objs/ImagePolygon/ImagePolygon.h
@@ -269,7 +269,7 @@ namespace Isis {
 
       Brick *p_brick;     //!< Used to check for valid DNs
 
-      geos::geom::CoordinateSequence *p_pts; //!< The sequence of coordinates that compose the boundry of the image
+      geos::geom::CoordinateArraySequence *p_pts; //!< The sequence of coordinates that compose the boundary of the image
 
       geos::geom::MultiPolygon *p_polygons;  //!< The multipolygon of the image
 
