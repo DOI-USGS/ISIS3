ce7e4a663edb041c46febfe651c3871348180076
robotprogrammer22 <46390033+robotprogrammer22@users.noreply.github.com>
2021-06-04 09:34:49 -0700

    map2map conversion (#4435)
    * added code for two tests
    
    * test modifications
    
    * changed crop input and ouput function
    
    * fixed last two tests
    
    * added more tests
    
    * converted map2map
    
    * added conversion code for map2map
    
    * bring crop tests up to date with dev
    
    * added modifications to map2map and tests
    
    * final changes for map2map tests
    
    * Converted app and default test
    
    * Converted tests
    
    * Added histograms to tests.
    
    * Rename FunctionalTestsMap2Map.cpp to FunctionalTestsMap2map.cpp
    
    Co-authored-by: Amy Stamile <astamile@contractor.usgs.gov>
    Co-authored-by: Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>

--- isis/src/base/apps/map2map/map2map.h
+++ isis/src/base/apps/map2map/map2map.h
@@ -2,39 +2,50 @@
 #define map2map_h
 
 #include "Transform.h"
+#include "Cube.h"
+#include "UserInterface.h"
+#include "TProjection.h"
 
-/**
- * @author ????-??-?? Unknown
- *
- * @internal
- *   @history 2012-12-06 Debbie A. Cook - Changed to use TProjection instead of Projection.
- *                          References #775.
- */
-class map2map : public Isis::Transform {
-  private:
-    Isis::TProjection *p_inmap;
-    Isis::TProjection *p_outmap;
-    int p_inputSamples;
-    int p_inputLines;
-    bool p_trim;
-    int p_outputSamples;
-    int p_outputLines;
-    int p_inputWorldSize;
 
-  public:
-    // constructor
-    map2map(const int inputSamples, const int inputLines, Isis::TProjection *inmap,
-            const int outputSamples, const int outputLines, Isis::TProjection *outmap,
-            bool trim);
+namespace Isis {
+  /**
+   * @author ????-??-?? Unknown
+   *
+   * @internal
+   *   @history 2012-12-06 Debbie A. Cook - Changed to use TProjection instead of Projection.
+   *                          References #775.
+   */
+  class Map2map : public Isis::Transform {
+    private:
+      Isis::TProjection *p_inmap;
+      Isis::TProjection *p_outmap;
+      int p_inputSamples;
+      int p_inputLines;
+      bool p_trim;
+      int p_outputSamples;
+      int p_outputLines;
+      int p_inputWorldSize;
 
-    // destructor
-    ~map2map() {};
+    public:
+      // constructor
+      Map2map(const int inputSamples, const int inputLines, Isis::TProjection *inmap,
+              const int outputSamples, const int outputLines, Isis::TProjection *outmap,
+              bool trim);
 
-    // Implementations for parent's pure virtual members
-    bool Xform(double &inSample, double &inLine,
-               const double outSample, const double outLine);
-    int OutputSamples() const;
-    int OutputLines() const;
-};
+      // destructor
+      ~Map2map() {};
+
+      // Implementations for parent's pure virtual members
+      bool Xform(double &inSample, double &inLine,
+                 const double outSample, const double outLine);
+      int OutputSamples() const;
+      int OutputLines() const;
+  };
+
+  extern void map2map(Cube *incube, UserInterface &ui, Pvl *log=nullptr);
+  extern void map2map(UserInterface &ui, Pvl *log=nullptr);
+}
+
+
 
 #endif
