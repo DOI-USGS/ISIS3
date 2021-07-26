5308d2dd2042b2df818522909fa152822ed5e9f5
Jesse Mapel <jmapel@usgs.gov>
2021-07-26 10:50:35 -0700

    Remove null initialize in constructor

--- isis/src/control/objs/BundleUtilities/BundleMeasure.cpp
+++ isis/src/control/objs/BundleUtilities/BundleMeasure.cpp
@@ -29,9 +29,6 @@ namespace Isis {
                                BundleControlPoint *bundleControlPoint) {
     m_controlMeasure = controlMeasure;
     m_parentControlPoint = bundleControlPoint;
-    m_imagePartial = nullptr;
-    m_point3DPartial = nullptr;
-    m_targetPartial = nullptr;
   }
 
 
