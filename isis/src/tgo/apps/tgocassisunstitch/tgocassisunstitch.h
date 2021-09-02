5fe039f66c3085cd79ccafc14ab920b70d6a26f3
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2021-09-02 08:19:03 -0700

    Converted Tgo Cassis StitchUnstitch Module test to Gtest (#4610)
    * tgo app conversions and initial tgo_module_stitchUnstitch test conversion.
    
    * Completed stitchUnstitch module test conversion.
    
    * converted tgocassis2isis tests.
    
    * converted tgocassisstitch multiframe test.
    
    * Added singleframe test to tgocassisstitch tests.
    
    * Converted tgocassisunstitch tests.
    
    * Updated filename causing test failures

---
+++ isis/src/tgo/apps/tgocassisunstitch/tgocassisunstitch.h
@@ -0,0 +1,11 @@
+#ifndef tgocassisunstitch_h
+#define tgocassisunstitch_h
+
+#include "Cube.h"
+#include "UserInterface.h"
+
+namespace Isis{
+  extern void tgocassisunstitch(UserInterface &ui);
+}
+
+#endif
