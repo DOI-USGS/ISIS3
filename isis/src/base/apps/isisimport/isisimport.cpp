600e3fcdfafde2a4849ff3051926de4e7ae04c7c
acpaquette <acpaquette@usgs.gov>
2021-11-25 18:54:46 -0700

    IsisImport Tests (#4695)
    * Added lost file from previous pds4 merge
    
    * Fix import tests
    
    * Fixed variable name
    
    * Fixed comment
    
    * Inserted translation objects into test labels

--- isis/src/base/apps/isisimport/isisimport.cpp
+++ isis/src/base/apps/isisimport/isisimport.cpp
@@ -156,8 +156,15 @@ namespace Isis {
 
 
     ProcessImport importer;
-    importer.SetInputFile(inputFileName.expanded());
-
+    if (inputFileName.removeExtension().addExtension("dat").fileExists()){
+      importer.SetInputFile(inputFileName.removeExtension().addExtension("dat").expanded());
+    }
+    else if (inputFileName.removeExtension().addExtension("img").fileExists()) {
+      importer.SetInputFile(inputFileName.removeExtension().addExtension("img").expanded());
+    }
+    else {
+      importer.SetInputFile(inputFileName.expanded());
+    }
 
     // Use inja to get number of lines, samples, and bands from the input label
     std::string result = env.render_file(inputTemplate.expanded().toStdString(), jsonData);
