46415acb91c071e6b4ab76cf56c2584711959c3a
acpaquette <acp263@nau.edu>
2022-02-10 19:45:53 -0700

    Reversed normal test calls due to how the localnormal is calculated

--- isis/src/base/objs/Camera/Camera.h
+++ isis/src/base/objs/Camera/Camera.h
@@ -235,7 +235,7 @@ namespace Isis {
 
   class Camera : public Sensor {
     public:
-      // constructors 
+      // constructors
       Camera(Cube &cube);
 
       // destructor
@@ -279,7 +279,7 @@ namespace Isis {
       virtual double SampleResolution();
       virtual double DetectorResolution();
 
-      virtual double ObliqueDetectorResolution(bool useLocal = true);
+      virtual double ObliqueDetectorResolution(bool useLocal = false);
       virtual double ObliqueSampleResolution();
       virtual double ObliqueLineResolution();
       virtual double ObliquePixelResolution();
