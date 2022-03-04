47c3ea41960669e2ce4b4761cb59dea1b0d301c5
acpaquette <acpaquette@usgs.gov>
2022-03-04 16:46:11 -0700

    Update FindKakadu.cmake
    Added general mount point for kakadu

--- isis/cmake/FindKakadu.cmake
+++ isis/cmake/FindKakadu.cmake
@@ -9,7 +9,7 @@
 if(JP2KFLAG)
   find_path(KAKADU_INCLUDE_DIR
     NAME kdu_kernels.h
-    PATHS /usgs/apps/kakadu/v7_9_1-01762L/managed/all_includes/ /isisData/kakadu/ /Users/ssides/oldmac/kakadu/v7_9_1-01762L/managed/all_includes/
+    PATHS /usgs/apps/kakadu/v7_9_1-01762L/managed/all_includes/ /isisData/kakadu/ /Volumes/mac_1013_x86_64/apps/kakadu/v7_9_1-01762L/managed/all_includes/
 
   )
 
