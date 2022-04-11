072e9c2056a4bc4c80a17454262f97617d944aad
Kris J. Becker <kbecker@orex.lpl.arizona.edu>
2022-04-11 11:34:11 -0700

    Updated tests after feedback from PR review

--- isis/tests/FunctionalTestsMsi2isis.cpp
+++ isis/tests/FunctionalTestsMsi2isis.cpp
@@ -38,33 +38,33 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestDefault) {
 
   // Instrument Group
   PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_EQ(inst["SpacecraftName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(inst["InstrumentId"][0], "MSI" );
-  EXPECT_EQ(inst["TargetName"][0], "EROS" );
-  EXPECT_EQ(inst["SpacecraftClockStartCount"][0], "157063318856" );
-  EXPECT_EQ(inst["SpacecraftClockStopCount"][0], "157063318919" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0], "157063318.856" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0], "157063318.919" );
+  EXPECT_EQ(inst["SpacecraftName"][0].toStdString(), "NEAR EARTH ASTEROID RENDEZVOUS");
+  EXPECT_EQ(inst["InstrumentId"][0].toStdString(), "MSI" );
+  EXPECT_EQ(inst["TargetName"][0].toStdString(), "EROS" );
+  EXPECT_EQ(inst["SpacecraftClockStartCount"][0].toStdString(), "157063318856" );
+  EXPECT_EQ(inst["SpacecraftClockStopCount"][0].toStdString(), "157063318919" );
+  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0].toStdString(), "157063318.856" );
+  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0].toStdString(), "157063318.919" );
   ASSERT_DOUBLE_EQ(double(inst["ExposureDuration"]), 63.0);
-  EXPECT_EQ(inst["StartTime"][0], "2001-02-08T17:24:42.217" );
-  EXPECT_EQ(inst["StopTime"][0], "2001-02-08T17:24:42.280" );
+  EXPECT_EQ(inst["StartTime"][0].toStdString(), "2001-02-08T17:24:42.217" );
+  EXPECT_EQ(inst["StopTime"][0].toStdString(), "2001-02-08T17:24:42.280" );
   ASSERT_DOUBLE_EQ(double(inst["DpuDeckTemperature"]), 286.5);
-  EXPECT_EQ(inst["DpuDeckTemperature"].unit(), "K");
+  EXPECT_EQ(inst["DpuDeckTemperature"].unit().toStdString(), "K");
 
   // Archive Group
   PvlGroup &archive = isisLabel->findGroup("Archive", Pvl::Traverse);
-  EXPECT_EQ(archive["DataSetId"][0], "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
-  EXPECT_EQ(archive["ProductId"][0], "M0157063318F4_2P_IOF_GBL.FIT");
-  EXPECT_EQ(archive["InstrumentHostName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(archive["InstrumentName"][0], "MULTI-SPECTRAL IMAGER");
-  EXPECT_EQ(archive["InstrumentId"][0], "MSI");
-  EXPECT_EQ(archive["TargetName"][0], "EROS");
+  EXPECT_EQ(archive["DataSetId"][0].toStdString(), "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
+  EXPECT_EQ(archive["ProductId"][0].toStdString(), "M0157063318F4_2P_IOF_GBL.FIT");
+  EXPECT_EQ(archive["InstrumentHostName"][0].toStdString(), "NEAR EARTH ASTEROID RENDEZVOUS");
+  EXPECT_EQ(archive["InstrumentName"][0].toStdString(), "MULTI-SPECTRAL IMAGER");
+  EXPECT_EQ(archive["InstrumentId"][0].toStdString(), "MSI");
+  EXPECT_EQ(archive["TargetName"][0].toStdString(), "EROS");
 
   // BandBin Group
   PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
   EXPECT_EQ(int(bandbin["FilterNumber"]), 4);
   EXPECT_EQ(int(bandbin["Center"]), 950);
-  EXPECT_EQ(bandbin["Center"].unit(), "nm");
+  EXPECT_EQ(bandbin["Center"].unit().toStdString(), "nm");
 
  // Kernels Group
   PvlGroup &kernel = isisLabel->findGroup("Kernels", Pvl::Traverse);
@@ -80,10 +80,8 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestDefault) {
 
   // This section tests that the edge was trimmed as expected
   // (TRIM=TRUE, the default)
-  // Count the edges to make sure they are all NULLs
-  int n_nulls = 0;
-  int n_valid = 0;
-  int n_lines_all_null = 0;
+  // Count the number of edge pixels
+  int total_edge_pixels = 0;
 
   const int trimSize = 33;
   const int expectValid = cube.sampleCount() - ( 2 * trimSize);
@@ -94,59 +92,36 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestDefault) {
   ImageHistogram trimEdge(0.0, 1.0);
   ImageHistogram innerImage(0.0, 1.0);
 
-  int iline = 0;
-  double sum = 0;
-
   LineManager line(cube);
-  for ( line.begin() ; !line.end() ; line++, iline++ ) {
+  for ( line.begin() ; !line.end() ; line++ ) {
     cube.read(line);
     const double *lbuf = line.DoubleBuffer();
     if ( (line.Line() <= trimSize) || ( line.Line() > end_l ) ) {
       trimEdge.AddData(&lbuf[0], line.size());
-      n_lines_all_null++;
-
-      for ( int s = 0 ; s < line.size() ; s++) {
-        if ( IsSpecial( lbuf[s] ) ) {
-          n_nulls ++;
-        }
-        else {
-          sum += lbuf[s];
-          n_valid++;
-        }
-      }
+      total_edge_pixels += line.size();
     }
     else {
 
       // This is past full line trimming and is on the edges only
       trimEdge.AddData(&lbuf[0], trimSize);
       trimEdge.AddData(&lbuf[end_s], trimSize);
-      for ( int s = 0 ; s < line.size() ; s++) {
-        if ( IsSpecial( lbuf[s] ) ) {
-          n_nulls ++;
-        }
-        else {
-          sum += lbuf[s];
-          n_valid++;
-        }
-      }
+      total_edge_pixels += (trimSize + trimSize);
 
       // Add valid data in untrimmed area should match image histogram data
       innerImage.AddData( &lbuf[trimSize], expectValid );
     }
   }
 
-  // This compares the expectation of trimming to the image histogram results
+  // The whole image histogram should match the untrimmed section
+  // in the this test
   EXPECT_EQ(innerImage.Average(), hist->Average());
   EXPECT_EQ(innerImage.Sum(), hist->Sum());
   EXPECT_EQ(innerImage.ValidPixels(), hist->ValidPixels());
   EXPECT_EQ(innerImage.StandardDeviation(), hist->StandardDeviation());
 
-  EXPECT_EQ(innerImage.Sum(), sum);
-  EXPECT_EQ(innerImage.ValidPixels(), n_valid);
-
-  EXPECT_EQ(trimEdge.Sum(), 0.0);
-  EXPECT_EQ(trimEdge.NullPixels(), hist->NullPixels());
   EXPECT_EQ(trimEdge.ValidPixels(), 0);
+  EXPECT_EQ(trimEdge.Sum(), 0.0);
+  EXPECT_EQ(trimEdge.NullPixels(), total_edge_pixels);
 }
 
 
@@ -166,46 +141,13 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestNoTrim) {
     FAIL() << "Unable to ingest NEAR/MSI GBL file: " << e.toString().toStdString() << std::endl;
   }
   Cube cube(cubeFileName);
-  Pvl *isisLabel = cube.label();
 
   // Dimensions Group
   ASSERT_EQ(cube.sampleCount(), 537);
   ASSERT_EQ(cube.lineCount(), 412);
   ASSERT_EQ(cube.bandCount(), 1);
 
-  // Instrument Group
-  PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_EQ(inst["SpacecraftName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(inst["InstrumentId"][0], "MSI" );
-  EXPECT_EQ(inst["TargetName"][0], "EROS" );
-  EXPECT_EQ(inst["SpacecraftClockStartCount"][0], "157063318856" );
-  EXPECT_EQ(inst["SpacecraftClockStopCount"][0], "157063318919" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0], "157063318.856" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0], "157063318.919" );
-  ASSERT_DOUBLE_EQ(double(inst["ExposureDuration"]), 63.0);
-  EXPECT_EQ(inst["StartTime"][0], "2001-02-08T17:24:42.217" );
-  EXPECT_EQ(inst["StopTime"][0], "2001-02-08T17:24:42.280" );
-  ASSERT_DOUBLE_EQ(double(inst["DpuDeckTemperature"]), 286.5);
-  EXPECT_EQ(inst["DpuDeckTemperature"].unit(), "K");
-
-  // Archive Group
- PvlGroup &archive = isisLabel->findGroup("Archive", Pvl::Traverse);
-  EXPECT_EQ(archive["DataSetId"][0], "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
-  EXPECT_EQ(archive["ProductId"][0], "M0157063318F4_2P_IOF_GBL.FIT");
-  EXPECT_EQ(archive["InstrumentHostName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(archive["InstrumentName"][0], "MULTI-SPECTRAL IMAGER");
-  EXPECT_EQ(archive["InstrumentId"][0], "MSI");
-  EXPECT_EQ(archive["TargetName"][0], "EROS");
-
-  // BandBin Group
-  PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
-  EXPECT_EQ(int(bandbin["FilterNumber"]), 4);
-  EXPECT_EQ(int(bandbin["Center"]), 950);
-  EXPECT_EQ(bandbin["Center"].unit(), "nm");
-
- // Kernels Group
-  PvlGroup &kernel = isisLabel->findGroup("Kernels", Pvl::Traverse);
-  EXPECT_EQ(int(kernel["NaifFrameCode"]), -93001);
+  // Label content is unchanged from previous test
 
   std::unique_ptr<Histogram> hist (cube.histogram());
 
@@ -216,10 +158,8 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestNoTrim) {
   EXPECT_NEAR(hist->StandardDeviation(), 0.017675184374796, .0001);
 
   // This section tests that the edge was not trimmed (TRIM=FALSE)
-  // Count the edges to make sure they are no NULLs
-  int n_nulls = 0;
-  int n_valid = 0;
-  int n_lines_all_null = 0;
+  // Count the number of edge pixels
+  int total_edge_pixels = 0;
 
   const int trimSize = 33;
   const int expectValid = cube.sampleCount() - ( 2 * trimSize);
@@ -230,66 +170,45 @@ TEST_F(TempTestingFiles, Msi2isisTestGblIngestNoTrim) {
   ImageHistogram trimEdge(0.0, 1.0);
   ImageHistogram innerImage(0.0, 1.0);
 
-  int iline = 0;
-  double sum = 0;
-
   LineManager line(cube);
-  for ( line.begin() ; !line.end() ; line++, iline++ ) {
+  for ( line.begin() ; !line.end() ; line++ ) {
     cube.read(line);
     const double *lbuf = line.DoubleBuffer();
     if ( (line.Line() <= trimSize) || ( line.Line() > end_l ) ) {
       trimEdge.AddData(&lbuf[0], line.size());
-      n_lines_all_null++;
-
-      for ( int s = 0 ; s < line.size() ; s++) {
-        if ( IsSpecial( lbuf[s] ) ) {
-          n_nulls ++;
-        }
-        else {
-          sum += lbuf[s];
-          n_valid++;
-        }
-      }
+      total_edge_pixels += line.size();
     }
     else {
 
       // This is past full line trimming and is on the edges only
       trimEdge.AddData(&lbuf[0], trimSize);
       trimEdge.AddData(&lbuf[end_s], trimSize);
-      for ( int s = 0 ; s < line.size() ; s++) {
-        if ( IsSpecial( lbuf[s] ) ) {
-          n_nulls ++;
-        }
-        else {
-          sum += lbuf[s];
-          n_valid++;
-        }
-      }
+      total_edge_pixels += (trimSize + trimSize);
 
-      // Add valid data in untrimmed area should match image histogram data
+      // Add valid data in untrimmed area which should match the image
+      // histogram data of whole trimmed image in the first test
       innerImage.AddData( &lbuf[trimSize], expectValid );
     }
   }
 
-  // This compares the expectation of no trimming to the image histogram results
-  double composite_image_data_sum = innerImage.Sum() + trimEdge.Sum();
-  int composite_image_valid_pixels = innerImage.ValidPixels() + trimEdge.ValidPixels();
-  int composite_image_null_pixels = innerImage.NullPixels() + trimEdge.NullPixels();
+  // This innerImage area should be eqivalent to whole trimmed image
+  // in the first test
+  EXPECT_NEAR(innerImage.Average(), 0.042633522648316, .000001);
+  EXPECT_NEAR(innerImage.Sum(), 6947.8146519054, .0001);
+  EXPECT_EQ(innerImage.ValidPixels(), 162966);
+  EXPECT_EQ(innerImage.NullPixels(), 0);
+  EXPECT_NEAR(innerImage.StandardDeviation(), 0.016649588425538, .0001);
 
-  EXPECT_EQ(composite_image_data_sum, hist->Sum());
-  EXPECT_EQ(composite_image_data_sum, sum);
-  EXPECT_EQ(composite_image_valid_pixels, hist->ValidPixels());
-  EXPECT_EQ(composite_image_null_pixels, hist->NullPixels());
-
+  // Just to be sure...
   EXPECT_EQ(trimEdge.NullPixels(), 0);
-  EXPECT_EQ(trimEdge.NullPixels(), n_nulls);
 }
 
 TEST_F(TempTestingFiles, Msi2isisTestCubicConvolution) {
   Pvl appLog;
   QString cubeFileName = tempDir.path() + "/m0126865998f4_2p_iof.cubic.cub";
   QVector<QString> args = {"from=data/near/msi2isis/m0126865998f4_2p_iof.lbl",
-                            "to=" + cubeFileName };
+                            "to=" + cubeFileName,
+                            "interp=cubicconvolution" };
 
   UserInterface options(APP_XML, args);
   try {
@@ -308,33 +227,33 @@ TEST_F(TempTestingFiles, Msi2isisTestCubicConvolution) {
 
   // Instrument Group
   PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_EQ(inst["SpacecraftName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(inst["InstrumentId"][0], "MSI" );
-  EXPECT_EQ(inst["TargetName"][0], "EROS" );
-  EXPECT_EQ(inst["SpacecraftClockStartCount"][0], "126865998830" );
-  EXPECT_EQ(inst["SpacecraftClockStopCount"][0], "126865998919" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0], "126865998.830" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0], "126865998.919" );
+  EXPECT_EQ(inst["SpacecraftName"][0].toStdString(), "NEAR EARTH ASTEROID RENDEZVOUS");
+  EXPECT_EQ(inst["InstrumentId"][0].toStdString(), "MSI" );
+  EXPECT_EQ(inst["TargetName"][0].toStdString(), "EROS" );
+  EXPECT_EQ(inst["SpacecraftClockStartCount"][0].toStdString(), "126865998830" );
+  EXPECT_EQ(inst["SpacecraftClockStopCount"][0].toStdString(), "126865998919" );
+  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0].toStdString(), "126865998.830" );
+  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0].toStdString(), "126865998.919" );
   EXPECT_DOUBLE_EQ(double(inst["ExposureDuration"]), 89.0);
-  EXPECT_EQ(inst["StartTime"][0], "2000-02-25T05:16:12.656" );
-  EXPECT_EQ(inst["StopTime"][0], "2000-02-25T05:16:12.745" );
+  EXPECT_EQ(inst["StartTime"][0].toStdString(), "2000-02-25T05:16:12.656" );
+  EXPECT_EQ(inst["StopTime"][0].toStdString(), "2000-02-25T05:16:12.745" );
   EXPECT_DOUBLE_EQ(double(inst["DpuDeckTemperature"]), 286.5);
-  EXPECT_EQ(inst["DpuDeckTemperature"].unit(), "K");
+  EXPECT_EQ(inst["DpuDeckTemperature"].unit().toStdString(), "K");
 
   // Archive Group
   PvlGroup &archive = isisLabel->findGroup("Archive", Pvl::Traverse);
-  EXPECT_EQ(archive["DataSetId"][0], "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
-  EXPECT_EQ(archive["ProductId"][0], "M0126865998F4_2P_IOF.FIT");
-  EXPECT_EQ(archive["InstrumentHostName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(archive["InstrumentName"][0], "MULTI-SPECTRAL IMAGER");
-  EXPECT_EQ(archive["InstrumentId"][0], "MSI");
-  EXPECT_EQ(archive["TargetName"][0], "EROS");
+  EXPECT_EQ(archive["DataSetId"][0].toStdString(), "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
+  EXPECT_EQ(archive["ProductId"][0].toStdString(), "M0126865998F4_2P_IOF.FIT");
+  EXPECT_EQ(archive["InstrumentHostName"][0].toStdString(), "NEAR EARTH ASTEROID RENDEZVOUS");
+  EXPECT_EQ(archive["InstrumentName"][0].toStdString(), "MULTI-SPECTRAL IMAGER");
+  EXPECT_EQ(archive["InstrumentId"][0].toStdString(), "MSI");
+  EXPECT_EQ(archive["TargetName"][0].toStdString(), "EROS");
 
   // BandBin Group
   PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
   EXPECT_EQ(int(bandbin["FilterNumber"]), 4);
   EXPECT_EQ(int(bandbin["Center"]), 950);
-  EXPECT_EQ(bandbin["Center"].unit(), "nm");
+  EXPECT_EQ(bandbin["Center"].unit().toStdString(), "nm");
 
  // Kernels Group
   PvlGroup &kernel = isisLabel->findGroup("Kernels", Pvl::Traverse);
@@ -364,46 +283,13 @@ TEST_F(TempTestingFiles, Msi2isisTestNearestNeighbor) {
     FAIL() << "Unable to ingest NEAR/MSI file: " << e.toString().toStdString() << std::endl;
   }
   Cube cube(cubeFileName);
-  Pvl *isisLabel = cube.label();
 
   // Dimensions Group
   ASSERT_EQ(cube.sampleCount(), 537);
   ASSERT_EQ(cube.lineCount(), 412);
   ASSERT_EQ(cube.bandCount(), 1);
 
-  // Instrument Group
-  PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_EQ(inst["SpacecraftName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(inst["InstrumentId"][0], "MSI" );
-  EXPECT_EQ(inst["TargetName"][0], "EROS" );
-  EXPECT_EQ(inst["SpacecraftClockStartCount"][0], "126865998830" );
-  EXPECT_EQ(inst["SpacecraftClockStopCount"][0], "126865998919" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0], "126865998.830" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0], "126865998.919" );
-  EXPECT_DOUBLE_EQ(double(inst["ExposureDuration"]), 89.0);
-  EXPECT_EQ(inst["StartTime"][0], "2000-02-25T05:16:12.656" );
-  EXPECT_EQ(inst["StopTime"][0], "2000-02-25T05:16:12.745" );
-  EXPECT_DOUBLE_EQ(double(inst["DpuDeckTemperature"]), 286.5);
-  EXPECT_EQ(inst["DpuDeckTemperature"].unit(), "K");
-
-  // Archive Group
-  PvlGroup &archive = isisLabel->findGroup("Archive", Pvl::Traverse);
-  EXPECT_EQ(archive["DataSetId"][0], "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
-  EXPECT_EQ(archive["ProductId"][0], "M0126865998F4_2P_IOF.FIT");
-  EXPECT_EQ(archive["InstrumentHostName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(archive["InstrumentName"][0], "MULTI-SPECTRAL IMAGER");
-  EXPECT_EQ(archive["InstrumentId"][0], "MSI");
-  EXPECT_EQ(archive["TargetName"][0], "EROS");
-
-  // BandBin Group
-  PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
-  ASSERT_EQ(int(bandbin["FilterNumber"]), 4);
-  ASSERT_EQ(int(bandbin["Center"]), 950);
-  EXPECT_EQ(bandbin["Center"].unit(), "nm");
-
- // Kernels Group
-  PvlGroup &kernel = isisLabel->findGroup("Kernels", Pvl::Traverse);
-  EXPECT_EQ(int(kernel["NaifFrameCode"]), -93001);
+  // No changes in labels from previous tests
 
   std::unique_ptr<Histogram> hist (cube.histogram());
 
@@ -429,47 +315,13 @@ TEST_F(TempTestingFiles, Msi2isisTestBilinear) {
     FAIL() << "Unable to ingest NEAR/MSI file: " << e.toString().toStdString() << std::endl;
   }
   Cube cube(cubeFileName);
-  Pvl *isisLabel = cube.label();
-
 
   // Dimensions Group
   ASSERT_EQ(cube.sampleCount(), 537);
   ASSERT_EQ(cube.lineCount(), 412);
   ASSERT_EQ(cube.bandCount(), 1);
 
-  // Instrument Group
-  PvlGroup &inst = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_EQ(inst["SpacecraftName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(inst["InstrumentId"][0], "MSI" );
-  EXPECT_EQ(inst["TargetName"][0], "EROS" );
-  EXPECT_EQ(inst["SpacecraftClockStartCount"][0], "126865998830" );
-  EXPECT_EQ(inst["SpacecraftClockStopCount"][0], "126865998919" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStartCount"][0], "126865998.830" );
-  EXPECT_EQ(inst["OriginalSpacecraftClockStopCount"][0], "126865998.919" );
-  EXPECT_DOUBLE_EQ(double(inst["ExposureDuration"]), 89.0);
-  EXPECT_EQ(inst["StartTime"][0], "2000-02-25T05:16:12.656" );
-  EXPECT_EQ(inst["StopTime"][0], "2000-02-25T05:16:12.745" );
-  EXPECT_DOUBLE_EQ(double(inst["DpuDeckTemperature"]), 286.5);
-  EXPECT_EQ(inst["DpuDeckTemperature"].unit(), "K");
-
-  // Archive Group
-  PvlGroup &archive = isisLabel->findGroup("Archive", Pvl::Traverse);
-  EXPECT_EQ(archive["DataSetId"][0], "NEAR-A-MSI-3-EDR-EROS/ORBIT-V1.0");
-  EXPECT_EQ(archive["ProductId"][0], "M0126865998F4_2P_IOF.FIT");
-  EXPECT_EQ(archive["InstrumentHostName"][0], "NEAR EARTH ASTEROID RENDEZVOUS");
-  EXPECT_EQ(archive["InstrumentName"][0], "MULTI-SPECTRAL IMAGER");
-  EXPECT_EQ(archive["InstrumentId"][0], "MSI");
-  EXPECT_EQ(archive["TargetName"][0], "EROS");
-
-  // BandBin Group
-  PvlGroup &bandbin = isisLabel->findGroup("BandBin", Pvl::Traverse);
-  EXPECT_EQ(int(bandbin["FilterNumber"]), 4);
-  EXPECT_EQ(int(bandbin["Center"]), 950);
-  EXPECT_EQ(bandbin["Center"].unit(), "nm");
-
- // Kernels Group
-  PvlGroup &kernel = isisLabel->findGroup("Kernels", Pvl::Traverse);
-  EXPECT_EQ(int(kernel["NaifFrameCode"]), -93001);
+  // No changes in labels from previous tests
 
   std::unique_ptr<Histogram> hist (cube.histogram());
 
