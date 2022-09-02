72027c4a866728aeda8c83cc97963e2a2a5bff7c
acpaquette <acp263@nau.edu>
2022-09-02 12:55:25 -0700

    Updated msi2isis tests, again

--- isis/tests/FunctionalTestsMsi2isis.cpp
+++ isis/tests/FunctionalTestsMsi2isis.cpp
@@ -293,8 +293,8 @@ TEST_F(TempTestingFiles, Msi2isisTestNearestNeighbor) {
 
   std::unique_ptr<Histogram> hist (cube.histogram());
 
-  EXPECT_NEAR(hist->Average(), 0.0090829926093650964, .000001);
-  EXPECT_NEAR(hist->Sum(), 1481.5320246468336, .0001);
+  EXPECT_NEAR(hist->Average(), 0.0090912713562279774, .000001);
+  EXPECT_NEAR(hist->Sum(), 1481.5681278390484, .0001);
   EXPECT_EQ(hist->ValidPixels(), 162966);
   EXPECT_EQ(hist->NullPixels(), 58278);
   EXPECT_NEAR(hist->StandardDeviation(), 0.013550997159207, .0001);
@@ -325,8 +325,8 @@ TEST_F(TempTestingFiles, Msi2isisTestBilinear) {
 
   std::unique_ptr<Histogram> hist (cube.histogram());
 
-  EXPECT_NEAR(hist->Average(), 0.0090829926093650964, .000001);
-  EXPECT_NEAR(hist->Sum(), 1480.2189735777922, .0001);
+  EXPECT_NEAR(hist->Average(), 0.0090912713562279774, .000001);
+  EXPECT_NEAR(hist->Sum(), 1481.5681278390484, .0001);
   EXPECT_EQ(hist->ValidPixels(), 162966);
   EXPECT_EQ(hist->NullPixels(), 58278);
   EXPECT_NEAR(hist->StandardDeviation(), 0.013542099831273, .0001);
