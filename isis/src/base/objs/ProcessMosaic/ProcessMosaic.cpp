a16bdf8b2e2a235fb0b6b3d2799c882b7d533e19
Austin Sanders <arsanders@usgs.gov>
2022-12-01 09:07:53 -0700

    Adds mapping group to automos track cube (#5086)
    * Added mapping group to track cube
    
    * Updated changelog

--- isis/src/base/objs/ProcessMosaic/ProcessMosaic.cpp
+++ isis/src/base/objs/ProcessMosaic/ProcessMosaic.cpp
@@ -307,6 +307,10 @@ namespace Isis {
           bandBin.addKeyword(trackBand);
           trackingLabel->findObject("IsisCube").addGroup(bandBin);
 
+          // Write the mapping group to the tracking cube label
+          PvlGroup & mappingGroup = inLab->findGroup("Mapping", Pvl::Traverse);
+          trackingLabel->findObject("IsisCube").addGroup(mappingGroup);
+
           // Initialize an empty TrackingTable object to manage tracking table in tracking cube
           trackingTable = new TrackingTable();
         }
