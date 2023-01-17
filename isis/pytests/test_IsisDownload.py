385d80749b8cbf3c1591433aeb1ee073b6180934
antonhibl <antonhibl11@gmail.com>
2023-01-17 09:59:41 -0700

    added test to see if filtering on rclone ISISDATA worked

--- isis/pytests/test_IsisDownload.py
+++ isis/pytests/test_IsisDownload.py
@@ -6,7 +6,7 @@ import os
 import pytest 
 from unittest import mock
 from tempfile import TemporaryDirectory
-from pathlib import Path
+from pathlib import Path, exists
 
 from importlib.util import spec_from_loader, module_from_spec
 from importlib.machinery import SourceFileLoader 
@@ -59,3 +59,23 @@ def test_create_rclone_args():
         dest = Path(tdir)
         args = did.create_rclone_arguments(str(dest), "lro_naifKernels:", ntransfers=100, rclone_kwargs=["--dry_run"])
         assert args == ['lro_naifKernels:', str(dest/"lro"/"kernels"), '--progress', '--checkers=100', '--transfers=100', '--track-renames', '--log-level=WARNING', '--dry_run']
+
+
+def test_file_filtering():
+    with TemporaryDirectory() as tdir:
+        dest = Path(tdir)
+        # Run the rclone command to download files
+        did.rclone("lsf", "test", dest, extra_args=["-l", "-R", "--format", "p", "--files-only"], redirect_stdout=True, redirect_stderr=True)
+        # Check if the specified files are not present in the destination directory
+        assert not (dest / "a_older_versions").exists()
+        assert not (dest / "kernels" / "*" / "former_versions").exists()
+        assert not (dest / "corrupt_files").exists()
+        assert not (dest / "zzarchive").exists()
+        assert not (dest / "kernels" / "*" / "original").exists()
+        assert not (dest / "gallileo" / "kernels" / "ck" / "prime_mission" / "*" / "extended_mission").exists()
+        assert not (dest / "gallileo" / "kernels" / "*" / "prime_mission").exists()
+        assert not (dest / "galileo" / "kernels" / "ck" / "GEM").exists()
+        assert not (dest / "clementine1" / "kernels" / "ck" / "save").exists()
+        assert not (dest / "chandrayaan1" / "kernels" / "spk" / "SAVE_SCS_2017-11-22").exists()
+        assert not (dest / "*" / "kernels" / "fk" / "release.*" / "*").exists()
+        assert not (dest / "cassini" / "kernels" / "fk" / "Archive").exists()
