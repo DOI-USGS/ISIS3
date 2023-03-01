7d6c93cefb6ba8618c8f29d10635ab204eb09552
antonhibl <antonhibl11@gmail.com>
2023-03-01 15:50:40 -0700

    adds another optional arg, cubename
        lets the user specify an optional cubename parameter which replaces the
        timestamp style naming for the output stitched cube. If the parameter is not
        given it defaults to the timestamp style naming convention for retaining
        existing functionality.

--- isis/src/tgo/apps/tgocassisstitch/tgocassisstitch.cpp
+++ isis/src/tgo/apps/tgocassisstitch/tgocassisstitch.cpp
@@ -66,12 +66,19 @@ namespace Isis {
       throw IException(e, IException::Unknown, msg, _FILEINFO_);
     }
 
+    // read the optional cubename
+    FileName frameletCubeFlag(ui.GetCubeName("CUBENAME"));
+    QString frameletCubeName = frameletCubeFlag.expanded();
+
     // Stitch together the individual frames
     FileName outputPrefix(ui.GetCubeName("OUTPUTPREFIX"));
     FileName outputSuffix(ui.GetCubeName("OUTPUTSUFFIX"));
     QString outputPrefBaseName = outputPrefix.expanded();
     QString outputSuffBaseName = outputSuffix.expanded();
     QStringList frameKeys = frameMap.uniqueKeys();
+    foreach (const QString& key, frameKeys) {
+      std::cout << key.toStdString() << std::endl;
+    }
     Progress stitchProgress;
     stitchProgress.SetText("Stitching Frames");
     stitchProgress.SetMaximumSteps(frameKeys.size());
@@ -81,23 +88,49 @@ namespace Isis {
       try {
         QString frameIdentifier = frameKey.split("/").last();
         if ((outputPrefix != "nil") && (outputSuffix == "nil")) {
-          FileName frameFileName(outputPrefBaseName + "-" + frameIdentifier + ".cub");
-          stitchFrame( frameMap.values(frameKey), frameFileName );
-          stitchProgress.CheckStatus();
+          if (frameletCubeName != "nil") {
+            FileName frameFileName(outputPrefBaseName + "-" + frameletCubeName + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          } else {
+            FileName frameFileName(outputPrefBaseName + "-" + frameIdentifier + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          }
         } else if ((outputSuffix != "nil") && (outputPrefix == "nil")) {
-          FileName frameFileName(frameIdentifier + "-" + outputSuffBaseName + ".cub");
-          stitchFrame( frameMap.values(frameKey), frameFileName );
-          stitchProgress.CheckStatus();
+          if (frameletCubeName != "nil") {
+            FileName frameFileName(frameletCubeName + "-" + outputSuffBaseName + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          } else {
+            FileName frameFileName(frameIdentifier + "-" + outputSuffBaseName + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          }
         } else if ((outputPrefix != "nil") && (outputSuffix != "nil")) {
-          FileName frameFileName(outputPrefBaseName +
+          if (frameletCubeName != "nil") {
+            FileName frameFileName(outputPrefBaseName +
+                                   "-" + frameletCubeName +
+                                   "-" + outputSuffBaseName + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          } else {
+            FileName frameFileName(outputPrefBaseName +
                                  "-" + frameIdentifier +
                                  "-" + outputSuffBaseName + ".cub");
-          stitchFrame( frameMap.values(frameKey), frameFileName );
-          stitchProgress.CheckStatus();
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          }
         } else if ((outputPrefix == "nil") && (outputSuffix == "nil")) {
-          FileName frameFileName(frameIdentifier + ".cub");
-          stitchFrame( frameMap.values(frameKey), frameFileName );
-          stitchProgress.CheckStatus();
+           if (frameletCubeName != "nil") {
+            FileName frameFileName(frameletCubeName + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          } else {
+            FileName frameFileName(frameIdentifier + ".cub");
+            stitchFrame( frameMap.values(frameKey), frameFileName );
+            stitchProgress.CheckStatus();
+          }
         }
       }
       catch (IException &e) {
