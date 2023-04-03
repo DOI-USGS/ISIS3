60f8de572b7d76c1a91f1c19efd16e8f833456fe
Kris J. Becker <kbecker@orex.lpl.arizona.edu>
2023-04-03 15:21:54 -0700

    isisdataeval install $ISISROOT/bin/isisdata_mockup
    isisdata_mockup.py - Install this script in $ISISROOT/bin as isisdata_mockup (no extension)
    
    make_isisdata_mockup.sh - Renamed isisdata_mockup.py to isisdata_mockup
    
    CMakeLists.txt - Add install command for isisdata_mockup

--- isis/tests/data/isisdata/make_isisdata_mockup.sh
+++ isis/tests/data/isisdata/make_isisdata_mockup.sh
@@ -5,10 +5,10 @@
 mkdir -p mockup mockprocessing
 
 # Produce the mockup data. Its assumed isisdata_mockup.py is in a runtime path.
-isisdata_mockup.py --saveconfig --hasher all --isisdata $ISISDATA/base     --outpath mockprocessing/isisdatamockup/base     --ghostdir '$ISISDATA/base'     --tojson isisdata_mockup_base.json
-isisdata_mockup.py --saveconfig --hasher all --isisdata $ISISDATA/hayabusa --outpath mockprocessing/isisdatamockup/hayabusa --ghostdir '$ISISDATA/hayabusa' --tojson isisdata_mockup_hayabusa.json
-isisdata_mockup.py --saveconfig --hasher all --isisdata $ISISDATA/smart1   --outpath mockprocessing/isisdatamockup/smart1   --ghostdir '$ISISDATA/smart1'   --tojson isisdata_mockup_smart1.json
-isisdata_mockup.py --saveconfig --hasher all --isisdata $ISISDATA/voyager1 --outpath mockprocessing/isisdatamockup/voyager1 --ghostdir '$ISISDATA/voyager1' --tojson isisdata_mockup_voyager1.json
+isisdata_mockup --saveconfig --hasher all --isisdata $ISISDATA/base     --outpath mockprocessing/isisdatamockup/base     --ghostdir '$ISISDATA/base'     --tojson isisdata_mockup_base.json
+isisdata_mockup --saveconfig --hasher all --isisdata $ISISDATA/hayabusa --outpath mockprocessing/isisdatamockup/hayabusa --ghostdir '$ISISDATA/hayabusa' --tojson isisdata_mockup_hayabusa.json
+isisdata_mockup --saveconfig --hasher all --isisdata $ISISDATA/smart1   --outpath mockprocessing/isisdatamockup/smart1   --ghostdir '$ISISDATA/smart1'   --tojson isisdata_mockup_smart1.json
+isisdata_mockup --saveconfig --hasher all --isisdata $ISISDATA/voyager1 --outpath mockprocessing/isisdatamockup/voyager1 --ghostdir '$ISISDATA/voyager1' --tojson isisdata_mockup_voyager1.json
 
 # Copy/install the desired files for the test
 rsync -av --files-from=isisdataeval_isisdata_mockup_files.lis mockprocessing/isisdatamockup/ mockup/
