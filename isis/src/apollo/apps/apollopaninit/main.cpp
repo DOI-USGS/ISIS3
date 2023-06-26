b2662ee34219cfe214e58c92f2e8f909297c66bb
antonhibl <antonhibl11@gmail.com>
2023-06-26 12:33:53 -0700

    fixes for apollopaninit.

--- isis/src/apollo/apps/apollopaninit/main.cpp
+++ isis/src/apollo/apps/apollopaninit/main.cpp
@@ -117,11 +117,11 @@ void IsisMain() {
   insCode = scFrameCode - 230;
 
   try {
-    panCube.open(ui.GetCubeName("FROM"),"rw");
+    panCube.open(ui.GetFileName("FROM"),"rw");
   }
   catch (IException &e) {
     throw IException(IException::User,
-                     "Unable to open the file [" + ui.GetCubeName("FROM") + "] as a cube.",
+                     "Unable to open the file [" + ui.GetFileName("FROM") + "] as a cube.",
                      _FILEINFO_);
   }
 
@@ -639,7 +639,7 @@ void IsisMain() {
   CentroidApolloPan centroid(resolution);
   Chip inputChip,selectionChip;
   inputChip.SetSize(int(ceil(200*5.0/resolution)), int(ceil(200*5.0/resolution)));
-  fileName = ui.GetCubeName("FROM");
+  fileName = ui.GetFileName("FROM");
   if( panCube.pixelType() == 1)  //UnsignedByte
     centroid.setDNRange(12, 1e99);  //8 bit bright target
   else
@@ -768,7 +768,7 @@ void IsisMain() {
   panCube.write(tableFid);
   //close the new cube
   panCube.close(false);
-  panCube.open(ui.GetCubeName("FROM"),"rw");
+  panCube.open(ui.GetFileName("FROM"),"rw");
 
   delete spPos;
   delete spRot;
