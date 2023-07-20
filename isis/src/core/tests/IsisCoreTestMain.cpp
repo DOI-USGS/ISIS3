39034c8fab9295a0e3be1c735944585c08ecd35a
acpaquette <acpaquette@usgs.gov>
2023-07-20 15:48:27 -0700

    Enforce that core unit tests use the test preferences

--- isis/src/core/tests/IsisCoreTestMain.cpp
+++ isis/src/core/tests/IsisCoreTestMain.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
 
+#include "Preference.h"
+
+using namespace Isis;
+
 int main(int argc, char **argv) {
+   Isis::Preference::Preferences(true);
+
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
