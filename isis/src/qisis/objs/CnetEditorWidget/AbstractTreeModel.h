7bf8d3c63ec6a52d3ae0c05ba7066bc93d0c4a26
Adam Paquette <acpaquette@usgs.gov>
2023-08-25 05:56:47 +0000

    Fixed up includes in failing linux builds

--- isis/src/qisis/objs/CnetEditorWidget/AbstractTreeModel.h
+++ isis/src/qisis/objs/CnetEditorWidget/AbstractTreeModel.h
@@ -8,6 +8,7 @@ For more details about the LICENSE terms and the AUTHORS, you will
 find files of those names at the top level of this repository. **/
 
 /* SPDX-License-Identifier: CC0-1.0 */
+#include <functional>
 
 #include <QObject>
 
