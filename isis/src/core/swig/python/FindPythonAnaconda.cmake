d2b718ea6dc0722a3971b9e400bedeec0632817e
acpaquette <acpaquette@usgs.gov>
2023-10-10 21:28:38 -0700

    Fixed python find for python 3.9

--- isis/src/core/swig/python/FindPythonAnaconda.cmake
+++ isis/src/core/swig/python/FindPythonAnaconda.cmake
@@ -64,6 +64,7 @@ if(NOT CMAKE_FIND_ANACONDA_PYTHON_INCLUDED)
 
     if( (${_py_version_major} MATCHES 2) OR
         (${_py_version_major} MATCHES 3 AND ${_py_version_minor} MATCHES 8) OR
+        (${_py_version_major} MATCHES 3 AND ${_py_version_minor} MATCHES 9) OR
         (${_py_version_major} MATCHES 3 AND ${_py_version_minor} MATCHES 10) )
       set( _py_ext "")
     else()
