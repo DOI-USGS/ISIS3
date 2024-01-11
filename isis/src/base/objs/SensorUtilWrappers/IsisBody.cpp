eb8fb6698e8e7a7f0ef9131053e650bf8b613790
Amy Stamile <astamile@usgs.gov>
2024-01-11 11:16:35 -0700

    Added Body Tests

--- isis/src/base/objs/SensorUtilWrappers/IsisBody.cpp
+++ isis/src/base/objs/SensorUtilWrappers/IsisBody.cpp
@@ -29,14 +29,17 @@ namespace Isis {
     if (timeChanged) {
       m_rot->SetEphemerisTime(oldTime);
     }
+    // ISIS uses Km, so convert to meters
+    for (int i = 0; i < 9; i++ ){
+      bodyRotMat[i] = bodyRotMat[i] * 1000.0;
+    }
     return bodyRotMat;
   }
 
   SensorUtilities::Vec IsisBody::fixedVector(SensorUtilities::Vec pos) {
-    vector<double> instPos(pos);
+    std::vector<double> instPos(pos);
     std::vector<double> sB = m_rot->ReferenceVector(instPos);
 
-    SensorUtilities::Vec fixVec = {sB[0], sB[1], sB[2]};
-    return fixVec;
+    return {sB[0] * 1000.0, sB[1] * 1000.0, sB[2] * 1000.0};
   }
 }
