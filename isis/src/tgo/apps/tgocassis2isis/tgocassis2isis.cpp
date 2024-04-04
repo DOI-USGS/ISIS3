35b21fc9107b186984beee954ce321deb32426d9
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2024-04-03 21:24:02 -0700

    Tgocassis ingest fixes for cas_cal_sc images (#5435)
    * fixes cas_cal_sc ingest errors
    
    * fixed tests

--- isis/src/tgo/apps/tgocassis2isis/tgocassis2isis.cpp
+++ isis/src/tgo/apps/tgocassis2isis/tgocassis2isis.cpp
@@ -36,6 +36,16 @@ namespace Isis {
   void tgocassis2isis(UserInterface &ui) {
     FileName xmlFileName = ui.GetFileName("FROM");
 
+    if (!xmlFileName.removeExtension().addExtension("dat").fileExists() && !xmlFileName.removeExtension().addExtension("img").fileExists()) {
+        QString msg = "Cannot find image file for [" + xmlFileName.name() + "]. Confirm that the "
+        ".dat or .img file for this XML exists and is located in the same directory.";
+      throw IException(IException::User, msg, _FILEINFO_);
+    }
+    if(xmlFileName.name().contains("cas_raw_sc")){
+        QString msg = "tgocassis2isis is unable to process cas_raw_sc data. Please use cas_cal_sc data instead.";
+      throw IException(IException::User, msg, _FILEINFO_);
+    }
+
     try {
       ProcessImport importer;
       translateCoreInfo(xmlFileName, importer);
@@ -59,9 +69,11 @@ namespace Isis {
       QFile xmlFile(xmlFileName.expanded());
       QDomDocument xmlDoc;
       xmlDoc.setContent(&xmlFile, true);
-      // If any instances of "Optical_Filter" exist, use PSA .trn file
+      // If any instances of "Optical_Filter" or "Mission_Area" exist, use PSA .trn file
       QString transExportFile;
       if (xmlDoc.elementsByTagName("Optical_Filter").size()){
+        transExportFile = "TgoCassisExportedInstrument_PSA_Optical_Filter.trn";
+      } else if (xmlDoc.elementsByTagName("Mission_Area").size()) {
         transExportFile = "TgoCassisExportedInstrument_PSA.trn";
       } else {
         transExportFile = "TgoCassisExportedInstrument.trn";
