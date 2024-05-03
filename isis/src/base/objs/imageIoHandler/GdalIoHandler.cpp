0f105c58b00cac3a495f45481079b6939aae01f6
Kelvin <krodriguez@usgs.gov>
2024-05-03 14:18:42 -0700

    added write + tests

--- isis/src/base/objs/imageIoHandler/GdalIoHandler.cpp
+++ isis/src/base/objs/imageIoHandler/GdalIoHandler.cpp
@@ -16,7 +16,7 @@ namespace Isis {
   GdalIoHandler::GdalIoHandler(QString &dataFilePath, const QList<int> *virtualBandList, const Pvl &label) : 
                  ImageIoHandler(virtualBandList) {
     GDALAllRegister();
-    const GDALAccess eAccess = GA_ReadOnly;
+    const GDALAccess eAccess = GA_Update;
     std::string dataFilePathStr = dataFilePath.toUtf8().constData();
     const char *charDataFilePath = dataFilePathStr.c_str();
     m_geodataSet = GDALDatasetUniquePtr(GDALDataset::FromHandle(GDALOpen(charDataFilePath, eAccess)));
@@ -37,7 +37,8 @@ namespace Isis {
     // as gdal reads 0 based lines and samples
     int lineStart = bufferToFill.Line() - 1;
     int sampleStart = bufferToFill.Sample() - 1;
-    poBand->RasterIO(GF_Read, sampleStart, lineStart, 
+    // silence warnings
+    CPLErr err = poBand->RasterIO(GF_Read, sampleStart, lineStart, 
                      bufferToFill.SampleDimension(), bufferToFill.LineDimension(),
                      bufferToFill.DoubleBuffer(), 
                      bufferToFill.SampleDimension(), bufferToFill.LineDimension(), 
@@ -45,7 +46,22 @@ namespace Isis {
                      0, 0);
   }
 
-  void GdalIoHandler::write(const Buffer &bufferToWrite) {}
+  void GdalIoHandler::write(const Buffer &bufferToWrite) {
+    GDALRasterBand  *poBand;
+    int band = bufferToWrite.Band();
+    poBand = m_geodataSet->GetRasterBand(band);
+
+    int lineStart = bufferToWrite.Line() - 1;
+    int sampleStart = bufferToWrite.Sample() - 1;
+    
+    // silence warning
+    CPLErr err = poBand->RasterIO(GF_Write, sampleStart, lineStart, 
+                     bufferToWrite.SampleDimension(), bufferToWrite.LineDimension(),
+                     bufferToWrite.DoubleBuffer(), 
+                     bufferToWrite.SampleDimension(), bufferToWrite.LineDimension(), 
+                     GDT_Float64,
+                     0, 0);
+  }
 
   BigInt GdalIoHandler::getDataSize() const {
     return 0;
--- isis/tests/GdalIoHandlerTests.cpp
+++ isis/tests/GdalIoHandlerTests.cpp
@@ -16,7 +16,7 @@ using json = nlohmann::json;
 using namespace Isis;
 
 TEST(GdalIoTests, DefaultRead) {
-  QString filePath = "/Users/acpaquette/repos/ISIS3/build/B10_013341_1010_XN_79S172W.tiff";
+  QString filePath = "/Users/krodriguez/cubes/gdal/B10_013341_1010_XN_79S172W.tiff";
   const QList<int> *bandList = new QList<int>;
   Pvl label;
 
@@ -54,4 +54,44 @@ TEST(GdalIoTests, DefaultRead) {
   // }
   // std::cout << sum << std::endl;
   // delete dataFile;
+}
+
+
+TEST(GdalIoTests, DefaulWrite) {
+  QString filePath = "/Users/krodriguez/cubes/gdal/B10_013341_1010_XN_79S172W.tiff";
+  
+  {
+    const QList<int> *bandList = new QList<int>;
+    Pvl label;
+
+    GdalIoHandler handler(filePath, bandList, label);
+    
+    Brick localBrick(200, 200, 1, SignedWord);
+
+    localBrick.SetBasePosition(1, 1, 1);
+    // init everything to 1
+    for (int i = 0; i < localBrick.size(); i++) {
+      localBrick[i] = 100;
+    }
+
+    localBrick.SetBasePosition(1, 1, 1);
+    handler.write(localBrick);
+  } // file is closed  
+
+  // read it back 
+  GDALDatasetUniquePtr ds = GDALDatasetUniquePtr(GDALDataset::FromHandle(GDALOpen(filePath.toStdString().c_str(), GA_ReadOnly)));
+  double* dbuf = (double *) CPLMalloc(sizeof(double)*200*200); 
+  GDALRasterBand  *poBand = ds->GetRasterBand(1);
+  CPLErr err = poBand->RasterIO(GF_Read, 0, 0, 
+                  200, 200,
+                  dbuf, 
+                  200, 200, 
+                  GDT_Float64,
+                  0, 0);
+
+  
+  for (int i = 0; i<200*200; i++) { 
+    EXPECT_EQ(dbuf[i], 1)00;
+  }
+
 }
