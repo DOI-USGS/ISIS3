7d2c134ecfba1f50ba06d071afbef5166d03d8ca
acpaquette <acpaquette@usgs.gov>
2024-07-12 23:08:28 +0000

    Ported Fits fix

--- isis/tests/FitsToJsonTests.cpp
+++ isis/tests/FitsToJsonTests.cpp
@@ -19,21 +19,21 @@ TEST(FitsToJson, FitsConversion) {
     FAIL() << "Unable to convert lorri fits label to json " << e.toString().toStdString().c_str() << std::endl;
   }
   // Instrument info
-  EXPECT_EQ(jsonData["FitsLabels"]["HOSTNAME"]["Value"], "NEW HORIZONS");
-  EXPECT_EQ(jsonData["FitsLabels"]["INSTRU"]["Value"], "lor");
-  EXPECT_EQ(jsonData["FitsLabels"]["TARGET"]["Value"], "IO");
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCUTCAL"]["Value"], "2007-02-28T13:14:19.358");
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCSCLK"]["Value"], "1/0034974376:48500");
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCSCDP"]["Value"], "1748718848500.0");
-  EXPECT_EQ(jsonData["FitsLabels"]["EXPTIME"]["Value"], "0.02");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["HOSTNAME"]["Value"], "NEW HORIZONS");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["INSTRU"]["Value"], "lor");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["TARGET"]["Value"], "IO");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCUTCAL"]["Value"], "2007-02-28T13:14:19.358");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCSCLK"]["Value"], "1/0034974376:48500");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCSCDP"]["Value"], "1748718848500.0");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["EXPTIME"]["Value"], "0.02");
 
   // Archive info
-  EXPECT_EQ(jsonData["FitsLabels"]["HSCOMPR"]["Value"], "LOSSLESS");
-  EXPECT_EQ(jsonData["FitsLabels"]["OBSCOMPL"]["Value"], "COMPLETE");
-  EXPECT_EQ(jsonData["FitsLabels"]["REQDESC"]["Value"], "Jupiter shine");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["HSCOMPR"]["Value"], "LOSSLESS");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["OBSCOMPL"]["Value"], "COMPLETE");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["REQDESC"]["Value"], "Jupiter shine");
 
   // Kernels info
-  EXPECT_EQ(jsonData["FitsLabels"]["FORMAT"]["Value"], "0");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["FORMAT"]["Value"], "0");
 
 
   fileTemplate = "data/leisa2isis/lsb_0034933739_0x53c_sci_1_cropped.fit";
@@ -45,21 +45,21 @@ TEST(FitsToJson, FitsConversion) {
   }
 
   // Instrument info
-  EXPECT_EQ(jsonData["FitsLabels"]["HOSTNAME"]["Value"], "NEW HORIZONS");
-  EXPECT_EQ(jsonData["FitsLabels"]["INSTRU"]["Value"], "lei");
-  EXPECT_EQ(jsonData["FitsLabels"]["TARGET"]["Value"], "EUROPA");
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCSCLK0"]["Value"], "1/0034931099:00000");
-  EXPECT_EQ(jsonData["FitsLabels"]["RALPHEXP"]["Value"], "0.676");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["HOSTNAME"]["Value"], "NEW HORIZONS");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["INSTRU"]["Value"], "lei");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["TARGET"]["Value"], "EUROPA");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCSCLK0"]["Value"], "1/0034931099:00000");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["RALPHEXP"]["Value"], "0.676");
 
   // Archive info
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCSCET"]["Value"], "225897372.0736388");
-  EXPECT_EQ(jsonData["FitsLabels"]["DURMET"]["Value"], "251.0");
-  EXPECT_EQ(jsonData["FitsLabels"]["DETECTOR"]["Value"], "LEISA");
-  EXPECT_EQ(jsonData["FitsLabels"]["SCANTYPE"]["Value"], "LEISA");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCSCET"]["Value"], "225897372.0736388");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["DURMET"]["Value"], "251.0");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["DETECTOR"]["Value"], "LEISA");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SCANTYPE"]["Value"], "LEISA");
 
   // BandBin info
-  EXPECT_EQ(jsonData["FitsLabels"]["FILTER"]["Value"], "WEDGE");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["FILTER"]["Value"], "WEDGE");
 
   // Kernels info
-  EXPECT_EQ(jsonData["FitsLabels"]["SPCINSID"]["Value"], "-98201");
+  EXPECT_EQ(jsonData["FitsLabels"][0]["SPCINSID"]["Value"], "-98201");
 }
