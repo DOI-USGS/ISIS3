21cdc2357fd8f776514b4cf03a6dea670771a3d7
Austin Sanders <arsanders@usgs.gov>
2024-07-23 01:36:52 -0600

    Change FindTool's UniversalGroundMap priority to Projection

--- isis/src/qisis/objs/FindTool/FindTool.cpp
+++ isis/src/qisis/objs/FindTool/FindTool.cpp
@@ -631,9 +631,10 @@ namespace Isis {
    */
   Distance FindTool::distancePerPixel(MdiCubeViewport *viewport,
                                       double lat, double lon) {
-    UniversalGroundMap *groundMap = viewport->universalGroundMap();
+    // UniversalGroundMaps default to camera priority, so create a new one so that we can use projection if it exists.
+    std::unique_ptr<UniversalGroundMap> groundMap(new UniversalGroundMap(*(viewport->cube()), UniversalGroundMap::ProjectionFirst));
     Distance viewportResolution;
-    if (viewport->camera() != NULL){
+    if (groundMap->Camera() != NULL){
       if (groundMap->Camera()->target()->isSky()) {
         return Distance(groundMap->Camera()->RaDecResolution(), Distance::Units::Meters);
       }
