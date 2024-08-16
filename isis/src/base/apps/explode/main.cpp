490041efe00c313fe7c0f49f1c20b7a560780686
kledmundson <k.l.edmundson@gmail.com>
2024-08-16 13:16:22 -0700

    Updating main.cpp and removed Makefile tests for explode conversion to callable app. Addresses #5557.

--- isis/src/base/apps/explode/main.cpp
+++ isis/src/base/apps/explode/main.cpp
@@ -1,63 +1,20 @@
-#include "Isis.h"
-#include "ProcessByLine.h"
-#include "IException.h"
-#include "FileName.h"
+/** This is free and unencumbered software released into the public domain.
 
-using namespace std;
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
+
+#include "Isis.h"
+
+#include "explode.h"
+
+#include "Application.h"
+
 using namespace Isis;
 
-void CopyBand(Buffer &in, Buffer &out);
-
 void IsisMain() {
-  // Get the cube to explode
-  Process p;
-  Cube *icube = p.SetInputCube("FROM");
-  int samps = icube->sampleCount();
-  int lines = icube->lineCount();
-  int bands = icube->bandCount();
-  QString infile = icube->fileName();
-
-  // We the output filename so we can add attributes and extensions
   UserInterface &ui = Application::GetUserInterface();
-  QString outbase = ui.GetCubeName("TO");
-  CubeAttributeOutput &outatt = ui.GetOutputAttribute("TO");
-
-  // Loop and extract each band
-  for(int band = 1; band <= bands; band++) {
-    int pband = icube->physicalBand(band);
-    QString sband(toString(pband));
-
-    ProcessByLine p2;
-    Progress *prog = p2.Progress();
-    prog->SetText("Exploding band " + sband);
-
-    CubeAttributeInput inatt("+" + sband);
-    p2.SetInputCube(infile, inatt);
-
-    QString outfile = outbase + ".band";
-    if(pband / 1000 == 0) {
-      outfile += "0";
-      if(pband / 100 == 0) {
-        outfile += "0";
-        if(pband / 10 == 0) {
-          outfile += "0";
-        }
-      }
-    }
-    outfile += sband + ".cub";
-    p2.SetOutputCube(outfile, outatt, samps, lines, 1);
-
-    p2.StartProcess(CopyBand);
-    p2.EndProcess();
-  }
-
-  // Cleanup
-  p.EndProcess();
-}
-
-// Line processing routine
-void CopyBand(Buffer &in, Buffer &out) {
-  for(int i = 0; i < in.size(); i++) {
-    out[i] = in[i];
-  }
+  explode(ui);
 }
