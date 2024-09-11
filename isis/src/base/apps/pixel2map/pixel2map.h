aebfea53207410d508b67aac3976606114a586d7
Alessandro Frigeri <alessandro.frigeri@inaf.it>
2024-09-11 12:25:06 +0200

    pixel2map definition re-inserted into Isis namespace (which was deleted during code cleanup)

--- isis/src/base/apps/pixel2map/pixel2map.h
+++ isis/src/base/apps/pixel2map/pixel2map.h
@@ -23,36 +23,10 @@
 #include "UserInterface.h"
 
 
-
+namespace Isis {
+	
 extern void pixel2map(UserInterface &ui, Pvl *log=nullptr);
-  
 
-/**
- * @author 2008-02-13 Stacy Alley
- *
- * @internal 
- *   @history 2013-07-30 Stuart C. Sides & Tracie Sucharski Renamed from vim2map
- */
-
-  /**
-class pixel2mapForward : public Isis::Transform {
-  private:
-    Isis::Camera *p_incam;
-    Isis::Projection *p_outmap;
-
-  public:
-    // constructor
-    pixel2mapForward(const int inputSamples, const int inputLines, Isis::Camera *incam,
-             const int outputSamples, const int outputLines, Isis::Projection *outmap,
-             bool trim);
-
-    // destructor
-    ~pixel2mapForward() {};
-
-
-};
-**/
-
-} // namespace Isis
+} 
 
 #endif
