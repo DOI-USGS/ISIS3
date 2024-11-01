d8b4f1d259809a67cca61a4f96c97c2ed968c84a
Sanders <arsanders@igswza281l2314.gs.doi.net>
2024-11-01 16:50:34 -0600

    Chandrayaan2tmc updates and fixes

--- isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/unitTest.cpp
+++ isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/unitTest.cpp
@@ -26,16 +26,16 @@ int main(void) {
   Preference::Preferences(true);
 
 
-  cout << "Unit Test for Chandrayaan2TmcCamera..." << endl;
+  cout << "Unit Test for Chandrayaan1M3Camera..." << endl;
   try {
     // These should be lat/lon at center of image. To obtain these numbers for a new cube/camera,
     // set both the known lat and known lon to zero and copy the unit test output "Latitude off by: "
     // and "Longitude off by: " values directly into these variables. NOTE: These are only used
     // for the center of the image test, not the corners.
-    double knownLat = -14.4676517916533811;
-    double knownLon = 131.0534566173186306;
+    double knownLat = 61.50040250242506;
+    double knownLon = 74.89590535143694;
 
-    Cube c("$ISISTESTDATA/isis/src/chandrayaan1/unitTestData/M3T20090630T083407_V03_RDN.cub", "r");
+    Cube c("/Users/arsanders/chandrayaan/data/calibrated/20191127/out.cub", "r");
     Camera *cam = CameraFactory::Create(c);
     cout << "FileName: " << FileName(c.fileName()).name() << endl;
     cout << "CK Frame: " << cam->instrumentRotation()->Frame() << endl << endl;
@@ -121,3 +121,4 @@ void TestLineSamp(Camera *cam, double samp, double line) {
     cout << "DeltaLine = ERROR" << endl << endl;
   }
 }
+
