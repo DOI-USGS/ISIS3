b44539c74574aeed399230e62e95c9cf197dad81
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-06-11 15:35:35 -0700

    Bugfix for Chandrayaan OHRC

--- isis/src/base/apps/isisimport/isisimport.cpp
+++ isis/src/base/apps/isisimport/isisimport.cpp
@@ -456,6 +456,16 @@ namespace Isis {
       importer.SetFileHeaderBytes(0);
     }
 
+    // Bugfix for OHRC, the exposure duration is off by a factor of 1000.
+    PvlGroup &inst = newLabel.findGroup("Instrument", Pvl::Traverse); // alias
+    QString spacecraft = inst["SpacecraftName"];
+    QString instrumentId = inst["InstrumentId"];
+    if (spacecraft == "Chandrayaan-2" && instrumentId == "OHRC") {
+      double val = toDouble(inst["LineExposureDuration"]);
+      val /= 1000.0;
+      inst["LineExposureDuration"].setValue(toString(val), "ms");
+    }
+
     // Checks that are unique to mgsmoc
     if (translation.hasKeyword("compressed") && translation.hasKeyword("projected")) {
       if (toBool(translation["compressed"])) {
