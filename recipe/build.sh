f7491fce8987a54b09a5b18c0b090bb4334c2c6c
Kelvin <krodriguez@usgs.gov>
2025-08-18 11:27:04 -0700

    addingpython prefixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,19 @@
 mkdir build
 cd build
 export ISISROOT=$PWD
-cmake -GNinja -DJP2KFLAG=ON -Dpybindings=ON -DKAKADU_INCLUDE_DIR=/isisData/kakadu -DbuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../isis
+export CMAKE_PREFIX_PATH="$PREFIX"
+cmake -GNinja \
+  -DJP2KFLAG=ON \
+  -Dpybindings=ON \
+  -DKAKADU_INCLUDE_DIR=/Users/krodriguez/kakadu_7_9 \
+  -DbuildTests=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DPython3_EXECUTABLE=$PYTHON \
+  -DPython_EXECUTABLE=$PYTHON \
+  -DPython3_ROOT_DIR=$PREFIX \
+  -DPython_ROOT_DIR=$PREFIX \
+  -DCMAKE_FIND_FRAMEWORK=LAST \
+  ../isis
 ninja install
