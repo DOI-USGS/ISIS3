792769784dcbe81325b8eea3b6de474ac4c6b892
amystamile-usgs <astamile@usgs.gov>
2025-09-19 10:11:21 -0700

    remove original changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,5 @@
 mkdir build
 cd build
 export ISISROOT=$PWD
-export CMAKE_PREFIX_PATH="$PREFIX"
-cmake -GNinja \
-  -DJP2KFLAG=ON \
-  -Dpybindings=ON \
-  -DKAKADU_INCLUDE_DIR=/isisData/kakadu \
-  -DbuildTests=OFF \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DPython3_EXECUTABLE=$PYTHON \
-  -DPython_EXECUTABLE=$PYTHON \
-  -DPython3_ROOT_DIR=$PREFIX \
-  -DPython_ROOT_DIR=$PREFIX \
-  -DCMAKE_FIND_FRAMEWORK=LAST \
-  ../isis
+cmake -GNinja -DJP2KFLAG=ON -Dpybindings=ON -DKAKADU_INCLUDE_DIR=/isisData/kakadu -DbuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../isis
 ninja install
