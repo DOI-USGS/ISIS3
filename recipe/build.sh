f81c7222167c7fd7be2d435866422b2752171f2b
Jacob Cain <jrcain@usgs.gov>
2025-12-11 17:46:13 -0800

    fix for conda builds on mac 15

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 mkdir build
 cd build
 export ISISROOT=$PWD
-cmake -GNinja -DJP2KFLAG=ON -Dpybindings=ON -DKAKADU_INCLUDE_DIR=/isisData/kakadu -DbuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../isis
+cmake -GNinja -DJP2KFLAG=ON -Dpybindings=ON -DKAKADU_INCLUDE_DIR=/tmp/kakadu_7_9 -DbuildTests=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ../isis
 ninja install
