40ffd98b157f2d928e81a5641773c6030e4827ec
acpaquette <acpaquette@usgs.gov>
2025-12-17 13:53:22 -0700

    Fixed include formating in Chip unitTest

--- isis/src/base/objs/Chip/unitTest.cpp
+++ isis/src/base/objs/Chip/unitTest.cpp
@@ -3,7 +3,8 @@ The authors of ISIS do not claim copyright on the contents of this file.
 For more details about the LICENSE terms and the AUTHORS, you will
 find files of those names at the top level of this repository. **/
 
-/* SPDX-License-Identifier: CC0-1.0 */#include <cstdlib>
+/* SPDX-License-Identifier: CC0-1.0 */
+#include <cstdlib>
 
 #include <iomanip>
 #include <iostream>
