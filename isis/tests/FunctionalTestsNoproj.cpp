0e05bdafb61685ae9af623b92786d411612ffd98
acpaquette <acpaquette@usgs.gov>
2026-02-03 13:55:26 -0700

    Update necessary gtests

--- isis/tests/FunctionalTestsNoproj.cpp
+++ isis/tests/FunctionalTestsNoproj.cpp
@@ -4,11 +4,9 @@
 #include "CameraFixtures.h"
 #include "Histogram.h"
 #include "LineManager.h"
-#include "Pvl.h"
 #include "PvlGroup.h"
 #include "TestUtilities.h"
 #include "skypt.h"
-#include "getsn.h"
 
 #include "gmock/gmock.h"
 
@@ -400,10 +398,10 @@ TEST_F(LineScannerCube, FunctionalTestNoprojLineScanner) {
   EXPECT_PRED_FORMAT2(AssertPvlGroupKeywordsEqual, origInst, testCubeInst); //REFACTOR
 
   std::unique_ptr<Histogram> hist (oCube.histogram(1));
-  EXPECT_NEAR(hist->Average(), 26.259903800874739, .000001);
-  EXPECT_NEAR(hist->Sum(), 78070.6940000006, .0001);
+  EXPECT_NEAR(hist->Average(), 26.259685166498699, .000001);
+  EXPECT_NEAR(hist->Sum(), 78070.044000000635, .0001);
   EXPECT_EQ(hist->ValidPixels(), 2973);
-  EXPECT_NEAR(hist->StandardDeviation(), 11.938337629048096, .0001);
+  EXPECT_NEAR(hist->StandardDeviation(), 11.938131126485441, .0001);
 }
 
 /**
@@ -854,32 +852,3 @@ TEST_F(DefaultCube, FunctionalTestNoprojOffBodyTrueOffBodyTrimTrue) {
 
 }
 
-TEST_F(DefaultCube, FunctionalTestNoprojSn) {
-  QString cubeFileName = tempDir.path() + "/output.cub";
-  QVector<QString> args = {"to=" + cubeFileName};
-  UserInterface options(APP_XML, args);
-  
-  noproj(testCube, NULL, options);
-
-  Cube oCube(cubeFileName);
-  Pvl *isisLabel = oCube.label();
-  PvlGroup instGroup = isisLabel->findGroup("Instrument", Pvl::Traverse);
-  EXPECT_PRED_FORMAT2(AssertQStringsEqual, instGroup.findKeyword("SpacecraftName"), "IdealSpacecraft");
-  EXPECT_PRED_FORMAT2(AssertQStringsEqual, instGroup.findKeyword("InstrumentId"), "IdealCamera");
-  EXPECT_DOUBLE_EQ((double)instGroup.findKeyword("EphemerisTime"), -709401200.26114);
-
-  // Get SN
-  QString APP_XML_SN = FileName("$ISISROOT/bin/xml/getsn.xml").expanded();
-  QVector<QString> sn_args = {"from=" + cubeFileName,
-                              "file=true",
-                              "observation=true"};
-  UserInterface sn_options(APP_XML_SN, sn_args);
-  Pvl appLog;
-  getsn(sn_options, &appLog);
-
-  PvlGroup results = appLog.findGroup("Results");
-  EXPECT_PRED_FORMAT2(AssertQStringsEqual, results.findKeyword("Filename"), cubeFileName);
-  EXPECT_PRED_FORMAT2(AssertQStringsEqual, results.findKeyword("SerialNumber"), "IdealSpacecraft/IdealCamera/-709401200.26114");
-  EXPECT_PRED_FORMAT2(AssertQStringsEqual, results.findKeyword("ObservationNumber"), "IdealSpacecraft/IdealCamera/-709401200.26114");
-  
-}
