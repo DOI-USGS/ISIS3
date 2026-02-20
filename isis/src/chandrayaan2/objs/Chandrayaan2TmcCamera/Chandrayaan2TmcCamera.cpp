d3f1de9200d8e560f40716c94719776fe1f2b3cf
amystamile-usgs <astamile@usgs.gov>
2026-02-20 15:04:33 -0700

    fore and aft exception

--- isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/Chandrayaan2TmcCamera.cpp
+++ isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/Chandrayaan2TmcCamera.cpp
@@ -83,14 +83,22 @@ namespace Isis {
     // Set proper end frame
     int tmcFrame(0);
     if (naifIkCode() == -152210) {
-      // Frame DAWN_VIR_VIS : DAWN_VIR_VIS_ZERO
       tmcFrame = -152220;
     }
-    else if (naifIkCode() == -152211) { // (channelId == "IR)
-      // Frame DAWN_VIR_IR : DAWN_VIR_IR_ZERO
+    else if (naifIkCode() == -152211) {
+      QString msg = "Cube [" + cube.fileName() + "] has instrument [" + inst["InstrumentId"] + "]. "
+        "Support for AFT or FORE cubes is not available in the Chandrayaan2 TMC-2 ISIS camera model. Please use the CSM camera model by running csminit."
+        "For more information, see: "
+        "https://astrogeology.usgs.gov/docs/concepts/missions/chandrayaan2/";
+      throw IException(IException::User, msg, _FILEINFO_);
       tmcFrame = -152221;
     }
     else if (naifIkCode() == -152212) {
+      QString msg = "Cube [" + cube.fileName() + "] has instrument [" + inst["InstrumentId"] + "]. "
+          "Support for AFT or FORE cubes is not available in the Chandrayaan2 TMC-2 ISIS camera model. Please use the CSM camera model by running csminit."
+          "For more information, see: "
+          "https://astrogeology.usgs.gov/docs/concepts/missions/chandrayaan2/";
+      throw IException(IException::User, msg, _FILEINFO_);
       tmcFrame = -152222;
     }
     else {
