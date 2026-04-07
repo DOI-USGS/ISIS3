5a894d7463fe7c794de7f6b0c07ba4998a4610ba
Cordell Michaud <10409047+cordellmichaud@users.noreply.github.com>
2026-04-07 13:22:25 -0700

    Refactor ShadowCamUtilities
    - Move all functions into a ShadowCam sub-namespace
    - Move function docstrings into the header and correct them
    - Add missing includes, remove unused includes, and add include guard
    - Remove using namespace std declaration and scope any standard library
      members
    - Modify GetTdiFactor to throw an error if an invalid TDIDirection value
      is encountered, rather than ignoring it and defaulting to 0
    - Modify ToLower to leverage the functionality already available in QString

--- isis/src/kplo/objs/ShadowCamUtilities/ShadowCamUtilities.h
+++ isis/src/kplo/objs/ShadowCamUtilities/ShadowCamUtilities.h
@@ -1,44 +1,123 @@
-#pragma once 
+#ifndef ShadowCamUtilities_h
+#define ShadowCamUtilities_h
 
-#include <QString>
-#include <iostream>
-#include <fstream>
+#include <cstddef>
+#include <cstdint>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <cmath>
-#include <stdexcept>
-#include "IException.h"
-#include "CubeAttribute.h"
-#include "SpecialPixel.h"
-#include "FileName.h"
-#include "Pvl.h"
-#include "PvlKeyword.h"
-#include "PvlContainer.h"
-#include "IException.h"
-#include "Preference.h"
-#include "ShadowCamConstants.h"
 
-using namespace std;
+#include <QString>
+
+#include "PvlContainer.h"
+#include "PvlGroup.h"
 
 namespace Isis {
+namespace ShadowCam {
 
-  float Set8bitMaxMintoSpecialPixelsHIS4LIS4(uint16_t in);
+  /**
+   * @brief Sets 8 bit max and min to special pixels.
+   *
+   * @param in Input line buffer
+   *
+   * @return float Converted value
+   */
+  float Set8bitMaxMintoSpecialPixelsHIS4LIS4(std::uint16_t in);
 
-  uint16_t Set_LIS_HIS_SpecialPixelsTo_0_255(uint16_t in);
+  /**
+   * @brief Sets special pixels to 0 or 255.
+   *
+   * @param in Input line buffer
+   *
+   * @return uint16_t Converted value
+   */
+  std::uint16_t Set_LIS_HIS_SpecialPixelsTo_0_255(std::uint16_t in);
 
+  /**
+   * @brief Checks if pixel is a special pixel.
+   * @details This should be checked after decompanding since in decompanding step, 
+   *          0 and 255 are set only to HIS and LIS. You may miss LRS or HRS after 
+   *          decompanding.
+   *
+   * @param in Input line buffer
+   *
+   * @return bool True if pixel is special pixel, false if not.
+   */
   bool IsSpecialPixelSHC(double in);
 
-  size_t GetDataIndex(int channel, int channel_width, int pixel);
-  
-  std::string GetVersionedFilename(QString q_filename);
+  /**
+   * @brief Return data index based on channel, channel width, and pixel column
+   *
+   * @param channel The channel of the ccd
+   * @param channel_width The width in pixels of shc channel
+   * @param pixel The sample value 
+   *
+   * @return size_t Data index in cube
+   */
+  std::size_t GetDataIndex(int channel, int channel_width, int pixel);
 
-  QString GetFromLabels(PvlContainer pvlContainer, QString pvlKeyword);
+  /**
+   * @brief Gets the highest version of a filename and expands any ISIS variables in it, if present.
+   *
+   * @param filename The input filename
+   *
+   * @throws IException::User If the file does not exist after version resolution and variable expansion
+   * 
+   * @return std::string The version-resolved and expanded filename as an std::string
+   */
+  std::string GetVersionedFilename(const QString &filename);
 
-  int GetTdiFactor(PvlGroup instrument);
+  /**
+   * @brief Get the value from a keyword in the KPLO ShadowCam object labels.
+   *
+   * @param pvlContainer The PVL container in which to search for the keyword
+   * @param pvlKeyword The PVL keyword from which to retrieve the value 
+   *
+   * @throws IException::User If the keyword is not found in the PVL container
+   * 
+   * @return QString The value for that keyword
+   * @internal
+   *    @history 2024-07-12 Victor Silva
+   */
+  QString GetFromLabels(const PvlContainer &pvlContainer, const QString &pvlKeyword);
 
-  std::string ToLower(const std::string& str);
+  /**
+   * @brief Get the TDI factor from TDI direction.
+   *
+   * @param instrument The PVL group containing the TDIDirection keyword
+   * 
+   * @throws IException::User If the TDIDirection keyword is not found in the instrument PVL group
+   * @throws IException::User If the TDIDirection value is not "A" or "B"
+   * 
+   * @return int The TDI factor
+   * 
+   * @internal
+   *   @history 2026-03-18 Cordell Michaud - Updated to throw an error if TDIDirection value is not "A" or "B", rather than defaulting to 0.
+   */
+  int GetTdiFactor(const PvlGroup &instrument);
 
-  bool ContainsKeyword(const std::string& line, const vector<std::string>& keywords);
+  /**
+   * @brief Converts a string to lowercase.
+   *
+   * @param str The string to convert
+   * 
+   * @return string The string converted to lowercase
+   * 
+   * @internal
+   *   @history 2026-03-18 Cordell Michaud - Modified to use QString for conversion and return std::string.
+   */
+  std::string ToLower(const std::string &str);
 
-};
+  /**
+   * @brief Check if a line contains any keyword
+   *
+   * @param line The line in which to check for keywords
+   * @param keywords The vector of keywords to search for
+   * 
+   * @return bool True if any keyword is found, false otherwise
+   */
+  bool ContainsKeyword(const std::string &line, const std::vector<std::string> &keywords);
+
+}
+}
+
+#endif
