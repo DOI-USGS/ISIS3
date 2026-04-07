a2496019947deab2de456744b7d12107a38f7851
Cordell Michaud <10409047+cordellmichaud@users.noreply.github.com>
2026-04-07 13:22:24 -0700

    Move shadowcam macro constants to constexprs in a ShadowCam sub-namespace

--- isis/src/kplo/objs/ShadowCamUtilities/ShadowCamConstants.h
+++ isis/src/kplo/objs/ShadowCamUtilities/ShadowCamConstants.h
@@ -1,11 +1,18 @@
-#pragma once
+#ifndef ShadowCamConstants_h
+#define ShadowCamConstants_h
 
-#define TERMS 6
-#define DATA_OFFSET 65536
-#define SHC_BYTES 4096
+namespace Isis {
+namespace ShadowCam {
+  constexpr int TERMS = 6;
+  constexpr int DATA_OFFSET = 65536;
+  constexpr int SHC_BYTES = 4096;
 
-#define SHC_AFE_WIDTH 524
-#define SHC_CHANNELS 6
-#define SHC_SCENE 512
-#define SHC_SCENE_OFFSET 10
-#define SHC_BANDS 1
+  constexpr int SHC_AFE_WIDTH = 524;
+  constexpr int SHC_CHANNELS = 6;
+  constexpr int SHC_SCENE = 512;
+  constexpr int SHC_SCENE_OFFSET = 10;
+  constexpr int SHC_BANDS = 1;
+}
+}
+
+#endif
