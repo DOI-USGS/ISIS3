005d2c05d132e26bd62b44ca0d99dd5fec794ec4
Cordell Michaud <10409047+cordellmichaud@users.noreply.github.com>
2026-04-07 13:22:26 -0700

    Add license comment to ShadowCamConstants.h and ShadowCamUtilities.h

--- isis/src/kplo/objs/ShadowCamUtilities/ShadowCamConstants.h
+++ isis/src/kplo/objs/ShadowCamUtilities/ShadowCamConstants.h
@@ -1,5 +1,12 @@
 #ifndef ShadowCamConstants_h
 #define ShadowCamConstants_h
+/** This is free and unencumbered software released into the public domain.
+
+The authors of ISIS do not claim copyright on the contents of this file.
+For more details about the LICENSE terms and the AUTHORS, you will
+find files of those names at the top level of this repository. **/
+
+/* SPDX-License-Identifier: CC0-1.0 */
 
 namespace Isis {
 namespace ShadowCam {
