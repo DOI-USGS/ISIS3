fc9f25f6ec3e72e69a25b53c859693ef6deead23
Cordell Michaud <10409047+cordellmichaud@users.noreply.github.com>
2026-04-07 13:22:33 -0700

    Cleanup shadowcamcal.h to match shadowcamcal.cpp

--- isis/src/kplo/apps/shadowcamcal/shadowcamcal.h
+++ isis/src/kplo/apps/shadowcamcal/shadowcamcal.h
@@ -1,53 +1,39 @@
-#pragma once
+#ifndef shadowcamcal_h
+#define shadowcamcal_h
 
-#include <cmath>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cstdint>
-#include <cstdlib>
-#include <QString>
-#include <QFile>
-#include <QTemporaryDir>
-#include <QStandardPaths>
-#include <QFileInfo>
-#include <sstream>
-#include "IString.h"
-#include <stdexcept>
-
-#include "ShadowCamUtilities.h"
-#include "ShadowCamConstants.h"
-#include "BiasPixelSubtraction.h"
-#include "GainCorrection.h"
-#include "RadianceCoefficients.h"
-#include "FlatFieldCorrection.h"
-#include "DarkSubtraction.h"
-#include "WriteCube.h"
 #include "Cube.h"
-#include "Progress.h"
-#include "FileName.h"
-#include "PvlKeyword.h"
-#include "ProcessByLine.h"
-#include "SpecialPixel.h"
-#include "LineManager.h"
-#include "FileName.h"
-#include "IException.h"
 #include "UserInterface.h"
-#include "Preference.h"
 
 namespace Isis {
 
   /**
-   * @brief Main function to perform ShadowCam calibration
-   * 
+   * @brief Performs ShadowCam calibration
+   *
    * This function applies a series of corrections to the ShadowCam data, including bias pixel
    * removal, bias average subtraction, gain correction, dark subtraction, flat field correction,
    * and radiance correction. The results are written to a new calibrated cube.
    *
-   * @param ui Reference to the user interface for input parameters.
-   * @param log Optional log to record progress and errors.
+   * @param ui The user interface for input parameters.
+   *
+   * @internal
+   *    @history 2026-03-30 Cordell Michaud - Removed unused log parameter and refactored.
    */
-  extern void shadowcamcal(UserInterface &ui, Pvl *log = nullptr);
+  extern void shadowcamcal(UserInterface &ui);
 
+  /**
+   * @brief Performs ShadowCam calibration
+   *
+   * This function applies a series of corrections to the ShadowCam data, including bias pixel
+   * removal, bias average subtraction, gain correction, dark subtraction, flat field correction,
+   * and radiance correction. The results are written to a new calibrated cube.
+   *
+   * @param inCube The input cube to be calibrated.
+   * @param ui The user interface for input parameters.
+   *
+   * @internal
+   *    @history 2026-03-30 Cordell Michaud - Added this new overload for better testability and refactored.
+   */
+  extern void shadowcamcal(Cube *inCube, UserInterface &ui);
 }
+
+#endif
