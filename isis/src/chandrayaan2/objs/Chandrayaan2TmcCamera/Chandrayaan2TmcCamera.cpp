a64e5301a7b4b8ff2717a9cc4d34b4dd58f4d827
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2026-05-08 19:02:33 -0700

    chandrayaan2 TMC: drop FORE/AFT IException to allow spiceinit
    Removes the IException added in PR #5972 that blocked spiceinit on
    CH2_TMC_FORE/AFT cubes. spiceinit now succeeds for those detectors
    when stacked on top of PR #5955's LineScanCameraGroundMap fix.
    
    Note: this is part of an exploratory branch testing #5955 + #5835.
    The native camera still produces rays ~84 deg off from CSM truth for
    FORE/AFT, so additional work in the camera setup is needed before
    this is safe to ship. Not for PR yet.

--- isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/Chandrayaan2TmcCamera.cpp
+++ isis/src/chandrayaan2/objs/Chandrayaan2TmcCamera/Chandrayaan2TmcCamera.cpp
@@ -79,23 +79,15 @@ namespace Isis {
     new LineScanCameraGroundMap(this);
     new LineScanCameraSkyMap(this);
 
-    QString errorMsg = "Cube [" + cube.fileName() + "] has instrument [" + inst["InstrumentId"] + "]. "
-      "Support for AFT or FORE cubes is not available in the Chandrayaan2 TMC-2 ISIS camera model. Please use the CSM camera model by running csminit."
-      "For more information, see: "
-      "https://astrogeology.usgs.gov/docs/concepts/missions/chandrayaan2/";
-
-
     // Set proper end frame
     int tmcFrame(0);
     if (naifIkCode() == -152210) {
       tmcFrame = -152220;
     }
     else if (naifIkCode() == -152211) {
-      throw IException(IException::User, errorMsg, _FILEINFO_);
       tmcFrame = -152221;
     }
     else if (naifIkCode() == -152212) {
-      throw IException(IException::User, errorMsg, _FILEINFO_);
       tmcFrame = -152222;
     }
     else {
