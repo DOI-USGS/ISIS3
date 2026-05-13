316de7af7556b8c15222d364e3915ffd6d37ab8b
Adam Paquette <acpaquette@usgs.gov>
2026-05-13 14:05:37 -0700

    Dev Fixes (#6054)
    * Added ShowDeprecated key to TestPreferences defaulted to Off
    
    * Remove cerr message and revert shapemodel test changes
    
    * Remove deprecation warning
    
    (cherry picked from commit 20ff6f87883cd81d870e2ad0a2de7b7bfdeb6f13)

--- isis/src/base/objs/ShapeModel/ShapeModel.cpp
+++ isis/src/base/objs/ShapeModel/ShapeModel.cpp
@@ -657,13 +657,6 @@ namespace Isis {
    *
    */
   void ShapeModel::setHasIntersection(bool b) {
-    if (Isis::Preference::Preferences().getShowDeprecatedPref()) {
-        QString msg = "Shapemodel::setHasIntersection will be deprecated and moved "
-                      "to protected in a future release. Please avoid calling this "
-                      "from public scope.";
-        std::cerr << msg.toStdString() << std::endl;
-    }
-
     m_hasIntersection  = b;
     setHasNormal(false);
     setHasLocalNormal(false);
