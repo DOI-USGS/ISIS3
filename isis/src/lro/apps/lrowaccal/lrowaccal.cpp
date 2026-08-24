99e34e69e715c77c7fe1b8c23018603d6a27b949
Cordell Michaud <10409047+cordellmichaud@users.noreply.github.com>
2026-08-24 12:26:19 -0700

    Move DarkFileInfo and DarkComp into the LroWacCal namespace

--- isis/src/lro/apps/lrowaccal/lrowaccal.cpp
+++ isis/src/lro/apps/lrowaccal/lrowaccal.cpp
@@ -36,6 +36,48 @@ namespace Isis {
     static constexpr int UV_LINES = 4;
     static constexpr int UV_BANDS = 2;
     static constexpr double KM_PER_AU = 149597871;
+
+    /**
+     * @brief Structure for storing list of available dark file temps/times.
+     */
+    struct DarkFileInfo {
+      double temp;
+      int time;
+
+      DarkFileInfo(double temp, int time) {
+        this->temp = temp;
+        this->time = time;
+      }
+    };
+
+    /**
+     * @brief DarkFileInfo comparison object.
+     *
+     * Used for sorting DarkFileInfo objects. Sort first by difference from WAC temp, then difference from WAC time
+     */
+    struct DarkComp {
+      double wacTemp;
+      int wacTime;
+
+      DarkComp(double wacTemp, int wacTime) {
+        this->wacTemp = wacTemp;
+        this->wacTime = wacTime;
+      }
+
+      // sort dark files by distance from wac temp, then distance from wac time
+      bool operator()(const DarkFileInfo &A, const DarkFileInfo &B) {
+        if (std::abs(wacTemp - A.temp) < std::abs(wacTemp - B.temp)) {
+          return true;
+        }
+        if (std::abs(wacTemp - A.temp) > std::abs(wacTemp - B.temp)) {
+          return false;
+        }
+        if (std::abs(wacTime - A.time) < std::abs(wacTime - B.time)) {
+          return true;
+        }
+        return false;
+      }
+    };
   }
 
   /**
@@ -69,44 +111,6 @@ namespace Isis {
    * @param ui the User Interface to parse the parameters from
    */
   void lrowaccal(Cube *icube, UserInterface &ui) {
-    /**
-    * Structure for storing list of available dark file temps/times.
-    */
-    struct DarkFileInfo {
-      double temp;
-      int time;
-
-      DarkFileInfo(double temp, int time)
-      {
-        this->temp = temp;
-        this->time = time;
-      }
-    };
-
-    /**
-    * @brief DarkFileInfo comparison object.
-    *
-    * Used for sorting DarkFileInfo objects. Sort first by difference from WAC temp, then difference from WAC time
-    */
-    struct DarkComp {
-      double wacTemp;
-      int wacTime;
-
-      DarkComp(double wacTemp, int wacTime)
-      {
-        this->wacTemp = wacTemp;
-        this->wacTime = wacTime;
-      }
-
-      // sort dark files by distance from wac temp, then distance from wac time
-      bool operator() (const DarkFileInfo &A, const DarkFileInfo &B) {
-        if (abs(wacTemp - A.temp) < abs(wacTemp - B.temp)) return true;
-        if (abs(wacTemp - A.temp) > abs(wacTemp - B.temp)) return false;
-        if (abs(wacTime - A.time) < abs(wacTime - B.time)) return true;
-        return false;
-      }
-    };
-    
     vector<double> g_iofResponsivity;
     vector<double> g_radianceResponsivity;
     double g_TempratureConstants[7][2];
