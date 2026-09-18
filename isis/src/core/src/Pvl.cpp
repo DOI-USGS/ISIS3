6e96710c755e892ea23e7d62d8af83764a1664c0
Adam Paquette <acpaquette@usgs.gov>
2026-09-18 11:00:41 -0600

    Added band check for pixel group

--- isis/src/core/src/Pvl.cpp
+++ isis/src/core/src/Pvl.cpp
@@ -199,12 +199,10 @@ namespace Isis {
           dims["Bands"] = toString(dataset->GetRasterCount());
         }
 
-        if (core.hasGroup("Pixels")) {
-          GDALRasterBand *band = dataset->GetRasterBand(1);
-
+        GDALRasterBand *band = dataset->GetRasterBand(1);
+        if (band != nullptr && core.hasGroup("Pixels")) {
           PvlGroup &ptype = core.findGroup("Pixels");
           ptype["Type"] = PixelTypeName(GdalPixelToIsis(band->GetRasterDataType()));
-
           ptype["Base"] = toString(band->GetOffset());
           ptype["Multiplier"] = toString(band->GetScale());
         }
